33fa4d402ac7ee6ec07342cf8d6bbd69217cce0a
Kenny Lam <kenny.lam@ibm.com>
2019-07-09 09:31:41 -0400

    chore(yarn): fix yarn build errors

--- packages/components/src/globals/js/components.js
+++ packages/components/src/globals/js/components.js
@@ -5,4 +5,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export default from './Masthead';
+export { default as Masthead } from '../../components/masthead/masthead';
--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -5,4 +5,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export default from './Masthead';
+export Masthead from './components/Masthead';
