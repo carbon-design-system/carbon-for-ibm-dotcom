bf05b3c51eda3efd5aad514113318d3a721a3ed4
James Dow <james.dow@us.ibm.com>
2019-07-29 01:24:25 -0500

    feat(footer): laying down the basework for the footer

---
+++ packages/react/src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+
+/** Footer component */
+class Footer extends React.Component {
+  /**
+   * Renders the footer component
+   *
+   * @returns {object} JSX object
+   */
+  render() {
+    return (
+      <footer>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</footer>
+    );
+  }
+}
+
+export default Footer;
