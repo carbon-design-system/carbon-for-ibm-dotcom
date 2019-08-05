72fba4f6ab9d2e5059b62cd731a07590a3e6bf7c
Jeff Chew <jeff.chew@gmail.com>
2019-08-05 17:42:40 -0400

    feat(search): adding initial search services

--- packages/services/config/jest/setup.js
+++ packages/services/config/jest/setup.js
@@ -6,4 +6,9 @@ jest.unmock('object-assign');
 
 global.__DEV__ = true;
 
+process.env.SEARCH_TYPEAHEAD_HOST = 'https://ibm.com';
+process.env.SEARCH_TYPEAHEAD_VERSION = 'v1';
+process.env.MARKETING_SEARCH_HOST = 'https://ibm.com';
+process.env.MARKETING_SEARCH_VERSION = 'v1';
+
 require('../polyfills');
