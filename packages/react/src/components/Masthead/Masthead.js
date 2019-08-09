45f49ce20bd98171d31dcec7f871ccd20fd122d5
Kenny Lam <kennylam@protonmail.com>
2019-08-09 07:41:06 -0400

    feat(component): move masthead skeleton from dev

--- packages/react/src/components/Masthead/Masthead.js
+++ packages/react/src/components/Masthead/Masthead.js
@@ -5,17 +5,38 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
+import Search20 from '@carbon/icons-react/lib/search/20';
+import UserProfile20 from '@carbon/icons-react/lib/user--profile/20';
+import { ReactComponent as Logo } from '../Icons/svg/ibm-logo.svg';
+import HeaderContainer from 'carbon-components-react/lib/components/UIShell/HeaderContainer';
+import {
+  Header,
+  HeaderMenuButton,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  HeaderNavigation,
+  HeaderMenu,
+  HeaderMenuItem,
+  HeaderSideNavItems,
+  SkipToContent,
+  SideNav,
+  SideNavItems,
+  SideNavLink,
+  SideNavMenu,
+  SideNavMenuItem,
+} from 'carbon-components-react/lib/components/UIShell';
 
 /**
  * Test MastHead component
  *
- * @param {string} type Type of masthead
+ * @typedef {object} navigation Object containing navigation elements
+ * @param {string} type Standard or Branded masthead
  * @returns {*} Masthead component
  * @class
  */
-const Masthead = ({ type }) => {
+const Masthead = ({ navigation, type }) => {
   if (type === 'branded') {
     return (
       <div className="masthead">
@@ -24,14 +45,101 @@ const Masthead = ({ type }) => {
     );
   } else {
     return (
-      <div className="masthead">
-        <p>This is the standard masthead</p>
+      <div className="container">
+        <HeaderContainer
+          render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+            <>
+              <Header aria-label="IBM">
+                <SkipToContent />
+                <HeaderMenuButton
+                  aria-label="Open menu"
+                  onClick={onClickSideNavExpand}
+                  isActive={isSideNavExpanded}
+                />
+                <Logo />
+                <HeaderNavigation aria-label="IBM">
+                  {navigation.map(item => {
+                    if (item.subnav) {
+                      return (
+                        <HeaderMenu
+                          aria-label={item.name}
+                          menuLinkName={item.name}>
+                          {item.subnav.map(subnav => {
+                            return (
+                              <HeaderMenuItem href={subnav.path}>
+                                {subnav.name}
+                              </HeaderMenuItem>
+                            );
+                          })}
+                        </HeaderMenu>
+                      );
+                    } else {
+                      return (
+                        <HeaderMenuItem href={item.path}>
+                          {item.name}
+                        </HeaderMenuItem>
+                      );
+                    }
+                  })}
+                </HeaderNavigation>
+                <HeaderGlobalBar>
+                  <HeaderGlobalAction aria-label="Search" onClick={() => {}}>
+                    <Search20 />
+                  </HeaderGlobalAction>
+                  <HeaderGlobalAction
+                    aria-label="User Profile"
+                    onClick={() => {}}>
+                    <UserProfile20 />
+                  </HeaderGlobalAction>
+                </HeaderGlobalBar>
+                <SideNav
+                  aria-label="Side navigation"
+                  expanded={isSideNavExpanded}
+                  isPersistent={false}>
+                  <SideNavItems>
+                    <HeaderSideNavItems>
+                      {navigation.map(item => {
+                        if (item.subnav) {
+                          return (
+                            <SideNavMenu
+                              aria-label={item.name}
+                              title={item.name}>
+                              {item.subnav.map(subnav => {
+                                return (
+                                  <SideNavMenuItem href={subnav.path}>
+                                    {subnav.name}
+                                  </SideNavMenuItem>
+                                );
+                              })}
+                            </SideNavMenu>
+                          );
+                        } else {
+                          return (
+                            <SideNavLink href={item.path}>
+                              {item.name}
+                            </SideNavLink>
+                          );
+                        }
+                      })}
+                    </HeaderSideNavItems>
+                  </SideNavItems>
+                </SideNav>
+              </Header>
+            </>
+          )}
+        />
       </div>
     );
   }
 };
 
 Masthead.propTypes = {
+  /**
+   * Navigation elements object to populate the masthead.
+   * See ./MastheadLinks.js for example structure.
+   */
+  navigation: PropTypes.object,
+
   /**
    * Specify Masthead type
    */
