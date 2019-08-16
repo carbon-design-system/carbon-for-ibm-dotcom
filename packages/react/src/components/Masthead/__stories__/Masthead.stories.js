1726278af515d41a9ada8347b9768e55330af5f5
Jeff Chew <jeff.chew@gmail.com>
2019-08-16 17:05:33 -0400

    fix(netlify): removed knobs from masthead for testing

--- packages/react/src/components/Masthead/__stories__/Masthead.stories.js
+++ packages/react/src/components/Masthead/__stories__/Masthead.stories.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, select } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 import Masthead from '../Masthead';
-import knobData from './data/knobs.json';
+// import knobData from './data/knobs.json';
 
 storiesOf('Masthead: Test', module)
   .addDecorator(withKnobs)
-  .add('Default', () => (
-    <Masthead type={select('Type', knobData.type, 'branded')} />
-  ));
+  .add('Default', () => <Masthead />);
