701ecdb833f8b0214e2cbf6ce70f1f54862a80eb
Jeff Chew <jeff.chew@gmail.com>
2019-08-20 16:08:38 -0400

    fix(netlify): adding babel transform async to generator

--- packages/react/babel.config.js
+++ packages/react/babel.config.js
@@ -8,11 +8,7 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-export-default-from',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        regenerator: true,
-      },
-    ],
+    '@babel/plugin-transform-async-to-generator',
+    '@babel/plugin-transform-runtime',
   ],
 };
