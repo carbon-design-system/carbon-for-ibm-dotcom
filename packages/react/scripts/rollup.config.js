2e3f1f2e0370f1f124ffc96c03fafb3a10321ad9
Jeff Chew <jeff.chew@gmail.com>
2019-08-28 16:39:51 -0400

    fix(rollup): adding back peerDependencies handling in rollup config

--- packages/react/scripts/rollup.config.js
+++ packages/react/scripts/rollup.config.js
@@ -17,9 +17,9 @@ const sizes = require('rollup-plugin-sizes');
 const visualizer = require('rollup-plugin-visualizer');
 
 const packageJson = require('../package.json');
-/*const peerDependencies = Object.keys(packageJson.peerDependencies || {}).concat(
+const peerDependencies = Object.keys(packageJson.peerDependencies || {}).concat(
   ['classnames', 'prop-types']
-);*/
+);
 
 const env = process.env.NODE_ENV || 'production';
 const prodSettings =
@@ -107,6 +107,13 @@ module.exports = {
     json(),
     ...prodSettings,
   ],
+  treeshake: {
+    propertyReadSideEffects: false,
+    pureExternalModules: true,
+  },
+  external: peerDependencies.filter(
+    dependency => dependency !== 'carbon-components'
+  ),
   output: {
     name: 'IBMDotcomReact',
     format: 'umd',
