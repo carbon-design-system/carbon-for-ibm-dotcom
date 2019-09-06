71ae26361c901aedc455343e640afd1c53bbb180
Jeff Chew <jeff.chew@gmail.com>
2019-09-06 13:51:34 -0400

    fix(react): removed test Translation API call

--- packages/react/.storybook/Container.js
+++ packages/react/.storybook/Container.js
@@ -3,17 +3,12 @@ import './polyfills';
 import './_container.scss';
 import { settings } from 'carbon-components';
 
-import { TranslationAPI } from '../../services/src/services/Translation';
-
 const { prefix } = settings;
 export default class Container extends Component {
   componentDidMount() {
     if (process.env.CARBON_REACT_STORYBOOK_USE_RTL === 'true') {
       document.documentElement.dir = 'rtl';
     }
-    TranslationAPI.getTranslation().then(response => {
-      console.log(response);
-    });
   }
 
   render() {
