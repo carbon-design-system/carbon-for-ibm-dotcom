30a3ec71e706ed29fde65f5f501b51cbc264e251
Jeff Chew <jeff.chew@gmail.com>
2019-09-11 18:49:56 -0400

    fix(jest): fixes to the jest env settings to share across packages

---
+++ tasks/jest/env.js
@@ -0,0 +1,7 @@
+process.env.SEARCH_TYPEAHEAD_HOST = 'https://ibm.com';
+process.env.SEARCH_TYPEAHEAD_VERSION = 'v1';
+process.env.MARKETING_SEARCH_HOST = 'https://ibm.com';
+process.env.MARKETING_SEARCH_VERSION = 'v1';
+process.env.PROFILE_HOST = 'https://ibm.com';
+process.env.PROFILE_VERSION = 'v1';
+process.env.TRANSLATION_HOST = 'https://ibm.com';
