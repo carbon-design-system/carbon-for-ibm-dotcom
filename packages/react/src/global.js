4620e86746ddc04ad5c53e0f60e6dd188a6af47b
Jeff Chew <jeff.chew@gmail.com>
2019-09-27 12:21:24 -0400

    feat(ddo): adding setVersion of DDO in the global import

--- packages/react/src/global.js
+++ packages/react/src/global.js
@@ -1,5 +1,9 @@
 /* eslint-disable tree-shaking/no-side-effects-in-initialization */
 import { AnalyticsAPI } from '@carbon/ibmdotcom-services';
+import { DDOAPI } from '@carbon/ibmdotcom-services';
+
+// Sets the version of the library in the DDO
+DDOAPI.setVersion();
 
 // analytics scroll tracking
 AnalyticsAPI.initScrollTracker();
