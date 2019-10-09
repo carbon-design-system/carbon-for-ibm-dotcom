c5e9fee05c0b6c6c8b3b1d3c39ae947dfbac2548
Anna Wen <anna.wen@ibm.com>
2019-10-09 15:00:27 -0400

    chore(jsdocs): add small example to geolocation utility

--- packages/utilities/src/utilities/geolocation/geolocation.js
+++ packages/utilities/src/utilities/geolocation/geolocation.js
@@ -5,6 +5,11 @@ const _endpoint = process.env.GEO_API;
  * Utility to retrieve user's country code based on their IP address
  * and the language code from the browser language preference
  *
+ * @example
+ * import { geolocation } from '@carbon/ibmdotcom-utilities';
+ *
+ * const locationInfo = await geolocation();
+ *
  * @returns {object} object with cc and lc data
  *
  */
