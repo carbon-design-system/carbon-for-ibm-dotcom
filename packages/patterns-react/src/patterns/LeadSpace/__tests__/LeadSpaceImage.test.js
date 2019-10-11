5c00667ce77d6000da0ec20c006e83849dc7f181
Anna Wen <anna.wen@ibm.com>
2019-10-11 09:21:51 -0400

    chore(unit-test): update unit test for leadspace image

--- packages/patterns-react/src/patterns/LeadSpace/__tests__/LeadSpaceImage.test.js
+++ packages/patterns-react/src/patterns/LeadSpace/__tests__/LeadSpaceImage.test.js
@@ -4,25 +4,33 @@ import LeadSpaceImage from '../LeadSpaceImage';
 
 describe('<LeadSpaceImage />', () => {
   it('renders expected number of source elements', () => {
-    const image = {
-      sources: [
-        {
-          minWidth: 0,
-          url: 'https://picsum.photos/id/1076/320/370',
-        },
-        {
-          minWidth: 672,
-          url: 'https://picsum.photos/id/1076/672/400',
-        },
-        {
-          minWidth: 1056,
-          url: 'https://picsum.photos/id/1076/1056/480',
-        },
-      ],
+    const image = [
+      {
+        minWidth: 0,
+        url: 'https://picsum.photos/id/1076/320/370',
+      },
+      {
+        minWidth: 672,
+        url: 'https://picsum.photos/id/1076/672/400',
+      },
+      {
+        minWidth: 1056,
+        url: 'https://picsum.photos/id/1076/1056/480',
+      },
+    ];
+
+    const imageInfo = {
       default: 'https://picsum.photos/id/1076/1056/480',
       alt: 'lead space image',
     };
-    const leadspaceImage = mount(<LeadSpaceImage image={image} />);
+
+    const leadspaceImage = mount(
+      <LeadSpaceImage
+        images={image}
+        defaultImage={imageInfo.default}
+        alt={imageInfo.alt}
+      />
+    );
     expect(leadspaceImage.find('source')).toHaveLength(3);
   });
 });
