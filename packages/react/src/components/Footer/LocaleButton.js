9e88c5ca2e2cd43b97db0570ba9b66e961b884f7
Kenny Lam <kennylam@protonmail.com>
2019-11-05 09:44:13 -0500

    feat(component): add locale modal component

--- packages/react/src/components/Footer/LocaleButton.js
+++ packages/react/src/components/Footer/LocaleButton.js
@@ -1,10 +1,13 @@
-import { FOOTER_LOCALE_BUTTON } from '../../internal/FeatureFlags.js';
-import { featureFlag } from '@carbon/ibmdotcom-utilities';
-
-import React, { useState } from 'react';
-import { Button } from 'carbon-components-react';
-import LocaleModal from '../LocaleModal/LocaleModal';
+import React, { useState, useEffect } from 'react';
+import {
+  Button,
+  ComposedModal,
+  ModalHeader,
+  ModalBody,
+  ComboBox,
+} from 'carbon-components-react';
 import { settings as ddsSettings } from '@carbon/ibmdotcom-utilities';
+import { LocaleAPI } from '@carbon/ibmdotcom-services';
 import { settings } from 'carbon-components';
 import { Globe20 } from '@carbon/icons-react';
 import PropTypes from 'prop-types';
@@ -13,14 +16,40 @@ const { stablePrefix } = ddsSettings;
 const { prefix } = settings;
 
 /**
- * EXPERIMENTAL: Renders the locale button
+ * Renders the locale button
  *
- * @private
- *
- * @returns {object} JSX object
+ * @param {object} props props object
+ * @param {Function} props.selectItem method to handle selected item
+ * @returns {*} {object} JSX object
  */
-const LocaleButton = () => {
+const LocaleButton = ({ selectItem }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [list, setList] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const locale = await LocaleAPI.getLocale();
+      const list = locale && (await LocaleAPI.getList(locale));
+      setList(list);
+    })();
+  }, []);
+
+  /**
+   *  method to merge list and sort alphabetically by country
+   *
+   * @param {object} list country list
+   *
+   * @returns {object} list item
+   */
+  const sortList = list => {
+    let countryList = [];
+    list.regionList &&
+      list.regionList.map(region => {
+        countryList = countryList.concat(region.countryList);
+      });
+    countryList.sort((a, b) => (a.name > b.name ? 1 : -1));
+    return countryList;
+  };
 
   return (
     <div className={`${prefix}--locale-btn__container`}>
@@ -33,11 +62,27 @@ const LocaleButton = () => {
         United States — English
       </Button>
 
-      <LocaleModal
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        availabilityText="Choose your preferred location and language"
-      />
+      <ComposedModal
+        open={isOpen}
+        onClose={close}
+        data-autoid={`${stablePrefix}--locale-modal`}>
+        <ModalHeader
+          label="United States — English"
+          title="Select your region"
+        />
+        <ModalBody>
+          <ComboBox
+            id="id"
+            type="default"
+            itemToString={item =>
+              item ? `${item.name}-${item.locale[0][1]}` : ''
+            }
+            onChange={selectItem}
+            items={list ? sortList(list) : []}
+            placeholder="Select a country/region"
+          />
+        </ModalBody>
+      </ComposedModal>
     </div>
   );
 
@@ -48,6 +93,15 @@ const LocaleButton = () => {
    */
   function open() {
     setIsOpen(true);
+  }
+
+  /**
+   * Sets modal state to closed
+   *
+   * @private
+   */
+  function close() {
+    setIsOpen(false);
   }
 };
 
