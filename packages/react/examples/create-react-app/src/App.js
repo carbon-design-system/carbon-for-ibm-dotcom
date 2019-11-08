4aaaa73a09f596f54d1586ee9250be4d5b41c5ab
Jeff Chew <jeff.chew@gmail.com>
2019-11-08 17:53:49 -0500

    feat(examples): adding example with create-react-app

---
+++ packages/react/examples/create-react-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { DotcomShell } from '@carbon/ibmdotcom-react';
+import '@carbon/ibmdotcom-styles';
+
+function App() {
+  return <DotcomShell />;
+}
+
+export default App;
