624e94597059a60ffe6d7667d2a27c64b385a471
Jeff Chew <jeff.chew@gmail.com>
2019-11-12 15:15:01 -0500

    feat(nextjs): adding sample content in the DotcomShell

--- packages/react/examples/nextjs/next.config.js
+++ packages/react/examples/nextjs/next.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass({
-  env: {
-    CORS_PROXY: 'https://dds-proxy.mybluemix.net/',
-  },
-  sassLoaderOptions: {
-    includePaths: [path.resolve(__dirname, 'node_modules')],
-  },
-});
+module.exports = withCSS(
+  withSass({
+    env: {
+      CORS_PROXY: 'https://dds-proxy.mybluemix.net/',
+    },
+    sassLoaderOptions: {
+      includePaths: [path.resolve(__dirname, 'node_modules')],
+    },
+  })
+);
--- packages/react/examples/nextjs/pages/index.js
+++ packages/react/examples/nextjs/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import Head from 'next/head'
+import content from './content';
 import '@carbon/ibmdotcom-styles/scss/ibm-dotcom-styles.scss';
 import { DotcomShell } from '@carbon/ibmdotcom-react';
 
 /**
  * Initializes the homepage
  */
-const Home = () => <DotcomShell navigation="default"></DotcomShell>;
+const Home = () => <DotcomShell navigation="default">{content}</DotcomShell>;
 
 export default Home;
