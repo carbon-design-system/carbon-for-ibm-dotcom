7b29d5219ebc26abfc67517b8a89e5b2ee20199a
andysherman2121 <andrew.sherman1@ibm.com>
2019-11-20 14:30:59 -0500

    feat(footer): first commit, added short/long versions of vanilla footer

---
+++ packages/vanilla/src/components/footer/__stories__/footer.stories.js
@@ -0,0 +1,28 @@
+import { storiesOf } from '@storybook/html';
+import { withKnobs, select } from '@storybook/addon-knobs';
+import footer from '../footer.template';
+import '../../../../../styles/scss/components/footer/index.scss';
+// import readme from '../README.md';
+
+storiesOf('footer', module)
+  .addDecorator(withKnobs)
+  // .addParameters({
+  //   readme: {
+  //     sidebar: readme
+  //   },
+  // })
+  .add('Default', () => {
+    const footerTypeOptions = {
+      tall: '',
+      short: 'short',
+    };
+    setTimeout(() => {
+      console.log('DOMContentLoaded');
+      const footerElement = document.querySelector('.bx--footer');
+      // eslint-disable-next-line
+      const footerInstance = footer.create(footerElement);
+    }, 5000);
+    return footer.template({
+      type: select('type', footerTypeOptions, footerTypeOptions.tall),
+    });
+  });
