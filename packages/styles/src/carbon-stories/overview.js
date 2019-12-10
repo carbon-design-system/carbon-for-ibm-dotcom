22926dd3144cbe86e3e6bf5a5f3e78ffb9cf4311
Jeff Chew <jeff.chew@gmail.com>
2019-12-10 14:26:15 -0500

    fix(storybook): removing unused imports

--- packages/styles/src/carbon-stories/overview.js
+++ packages/styles/src/carbon-stories/overview.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { addDecorator, storiesOf } from '@storybook/react';
+import { storiesOf } from '@storybook/react';
 import { withDocs } from 'storybook-readme';
 import README from '../../scss/themes/expressive/README.md';
-import { withInfo } from '@storybook/addon-info';
 
 storiesOf('Overview', module)
   .addParameters({
