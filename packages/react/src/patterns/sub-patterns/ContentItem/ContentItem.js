0a3af47e6fb1127db9618eaaa6384e737fd22ad3
Anna Wen <anna.wen@ibm.com>
2020-01-29 16:47:21 -0500

    chore(featureflag): remove featureflag from content item

--- packages/react/src/patterns/sub-patterns/ContentItem/ContentItem.js
+++ packages/react/src/patterns/sub-patterns/ContentItem/ContentItem.js
@@ -4,10 +4,9 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { featureFlag, markdownToHtml } from '@carbon/ibmdotcom-utilities';
-import { DDS_CONTENT_ITEM } from '../../../internal/FeatureFlags';
 import { settings as ddsSettings } from '@carbon/ibmdotcom-utilities';
 import { Image } from '../../../components/Image';
+import { markdownToHtml } from '@carbon/ibmdotcom-utilities';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { settings } from 'carbon-components';
@@ -24,28 +23,23 @@ const { prefix } = settings;
  * @param {object} props.image image object
  * @returns {*} JSX ContentItem component
  */
-const ContentItem = ({ copy, heading, image }) => {
-  return featureFlag(
-    DDS_CONTENT_ITEM,
-    <div
-      className={`${prefix}--content-item`}
-      data-autoid={`${stablePrefix}--content-item`}>
-      {heading && (
-        <h4 className={`${prefix}--content-item__heading`}>{heading}</h4>
-      )}
-      {image && (
-        <Image classname={`${prefix}--content-item__image`} {...image} />
-      )}
-      {copy && (
-        <span
-          className={`${prefix}--content-item__copy`}
-          dangerouslySetInnerHTML={{
-            __html: markdownToHtml(copy),
-          }}></span>
-      )}
-    </div>
-  );
-};
+const ContentItem = ({ copy, heading, image }) => (
+  <div
+    className={`${prefix}--content-item`}
+    data-autoid={`${stablePrefix}--content-item`}>
+    {heading && (
+      <h4 className={`${prefix}--content-item__heading`}>{heading}</h4>
+    )}
+    {image && <Image classname={`${prefix}--content-item__image`} {...image} />}
+    {copy && (
+      <span
+        className={`${prefix}--content-item__copy`}
+        dangerouslySetInnerHTML={{
+          __html: markdownToHtml(copy),
+        }}></span>
+    )}
+  </div>
+);
 
 ContentItem.propTypes = {
   copy: PropTypes.string,
--- packages/react/src/patterns/sub-patterns/ContentItem/__stories__/ContentItem.stories.js
+++ packages/react/src/patterns/sub-patterns/ContentItem/__stories__/ContentItem.stories.js
@@ -2,38 +2,35 @@ import './index.scss';
 
 import { object, text, withKnobs } from '@storybook/addon-knobs';
 import ContentItem from '../ContentItem';
-import { DDS_CONTENT_ITEM } from '../../../../internal/FeatureFlags';
 import React from 'react';
 import readme from '../README.md';
 import { storiesOf } from '@storybook/react';
 
-if (DDS_CONTENT_ITEM) {
-  storiesOf('Patterns (Sub-Patterns)|ContentItem', module)
-    .addDecorator(withKnobs)
-    .addParameters({
-      readme: {
-        sidebar: readme,
-      },
-    })
-    .add('Default', () => {
-      const heading = text('Component title:', 'Lorem ipsum dolor sit amet.');
-      const copy = text(
-        'Component copy:',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed interdum tortor. Sed id pellentesque diam. In ut quam id mauris finibus efficitur quis ut arcu. Praesent purus turpis, venenatis eget odio et, tincidunt bibendum sem. Curabitur pretium elit non blandit lobortis. Donec quis pretium odio, in dignissim sapien.'
-      );
+storiesOf('Patterns (Sub-Patterns)|ContentItem', module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('Default', () => {
+    const heading = text('Component title:', 'Lorem ipsum dolor sit amet.');
+    const copy = text(
+      'Component copy:',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed interdum tortor. Sed id pellentesque diam. In ut quam id mauris finibus efficitur quis ut arcu. Praesent purus turpis, venenatis eget odio et, tincidunt bibendum sem. Curabitur pretium elit non blandit lobortis. Donec quis pretium odio, in dignissim sapien.'
+    );
 
-      const image = {
-        images: object('Image assets:', [
-          { src: 'https://picsum.photos/id/2/288/144', minWidth: 'sm' },
-          { src: 'https://picsum.photos/id/2/448/224', minWidth: 'md' },
-          { src: 'https://picsum.photos/id/2/352/176', minWidth: 'lg' },
-        ]),
-        alt: text('alt', 'content item image'),
-        defaultImage: text(
-          'default image:',
-          'https://picsum.photos/id/2/352/176'
-        ),
-      };
-      return <ContentItem heading={heading} copy={copy} image={image} />;
-    });
-}
+    const image = {
+      images: object('Image assets:', [
+        { src: 'https://picsum.photos/id/2/288/144', minWidth: 'sm' },
+        { src: 'https://picsum.photos/id/2/448/224', minWidth: 'md' },
+        { src: 'https://picsum.photos/id/2/352/176', minWidth: 'lg' },
+      ]),
+      alt: text('alt', 'content item image'),
+      defaultImage: text(
+        'default image:',
+        'https://picsum.photos/id/2/352/176'
+      ),
+    };
+    return <ContentItem heading={heading} copy={copy} image={image} />;
+  });
