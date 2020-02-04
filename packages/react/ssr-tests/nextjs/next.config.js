8fe33116187d2d94ecc87b28239b5375e6107532
Jeff Chew <jeff.chew@gmail.com>
2020-02-04 14:35:49 -0500

    feat(ssr-test): fixes to production build

--- packages/react/ssr-tests/nextjs/next.config.js
+++ packages/react/ssr-tests/nextjs/next.config.js
@@ -2,13 +2,41 @@ const path = require('path');
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 
-module.exports = withCSS(
-  withSass({
+module.exports = withSass(
+  withCSS({
     env: {
       CORS_PROXY: 'https://dds-proxy.mybluemix.net/',
     },
     sassLoaderOptions: {
       includePaths: [path.resolve(__dirname, 'node_modules')],
+    },
+    webpack: config => {
+      // if (config.resolve.alias) {
+      //   delete config.resolve.alias['react'];
+      //   delete config.resolve.alias['react-dom'];
+      // }
+
+      config.module.rules.push({
+        test: /\.scss$/,
+        sideEffects: true,
+        use: [
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [path.resolve(__dirname, 'node_modules')],
+              data: `
+                $feature-flags: (
+                  ui-shell: true,
+                  grid-columns-16: true,
+                );
+              `,
+              sourceMap: true,
+            },
+          },
+        ],
+      });
+
+      return config;
     },
   })
 );
