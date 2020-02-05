997b62012420a73d7dbd848eeea476034f5e4510
karthik <challakarthik@gmail.com>
2020-02-05 08:23:51 -0500

    feat(pattern): removed feature flag

--- packages/react/src/patterns/sub-patterns/TableOfContents/__stories__/TableOfContents.stories.js
+++ packages/react/src/patterns/sub-patterns/TableOfContents/__stories__/TableOfContents.stories.js
@@ -7,76 +7,73 @@
 import './index.scss';
 import { object, select, text, withKnobs } from '@storybook/addon-knobs';
 import dataContent from './data/dataContent';
-import { DDS_TOC } from '../../../../internal/FeatureFlags';
 import React from 'react';
 import readme from '../README.md';
 import { storiesOf } from '@storybook/react';
 import TableOfContents from '../TableOfContents';
 
-if (DDS_TOC) {
-  const _menuLabel = text('menu label', 'Jump to');
+const _menuLabel = text('menu label', 'Jump to');
 
-  const _themes = {
-    g100: 'g100',
-    white: '',
-  };
-  storiesOf('Patterns (Sub-Patterns)|Table of Contents', module)
-    .addDecorator(withKnobs)
-    .addParameters({
-      readme: {
-        sidebar: readme,
+const _themes = {
+  g100: 'g100',
+  white: '',
+};
+storiesOf('Patterns (Sub-Patterns)|Table of Contents', module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('Manually define Menu Items', () => {
+    const menuItems = [
+      {
+        title: 'Cras molestie condimentum',
+        id: '8',
       },
-    })
-    .add('Manually define Menu Items', () => {
-      const menuItems = [
-        {
-          title: 'Cras molestie condimentum',
-          id: '8',
-        },
-        {
-          title: 'Praesent fermentum sodales',
-          id: '7',
-        },
-        {
-          title: 'Nulla tristique lacinia',
-          id: '2',
-        },
-        {
-          title: 'Morbi id nibh metus',
-          id: '3',
-        },
-        {
-          title: 'Integer non scelerisque',
-          id: '14',
-        },
-      ];
+      {
+        title: 'Praesent fermentum sodales',
+        id: '7',
+      },
+      {
+        title: 'Nulla tristique lacinia',
+        id: '2',
+      },
+      {
+        title: 'Morbi id nibh metus',
+        id: '3',
+      },
+      {
+        title: 'Integer non scelerisque',
+        id: '14',
+      },
+    ];
 
-      return (
-        <TableOfContents
-          theme={select('theme', _themes, _themes.white)}
-          menuItems={object('menuItems', menuItems)}
-          menuLabel={_menuLabel}>
-          {dataContent}
-        </TableOfContents>
-      );
-    })
-    .add('Dynamic Items', () => {
-      let menuItems = [];
-      dataContent.props.children.forEach(element => {
-        if (element.props['data-title']) {
-          menuItems.push({
-            title: element.props['data-title'],
-            id: element.props['name'],
-          });
-        }
-      });
-      return (
-        <TableOfContents
-          theme={select('theme', _themes, _themes.white)}
-          menuLabel={_menuLabel}
-          menuItems={menuItems}>
-          {dataContent}
-        </TableOfContents>
-      );
+    return (
+      <TableOfContents
+        theme={select('theme', _themes, _themes.white)}
+        menuItems={object('menuItems', menuItems)}
+        menuLabel={_menuLabel}>
+        {dataContent}
+      </TableOfContents>
+    );
+  })
+  .add('Dynamic Items', () => {
+    let menuItems = [];
+    dataContent.props.children.forEach(element => {
+      if (element.props['data-title']) {
+        menuItems.push({
+          title: element.props['data-title'],
+          id: element.props['name'],
+        });
+      }
     });
-}
+    return (
+      <TableOfContents
+        theme={select('theme', _themes, _themes.white)}
+        menuLabel={_menuLabel}
+        menuItems={menuItems}>
+        {dataContent}
+      </TableOfContents>
+    );
+  });
