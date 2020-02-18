50fd0f2a02880460db79b91ce1cb670e6cce2fe1
Kenny Lam <kennylam@protonmail.com>
2020-02-18 16:57:21 -0500

    chore(lint): clean up unused code; fix formatting

--- packages/react/src/patterns/blocks/ContentGroupSimple/__stories__/data/ContentGroupSimple.knobs.js
+++ packages/react/src/patterns/blocks/ContentGroupSimple/__stories__/data/ContentGroupSimple.knobs.js
@@ -4,9 +4,9 @@
  * type {{}}
  */
 
- const ContentGroupSimpleKnobs = {
-   heading: 'Lorem ipsum dolor sit amet',
-   mediaData: {
+const ContentGroupSimpleKnobs = {
+  heading: 'Lorem ipsum dolor sit amet',
+  mediaData: {
     images: [
       { src: 'https://picsum.photos/id/2/320/160', minWidth: 320 },
       { src: 'https://picsum.photos/id/2/400/400', minWidth: 400 },
@@ -14,12 +14,12 @@
     ],
     alt: 'lead space image',
     defaultImage: 'https://picsum.photos/id/2/672/672',
-   },
-   types: {
+  },
+  types: {
     image: 'image',
-   },
-   mediaType: {},
-   items: [
+  },
+  mediaType: {},
+  items: [
     {
       heading: 'Lorem ipsum dolor sit amet.',
       copy:
@@ -40,11 +40,11 @@
       copy:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed interdum tortor. Sed id pellentesque diam. In ut quam id mauris finibus efficitur quis ut arcu. Praesent purus turpis, venenatis eget odio et, tincidunt bibendum sem. Curabitur pretium elit non blandit lobortis. Donec quis pretium odio, in dignissim sapien.',
     },
-   ],
-   cta: {
+  ],
+  cta: {
     title: 'Lorem ipsum dolor',
     href: 'https://www.example.com',
-   },
- };
+  },
+};
 
- module.exports = ContentGroupSimpleKnobs;
+module.exports = ContentGroupSimpleKnobs;
