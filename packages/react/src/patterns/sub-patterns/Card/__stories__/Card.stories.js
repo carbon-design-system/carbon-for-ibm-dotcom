4fc9cbff2dcfdd4b6ac46887aa91ba31c2d5cd27
Anna Wen <anna.wen@ibm.com>
2020-02-18 22:39:13 -0500

    fix(storybook): add background to card

--- packages/react/src/patterns/sub-patterns/Card/__stories__/Card.stories.js
+++ packages/react/src/patterns/sub-patterns/Card/__stories__/Card.stories.js
@@ -1,5 +1,11 @@
 import './index.scss';
-import { select, text, withKnobs, boolean } from '@storybook/addon-knobs';
+import {
+  select,
+  text,
+  withKnobs,
+  boolean,
+  object,
+} from '@storybook/addon-knobs';
 import { ArrowRight20 } from '@carbon/icons-react';
 import { Card } from '../';
 import React from 'react';
@@ -8,17 +14,6 @@ import { storiesOf } from '@storybook/react';
 
 storiesOf('Patterns (Sub-Patterns)|Card', module)
   .addDecorator(withKnobs)
-  .addDecorator(Story => {
-    return (
-      <div className="bx--grid">
-        <div className="bx--row">
-          <div className="bx--col-sm-2 bx--col-md-3 bx--col-lg-6 bx--col-xlg-4 bx--no-gutter">
-            <Story />
-          </div>
-        </div>
-      </div>
-    );
-  })
   .addParameters({
     readme: {
       sidebar: readme,
@@ -33,6 +28,10 @@ storiesOf('Patterns (Sub-Patterns)|Card', module)
       copy: text('cta.copy', ''),
     };
     const inverse = boolean('inverse', false);
+    const image = object('image', {
+      defaultImage: 'https://picsum.photos/id/2/600/300',
+      alt: 'featured link image',
+    });
     const eyebrow = text('eyebrow', 'eyebrow text');
     const href = text('href (required)', 'https://example.com');
     const target = text('target', '');
@@ -52,41 +51,53 @@ storiesOf('Patterns (Sub-Patterns)|Card', module)
       g100: 'g100',
     };
 
+    const theme = select('theme', themes, themes.g100);
+
     return (
-      <div className={`bx--card--${select('theme', themes, themes.g100)}`}>
-        {!select('Ratio', ratio, ratio['none']) ? (
-          <Card
-            title={title}
-            eyebrow={eyebrow}
-            inverse={inverse}
-            copy={copy}
-            href={href}
-            cta={cta}
-            icon={ArrowRight20}
-            target={target}
-            type={type}
-          />
-        ) : (
-          <div
-            className={`bx--aspect-ratio bx--aspect-ratio--${select(
-              'Ratio',
-              ratio,
-              ratio['none']
-            )}`}>
-            <Card
-              title={title}
-              eyebrow={eyebrow}
-              inverse={inverse}
-              copy={copy}
-              cta={cta}
-              href={href}
-              icon={ArrowRight20}
-              target={target}
-              type={type}
-              className="bx--aspect-ratio--object"
-            />
+      <div
+        className="bx--grid"
+        style={{
+          backgroundColor: theme === 'g10' || theme === '' ? 'black' : 'white',
+        }}>
+        <div className="bx--row">
+          <div className={`bx--card--${theme}`}>
+            {!select('Ratio', ratio, ratio['none']) ? (
+              <Card
+                title={title}
+                eyebrow={eyebrow}
+                inverse={inverse}
+                image={image}
+                copy={copy}
+                href={href}
+                cta={cta}
+                icon={ArrowRight20}
+                target={target}
+                type={type}
+              />
+            ) : (
+              <div
+                className={`bx--aspect-ratio bx--aspect-ratio--${select(
+                  'Ratio',
+                  ratio,
+                  ratio['none']
+                )}`}>
+                <Card
+                  title={title}
+                  eyebrow={eyebrow}
+                  inverse={inverse}
+                  image={image}
+                  copy={copy}
+                  cta={cta}
+                  href={href}
+                  icon={ArrowRight20}
+                  target={target}
+                  type={type}
+                  className="bx--aspect-ratio--object"
+                />
+              </div>
+            )}
           </div>
-        )}
+        </div>
       </div>
     );
   })
@@ -99,6 +110,10 @@ storiesOf('Patterns (Sub-Patterns)|Card', module)
       copy: text('cta.copy', ''),
     };
     const inverse = boolean('inverse', false);
+    const image = object('image', {
+      defaultImage: 'https://picsum.photos/id/2/600/300',
+      alt: 'featured link image',
+    });
     const eyebrow = text('eyebrow', 'eyebrow text');
     const href = text('href (required)', 'https://example.com');
     const target = text('target', '');
@@ -118,41 +133,54 @@ storiesOf('Patterns (Sub-Patterns)|Card', module)
       g100: 'g100',
     };
 
+    const theme = select('theme', themes, themes.g100);
+
     return (
-      <div className={`bx--card--${select('theme', themes, themes.g100)}`}>
-        {!select('Ratio', ratio, ratio['none']) ? (
-          <Card
-            title={title}
-            eyebrow={eyebrow}
-            inverse={inverse}
-            copy={copy}
-            href={href}
-            cta={cta}
-            icon={ArrowRight20}
-            target={target}
-            type={type}
-          />
-        ) : (
-          <div
-            className={`bx--aspect-ratio bx--aspect-ratio--${select(
-              'Ratio',
-              ratio,
-              ratio['none']
-            )}`}>
-            <Card
-              title={title}
-              eyebrow={eyebrow}
-              inverse={inverse}
-              copy={copy}
-              cta={cta}
-              icon={ArrowRight20}
-              href={href}
-              target={target}
-              type={type}
-              className="bx--aspect-ratio--object"
-            />
+      <div
+        className="bx--grid"
+        style={{
+          padding: '2rem',
+          backgroundColor: theme === 'g10' || theme === '' ? 'black' : 'white',
+        }}>
+        <div className="bx--row">
+          <div className={`bx--card--${select('theme', themes, themes.g100)}`}>
+            {!select('Ratio', ratio, ratio['none']) ? (
+              <Card
+                title={title}
+                eyebrow={eyebrow}
+                inverse={inverse}
+                image={image}
+                copy={copy}
+                href={href}
+                cta={cta}
+                icon={ArrowRight20}
+                target={target}
+                type={type}
+              />
+            ) : (
+              <div
+                className={`bx--aspect-ratio bx--aspect-ratio--${select(
+                  'Ratio',
+                  ratio,
+                  ratio['none']
+                )}`}>
+                <Card
+                  title={title}
+                  eyebrow={eyebrow}
+                  inverse={inverse}
+                  image={image}
+                  copy={copy}
+                  cta={cta}
+                  icon={ArrowRight20}
+                  href={href}
+                  target={target}
+                  type={type}
+                  className="bx--aspect-ratio--object"
+                />
+              </div>
+            )}
           </div>
-        )}
+        </div>
       </div>
     );
   });
