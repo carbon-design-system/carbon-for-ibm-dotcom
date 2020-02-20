2ba212992c2b5b438bbca488003ef3ddcd61c10a
Mahesh Kothur <mkothur@us.ibm.com>
2020-02-20 08:35:20 -0500

    feat(pattern): created LinkList pattern

---
+++ packages/react/src/patterns/sub-patterns/LinkList/LinkList.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { CTA } from '../../../components/CTA';
+import { settings as ddsSettings } from '@carbon/ibmdotcom-utilities';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { settings } from 'carbon-components';
+
+const { stablePrefix } = ddsSettings;
+const { prefix } = settings;
+
+/**
+ * LinkList Component, for use with content arrays
+ *
+ * @param {object} props props object
+ * @param {string} props.heading  Heading string
+ * @param {Array} props.items array of item
+ * @returns {*} JSX LinkList component
+ */
+const LinkList = ({ heading, items }) => {
+  return (
+    <div
+      className={`${prefix}--link--list`}
+      data-autoid={`${stablePrefix}--link--list`}>
+      <h3 className={`${prefix}--link--list__heading`}>{heading}</h3>
+      <div className={`${prefix}--link--list__list`}>
+        {items.map((item, index) => {
+          return (
+            <div className={`${prefix}--link--list__list__CTA`} key={index}>
+              <h2>{item.heading}</h2>
+              <CTA type={item.type} {...item} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+LinkList.propTypes = {
+  heading: PropTypes.string,
+  items: PropTypes.array,
+};
+
+export default LinkList;
---
+++ packages/react/src/patterns/sub-patterns/LinkList/__stories__/LinkList.stories.js
@@ -0,0 +1,50 @@
+import './index.scss';
+import { select, text, withKnobs } from '@storybook/addon-knobs';
+import LinkList from '../LinkList';
+import React from 'react';
+import readme from '../README.md';
+import { settings } from 'carbon-components';
+import { storiesOf } from '@storybook/react';
+
+const { prefix } = settings;
+
+storiesOf('Patterns (Sub-Patterns)|LinkList', module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('Default', () => {
+    const heading = text('heading(required):', 'Tutorials');
+    const types = ['local', 'external'];
+    const type = [
+      select('cta 1 type', types, types[0]),
+      select('cta 2 type', types, types[0]),
+    ];
+    const items = [
+      {
+        heading: 'Containerization: A Complete Guide',
+        type: type[0],
+        style: 'text',
+        title: 'Lorem ipsum dolor sit amet',
+        href: 'https://ibm.com',
+      },
+      {
+        heading: 'Why should you use microservices and containers?',
+        type: type[1],
+        style: 'text',
+        title: 'Lorem ipsum dolor sit amet',
+        href: 'https://ibm.com',
+      },
+    ];
+    return (
+      <div className={`${prefix}--grid ${prefix}--link--list-story`}>
+        <div className="bx--row">
+          <div className="bx--col-sm-4 bx--col-lg-8 bx--offset-lg-4">
+            <LinkList heading={heading} items={items} />
+          </div>
+        </div>
+      </div>
+    );
+  });
