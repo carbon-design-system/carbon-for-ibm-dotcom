a0812bd12372eae8658da8a8ee642c265e1e21bc
Kenny Lam <kennylam@protonmail.com>
2020-02-26 11:09:59 -0500

    feat(util): add decode string utility

---
+++ packages/utilities/src/utilities/decodeString/decodeString.js
@@ -0,0 +1,21 @@
+/**
+ * Utility function to parse and decode text content.
+ * Strings can become encoded when passed as props for various reasons.
+ * This utility decodes those strings.
+ *
+ * @param {string} str String to decode
+ * @returns {string} Final string with decoded characters
+ * @example
+ * import { decodeString } from '@carbon/ibmdotcom-utilities'
+ *
+ * const str = decodeString('https://www.ibm.com/search?lang=en&amp;cc=us&amp;q=cloud');
+ * console.log(str); // https://www.ibm.com/search?lang=en&cc=us&q=cloud
+ *
+ */
+function decodeString(str) {
+  const parser = new DOMParser();
+  return parser.parseFromString(`<!doctype html><body>${str}`, 'text/html').body
+    .textContent;
+}
+
+export default decodeString;
