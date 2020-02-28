6988c83f384c4454384770c0c3328a739b565bf6
Mahesh Kothur <mkothur@us.ibm.com>
2020-02-28 15:08:42 -0500

    fix(pattern): updated the id and aria labelledby value

--- packages/react/src/patterns/sections/CardSection/CardSection.js
+++ packages/react/src/patterns/sections/CardSection/CardSection.js
@@ -91,7 +91,7 @@ const _renderCards = (cards, containerRef, cta) => (
         <div
           className={`${prefix}--card-section__cards__col`}
           role="region"
-          aria-labelledby={`region-${card.heading}`}>
+          aria-labelledby={card.heading}>
           <Card
             key={index}
             image={card.image}
@@ -105,7 +105,7 @@ const _renderCards = (cards, containerRef, cta) => (
               },
             }}
             type="link"
-            id={`region-${card.heading}`}
+            id={card.heading}
           />
         </div>
       );
