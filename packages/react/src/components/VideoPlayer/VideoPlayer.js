951e0454aa4cbd13457436bea04291913597376f
Kenny Lam <kennylam@protonmail.com>
2020-03-13 14:56:14 -0400

    feat(component): add video description and duration

--- packages/react/src/components/VideoPlayer/VideoPlayer.js
+++ packages/react/src/components/VideoPlayer/VideoPlayer.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { settings as ddsSettings } from '@carbon/ibmdotcom-utilities';
 import PropTypes from 'prop-types';
 import { settings } from 'carbon-components';
@@ -20,23 +20,54 @@ const { prefix } = settings;
  * @param {string} videoId Kaltura video id
  * @returns {*} VideoPlayer components
  */
-const VideoPlayer = ({ videoId }) => {
+const VideoPlayer = ({ videoId, showDescription }) => {
   const videoPlayerId = `video-player__video-${videoId}`;
+  const [videoData, setVideoData] = useState({});
+  const [videoDuration, setVideoDuration] = useState(0);
 
   useEffect(() => {
     (async () => {
       await VideoPlayerAPI.embedVideo(videoId, `${prefix}--${videoPlayerId}`);
+      const getVideoData = await VideoPlayerAPI.api(videoId);
+      setVideoData(getVideoData);
     })();
-  });
+  }, [videoId, videoPlayerId]);
+
+  /**
+   * Convert video duration in milliseconds to HH:MM:SS
+   *
+   * @param {string} duration video duration in milliseconds
+   * @returns {*} converted duration
+   */
+  function videoTime(duration) {
+    let seconds = Math.floor((duration / 1000) % 60);
+    let minutes = Math.floor((duration / (1000 * 60)) % 60);
+    let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    hours = hours > 0 ? hours + ':' : '';
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    return hours + minutes + ':' + seconds;
+  }
+
+  useEffect(() => {
+    setVideoDuration(videoTime(videoData.msDuration));
+  }, [videoData.msDuration]);
 
   return (
-    <div
-      className={`${prefix}--video-player__container`}
-      data-autoid={`${stablePrefix}--${videoPlayerId}`}>
+    <>
       <div
-        className={`${prefix}--video-player__video`}
-        id={`${prefix}--${videoPlayerId}`}></div>
-    </div>
+        className={`${prefix}--video-player__container`}
+        data-autoid={`${stablePrefix}--${videoPlayerId}`}>
+        <div
+          className={`${prefix}--video-player__video`}
+          id={`${prefix}--${videoPlayerId}`}></div>
+      </div>
+      {showDescription && (
+        <div className={`${prefix}--video-player__description`}>
+          {videoData.description} ({videoDuration})
+        </div>
+      )}
+    </>
   );
 };
 
@@ -44,10 +75,11 @@ const VideoPlayer = ({ videoId }) => {
  * @property propTypes
  * @description Defined property types for component
  *
- * @type {{videoId: string}}
+ * @type {{videoId: string, showDescription: bool}}
  */
 VideoPlayer.propTypes = {
   videoId: PropTypes.string.isRequired,
+  showDescription: PropTypes.bool,
 };
 
 export default VideoPlayer;
