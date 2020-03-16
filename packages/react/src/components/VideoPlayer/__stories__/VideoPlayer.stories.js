47725848db67bb666668062f9712ad769cb99fc5
kennylam <kennylam@protonmail.com>
2020-03-16 16:28:53 -0400

    Feat/video player (#1699)
    * feat(component): adds embedded Kaltura video player
    * feat(service): add video duration converter to video player service
    * feat(css): add VideoPlayer styles
    * feat(storybook): add VideoPlayer story and docs

---
+++ packages/react/src/components/VideoPlayer/__stories__/VideoPlayer.stories.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright IBM Corp. 2016, 2018
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import './index.scss';
+import { boolean, withKnobs } from '@storybook/addon-knobs';
+import React from 'react';
+import readme from '../README.md';
+import { storiesOf } from '@storybook/react';
+import VideoPlayer from '../VideoPlayer';
+
+storiesOf('Components|VideoPlayer', module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('Default', () => {
+    return (
+      <div className="bx--grid">
+        <div class="bx--row">
+          <div class="bx--col-sm-4 bx--col-lg-8 bx--offset-lg-4">
+            <VideoPlayer
+              videoId="0_uka1msg4"
+              showDescription={boolean('Show description', true)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  });
