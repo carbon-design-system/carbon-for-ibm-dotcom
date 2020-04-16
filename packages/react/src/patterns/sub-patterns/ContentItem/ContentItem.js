e62a1673632825e31462324c1fb3237b624b3d4d
Anna Wen <anna.wen@ibm.com>
2020-04-15 20:03:14 -0400

    feat(videoplayer): update patterns to use videoplayer

--- packages/react/src/patterns/sub-patterns/ContentItem/ContentItem.js
+++ packages/react/src/patterns/sub-patterns/ContentItem/ContentItem.js
@@ -11,6 +11,7 @@ import { markdownToHtml } from '@carbon/ibmdotcom-utilities';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { settings } from 'carbon-components';
+import { VideoPlayer } from '../../../components/VideoPlayer';
 
 const { stablePrefix } = ddsSettings;
 const { prefix } = settings;
@@ -22,10 +23,11 @@ const { prefix } = settings;
  * @param {string} props.cta cta object
  * @param {string} props.copy copy text
  * @param {string} props.heading  heading object
- * @param {object} props.image image object
+ * @param {string} props.mediaType Determines the media type (image or video)
+ * @param {object} props.mediaData Data properties for image or video
  * @returns {*} JSX ContentItem component
  */
-const ContentItem = ({ cta, copy, heading, image }) => (
+const ContentItem = ({ cta, copy, heading, mediaType, mediaData }) => (
   <div
     className={`${prefix}--content-item`}
     data-autoid={`${stablePrefix}--content-item`}>
@@ -36,12 +38,7 @@ const ContentItem = ({ cta, copy, heading, image }) => (
         {heading}
       </h4>
     )}
-    {image && (
-      <ImageWithCaption
-        customClassName={`${prefix}--content-item__image`}
-        {...image}
-      />
-    )}
+    {_renderMedia(mediaType, mediaData)}
     {copy && (
       <div
         data-autoid={`${stablePrefix}--content-item__copy`}
@@ -62,11 +59,31 @@ const ContentItem = ({ cta, copy, heading, image }) => (
   </div>
 );
 
+/**
+ * renders either video or image content
+ *
+ * @param {string} type cta type ( external | jump | local)
+ * @param {object} data cta type ( external | jump | local)
+ * @private
+ * @returns {*} media component
+ */
+const _renderMedia = (type, data) => {
+  if (data) {
+    return (
+      <div data-autoid={`${stablePrefix}--content-item__media`}>
+        {type === 'image' && <ImageWithCaption {...data} />}
+        {type === 'video' && <VideoPlayer {...data} />}
+      </div>
+    );
+  }
+};
+
 ContentItem.propTypes = {
   cta: PropTypes.oneOfType(PropTypes.shape(CTA.propTypes)),
   copy: PropTypes.string,
   heading: PropTypes.string,
-  image: PropTypes.oneOfType(PropTypes.shape(ImageWithCaption.propTypes)),
+  mediaType: PropTypes.string,
+  mediaData: PropTypes.object,
 };
 
 export default ContentItem;
