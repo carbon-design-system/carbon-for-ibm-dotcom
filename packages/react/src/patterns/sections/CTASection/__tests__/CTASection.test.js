2cb72bb66b272de6a0d25bde91bf40cd0974a828
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2020-05-06 15:19:51 -0300

    test(cta-section):  unit test coverage and storyshot update

--- packages/react/src/patterns/sections/CTASection/__tests__/CTASection.test.js
+++ packages/react/src/patterns/sections/CTASection/__tests__/CTASection.test.js
@@ -1,12 +1,35 @@
+import { ContentItem } from '../../../sub-patterns/ContentItem';
 import CTASection from '../CTASection';
 import { mount } from 'enzyme';
 import React from 'react';
 
+const contentItemsProps = [
+  {
+    heading: 'Get connected',
+    copy: `
+      IBM DevOps partners have a wide range of expertise.
+      Find one to build the right solution for you.
+      `,
+    cta: {
+      copy: 'Find a partner',
+      type: 'external',
+      href: 'https://example.com/',
+    },
+  },
+  {
+    heading: 'Learn how',
+    copy: 'Dig into more self-directed learning about DevOps methodologies.',
+    cta: {
+      copy: 'Browse tutorials',
+      type: 'local',
+      href: 'https://example.com/',
+    },
+  },
+];
+
 describe('CTA Section', () => {
-  it('should render `booting component` string', () => {
-    const wrapper = mount(<CTASection />);
-    const divElement = wrapper.find('div#boot');
-    const text = divElement.text();
-    expect(text).toMatch('booting component');
+  it('renders two ContentItems', () => {
+    const wrapper = mount(<CTASection items={contentItemsProps} />);
+    expect(wrapper.find(ContentItem)).toHaveLength(contentItemsProps.length);
   });
 });
