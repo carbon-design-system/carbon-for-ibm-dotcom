1928097aa20df4938da94b26c5aa2e5058afbf1a
Raphael Amadeu <30945011+RaphaelAmadeu@users.noreply.github.com>
2020-05-22 13:56:15 -0300

    chore(test): updated test

---
+++ packages/react/src/components/CTA/__tests__/CTA.test.js
@@ -0,0 +1,82 @@
+/**
+ * Copyright IBM Corp. 2016, 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import CardCTA from '../CardCTA';
+import CTALogic from '../CTALogic';
+import FeatureCTA from '../FeatureCTA';
+import { mount } from 'enzyme';
+import React from 'react';
+import TextCTA from '../TextCTA';
+
+describe('CTA', () => {
+  it('Renders TextCTA | type jump', () => {
+    CTALogic.jump = jest.fn();
+    const cta = mount(<TextCTA type="jump" href="#" copy="Lorem Ipsum" />);
+    cta.find('a').simulate('click');
+    expect(CTALogic.jump).toHaveBeenCalled();
+  });
+
+  it('Renders TextCTA | type video', () => {
+    CTALogic.setLightBox = jest.fn();
+    const cta = mount(
+      <TextCTA type="video" href="#" videoTitle={[{ title: 'Lorem Ipsum' }]} />
+    );
+    cta.find('a').simulate('click');
+    expect(CTALogic.setLightBox).toHaveBeenCalled();
+  });
+
+  it('Renders FeatureCTA | type video', () => {
+    CTALogic.setLightBox = jest.fn();
+    CTALogic.launchLightBox = jest.fn();
+    const cta = mount(
+      <FeatureCTA
+        card={{
+          cta: {
+            icon: {
+              src: '',
+            },
+            media: {
+              src: '0_uka1msg4',
+              type: 'video',
+            },
+          },
+        }}
+        type="video"
+        renderLightBox={false}
+        heading="lorem ipsum"
+        videoTitle={[{ title: 'Lorem Ipsum' }]}
+      />
+    );
+    cta.find('a').simulate('click');
+    expect(CTALogic.launchLightBox).toHaveBeenCalled();
+  });
+
+  it('Renders CardCTA | type video', () => {
+    CTALogic.setLightBox = jest.fn();
+    CTALogic.launchLightBox = jest.fn();
+    const cta = mount(
+      <CardCTA type="video" videoTitle={[{ title: 'Lorem Ipsum' }]} />
+    );
+    expect(CTALogic.launchLightBox).toHaveBeenCalled();
+    cta.find('a').simulate('click');
+    expect(CTALogic.setLightBox).toHaveBeenCalled();
+  });
+
+  it('Renders CardCTA | type video', () => {
+    CTALogic.jump = jest.fn();
+    const cta = mount(
+      <CardCTA
+        type="jump"
+        cta={{
+          href: '#',
+        }}
+      />
+    );
+    cta.find('a').simulate('click');
+    expect(CTALogic.jump).toHaveBeenCalled();
+  });
+});
