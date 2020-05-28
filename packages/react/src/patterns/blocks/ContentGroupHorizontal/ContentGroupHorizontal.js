f14d9931d5b8e866d69480235922d347d4bbc494
Greeshma Reddy Gaddam <greeshma.reddy.gaddam@ibm.com>
2020-05-27 21:58:40 -0400

    fix(pattern): horizontal rule at the end

--- packages/react/src/patterns/blocks/ContentGroupHorizontal/ContentGroupHorizontal.js
+++ packages/react/src/patterns/blocks/ContentGroupHorizontal/ContentGroupHorizontal.js
@@ -8,6 +8,7 @@
 import { ContentBlock } from '../../sub-patterns/ContentBlock';
 import { ContentItemHorizontal } from '../../sub-patterns/ContentItemHorizontal';
 import { settings as ddsSettings } from '@carbon/ibmdotcom-utilities';
+import {HorizontalRule} from "../../../components/HorizontalRule";
 import PropTypes from 'prop-types';
 import React from 'react';
 import settings from 'carbon-components/es/globals/js/settings';
@@ -20,20 +21,24 @@ const { prefix } = settings;
  */
 const ContentGroupHorizontal = ({ heading, items }) => {
   return (
-    <ContentBlock
-      heading={heading}
-      autoid={`${stablePrefix}--content-group-horizontal`}
+    <div
+      data-autoid={`${stablePrefix}--content-group-horizontal`}
       className={`${prefix}--content-group-horizontal`}>
-      {items.map((item, index) => (
-        <ContentItemHorizontal
-          eyebrow={item.eyebrow}
-          heading={item.heading}
-          copy={item.copy}
-          cta={item.cta}
-          key={index}
-        />
-      ))}
-    </ContentBlock>
+      <ContentBlock
+        heading={heading}
+        >
+        {items.map((item, index) => (
+          <ContentItemHorizontal
+            eyebrow={item.eyebrow}
+            heading={item.heading}
+            copy={item.copy}
+            cta={item.cta}
+            key={index}
+          />
+        ))}
+      </ContentBlock>
+      <HorizontalRule />
+    </div>
   );
 };
 
