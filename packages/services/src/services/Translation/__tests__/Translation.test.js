37aa7b28e77f8fc74142918b4e94229475eee9fa
Jeff Chew <jeff.chew@gmail.com>
2020-06-02 17:05:43 -0400

    fix(jest): fixing mockAxios from showing up in JSDocs

--- packages/services/src/services/Translation/__tests__/Translation.test.js
+++ packages/services/src/services/Translation/__tests__/Translation.test.js
@@ -4,8 +4,9 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-const mockAxios = require('axios');
-const responseSuccess = require('./data/response.json');
+
+import mockAxios from 'axios';
+import responseSuccess from './data/response.json';
 
 jest.mock('../../Locale', () => ({
   LocaleAPI: {
