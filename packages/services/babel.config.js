81a45872e4c872f1629df2f34e9b3b550d8803e9
Jeffrey Chew <jeff.chew@gmail.com>
2020-06-03 08:26:44 -0400

    fix(corsproxy): removing cors proxy on production environments (#2633)
    This will make the proxy server blank if the call is being made from www.ibm.com
    
    In addition, this will include REACT_APP_CORS_PROXY as an option for adopters that
    are using create-react-app. Because CRA only allows environment variables that
    begin with REACT_APP_, the only way around this would be to eject then customize the env.js configuration to allow CORS_PROXY. This will hopefully make it easier
    to adopt the DotcomShell in an OOTB CRA application.
    
    One thing to note, I was trying to get the unit tests to switch environment variables
    in place, but the mock was not reinitializing for axios so setting this aside for now.

--- packages/services/babel.config.js
+++ packages/services/babel.config.js
@@ -15,6 +15,8 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
     [
       '@babel/plugin-transform-runtime',
       {
