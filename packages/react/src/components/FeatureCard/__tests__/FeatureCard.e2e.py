ac73ba40dfb3f18c689ff25caad93ee2ccde647d
Jeff Chew <jeff.chew@gmail.com>
2020-06-05 15:51:53 -0400

    fix(selenium): initial cleanup of e2e test files (WIP)

---
+++ packages/react/src/components/FeatureCard/__tests__/FeatureCard.e2e.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from dotenv import load_dotenv
+load_dotenv()
+import unittest
+import time
+import os
+
+# Setup Driver
+url = os.environ.get('SELENIUM_URL') or "https://ibmdotcom-react-canary.mybluemix.net/"
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.refresh()
+driver.get(url)
+
+time.sleep(11)
+# Selecting Feature Card
+FeatureCard_Select = driver.find_element_by_id("patterns-blocks-featurecardblockmedium")
+driver.execute_script("arguments[0].scrollIntoView();", FeatureCard_Select)
+FeatureCard_Select.click()
+print("Feature card selected")
+time.sleep(4)
+driver.switch_to_frame("storybook-preview-iframe")
+time.sleep(2)
+# Pixel values between sections
+featureCard_topSpacing = driver.find_element_by_css_selector(".bx--content-group").value_of_css_property("margin-top")
+print("Pixel value at top is :" + featureCard_topSpacing)
+featureCard_bottomSpacing = driver.find_element_by_css_selector(".bx--content-group").value_of_css_property("margin-bottom")
+print("Pixel value at top is :" + featureCard_bottomSpacing)
+heading_FC_spacing = driver.find_element_by_css_selector(".bx--content-group__title").value_of_css_property("margin-bottom")
+print("Pixel value between heading & FC is :" + heading_FC_spacing)
+time.sleep(4)
+driver.switch_to_default_content()
+driver.find_element_by_xpath("//button[contains(.,'Knobs')]").click()
+CardHeading_Update = driver.find_element_by_xpath("//textarea[@id='Card Heading:']")
+CardHeading_Update.clear()
+CardHeading_Update.send_keys("Explore AI use cases in all industries Curabitur malesuada varius mi eu posuere")
+time.sleep(2)
+driver.switch_to_default_content()
+Go_Full_Screen = driver.find_element_by_xpath("//*[contains(@title, 'Go full screen')]")
+Go_Full_Screen.click()
+driver.switch_to_frame("storybook-preview-iframe")
+time.sleep(2)
+Updated_Heading = driver.find_element_by_xpath("//h3[@class='bx--card__heading']")
+print ("Updated card heading is " + Updated_Heading.text)
+time.sleep(2)
+Card_click = driver.find_element_by_xpath("//div[@class='bx--card__wrapper']")
+Card_click.click()
+print("clicked on FC")
+time.sleep(2)
+
+driver.quit()
