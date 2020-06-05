ae985e803b70d1dc88575190fed07abfbdbeeddf
Anna Wen <>
2020-06-05 13:47:06 -0400

    chore(storybook): adjust knobs in card story

--- packages/react/src/components/Card/__stories__/Card.stories.js
+++ packages/react/src/components/Card/__stories__/Card.stories.js
@@ -18,31 +18,6 @@ const themes = {
   g100: 'g100',
 };
 
-const cardKnobs = ({ groupId }) => ({
-  image:
-    (boolean('image', false, groupId) && {
-      defaultSrc: 'https://dummyimage.com/600x300/ee5396/161616&text=2:1',
-      alt: 'Image alt text',
-    }) ||
-    undefined,
-  eyebrow: text('eyebrow', 'eyebrow text', groupId),
-  heading: text('title (required)', 'Lorem ipsum dolor sit amet', groupId),
-  copy: text('copy', '', groupId),
-  cta: object(
-    'cta',
-    {
-      type: 'local',
-      copy: 'click here',
-      href: 'https://example.com',
-      icon: {
-        src: ArrowRight20,
-      },
-    },
-    groupId
-  ),
-  inverse: boolean('inverse', false, groupId),
-});
-
 export default {
   title: 'Components|Card',
 
@@ -50,26 +25,38 @@ export default {
     ...readme.parameters,
     knobs: {
       Card: ({ groupId }) => ({
-        ...cardKnobs({ groupId }),
+        image:
+          (boolean('image', false, groupId) && {
+            defaultSrc: 'https://dummyimage.com/600x300/ee5396/161616&text=2:1',
+            alt: 'Image alt text',
+          }) ||
+          undefined,
+        eyebrow: text('eyebrow', 'eyebrow text', groupId),
+        heading: text(
+          'title (required)',
+          'Lorem ipsum dolor sit amet',
+          groupId
+        ),
+        copy: text('copy', '', groupId),
+        inverse: boolean('inverse', false, groupId),
       }),
       Other: ({ groupId }) => ({
         theme: select('theme', themes, themes.white, groupId),
-      }),
-      ClickableCard: ({ groupId }) => ({
-        ...cardKnobs({ groupId }),
-        cta: object('cta', {
-          type: 'local',
-          href: 'https://example.com',
-          icon: {
-            src: ArrowRight20,
-          },
-        }),
       }),
     },
   },
 };
 
 export const Static = ({ parameters }) => {
+  const cta = object('cta', {
+    type: 'local',
+    copy: 'click here',
+    href: 'https://example.com',
+    icon: {
+      src: ArrowRight20,
+    },
+  });
+
   const { theme } = parameters?.props?.Other ?? {};
 
   return (
@@ -77,7 +64,11 @@ export const Static = ({ parameters }) => {
       <div className="bx--grid">
         <div className="bx--row">
           <div className="bx--col-sm-2 bx--col-md-3 bx--col-lg-6 bx--col-xlg-4 bx--no-gutter">
-            <Card {...(parameters?.props?.Card ?? {})} type="static" />
+            <Card
+              {...(parameters?.props?.Card ?? {})}
+              cta={cta}
+              type="static"
+            />
           </div>
         </div>
       </div>
@@ -86,6 +77,13 @@ export const Static = ({ parameters }) => {
 };
 
 export const LinkClickable = ({ parameters }) => {
+  const cta = object('cta', {
+    type: 'local',
+    href: 'https://example.com',
+    icon: {
+      src: ArrowRight20,
+    },
+  });
   const { theme } = parameters?.props?.Other ?? {};
 
   return (
@@ -93,7 +91,7 @@ export const LinkClickable = ({ parameters }) => {
       <div className="bx--grid">
         <div className="bx--row">
           <div className="bx--col-sm-2 bx--col-md-3 bx--col-lg-6 bx--col-xlg-4 bx--no-gutter">
-            <Card {...(parameters?.props?.ClickableCard ?? {})} type="link" />
+            <Card {...(parameters?.props?.Card ?? {})} cta={cta} type="link" />
           </div>
         </div>
       </div>
