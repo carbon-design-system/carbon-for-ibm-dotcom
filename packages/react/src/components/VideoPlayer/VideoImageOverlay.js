51412b7caced56160ccd18007d815cc94f8818f4
Anna Wen <>
2020-06-10 16:14:41 -0400

    fix(video-player): adjust outline on focus to be more prominent

--- packages/react/src/components/VideoPlayer/VideoImageOverlay.js
+++ packages/react/src/components/VideoPlayer/VideoImageOverlay.js
@@ -4,11 +4,16 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { settings as ddsSettings } from '@carbon/ibmdotcom-utilities';
 import { Image } from '../Image';
 import PlayIcon from '@carbon/ibmdotcom-styles/icons/svg/play-video.svg';
 import PropTypes from 'prop-types';
 import React from 'react';
+import settings from 'carbon-components/es/globals/js/settings';
 import { VideoPlayerAPI } from '@carbon/ibmdotcom-services';
+
+const { stablePrefix } = ddsSettings;
+const { prefix } = settings;
 
 /**
  * VideoPlayer Image Overlay component
@@ -17,12 +22,13 @@ const VideoImageOverlay = ({ videoId, videoData, embedVideo }) => {
   return (
     <button
       id="video-thumbnail-overlay"
+      className={`${prefix}--video-player__image-overlay`}
+      data-autoid={`${stablePrefix}--video-player__image-overlay`}
       onClick={() => _embedPlayer({ embedVideo })}>
       <Image
         defaultSrc={VideoPlayerAPI.getThumbnailUrl({
           videoId,
           width: '655',
-          height: '378',
         })}
         alt={videoData.name}
         icon={PlayIcon}
