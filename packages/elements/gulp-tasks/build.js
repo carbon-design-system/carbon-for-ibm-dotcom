asudoh/ibm-dotcom-library

chore(packages): add elements package
This change introduces a variant of `@carbon/ibmdotcom-vanilla` that
uses the latest web standards, which are, Shadow DOM and Custom
Elements. The version number is set as "under development" for now.

Refs #2810.

packages/elements/gulp-tasks/build.js
@@ -1,0 +1,131 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const { readFile } = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const asyncDone = require('async-done');
+const gulp = require('gulp');
+const filter = require('gulp-filter');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cleanCSS = require('gulp-clean-css');
+const prettier = require('gulp-prettier');
+const typescript = require('gulp-typescript');
+const header = require('gulp-header');
+const through2 = require('through2');
+const stripComments = require('strip-comments');
+const autoprefixer = require('autoprefixer');
+const rtlcss = require('rtlcss');
+const replaceExtension = require('replace-ext');
+const babelPluginResourceJSPaths = require('../tools/babel-plugin-resource-js-paths');
+const fixHostPseudo = require('../tools/postcss-fix-host-pseudo');
+
+const config = require('./config');
+
+const readFileAsync = promisify(readFile);
+const promisifyStream = promisify(asyncDone);
+
+const cssStream = ({ banner, dir }) =>
+  gulp
+    .src(`${config.srcDir}/**/*.scss`)
+    .pipe(
+      header(`
+        $feature-flags: (
+          enable-css-custom-properties: true
+        );
+      `)
+    )
+    .pipe(
+      sass({
+        includePaths: ['node_modules'],
+      })
+    )
+    .pipe(
+      postcss([
+        fixHostPseudo(),
+        autoprefixer({
+          // TODO: Optimize for modern browsers here
+          browsers: ['last 1 version', 'Firefox ESR', 'ie >= 11'],
+        }),
+        ...(dir === 'rtl' ? [rtlcss] : []),
+      ])
+    )
+    .pipe(cleanCSS())
+    .pipe(
+      through2.obj((file, enc, done) => {
+        file.contents = Buffer.from(`
+        import { css } from 'lit-element';
+        export default css([${JSON.stringify(String(file.contents))}]);
+      `);
+        file.path = replaceExtension(file.path, dir === 'rtl' ? '.rtl.css.js' : '.css.js');
+        done(null, file);
+      })
+    )
+    .pipe(prettier())
+    .pipe(header(banner))
+    .pipe(gulp.dest(path.resolve(config.jsDestDir)));
+
+module.exports = {
+  modules: {
+    async css() {
+      const banner = await readFileAsync(path.resolve(__dirname, '../../../tasks/license.js'), 'utf8');
+      await Promise.all([promisifyStream(() => cssStream({ banner })), promisifyStream(() => cssStream({ banner, dir: 'rtl' }))]);
+    },
+
+    scripts() {
+      return (
+        gulp
+          .src([
+            `${config.srcDir}/**/*.ts`,
+            `!${config.srcDir}/**/*-story*.ts*`,
+            `!${config.srcDir}/**/stories/*.ts`,
+            `!${config.srcDir}/**/*.d.ts`,
+          ])
+          .pipe(sourcemaps.init())
+          .pipe(
+            babel({
+              presets: ['@babel/preset-modules'],
+              // `version: '7.3.0'` ensures `@babel/plugin-transform-runtime` is applied to decorator helper
+              plugins: [babelPluginResourceJSPaths],
+            })
+          )
+          // Avoids generating `.js` from interface-only `.ts` files
+          .pipe(filter(file => stripComments(file.contents.toString(), { sourceType: 'module' }).replace(/\s/g, '')))
+          .pipe(sourcemaps.write('.'))
+          .pipe(gulp.dest(config.jsDestDir))
+      );
+    },
+
+    types() {
+      const tsProject = typescript.createProject(path.resolve(__dirname, '../tsconfig.json'));
+      const { dts } = gulp
+        .src([`${config.srcDir}/**/*.ts`, `!${config.srcDir}/**/*-story*.ts*`, `!${config.srcDir}/**/stories/**/*.ts*`])
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+      return dts
+        .pipe(
+          through2.obj((file, enc, done) => {
+            file.contents = Buffer.from(`${file.contents.toString()}\n//# sourceMappingURL=${path.basename(file.path)}.map\n`);
+            done(null, file);
+          })
+        )
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.jsDestDir));
+    },
+  },
+
+  sass() {
+    return gulp.src([`${config.srcDir}/**/*.scss`, `!${config.srcDir}/**/*-story.scss`]).pipe(gulp.dest(config.sassDestDir));
+  },
+};

packages/elements/tools/svg-result-ibmdotcom-icon-loader.js
@@ -1,0 +1,80 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const svg2js = require('svgo/lib/svgo/svg2js');
+const createSVGResultFromCarbonIcon = require('./svg-result-carbon-icon');
+
+/**
+ * @param {Object} node The node in SVG2JS result.
+ * @returns {Object} The first `<svg>` in the given SVG2JS result.
+ */
+function findRootNode(node) {
+  return node.elem === 'svg' ? node : node.content && node.content.find(item => findRootNode(item));
+}
+
+/**
+ * Converts `attrs` properties in each node, recursively, from `attrName: { value: attrValue }` to `attrName: attrValue`.
+ * @param {Object} node The node in SVG2JS result.
+ * @returns {Object} The given node, after the `attrs` property is converted.
+ */
+function convertAttrs(node) {
+  const { attrs, content } = node || {};
+  if (!node || (!attrs && !content)) {
+    return node;
+  }
+  const result = {
+    ...node,
+  };
+  if (attrs) {
+    result.attrs = Object.keys(attrs).reduce(
+      (acc, name) => ({
+        ...acc,
+        [name]: node.attrs[name].value,
+      }),
+      {}
+    );
+  }
+  if (content) {
+    result.content = content.map(item => convertAttrs(item));
+  }
+  return result;
+}
+
+/**
+ * A WebPack loader to generate `lit-html`'s `SVGResult` from an icon descriptor from `@carbon/icons`.
+ * @returns {string} The massaged module content.
+ */
+function svgResultCarbonIconLoader(content) {
+  const callback = this.async();
+  svg2js(content, result => {
+    const { error: message } = result;
+    if (message) {
+      callback(new Error(message));
+    } else {
+      const svgNode = findRootNode(result);
+      if (!svgNode) {
+        callback(new Error(`Wrong SVG2JS result found in: ${this.resourcePath}`));
+      } else {
+        callback(
+          null,
+          `
+          import { svg } from 'lit-html';
+          import spread from 'carbon-custom-elements/es/globals/directives/spread';
+          const svgResultCarbonIcon = ${createSVGResultFromCarbonIcon(convertAttrs(svgNode))};
+          export default svgResultCarbonIcon;
+        `
+        );
+      }
+    }
+  });
+}
+
+module.exports = svgResultCarbonIconLoader;

carbon-design-system/carbon-for-ibm-dotcom

packages/elements/gulp-tasks/build.js
@@ -1,0 +1,131 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const { readFile } = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const asyncDone = require('async-done');
+const gulp = require('gulp');
+const filter = require('gulp-filter');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cleanCSS = require('gulp-clean-css');
+const prettier = require('gulp-prettier');
+const typescript = require('gulp-typescript');
+const header = require('gulp-header');
+const through2 = require('through2');
+const stripComments = require('strip-comments');
+const autoprefixer = require('autoprefixer');
+const rtlcss = require('rtlcss');
+const replaceExtension = require('replace-ext');
+const babelPluginResourceJSPaths = require('../tools/babel-plugin-resource-js-paths');
+const fixHostPseudo = require('../tools/postcss-fix-host-pseudo');
+
+const config = require('./config');
+
+const readFileAsync = promisify(readFile);
+const promisifyStream = promisify(asyncDone);
+
+const cssStream = ({ banner, dir }) =>
+  gulp
+    .src(`${config.srcDir}/**/*.scss`)
+    .pipe(
+      header(`
+        $feature-flags: (
+          enable-css-custom-properties: true
+        );
+      `)
+    )
+    .pipe(
+      sass({
+        includePaths: ['node_modules'],
+      })
+    )
+    .pipe(
+      postcss([
+        fixHostPseudo(),
+        autoprefixer({
+          // TODO: Optimize for modern browsers here
+          browsers: ['last 1 version', 'Firefox ESR', 'ie >= 11'],
+        }),
+        ...(dir === 'rtl' ? [rtlcss] : []),
+      ])
+    )
+    .pipe(cleanCSS())
+    .pipe(
+      through2.obj((file, enc, done) => {
+        file.contents = Buffer.from(`
+        import { css } from 'lit-element';
+        export default css([${JSON.stringify(String(file.contents))}]);
+      `);
+        file.path = replaceExtension(file.path, dir === 'rtl' ? '.rtl.css.js' : '.css.js');
+        done(null, file);
+      })
+    )
+    .pipe(prettier())
+    .pipe(header(banner))
+    .pipe(gulp.dest(path.resolve(config.jsDestDir)));
+
+module.exports = {
+  modules: {
+    async css() {
+      const banner = await readFileAsync(path.resolve(__dirname, '../../../tasks/license.js'), 'utf8');
+      await Promise.all([promisifyStream(() => cssStream({ banner })), promisifyStream(() => cssStream({ banner, dir: 'rtl' }))]);
+    },
+
+    scripts() {
+      return (
+        gulp
+          .src([
+            `${config.srcDir}/**/*.ts`,
+            `!${config.srcDir}/**/__stories__/*.ts`,
+            `!${config.srcDir}/**/__tests__/*.ts`,
+            `!${config.srcDir}/**/*.d.ts`,
+          ])
+          .pipe(sourcemaps.init())
+          .pipe(
+            babel({
+              presets: ['@babel/preset-modules'],
+              // `version: '7.3.0'` ensures `@babel/plugin-transform-runtime` is applied to decorator helper
+              plugins: [babelPluginResourceJSPaths],
+            })
+          )
+          // Avoids generating `.js` from interface-only `.ts` files
+          .pipe(filter(file => stripComments(file.contents.toString(), { sourceType: 'module' }).replace(/\s/g, '')))
+          .pipe(sourcemaps.write('.'))
+          .pipe(gulp.dest(config.jsDestDir))
+      );
+    },
+
+    types() {
+      const tsProject = typescript.createProject(path.resolve(__dirname, '../tsconfig.json'));
+      const { dts } = gulp
+        .src([`${config.srcDir}/**/*.ts`, `!${config.srcDir}/**/__stories__/*.ts`, `!${config.srcDir}/**/__tests__/*.ts`])
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+      return dts
+        .pipe(
+          through2.obj((file, enc, done) => {
+            file.contents = Buffer.from(`${file.contents.toString()}\n//# sourceMappingURL=${path.basename(file.path)}.map\n`);
+            done(null, file);
+          })
+        )
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.jsDestDir));
+    },
+  },
+
+  sass() {
+    return gulp.src([`${config.srcDir}/**/*.scss`, `!${config.srcDir}/**/*-story.scss`]).pipe(gulp.dest(config.sassDestDir));
+  },
+};

packages/elements/tools/svg-result-ibmdotcom-icon-loader.js
@@ -1,0 +1,80 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const svg2js = require('svgo/lib/svgo/svg2js');
+const createSVGResultFromCarbonIcon = require('./svg-result-carbon-icon');
+
+/**
+ * @param {object} node The node in SVG2JS result.
+ * @returns {object} The first `<svg>` in the given SVG2JS result.
+ */
+function findRootNode(node) {
+  return node.elem === 'svg' ? node : node.content && node.content.find(item => findRootNode(item));
+}
+
+/**
+ * Converts `attrs` properties in each node, recursively, from `attrName: { value: attrValue }` to `attrName: attrValue`.
+ *
+ * @param {object} node The node in SVG2JS result.
+ * @returns {object} The given node, after the `attrs` property is converted.
+ */
+function convertAttrs(node) {
+  const { attrs, content } = node || {};
+  if (!node || (!attrs && !content)) {
+    return node;
+  }
+  const result = {
+    ...node,
+  };
+  if (attrs) {
+    result.attrs = Object.keys(attrs).reduce(
+      (acc, name) => ({
+        ...acc,
+        [name]: node.attrs[name].value,
+      }),
+      {}
+    );
+  }
+  if (content) {
+    result.content = content.map(item => convertAttrs(item));
+  }
+  return result;
+}
+
+/**
+ * A WebPack loader to generate `lit-html`'s `SVGResult` from an icon descriptor from `@carbon/icons`.
+ */
+function svgResultIBMDotcomIconLoader(content) {
+  const callback = this.async();
+  svg2js(content, result => {
+    const { error: message } = result;
+    if (message) {
+      callback(new Error(message));
+    } else {
+      const svgNode = findRootNode(result);
+      if (!svgNode) {
+        callback(new Error(`Wrong SVG2JS result found in: ${this.resourcePath}`));
+      } else {
+        callback(
+          null,
+          `
+          import { svg } from 'lit-html';
+          import spread from 'carbon-custom-elements/es/globals/directives/spread';
+          const svgResultCarbonIcon = ${createSVGResultFromCarbonIcon(convertAttrs(svgNode))};
+          export default svgResultCarbonIcon;
+        `
+        );
+      }
+    }
+  });
+}
+
+module.exports = svgResultIBMDotcomIconLoader;