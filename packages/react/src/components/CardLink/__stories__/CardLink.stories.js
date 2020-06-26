85c59ebce4c7f21446c19f1bdf8f4377026e47b5
ariellagilmore <ariellalgilmore@gmail.com>
2020-06-26 13:23:16 -0700

    fix(CardLink): grid alignment with ratio

--- packages/react/src/components/CardLink/__stories__/CardLink.stories.js
+++ packages/react/src/components/CardLink/__stories__/CardLink.stories.js
@@ -58,7 +58,7 @@ export const Default = ({ parameters }) => {
   return (
     <div className="bx--grid bx--grid--condensed">
       <div className="bx--row">
-        <div className="bx--col-sm-4 bx--col-lg-8 bx--offset-lg-4">
+        <div className="bx--col-sm-4 bx--col-md-4 bx--col-lg-4 bx--offset-lg-4 bx--aspect-ratio--2x1">
           <CardLink card={card} disabled={disabled} />
         </div>
       </div>
