5060cc3cea9fc20e6656e452cb46e3593d4b7398
Jeff Chew <jeff.chew@gmail.com>
2020-06-26 09:59:04 -0400

    test(CardSectionImages): fixes to CardSectionImages e2e tests

--- packages/react/src/patterns/sections/CardSectionImages/__tests__/CardSectionImages.e2e.js
+++ packages/react/src/patterns/sections/CardSectionImages/__tests__/CardSectionImages.e2e.js
@@ -23,18 +23,16 @@ const _url =
 const _path = '?path=/story/patterns-sections-cardsectionimages--default';
 
 describe('CardSectionImages', () => {
-  let headingField, themeField, iframe;
   beforeAll(() => {
     browser.url(_url + _path);
-    headingField = $('textarea[id="Heading (required):_CardSectionImages"]');
-    themeField = $('select[name="theme_CardSectionImages"]');
-    iframe = $('#storybook-preview-iframe');
     browser.setWindowSize(1200, 800);
+    browser.pause(1000);
   });
 
   it('should load the g10 theme correctly', () => {
-    themeField.selectByAttribute('value', 'g10');
-    browser.switchToFrame(iframe);
+    $('button=Carbon theme').click();
+    $('select[name="carbon-theme"]').selectByAttribute('value', 'g10');
+    browser.switchToFrame($('#storybook-preview-iframe'));
     const background = $('.bx--card-group.bx--card-group--g10').getCSSProperty(
       'background'
     );
@@ -43,8 +41,9 @@ describe('CardSectionImages', () => {
   });
 
   it('should load the g90 theme correctly', () => {
-    themeField.selectByAttribute('value', 'g90'); // TODO: can't seem to fetch using $('select[name="theme"])
-    browser.switchToFrame(iframe);
+    $('button=Carbon theme').click();
+    $('select[name="carbon-theme"]').selectByAttribute('value', 'g90');
+    browser.switchToFrame($('#storybook-preview-iframe'));
     const background = $('.bx--card-group.bx--card-group--g90').getCSSProperty(
       'background'
     );
@@ -53,8 +52,9 @@ describe('CardSectionImages', () => {
   });
 
   it('should load the g100 theme correctly', () => {
-    themeField.selectByAttribute('value', 'g100'); // TODO: can't seem to fetch using $('select[name="theme"])
-    browser.switchToFrame(iframe);
+    $('button=Carbon theme').click();
+    $('select[name="carbon-theme"]').selectByAttribute('value', 'g100'); // TODO: can't seem to fetch using $('select[name="theme"])
+    browser.switchToFrame($('#storybook-preview-iframe'));
     const background = $('.bx--card-group.bx--card-group--g100').getCSSProperty(
       'background'
     );
@@ -63,11 +63,15 @@ describe('CardSectionImages', () => {
   });
 
   it('should set the heading correctly', () => {
+    $('button=Knobs').click();
     const heading =
       'Serving society ethically in the age of Artificial Intelligence.';
+    const headingField = $(
+      'textarea[id="Heading (required):_CardSectionImages"]'
+    );
     headingField.setValue('');
     headingField.setValue(heading);
-    browser.switchToFrame(iframe);
+    browser.switchToFrame($('#storybook-preview-iframe'));
     browser.pause(1000);
     const headingText = $('.bx--content-section__heading').getText();
     expect(headingText).toEqual(heading);
