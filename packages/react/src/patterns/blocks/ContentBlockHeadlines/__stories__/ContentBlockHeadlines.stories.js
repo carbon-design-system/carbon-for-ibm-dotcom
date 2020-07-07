93240aca9032ef34712089f23de9a20dfe736856
Kenny Lam <kennylam@protonmail.com>
2020-07-06 21:16:52 -0400

    feat(ContentBlockHeadlines): add new pattern

---
+++ packages/react/src/patterns/blocks/ContentBlockHeadlines/__stories__/ContentBlockHeadlines.stories.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright IBM Corp. 2016, 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { object, text } from '@storybook/addon-knobs';
+import ContentBlockHeadlines from '../ContentBlockHeadlines';
+import items from './data/items.json';
+import React from 'react';
+import readme from '../README.stories.mdx';
+
+export default {
+  title: 'Patterns (Blocks)|ContentBlockHeadlines',
+
+  parameters: {
+    ...readme.parameters,
+    knobs: {
+      ContentBlockHeadlines: ({ groupId }) => ({
+        heading: text('Heading', 'Aliquam condimentum', groupId),
+        copy: text(
+          'Copy',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est. Mauris iaculis eget dolor nec hendrerit. Phasellus at elit sollicitudin, sodales nulla quis, consequat libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est. Mauris iaculis eget dolor nec hendrerit.',
+          groupId
+        ),
+        items: object('Items', items.items, groupId),
+      }),
+    },
+    props: {
+      ContentBlockHeadlines: {
+        items: items.items,
+      },
+    },
+  },
+};
+
+export const Default = ({ parameters }) => {
+  const { heading, copy, items } =
+    parameters?.props?.ContentBlockHeadlines ?? {};
+  return (
+    <div className="bx--grid">
+      <div className="bx--row">
+        <div className="bx--col-sm-4 bx--col-lg-10 bx--offset-lg-4">
+          <ContentBlockHeadlines heading={heading} copy={copy} items={items} />
+        </div>
+      </div>
+    </div>
+  );
+};
