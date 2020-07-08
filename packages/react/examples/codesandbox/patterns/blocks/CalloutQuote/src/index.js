6380e68e5fad71bb15e2d9fb9061fba42a4dc90c
Kenny Lam <kennylam@protonmail.com>
2020-07-08 09:47:11 -0400

    chore(examples): add codesandbox pattern examples

---
+++ packages/react/examples/codesandbox/patterns/blocks/CalloutQuote/src/index.js
@@ -0,0 +1,40 @@
+/**
+  Copyright IBM Corp. 2016, 2020
+
+  This source code is licensed under the Apache-2.0 license found in the
+  LICENSE file in the root directory of this source tree.
+ */
+
+import "./styles.scss";
+
+import React from "react";
+import ReactDom from "react-dom";
+import { CalloutQuote } from "@carbon/ibmdotcom-react";
+
+const quote = {
+  copy:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus est purus, posuere at est vitae, ornare rhoncus sem. Suspendisse vitae tellus fermentum, hendrerit augue eu, placerat magna.",
+  markType: "doubleCurved",
+  source: {
+    heading: "Lorem ipsum dolor sit amet",
+    copy: "consectetur adipiscing elit",
+    copy2: "IBM Cloud",
+  },
+  cta: {
+    copy: "Link with Icon",
+    type: "local",
+    href: "https://example.com",
+  },
+};
+
+const App = () => (
+  <div className="bx--grid">
+    <div className="bx--row">
+      <div className="bx--col-lg-12">
+        <CalloutQuote quote={quote} />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDom.render(<App />, document.getElementById("app"));
---
+++ packages/react/examples/codesandbox/patterns/blocks/LeadSpaceBlock/src/index.js
@@ -0,0 +1,96 @@
+/**
+  Copyright IBM Corp. 2016, 2020
+
+  This source code is licensed under the Apache-2.0 license found in the
+  LICENSE file in the root directory of this source tree.
+ */
+
+import "./styles.scss";
+
+import React from "react";
+import ReactDom from "react-dom";
+import { LeadSpaceBlock } from "@carbon/ibmdotcom-react";
+
+const title = "Continuous delivery";
+const heading = "Innovate like a startup and scale for the enterprise ";
+const copy = `Automate your software release process with continuous delivery (CD)—the most
+            critical part of adopting DevOps. Build, test, and deploy code changes quickly,
+            ensuring software is always ready for deployment.`;
+const mediaData = {
+  heading: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  image: {
+    sources: [
+      {
+        src: "https://dummyimage.com/320x180/ee5396/161616&text=16:9",
+        breakpoint: 320,
+      },
+      {
+        src: "https://dummyimage.com/400x225/ee5396/161616&text=16:9",
+        breakpoint: 400,
+      },
+      {
+        src: "https://dummyimage.com/672x378/ee5396/161616&text=16:9",
+        breakpoint: 672,
+      },
+    ],
+    alt: "Image alt text",
+    defaultSrc: "https://dummyimage.com/672x378/ee5396/161616&text=16:9",
+  },
+};
+const items = {
+  heading: "Featured products",
+  items: [
+    {
+      type: "local",
+      copy: "IBM Cloud Continuous Delivery",
+      cta: {
+        href: "https://ibm.com",
+      },
+    },
+    {
+      type: "local",
+      copy: "UrbanCode",
+      cta: {
+        href: "https://ibm.com",
+      },
+    },
+    {
+      type: "download",
+      copy: "View all products",
+      cta: {
+        href: "https://ibm.com",
+      },
+    },
+  ],
+};
+const cta = {
+  style: "button",
+  type: "local",
+  buttons: [
+    {
+      type: "local",
+      copy: "Contact sales",
+      href: "https://example.com/",
+    },
+  ],
+};
+
+const App = () => (
+  <div className="bx--grid">
+    <div className="bx--row">
+      <div className="bx--col-lg-8">
+        <LeadSpaceBlock
+          title={title}
+          copy={copy}
+          heading={heading}
+          mediaType="image"
+          mediaData={mediaData}
+          items={items}
+          cta={cta}
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDom.render(<App />, document.getElementById("app"));
