3e5e5f6dabeef148c5a48bd784794d6b5b16f208
kennylam <kennylam@protonmail.com>
2020-07-10 04:02:50 -0400

    chore(CardSectionSimple): separate story variations (#3098)

--- packages/react/src/patterns/sections/CardSectionSimple/__stories__/CardSectionSimple.stories.js
+++ packages/react/src/patterns/sections/CardSectionSimple/__stories__/CardSectionSimple.stories.js
@@ -5,46 +5,57 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { boolean, object, text } from '@storybook/addon-knobs';
 import cards from '../../../../components/CardGroup/__stories__/data/cards.json';
 import CardSectionSimple from '../CardSectionSimple';
 import React from 'react';
 import readme from '../README.stories.mdx';
+import { text } from '@storybook/addon-knobs';
+
+/**
+ * @param {object} options The options.
+ * @param {string} options.groupId The knob group ID.
+ * @returns {object} The knobs data.
+ */
+const getBaseKnobs = ({ groupId }) => {
+  return {
+    heading: text(
+      'Heading (heading):',
+      'Aliquam condimentum interdum',
+      groupId
+    ),
+    cards: cards.Simple,
+  };
+};
 
 export default {
   title: 'Patterns (Sections)|CardSectionSimple',
 
   parameters: {
     ...readme.parameters,
+  },
+};
+
+export const Default = ({ parameters }) => {
+  const { heading, cards } = parameters?.props?.CardSectionSimple ?? {};
+  const theme = document.documentElement.getAttribute('storybook-carbon-theme');
+  return <CardSectionSimple heading={heading} theme={theme} cards={cards} />;
+};
+
+Default.story = {
+  parameters: {
     knobs: {
       CardSectionSimple: ({ groupId }) => {
+        const knobs = getBaseKnobs({ groupId });
+
         return {
-          heading: text(
-            'Heading (required):',
-            'Aliquam condimentum interdum',
-            groupId
-          ),
-          cards: object('Data', cards.Simple, groupId),
-          cta: boolean('cta', true, groupId) && {
-            heading: 'Top level card link',
-            cta: {
-              href: 'https://www.example.com',
-            },
-          },
+          ...knobs,
         };
-      },
-    },
-    propsSet: {
-      default: {
-        CardSectionSimple: {
-          cards: cards.Simple,
-        },
       },
     },
   },
 };
 
-export const Default = ({ parameters }) => {
+export const WithCTA = ({ parameters }) => {
   const { heading, cards, cta } = parameters?.props?.CardSectionSimple ?? {};
   const theme =
     document.documentElement.getAttribute('storybook-carbon-theme') || 'white';
@@ -56,4 +67,36 @@ export const Default = ({ parameters }) => {
       cta={cta}
     />
   );
+};
+
+WithCTA.story = {
+  parameters: {
+    knobs: {
+      CardSectionSimple: ({ groupId }) => {
+        const knobs = getBaseKnobs({ groupId });
+
+        return {
+          ...knobs,
+          cta: {
+            heading: 'Top level card link',
+            cta: {
+              href: 'https://www.example.com',
+            },
+          },
+        };
+      },
+    },
+    propsSet: {
+      default: {
+        CardSectionSimple: {
+          cta: {
+            heading: 'Top level card link',
+            cta: {
+              href: 'https://www.example.com',
+            },
+          },
+        },
+      },
+    },
+  },
 };
