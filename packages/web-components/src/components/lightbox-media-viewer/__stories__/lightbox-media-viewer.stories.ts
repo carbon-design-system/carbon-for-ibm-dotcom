fd497a7add77ba9b93c5335b33bf5fe4803215eb
Akira Sudoh <asudoh@gmail.com>
2020-07-14 22:07:11 +0900

    feat(cta): introduce text CTA for Web Components (#3128)
    Introduces Web Components version of text CTA.
    
    The most notable difference from its React counterpart is how CTA links
    are handled, notably with the video CTA type. `CTAMixin` automatically
    sets `href` to `#` for video CTA type and fires `dds-cta-run-action`
    custom event when the link is clicked with the original `href` value,
    which is the video ID, in `event.detail`.
    
    This allows a separate component like `<dds-cta-container>` to create a
    media viewer dialog, instead of having to put in all media viewer logic
    into text CTA UI as a dependency.

---
+++ packages/web-components/src/components/lightbox-media-viewer/__stories__/lightbox-media-viewer.stories.ts
@@ -0,0 +1,64 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html } from 'lit-element';
+import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
+import 'carbon-custom-elements/es/components/modal/modal-close-button';
+import textNullable from '../../../../.storybook/knob-text-nullable';
+import '../../modal/modal';
+import '../lightbox-media-viewer-body';
+import '../lightbox-video-player-container';
+import styles from './lightbox-media-viewer.stories.scss';
+import readme from './README.stories.mdx';
+
+export const EmbeddedVideoPlayer = ({ parameters }) => {
+  const { open, disableClose, onBeforeClose, onClose } = parameters?.props?.['dds-modal'] ?? {};
+  const { hideCaption, videoId } = parameters?.props?.['dds-lightbox-video-player-container'] ?? {};
+  const handleBeforeClose = (event: CustomEvent) => {
+    onBeforeClose?.(event);
+    if (disableClose) {
+      event.preventDefault();
+    }
+  };
+  return html`
+    <style>
+      ${styles}
+    </style>
+    <dds-lightbox-video-player-container ?hide-caption="${hideCaption}" video-id="${videoId}">
+      <dds-modal ?open="${open}" size="full-width" @dds-modal-beingclosed="${handleBeforeClose}" @dds-modal-closed="${onClose}">
+        <bx-modal-close-button></bx-modal-close-button>
+        <dds-lightbox-media-viewer-body></dds-lightbox-media-viewer-body>
+      </dds-modal>
+    </dds-lightbox-video-player-container>
+  `;
+};
+
+export default {
+  title: 'Components/Lightbox media viewer',
+  parameters: {
+    ...readme.parameters,
+    knobs: {
+      'dds-modal': ({ groupId }) => ({
+        open: boolean('Open (open)', true, groupId),
+        disableClose: boolean(
+          'Disable user-initiated close action (Call event.preventDefault() in dds-modal-beingclosed event)',
+          false,
+          groupId
+        ),
+        onBeforeClose: action('dds-modal-beingclosed'),
+        onClose: action('dds-modal-closed'),
+      }),
+      'dds-lightbox-video-player-container': ({ groupId }) => ({
+        hideCaption: boolean('hide caption (hide-caption)', false, groupId),
+        videoId: textNullable('Video ID (video-id)', '0_uka1msg4', groupId),
+      }),
+    },
+  },
+};
---
+++ packages/web-components/src/components/video-player/__tests__/video-player.test.ts
@@ -0,0 +1,87 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html, render } from 'lit-html';
+import ifNonNull from 'carbon-custom-elements/es/globals/directives/if-non-null';
+import '../video-player';
+
+const template = (props?) => {
+  const { duration, formatCaption, hideCaption, name } = props ?? {};
+  return html`
+    <dds-video-player
+      duration="${ifNonNull(duration)}"
+      ?hide-caption="${hideCaption}"
+      name="${ifNonNull(name)}"
+      .formatCaption="${ifNonNull(formatCaption)}"
+    >
+    </dds-video-player>
+  `;
+};
+
+describe('dds-video-player', function() {
+  it('should render with minimum attributes', async function() {
+    render(template(), document.body);
+    await Promise.resolve();
+    expect(document.querySelector('dds-video-player')).toMatchSnapshot({ mode: 'shadow' });
+  });
+
+  it('should render with various attributes', async function() {
+    render(
+      template({
+        duration: 30000,
+        name: 'video-name-foo',
+      }),
+      document.body
+    );
+    await Promise.resolve();
+    expect(document.querySelector('dds-video-player')).toMatchSnapshot({ mode: 'shadow' });
+  });
+
+  it('should support hiding the caption', async function() {
+    render(
+      template({
+        hideCaption: true,
+      }),
+      document.body
+    );
+    await Promise.resolve();
+    expect(document.querySelector('dds-video-player')!.shadowRoot!.querySelector('.bx--video-player__video-caption')).toBeNull();
+  });
+
+  it('should support zero-fill in formatting caption', async function() {
+    render(
+      template({
+        duration: 65000,
+        name: 'video-name-foo',
+      }),
+      document.body
+    );
+    await Promise.resolve();
+    expect(document.querySelector('dds-video-player')!.getAttribute('aria-label')).toBe('video-name-foo (1:05)');
+  });
+
+  it('should support custom duration formatter for localization', async function() {
+    render(
+      template({
+        duration: 60000,
+        name: 'video-name-foo',
+        formatCaption({ duration, name }: { duration?: number; name: string }) {
+          return `${name}-${duration}`;
+        },
+      }),
+      document.body
+    );
+    await Promise.resolve();
+    expect(document.querySelector('dds-video-player')!.getAttribute('aria-label')).toBe('video-name-foo-60000');
+  });
+
+  afterEach(function() {
+    render(undefined!, document.body);
+  });
+});
