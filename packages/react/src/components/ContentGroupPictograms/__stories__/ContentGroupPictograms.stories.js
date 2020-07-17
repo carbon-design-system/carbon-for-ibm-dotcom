5fd1ce7858722e086f4a308dd4ea4ab549dfa9c6
kennylam <kennylam@protonmail.com>
2020-07-17 11:58:54 -0400

    chore(packages): merge blocks/sections into components (#3179)
    ### Related Ticket(s)
    
    #3132 
    
    ### Description
    
    This merges `Block` and `Section` patterns into `Components`.
    
    ### Changelog
    
    **Changed**
    
    - `block` and `section` patterns merged into `components`
    
    <!-- Deploy Previews are enabled by applying the following labels for the corresponding package: -->
    <!-- *** "package: react": React -->
    <!-- *** "package: web components": Web Components -->
    <!-- *** "package: vanilla": Vanilla -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive, React (Expressive) -->
    <!-- *** "RTL": React (RTL) -->
    <!-- *** "feature flag": React (experimental) -->

---
+++ packages/react/src/components/ContentGroupPictograms/__stories__/ContentGroupPictograms.stories.js
@@ -0,0 +1,153 @@
+/**
+ * Copyright IBM Corp. 2016, 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Desktop, Pattern, Touch } from '@carbon/pictograms-react';
+import { select, text, boolean, number } from '@storybook/addon-knobs';
+import classNames from 'classnames';
+import ContentGroupPictograms from '../ContentGroupPictograms';
+import React from 'react';
+import readme from '../README.stories.mdx';
+
+const pictograms = {
+  Desktop: 'Desktop',
+  Touch: 'Touch',
+  Pattern: 'Pattern',
+};
+
+/**
+ * Returns the react component based on the value in the pictogram variables
+ *
+ * @param {string} sel string that defines the returning pictogram
+ * @returns {*} JSX pictogram component
+ */
+const selectPictogram = sel => {
+  switch (sel) {
+    case 'Desktop':
+      return Desktop;
+    case 'Pattern':
+      return Pattern;
+    case 'Touch':
+      return Touch;
+  }
+};
+
+/**
+ * Toggles CTA data based on item value
+ *
+ * @param {boolean} item defines if cta will be rendered
+ * @returns {*} if true returns cta data, if false, returns null
+ */
+const toggleCTA = item => {
+  if (item) {
+    return {
+      type: 'local',
+      href: 'https://www.example.com',
+      copy: 'Lorem ipsum dolor',
+    };
+  } else {
+    return null;
+  }
+};
+
+export default {
+  title: 'Components|ContentGroupPictograms',
+
+  parameters: {
+    ...readme.parameters,
+    knobs: {
+      ContentGroupPictograms: ({ groupId }) => {
+        const pictogramCount = number(
+          'Number of PictogramItems',
+          3,
+          {},
+          groupId
+        );
+        const items = [];
+
+        for (let i = 0; i < pictogramCount; i++) {
+          items.push({
+            heading: text(
+              `Item ${i + 1} Heading (items.heading)`,
+              'Aliquam condimentum interdum',
+              groupId
+            ),
+            copy: text(
+              `Item ${i + 1} Copy (items.copy)`,
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est. Mauris iaculis eget dolor nec hendrerit. Phasellus at elit sollicitudin, sodales nulla quis, consequat libero.',
+              groupId
+            ),
+            cta: toggleCTA(
+              boolean(`Item ${i + 1} CTA (items.cta)`, false, groupId)
+            ),
+            pictogram: {
+              src: selectPictogram(
+                select(
+                  `Item ${i + 1} Pictogram (pictogram)`,
+                  pictograms,
+                  pictograms.Desktop,
+                  groupId
+                )
+              ),
+              'aria-label': 'Pictogram',
+            },
+          });
+        }
+        return {
+          heading: text(
+            'Pattern title (heading)',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            groupId
+          ),
+          copy: text(
+            'Copy (copy)',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            groupId
+          ),
+          items,
+        };
+      },
+    },
+    propsSet: {
+      default: {
+        ContentGroupPictograms: {
+          items: [
+            {
+              heading: 'Aliquam condimentum interdum',
+              copy:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est. Mauris iaculis eget dolor nec hendrerit. Phasellus at elit sollicitudin, sodales nulla quis, consequat libero.',
+              pictogram: {
+                src: selectPictogram(pictograms.Desktop),
+                'aria-label': 'Pictogram',
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+};
+
+export const Default = ({ parameters }) => {
+  const { heading, copy, items } =
+    parameters?.props?.ContentGroupPictograms ?? {};
+  return (
+    <div className="bx--grid">
+      <div className="bx--row">
+        <ContentGroupPictograms
+          className={classNames(
+            `bx--col-sm-4`,
+            `bx--col-lg-8`,
+            `bx--offset-lg-4`
+          )}
+          heading={heading}
+          copy={copy}
+          items={items}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ packages/styles/scss/components/logo-grid/README.md
@@ -0,0 +1,26 @@
+### SCSS
+
+#### Usage
+
+Import the package css into the top of your main CSS file.
+
+```css
+@import '@carbon/ibmdotcom-styles/scss/patterns/blocks/logo-grid/index';
+```
+
+#### Elements
+
+| Class                       | Description                                                                     |
+| --------------------------- | ------------------------------------------------------------------------------- |
+| `.bx--logo-grid__container` | Element containing all logo grid rows                                           |
+| `.bx--logo-grid__wrapper`   | Wrapper for Logo placeholders                                                   |
+| `.bx--logo-grid__row`       | Horizontal divisions across the viewport                                        |
+| `.bx--logo-grid__col`       | Vertical divisions across the viewport and change in width at different screen. |
+| `.bx--logo-grid__link`      | Url of the Logo                                                                 |
+| `.bx--logo-grid__logo`      | Placeholder for logo grid                                                       |
+
+#### Blocks
+
+| Class            | Description                                         |
+| ---------------- | --------------------------------------------------- |
+| `.bx--logo-grid` | Highest level class name for the logo grid pattern. |
