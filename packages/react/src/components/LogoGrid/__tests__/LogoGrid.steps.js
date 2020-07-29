b97771b451f67ea3dbe65a92c89ed3866fa3a472
Akira Sudoh <asudoh@gmail.com>
2020-07-29 11:37:00 +0900

    fix(components): revert type size back to one of v1.8.0 (#3293)
    This change reverts type sizes of several components back to one of
    `v1.8.0`.
    
    The (presumably not intentional) change happened due to global
    application of expressive type tokens.
    
    This change adds UI integration tests to React to test the type size,
    using `v1.8.0` style as the baseline.
    
    Refs #3287.

---
+++ packages/react/src/components/LogoGrid/__tests__/LogoGrid.steps.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+describe('LogoGrid', () => {
+  beforeAll(async () => {
+    await page.goto(
+      `http://localhost:${process.env.PORT}/iframe.html?id=components-logogrid--default`
+    );
+    const copy = await page.waitForSelector('.bx--card__copy');
+    await page.evaluate(copy => {
+      copy.insertAdjacentHTML(
+        'beforeBegin',
+        '<div class="bx--card__heading">Heading</div>'
+      );
+    }, copy);
+  });
+
+  it('should have the right type for the heading', async () => {
+    const heading = await page.waitForSelector('.bx--card__heading');
+    const styleValues = await page.evaluate(
+      heading =>
+        window.getStyleValues(heading, [
+          'font-size',
+          'font-weight',
+          'line-height',
+          'letter-spacing',
+        ]),
+      heading
+    );
+    expect(styleValues).toEqual({
+      'font-size': '14px',
+      'font-weight': '600',
+      'line-height': '18px',
+      'letter-spacing': '0.16px',
+    });
+  });
+});
