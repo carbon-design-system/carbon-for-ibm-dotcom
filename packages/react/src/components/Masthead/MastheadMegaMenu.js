6227274187d01c9886616004ebbe44bce43bc9ef
Anna Wen <>
2020-07-30 18:28:35 -0400

    feat(MegaMenu): view all cta styled in megamenu layout

--- packages/react/src/components/Masthead/MastheadMegaMenu.js
+++ packages/react/src/components/Masthead/MastheadMegaMenu.js
@@ -8,7 +8,6 @@
 import ArrowRight16 from '@carbon/icons-react/es/arrow--right/16';
 import classnames from 'classnames';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
-import HorizontalRule from '../HorizontalRule/HorizontalRule';
 import LinkWithIcon from '../LinkWithIcon/LinkWithIcon';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -25,8 +24,6 @@ const MastheadMegaMenu = ({ data }) => {
   let viewAllLink;
   let menuItems = [];
 
-  console.log('rendering megamenu', data);
-
   data.menuSections[0].menuItems.forEach(item => {
     if (item.highlighted) return highlightedItems.push(item);
     if (item.megaPanelViewAll) return (viewAllLink = item);
@@ -37,32 +34,37 @@ const MastheadMegaMenu = ({ data }) => {
 
   return (
     <>
-      <div className={`${prefix}--masthead__megamenu__overlay`}></div>
       <section
-        className={classnames(`${prefix}--masthead__megamenu__container`, {
+        className={classnames(`${prefix}--masthead__megamenu`, {
           [`${prefix}--masthead__megamenu__container--hasHighlights`]: hasHighlights,
         })}
         data-autoid={`${stablePrefix}--masthead__megamenu`}>
-        {hasHighlights && (
-          <div className={`${prefix}--masthead__megamenu__highlight-section`}>
-            {_renderMenuCategory(highlightedItems)}
-          </div>
-        )}
-        <div className={`${prefix}--masthead__megamenu__categories-section`}>
-          <div className={`${prefix}--masthead__megamenu__categories`}>
-            {_renderMenuCategory(menuItems)}
-          </div>
-          {viewAllLink && (
-            <div className={`${prefix}--masthead__megamenu__view-all-cta`}>
-              <HorizontalRule contrasts="low-contrast" />
-              <LinkWithIcon href={viewAllLink.url}>
-                <span>{viewAllLink.title}</span>
-                <ArrowRight16 />
-              </LinkWithIcon>
+        <div className={`${prefix}--masthead__megamenu__container`}>
+          <div className={`${prefix}--masthead__megamenu__container--row`}>
+            {hasHighlights && (
+              <div
+                className={`${prefix}--masthead__megamenu__highlight-section`}>
+                {_renderMenuCategory(highlightedItems)}
+              </div>
+            )}
+            <div
+              className={`${prefix}--masthead__megamenu__categories-section`}>
+              <div className={`${prefix}--masthead__megamenu__categories`}>
+                {_renderMenuCategory(menuItems)}
+              </div>
+              {viewAllLink && (
+                <LinkWithIcon
+                  className={`${prefix}--masthead__megamenu__view-all-cta`}
+                  href={viewAllLink.url}>
+                  <span>{viewAllLink.title}</span>
+                  <ArrowRight16 />
+                </LinkWithIcon>
+              )}
             </div>
-          )}
+          </div>
         </div>
       </section>
+      <div className={`${prefix}--masthead__megamenu__overlay`}></div>
     </>
   );
 };
