a3cd5d2b1a4e48823b3b897d087fc70e61057a42
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2020-08-04 17:27:21 -0300

    feat(VideoPlayer): adding story for 1x1 aspect ratio

--- packages/react/src/components/VideoPlayer/__stories__/VideoPlayer.stories.js
+++ packages/react/src/components/VideoPlayer/__stories__/VideoPlayer.stories.js
@@ -29,20 +29,22 @@ export default {
           ratios[0],
           groupId
         ),
+        videoId: '0_uka1msg4',
       }),
     },
   },
 };
 
 export const Default = ({ parameters }) => {
-  const { showCaption, aspectRatio } = parameters?.props?.VideoPlayer ?? {};
+  const { showCaption, aspectRatio, videoId } =
+    parameters?.props?.VideoPlayer ?? {};
 
   return (
     <div className="bx--grid">
       <div className="bx--row">
         <div className="bx--col-sm-4 bx--col-lg-8 bx--offset-lg-4">
           <VideoPlayer
-            videoId="0_uka1msg4"
+            videoId={videoId}
             showCaption={showCaption}
             aspectRatio={aspectRatio}
           />
@@ -50,4 +52,24 @@ export const Default = ({ parameters }) => {
       </div>
     </div>
   );
+};
+
+export const aspectRatio1x1 = ({ parameters }) => {
+  console.log(parameters.props.VideoPlayer);
+  return <Default parameters={parameters} />;
+};
+
+aspectRatio1x1.story = {
+  name: 'Aspect ratio 1x1',
+  parameters: {
+    knobs: {
+      VideoPlayer: () => {
+        return {
+          showCaption: true,
+          aspectRatio: '1x1',
+          videoId: '1_9h94wo6b',
+        };
+      },
+    },
+  },
 };
