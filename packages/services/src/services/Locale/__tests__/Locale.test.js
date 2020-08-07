9d6ed0d7ccf36523b7b91f135665812689db3f13
Chris Wilson <ccwilson@us.ibm.com>
2020-08-07 09:40:34 -0500

    chore(test): improve locale test coverage

--- packages/services/src/services/Locale/__tests__/Locale.test.js
+++ packages/services/src/services/Locale/__tests__/Locale.test.js
@@ -5,40 +5,43 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { geolocation, ipcinfoCookie } from '@carbon/ibmdotcom-utilities';
-import digitalDataResponse from '../../DDO/__tests__/data/response.json';
-import LocaleAPI from '../Locale';
-import mockAxios from 'axios';
-import response from './data/response.json';
-import root from 'window-or-global';
-
-const mockDigitalDataResponse = digitalDataResponse;
-
-jest.mock(
-  '@carbon/ibmdotcom-utilities/lib/utilities/ipcinfoCookie/ipcinfoCookie',
-  () => ({
-    get: jest.fn(() => Promise.resolve({ cc: 'us', lc: 'en' })),
-    set: jest.fn(() => Promise.resolve({})),
-  })
-);
-
-jest.mock(
-  '@carbon/ibmdotcom-utilities/lib/utilities/geolocation/geolocation',
-  () => jest.fn(() => Promise.resolve('us'))
-);
-
 describe('LocaleAPI', () => {
-  const _cc = 'us';
-  const _lc = 'en';
-
-  const endpoint = `${process.env.REACT_APP_CORS_PROXY}${process.env.TRANSLATION_HOST}/common/js/dynamicnav/www/countrylist/jsononly`;
-  const fetchUrl = `${endpoint}/${_cc}${_lc}-utf8.json`;
-
   beforeEach(function() {
-    mockAxios.get.mockImplementation(() =>
-      Promise.resolve({
-        data: response,
-      })
+    jest.resetModules();
+
+    this.testDDO = {
+      page: {
+        pageInfo: {
+          ibm: {},
+          language: 'de-DE',
+        },
+      },
+    };
+
+    this.testAxios = 'testData';
+
+    this.mockAxios = {
+      get: jest.fn(() => Promise.resolve({ data: this.testAxios })),
+    };
+    jest.doMock('axios', () => this.mockAxios);
+
+    this.testCookie = {
+      cc: 'us',
+      lc: 'en',
+    };
+    this.mockCookieInfo = {
+      get: jest.fn(() => this.testCookie),
+      set: jest.fn(),
+    };
+    jest.doMock(
+      '@carbon/ibmdotcom-utilities/lib/utilities/ipcinfoCookie/ipcinfoCookie',
+      () => this.mockCookieInfo
+    );
+
+    this.mockGeoLoc = jest.fn(() => Promise.resolve('us'));
+    jest.doMock(
+      '@carbon/ibmdotcom-utilities/lib/utilities/geolocation/geolocation',
+      () => this.mockGeoLoc
     );
 
     root.digitalData = mockDigitalDataResponse;
@@ -46,85 +49,412 @@ describe('LocaleAPI', () => {
     LocaleAPI.clearCache();
   });
 
-  afterEach(() => {
+  afterEach(function() {
     jest.clearAllMocks();
     sessionStorage.clear();
   });
 
-  it('should fetch the lang from the html attribute', async () => {
-    Object.defineProperty(window.document.documentElement, 'lang', {
-      value: 'fr-ca',
-      configurable: true,
+  describe('getLang', function() {
+    it('should fetch the lang from the html attribute', async function() {
+      Object.defineProperty(window.document.documentElement, 'lang', {
+        value: 'fr-ca',
+        configurable: true,
+      });
+
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'ca',
+        lc: 'fr',
+      });
     });
 
-    const lang = await LocaleAPI.getLang();
+    it('should default to en-us from the html attribute if cc and lc are not defined', async function() {
+      Object.defineProperty(window.document.documentElement, 'lang', {
+        value: 'it',
+        configurable: true,
+      });
 
-    expect(lang).toEqual({
-      cc: 'ca',
-      lc: 'fr',
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should default to en-us if lang is not defined', async function() {
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should default when ddo is undefined', async function() {
+      this.testDDO = undefined;
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should default when no ddo.page', async function() {
+      this.testDDO.page = false;
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should default when no ddo.page.pageInfo', async function() {
+      this.testDDO.page.pageInfo = false;
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should default when no ddo.page.pageInfo.ibm', async function() {
+      this.testDDO.page.pageInfo.ibm = false;
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should default when no ddo.page.pageInfo.ibm.country', async function() {
+      this.testDDO.page.pageInfo.ibm.country = false;
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should default when no ddo.page.pageInfo.language', async function() {
+      this.testDDO.page.pageInfo.language = false;
+      this.testDDO.page.pageInfo.ibm.country = 'de';
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should get from DDO', async function() {
+      this.testDDO.page.pageInfo.language = 'de-DE';
+      this.testDDO.page.pageInfo.ibm.country = 'de';
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'de',
+        lc: 'de',
+      });
+    });
+
+    it('should handle multiple countries', async function() {
+      this.testDDO.page.pageInfo.language = 'de-DE';
+      this.testDDO.page.pageInfo.ibm.country = 'de,ca';
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'de',
+        lc: 'de',
+      });
+    });
+
+    it('should handle map gb to uk', async function() {
+      this.testDDO.page.pageInfo.language = 'de-DE';
+      this.testDDO.page.pageInfo.ibm.country = 'gb';
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'uk',
+        lc: 'de',
+      });
+    });
+
+    it('should handle map zz to us', async function() {
+      this.testDDO.page.pageInfo.language = 'de-DE';
+      this.testDDO.page.pageInfo.ibm.country = 'zz';
+      const lang = await this.LocaleAPI.getLang();
+
+      expect(lang).toEqual({
+        cc: 'us',
+        lc: 'de',
+      });
     });
   });
 
-  it('should default to en-us from the html attribute if cc and lc are not defined', async () => {
-    Object.defineProperty(window.document.documentElement, 'lang', {
-      value: 'it',
-      configurable: true,
+  describe('getList', function() {
+    it('should get countries', async function() {
+      const countries = await this.LocaleAPI.getList({
+        cc: 'testCC',
+        lc: 'testLC',
+      });
+
+      expect(this.mockAxios.get).toHaveBeenCalledTimes(1);
+      expect(
+        this.mockAxios.get
+      ).toHaveBeenCalledWith(
+        'https://cra-myproxy.com/https://ibm.com/common/js/dynamicnav/www/countrylist/jsononly/testCCtestLC-utf8.json',
+        { headers: { 'Content-Type': 'application/json; charset=utf-8' } }
+      );
+
+      expect(countries).toEqual(this.testAxios);
     });
 
-    const lang = await LocaleAPI.getLang();
+    it('should get countries from session cache', async function() {
+      const countries1 = await this.LocaleAPI.getList({
+        cc: 'testCC',
+        lc: 'testLC',
+      });
+      const countries2 = await this.LocaleAPI.getList({
+        cc: 'testCC',
+        lc: 'testLC',
+      });
 
-    expect(lang).toEqual({
-      cc: 'us',
-      lc: 'en',
+      expect(this.mockAxios.get).toHaveBeenCalledTimes(1);
+      expect(countries1).toEqual(this.testAxios);
+      expect(countries2).toEqual(this.testAxios);
+    });
+
+    it('should exhaust retries', function(done) {
+      this.mockAxios.get.mockImplementation(() => new Promise(() => {}));
+
+      this.LocaleAPI.getList({ cc: 'testCC', lc: 'testLC' });
+      const listPromise = this.LocaleAPI.getList({
+        cc: 'testCC',
+        lc: 'testLC',
+      });
+
+      jest.advanceTimersByTime(100 * 51);
+
+      return listPromise.then(done.fail).catch(() => {
+        done();
+      });
+    }, 6000);
+
+    it('should get default on inital reject', async function() {
+      this.mockAxios.get.mockReturnValueOnce(
+        Promise.reject(),
+        Promise.resolve({ data: this.testAxios })
+      );
+
+      const countries = await this.LocaleAPI.getList({
+        cc: 'testCC',
+        lc: 'testLC',
+      });
+      expect(this.mockAxios.get).toHaveBeenCalledTimes(2);
+      expect(
+        this.mockAxios.get
+      ).toHaveBeenCalledWith(
+        'https://cra-myproxy.com/https://ibm.com/common/js/dynamicnav/www/countrylist/jsononly/testCCtestLC-utf8.json',
+        { headers: { 'Content-Type': 'application/json; charset=utf-8' } }
+      );
+      expect(
+        this.mockAxios.get
+      ).toHaveBeenCalledWith(
+        'https://cra-myproxy.com/https://ibm.com/common/js/dynamicnav/www/countrylist/jsononly/usen-utf8.json',
+        { headers: { 'Content-Type': 'application/json; charset=utf-8' } }
+      );
+
+      expect(countries).toEqual(this.testAxios);
+    });
+
+    it('should reject', function(done) {
+      this.mockAxios.get.mockImplementation(() => Promise.reject());
+
+      this.LocaleAPI.getList({ cc: 'us', lc: 'en' })
+        .then(done.fail)
+        .catch(done);
     });
   });
 
-  it('should default to en-us if lang is not defined', async () => {
-    const lang = await LocaleAPI.getLang();
+  describe('verifyLocale', function() {
+    beforeEach(function() {
+      this.testList = {
+        regionList: [
+          {
+            countryList: [
+              {
+                locale: [['en-us']],
+              },
+              {
+                locale: [['de-de']],
+              },
+            ],
+          },
+        ],
+      };
+    });
 
-    expect(lang).toEqual({
-      cc: 'us',
-      lc: 'en',
+    it('should get locale', function() {
+      const locale = this.LocaleAPI.verifyLocale('us', 'en', this.testList);
+
+      expect(locale).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should get priority locale', function() {
+      const locale = this.LocaleAPI.verifyLocale('us', 'fr', this.testList);
+
+      expect(locale).toEqual({
+        cc: 'us',
+        lc: 'en',
+      });
+    });
+
+    it('should get undefined', function() {
+      const locale = this.LocaleAPI.verifyLocale('us', 'en', false);
+
+      expect(locale).toBeUndefined();
     });
   });
 
-  it('should fetch the display name based on language/locale combination', async () => {
-    const data = await LocaleAPI.getLangDisplay();
-    expect(data).toEqual('United States — English');
-  });
+  describe('getLangDisplay', function() {
+    beforeEach(function() {
+      this.testLang = {
+        cc: 'testCC',
+        lc: 'testLC',
+      };
 
-  it('should fetch locale from cookie if availiable', async () => {
-    Object.defineProperty(window.document, 'cookie', {
-      writable: true,
-      value: 'ipcInfo=cc%253Dus%253Blc%253Den',
+      this.testList = {
+        regionList: [
+          {
+            countryList: [
+              {
+                name: 'testName',
+                locale: [
+                  ['testLC-testCC', 'testDisplay'],
+                  ['altLocale', 'altDisplay'],
+                ],
+              },
+              {
+                locale: [['testLC2-testCC2', 'testDisplay2']],
+              },
+            ],
+          },
+        ],
+      };
+
+      this.getLangSpy = jest.spyOn(this.LocaleAPI, 'getLang');
+      this.getLangSpy.mockReturnValue(Promise.resolve(this.testLang));
+
+      this.getListSpy = jest.spyOn(this.LocaleAPI, 'getList');
+      this.getListSpy.mockReturnValue(Promise.resolve(this.testList));
     });
 
-    const locale = await LocaleAPI.getLocale();
+    it('should get display', async function() {
+      const display = await this.LocaleAPI.getLangDisplay({
+        cc: 'testCC',
+        lc: 'testLC',
+      });
 
-    expect(locale).toEqual({ cc: 'us', lc: 'en' });
+      expect(display).toEqual('testName — testDisplay');
+    });
+
+    it('should get lang for display', async function() {
+      const display = await this.LocaleAPI.getLangDisplay(false);
+
+      expect(display).toEqual('testName — testDisplay');
+    });
+
+    it('should get default display', async function() {
+      const display = await this.LocaleAPI.getLangDisplay({
+        cc: 'missingCC',
+        lc: 'missingLC',
+      });
+
+      expect(display).toEqual('United States — English');
+    });
   });
 
-  it('should verify the locale and return existing combo', () => {
-    const locale = LocaleAPI.verifyLocale('us', 'es', response);
+  describe('getLocale', function() {
+    beforeEach(function() {
+      this.getLangSpy = jest.spyOn(this.LocaleAPI, 'getLang');
+      this.getLangSpy.mockReturnValue(Promise.resolve('testLang'));
 
-    expect(locale).toEqual({ cc: 'us', lc: 'en' });
-  });
+      this.getListSpy = jest.spyOn(this.LocaleAPI, 'getList');
+      this.getListSpy.mockReturnValue(Promise.resolve('testList'));
 
-  xit('should set the ipcInfo cookie once combo has been verified', async () => {
-    await LocaleAPI.getLocale();
+      this.verifyLocaleSpy = jest.spyOn(this.LocaleAPI, 'verifyLocale');
+      this.verifyLocaleSpy.mockReturnValue('testVerified');
+    });
 
-    expect(ipcinfoCookie.set).toHaveBeenCalledTimes(1);
-    expect(geolocation).toHaveBeenCalledTimes(1);
-  });
+    it('should use getLang', async function() {
+      const locale = await this.LocaleAPI.getLocale();
 
-  it('should make the call for the country list', async () => {
-    const data = await LocaleAPI.getList({ cc: _cc, lc: _lc });
+      expect(locale).toEqual('testLang');
+    });
 
-    expect(data).toEqual(response);
-    expect(mockAxios.get).toHaveBeenCalledWith(fetchUrl, {
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
+    it('should use cookies', async function() {
+      this.getLangSpy.mockReturnValue(false);
+
+      const locale = await this.LocaleAPI.getLocale();
+
+      expect(locale).toEqual(this.testCookie);
+    });
+
+    it('should use geolocation on missing cookie', async function() {
+      this.getLangSpy.mockReturnValue(Promise.resolve(false));
+      this.testCookie = false;
+
+      await this.LocaleAPI.getLocale();
+
+      expect(this.mockGeoLoc).toHaveBeenCalledTimes(1);
+    });
+
+    it('should use geolocation on missing cookie lc', async function() {
+      this.getLangSpy.mockReturnValue(Promise.resolve(false));
+      this.testCookie = { cc: 'testCC' };
+
+      await this.LocaleAPI.getLocale();
+
+      expect(this.mockGeoLoc).toHaveBeenCalledTimes(1);
+    });
+
+    it('should use geolocation on missing cookie cc', async function() {
+      this.getLangSpy.mockReturnValue(Promise.resolve(false));
+      this.testCookie = { lc: 'testLC' };
+
+      await this.LocaleAPI.getLocale();
+
+      expect(this.mockGeoLoc).toHaveBeenCalledTimes(1);
+    });
+
+    it('should use geolocation', async function() {
+      this.getLangSpy.mockReturnValue(Promise.resolve(false));
+      this.testCookie = false;
+
+      const locale = await this.LocaleAPI.getLocale();
+
+      expect(locale).toEqual('testVerified');
+      expect(this.mockCookieInfo.set).toHaveBeenCalledTimes(1);
+      expect(this.mockCookieInfo.set).toHaveBeenCalledWith('testVerified');
+    });
+
+    it('should return undefined on no cc', async function() {
+      this.getLangSpy.mockReturnValue(Promise.resolve(false));
+      this.testCookie = false;
+      this.mockGeoLoc.mockReturnValue(Promise.resolve(false));
+
+      const locale = await this.LocaleAPI.getLocale();
+      expect(locale).toBeUndefined();
     });
   });
 
