e1472d825478c1d7cf550f954b0a17730e4cb04c
Caio Bozato Aroca <bozato@ibm.com>
2020-08-07 16:02:55 -0300

    test(CTASection): fix console errors

--- packages/react/src/components/CTASection/__tests__/CTASection.test.js
+++ packages/react/src/components/CTASection/__tests__/CTASection.test.js
@@ -10,33 +10,45 @@ import CTASection from '../CTASection';
 import { mount } from 'enzyme';
 import React from 'react';
 
-const contentItemsProps = [
-  {
-    heading: 'Get connected',
-    copy: `
-      IBM DevOps partners have a wide range of expertise.
-      Find one to build the right solution for you.
-      `,
-    cta: {
-      copy: 'Find a partner',
-      type: 'external',
-      href: 'https://example.com/',
+const contentItemsProps = {
+  heading: 'Content item heading',
+  copy: 'Content item copy',
+  items: [
+    {
+      heading: 'Get connected',
+      copy: `
+        IBM DevOps partners have a wide range of expertise.
+        Find one to build the right solution for you.
+        `,
+      cta: {
+        copy: 'Find a partner',
+        type: 'external',
+        href: 'https://example.com/',
+      },
     },
-  },
-  {
-    heading: 'Learn how',
-    copy: 'Dig into more self-directed learning about DevOps methodologies.',
-    cta: {
-      copy: 'Browse tutorials',
-      type: 'local',
-      href: 'https://example.com/',
+    {
+      heading: 'Learn how',
+      copy: 'Dig into more self-directed learning about DevOps methodologies.',
+      cta: {
+        copy: 'Browse tutorials',
+        type: 'local',
+        href: 'https://example.com/',
+      },
     },
-  },
-];
+  ],
+};
 
 describe('CTA Section', () => {
   it('renders two ContentItems', () => {
-    const wrapper = mount(<CTASection items={contentItemsProps} />);
-    expect(wrapper.find(ContentItem)).toHaveLength(contentItemsProps.length);
+    const wrapper = mount(
+      <CTASection
+        heading={contentItemsProps.heading}
+        copy={contentItemsProps.copy}
+        items={contentItemsProps.items}
+      />
+    );
+    expect(wrapper.find(ContentItem)).toHaveLength(
+      contentItemsProps.items.length
+    );
   });
 });
