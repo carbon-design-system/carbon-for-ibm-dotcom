3bdc14e1b7bce9585f3c8638874b1542bfd7b490
Guilherme L. Moraes <42848561+guilhermelMoraes@users.noreply.github.com>
2020-08-21 09:44:57 -0300

    feat(card-with-pictogram): adding new card variation (#3654)
    ### Description
    
    This card has a pictogram at the bottom left side, as per the visual specs for the [Support Page](https://app.abstract.com/projects/7f83c114-7627-4169-8af7-6a85e7147ea6/branches/master/commits/latest/files/01ee3bd8-c63d-4b3b-bddb-aabaddb5c31d/layers/1FA6F2AC-E027-419F-8CEF-F58B3AE2E495?collectionId=c963cb53-73c2-42a5-af39-f5538e24c454&collectionLayerId=a278337e-62ec-464c-8ba6-b368d4b9ed64).
    
    This variation was approved by Wonil, James, and Jeff and will be under a feature flag. Here's the [link to the Slack conversation](https://cognitive-app.slack.com/archives/G018JGL83QX/p1597692491002100).
    
    <img width="352" alt="Captura de Tela 2020-08-19 às 17 52 10" src="https://user-images.githubusercontent.com/42848561/90688351-b7f28800-e244-11ea-9503-5ce7dacb8fea.png">

--- packages/react/src/components/Card/__stories__/Card.stories.js
+++ packages/react/src/components/Card/__stories__/Card.stories.js
@@ -7,7 +7,9 @@
 
 import { text, boolean, select } from '@storybook/addon-knobs';
 import ArrowRight20 from '@carbon/icons-react/es/arrow--right/20';
+import Bee from '@carbon/pictograms-react/lib/bee';
 import { Card } from '../';
+import { DDS_CARD_WITH_PICTOGRAM } from '../../../internal/FeatureFlags';
 import React from 'react';
 import readme from '../README.stories.mdx';
 
@@ -17,35 +19,49 @@ export default {
   parameters: {
     ...readme.parameters,
     knobs: {
-      Card: ({ groupId }) => ({
-        image:
-          (boolean('image', false, groupId) && {
-            defaultSrc: 'https://dummyimage.com/600x300/ee5396/161616&text=2:1',
-            alt: 'Image alt text',
-          }) ||
-          undefined,
-        eyebrow: text('eyebrow', 'eyebrow text', groupId),
-        heading: text(
-          'title (required)',
-          'Lorem ipsum dolor sit amet',
-          groupId
-        ),
-        copy: text('copy', '', groupId),
-        inverse: boolean('inverse', false, groupId),
-        cta: {
-          href: text('Cta href (cta.href)', 'https://example.com', groupId),
-          copy: text('Cta copy (cta.copy)', 'Card cta text', groupId),
-          icon: {
-            src: ArrowRight20,
-          },
-          iconPlacement: select(
-            'Cta icon placement (cta.iconPlacement)',
-            ['left', 'right'],
-            'right',
+      Card: ({ groupId }) => {
+        let showPictogram = DDS_CARD_WITH_PICTOGRAM
+          ? boolean('Show pictogram (hides CTA)', false, groupId)
+          : null;
+        return {
+          image:
+            (boolean('image', false, groupId) && {
+              defaultSrc:
+                'https://dummyimage.com/600x300/ee5396/161616&text=2:1',
+              alt: 'Image alt text',
+            }) ||
+            undefined,
+          eyebrow: text('eyebrow', 'eyebrow text', groupId),
+          heading: text(
+            'title (required)',
+            'Lorem ipsum dolor sit amet',
             groupId
           ),
-        },
-      }),
+          copy: text('copy', '', groupId),
+          inverse: boolean('inverse', false, groupId),
+          cta:
+            showPictogram && DDS_CARD_WITH_PICTOGRAM
+              ? null
+              : {
+                  href: text(
+                    'Cta href (cta.href)',
+                    'https://example.com',
+                    groupId
+                  ),
+                  copy: text('Cta copy (cta.copy)', 'Card cta text', groupId),
+                  icon: {
+                    src: ArrowRight20,
+                  },
+                  iconPlacement: select(
+                    'Cta icon placement (cta.iconPlacement)',
+                    ['left', 'right'],
+                    'right',
+                    groupId
+                  ),
+                },
+          pictogram: showPictogram && DDS_CARD_WITH_PICTOGRAM ? <Bee /> : null,
+        };
+      },
     },
   },
 };
