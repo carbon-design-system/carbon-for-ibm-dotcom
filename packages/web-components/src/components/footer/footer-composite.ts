519046784141b5beac2b7e988bc8a0d7980d8534
Jeff Chew <jeff.chew@gmail.com>
2020-08-25 18:03:34 -0400

    fix(footer): fix import for dds-footer

--- packages/web-components/src/components/footer/footer-composite.ts
+++ packages/web-components/src/components/footer/footer-composite.ts
@@ -16,7 +16,13 @@ import ModalRenderMixin from '../../globals/mixins/modal-render';
 import { LocaleList } from '../../globals/services-store/types/localeAPI';
 import { BasicLink, BasicLinkSet, Translation } from '../../globals/services-store/types/translateAPI';
 import Handle from '../../globals/internal/handle';
+/* eslint-disable import/no-duplicates */
 import { FOOTER_SIZE } from './footer';
+import { LocaleModalLocaleList } from '../locale-modal/locale-modal-composite';
+// Above import is interface-only ref and thus code won't be brought into the build
+import '../locale-modal/locale-modal-composite';
+import './footer';
+/* eslint-enable import/no-duplicates */
 import './footer-logo';
 import './footer-nav';
 import './footer-nav-group';
@@ -25,11 +31,6 @@ import './locale-button';
 import './legal-nav';
 import './legal-nav-item';
 import './legal-nav-cookie-preferences-placeholder';
-/* eslint-disable import/no-duplicates */
-import { LocaleModalLocaleList } from '../locale-modal/locale-modal-composite';
-// Above import is interface-only ref and thus code won't be brought into the build
-import '../locale-modal/locale-modal-composite';
-/* eslint-enable import/no-duplicates */
 
 const { stablePrefix: ddsPrefix } = ddsSettings;
 
