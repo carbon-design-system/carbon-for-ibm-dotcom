a1419a7386db6955b9886c5db4502ad467ca977c
Ariella Gilmore <ariellalgilmore@gmail.com>
2020-08-26 22:00:34 -0700

    feat(FeatureCard): new web component (#3725)
    ### Related Ticket(s)
    
    #3554
    
    ### Description
    
    Create FeatureCard web component
    
    ### Changelog
    
    **New**
    
    - add component, tests, and story
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: vanilla": Vanilla -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive, React (Expressive) -->
    <!-- *** "RTL": React (RTL) -->
    <!-- *** "feature flag": React (experimental) -->

---
+++ packages/web-components/src/components/feature-card/feature-card.ts
@@ -0,0 +1,42 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { customElement } from 'lit-element';
+import settings from 'carbon-components/es/globals/js/settings';
+import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
+import DDSCard from '../card/card';
+import styles from './feature-card.scss';
+
+const { prefix } = settings;
+const { stablePrefix: ddsPrefix } = ddsSettings;
+
+/**
+ * Feature Card.
+ *
+ * @element dds-feature-card
+ */
+@customElement(`${ddsPrefix}-feature-card`)
+class DDSFeatureCard extends DDSCard {
+  updated(changedProperties) {
+    super.updated(changedProperties);
+    const { _linkNode: linkNode } = this;
+    if (linkNode) {
+      linkNode.classList.add(`${prefix}--card--inverse`);
+      linkNode.classList.add(`${prefix}--feature-card`);
+    }
+  }
+
+  static get stableSelector() {
+    return `${ddsPrefix}--feature-card`;
+  }
+
+  static styles = styles;
+}
+
+export default DDSFeatureCard;
