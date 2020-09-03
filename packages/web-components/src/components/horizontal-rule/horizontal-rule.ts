3209599996a51ba4740b78cb58941ce8b82f0675
Anna Wen <>
2020-09-03 14:39:11 -0400

    feat(horizontal-rule): use css attribute selectors for styles

--- packages/web-components/src/components/horizontal-rule/horizontal-rule.ts
+++ packages/web-components/src/components/horizontal-rule/horizontal-rule.ts
@@ -7,13 +7,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { html, property, customElement, LitElement } from 'lit-element';
-// import ifNonNull from 'carbon-web-components/es/globals/directives/if-non-null';
-import settings from 'carbon-components/es/globals/js/settings';
+import { property, customElement, LitElement } from 'lit-element';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
 import styles from './horizontal-rule.scss';
 
-const { prefix } = settings;
 const { stablePrefix: ddsPrefix } = ddsSettings;
 
 /**
@@ -26,37 +23,32 @@ class DDSHorizontalRule extends LitElement {
   /**
    * Style of the HR (solid (default) | dashed)
    */
-  @property({ attribute: 'type' })
+  @property({ attribute: 'type', reflect: true })
   type = 'solid';
 
   /**
    * Length of the HR
    * (fluid (default) | small | medium | large)
    */
-  @property({ attribute: 'size' })
+  @property({ attribute: 'size', reflect: true })
   size = 'fluid';
 
   /**
    * Contrast of the HR
    * (medium-contrast (default) | low-contrast | high-contrast)
    */
-  @property({ attribute: 'contrast' })
+  @property({ attribute: 'contrast', reflect: true })
   contrast = 'medium-contrast';
 
   /**
    * Weight of the HR
    * (thin (default) | thick)
    */
-  @property({ attribute: 'weight' })
+  @property({ attribute: 'weight', reflect: true })
   weight = 'thin';
 
   render() {
-    const { type, size, contrast, weight } = this;
-    return html`
-      <hr
-        class="${prefix}--hr ${prefix}--hr--${type} ${prefix}--hr--${size} ${prefix}--hr--${contrast} ${prefix}--hr--${weight}"
-      />
-    `;
+    return super.render();
   }
 
   static get stableSelector() {
