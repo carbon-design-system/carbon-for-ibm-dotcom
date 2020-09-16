38a3f3dc7bb52e112e14d6b1a6e9d70a9cad1c4e
Akira Sudoh <asudoh@gmail.com>
2020-09-16 23:21:09 +0900

    chore(devenv): fix indent

--- packages/styles/src/carbon-stories/CodeSnippet/CodeSnippet-story.js
+++ packages/styles/src/carbon-stories/CodeSnippet/CodeSnippet-story.js
@@ -116,37 +116,37 @@ export const MultiLine = () => {
       {multilineProps.light && lightPropMessage}
       <CodeSnippet type="multi" {...multilineProps}>
         {`@mixin grid-container {
-width: 100%;
-padding-right: padding(mobile);
-padding-left: padding(mobile);
+  width: 100%;
+  padding-right: padding(mobile);
+  padding-left: padding(mobile);
 
-@include breakpoint(bp--xs--major) {
-padding-right: padding(xs);
-padding-left: padding(xs);
-}
+  @include breakpoint(bp--xs--major) {
+    padding-right: padding(xs);
+    padding-left: padding(xs);
+  }
 }
 
 $z-indexes: (
-modal : 9000,
-overlay : 8000,
-dropdown : 7000,
-header : 6000,
-footer : 5000,
-hidden : - 1,
-overflowHidden: - 1,
-floating: 10000
+  modal : 9000,
+  overlay : 8000,
+  dropdown : 7000,
+  header : 6000,
+  footer : 5000,
+  hidden : - 1,
+  overflowHidden: - 1,
+  floating: 10000
 );`}
       </CodeSnippet>
       <br />
       <CodeSnippet type="multi" {...multilineProps}>
         {`@mixin grid-container {
-width: 100%;
-padding-right: padding(mobile);
-padding-left: padding(mobile);
+  width: 100%;
+  padding-right: padding(mobile);
+  padding-left: padding(mobile);
 
-@include breakpoint(bp--xs--major) {
-padding-right: padding(xs);
-}
+  @include breakpoint(bp--xs--major) {
+    padding-right: padding(xs);
+  }
 }`}
       </CodeSnippet>
     </div>
