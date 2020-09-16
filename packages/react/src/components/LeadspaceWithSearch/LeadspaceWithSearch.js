655eb1bcd6bab90d1e06309e0522a82d9f3de152
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2020-09-16 17:41:51 -0300

    feat(leadspace-search): layout for sm breakpoint finished

--- packages/react/src/components/LeadspaceWithSearch/LeadspaceWithSearch.js
+++ packages/react/src/components/LeadspaceWithSearch/LeadspaceWithSearch.js
@@ -5,9 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import Button from 'carbon-components-react/lib/components/Button';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
+import FolderAdd from '@carbon/icons-react/lib/folder--add/20';
 import React from 'react';
+import Search from 'carbon-components-react/lib/components/Search';
 import settings from 'carbon-components/es/globals/js/settings';
+import View from '@carbon/icons-react/lib/view/20';
 
 const { stablePrefix } = ddsSettings;
 const { prefix } = settings;
@@ -16,7 +20,34 @@ const LeadspaceWithSearch = () => (
   <section
     data-autoid={`${stablePrefix}--leadspace-with-search`}
     className={`${prefix}--leadspace-with-search`}>
-    leadspace with search
+    <div className={`${prefix}--leadspace-with-search__row`}>
+      <div className={`${prefix}--leadspace-with-search__content`}>
+        <h1 className={`${prefix}--leadspace-with-search__heading`}>
+          Let's troubleshoot
+        </h1>
+        <Search
+          className={`${prefix}--leadspace-with-search__search`}
+          placeHolderText="Search keywords"
+        />
+        <ul className={`${prefix}--leadspace-with-search__button-group`}>
+          <li>
+            <Button
+              kind="tertiary"
+              renderIcon={View}
+              className={`${prefix}--leadspace-with-search__button`}>
+              <span>View your cases</span>
+            </Button>
+          </li>
+          <li>
+            <Button
+              renderIcon={FolderAdd}
+              className={`${prefix}--leadspace-with-search__button`}>
+              <span>Open a case</span>
+            </Button>
+          </li>
+        </ul>
+      </div>
+    </div>
   </section>
 );
 
