274b7bf7337a3218472604e0fbf441ce37dd7b0b
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2020-09-17 12:27:50 -0300

    feat(leadspace-search): adding data using props

--- packages/react/src/components/LeadspaceWithSearch/LeadspaceWithSearch.js
+++ packages/react/src/components/LeadspaceWithSearch/LeadspaceWithSearch.js
@@ -7,51 +7,59 @@
 
 import Button from 'carbon-components-react/lib/components/Button';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
-import FolderAdd from '@carbon/icons-react/lib/folder--add/20';
+import PropTypes from 'prop-types';
 import React from 'react';
 import Search from 'carbon-components-react/lib/components/Search';
 import settings from 'carbon-components/es/globals/js/settings';
-import View from '@carbon/icons-react/lib/view/20';
 
 const { stablePrefix } = ddsSettings;
 const { prefix } = settings;
 
-const LeadspaceWithSearch = () => (
+const LeadspaceWithSearch = ({ heading, searchProps, buttonsProps }) => (
   <section
     data-autoid={`${stablePrefix}--leadspace-with-search`}
     className={`${prefix}--leadspace-with-search`}>
     <div className={`${prefix}--leadspace-with-search__row`}>
       <div className={`${prefix}--leadspace-with-search__content`}>
         <h1 className={`${prefix}--leadspace-with-search__heading`}>
-          Let's troubleshoot
+          {heading}
         </h1>
         <Search
           className={`${prefix}--leadspace-with-search__search`}
-          placeHolderText="Search keywords"
+          {...searchProps}
         />
         <div
           className={`${prefix}--leadspace-with-search__button-group-container`}>
           <ul className={`${prefix}--leadspace-with-search__button-group`}>
-            <li>
-              <Button
-                kind="tertiary"
-                renderIcon={View}
-                className={`${prefix}--leadspace-with-search__button`}>
-                <span>View your cases</span>
-              </Button>
-            </li>
-            <li>
-              <Button
-                renderIcon={FolderAdd}
-                className={`${prefix}--leadspace-with-search__button`}>
-                <span>Open a case</span>
-              </Button>
-            </li>
+            {buttonsProps.map((button, index) => (
+              <li key={index}>
+                <Button
+                  {...button}
+                  className={`${prefix}--leadspace-with-search__button`}>
+                  <span>{button.children}</span>
+                </Button>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
     </div>
   </section>
 );
+
+LeadspaceWithSearch.propTypes = {
+  /**
+   *
+   */
+  heading: PropTypes.string.isRequired,
+  /**
+   *
+   */
+  searchProps: Search.propTypes,
+  /**
+   *
+   */
+  buttonsProps: PropTypes.arrayOf(Button.propTypes),
+};
 
 export default LeadspaceWithSearch;
--- packages/react/src/components/LeadspaceWithSearch/__stories__/LeadspaceWithSearch.stories.js
+++ packages/react/src/components/LeadspaceWithSearch/__stories__/LeadspaceWithSearch.stories.js
@@ -5,11 +5,50 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import FolderAdd from '@carbon/icons-react/lib/folder--add/20';
 import LeadspaceWithSearch from '../LeadspaceWithSearch';
 import React from 'react';
+import { text } from '@storybook/addon-knobs';
+import View from '@carbon/icons-react/lib/view/20';
 
 export default {
   title: 'Components|LeadspaceWithSearch',
+  parameters: {
+    knobs: {
+      LeadspaceWithSearch: ({ groupId }) => ({
+        heading: text('Heading (heading):', 'Lets troubleshoot', groupId),
+        searchProps: {
+          placeHolderText: text(
+            'Placeholder (searchProps.placeHolderText):',
+            'Search keywords',
+            groupId
+          ),
+        },
+        buttonsProps: [
+          {
+            renderIcon: View,
+            kind: 'tertiary',
+            children: 'View your cases',
+          },
+          {
+            children: 'Open a case',
+            renderIcon: FolderAdd,
+          },
+        ],
+      }),
+    },
+  },
 };
 
-export const Default = () => <LeadspaceWithSearch />;
+export const Default = ({ parameters }) => {
+  const { heading, searchProps, buttonsProps } =
+    parameters?.props?.LeadspaceWithSearch ?? {};
+
+  return (
+    <LeadspaceWithSearch
+      heading={heading}
+      searchProps={searchProps}
+      buttonsProps={buttonsProps}
+    />
+  );
+};
