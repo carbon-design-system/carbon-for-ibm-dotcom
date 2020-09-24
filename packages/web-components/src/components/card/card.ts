310b008a3acfc7fc06603a6a6f04341243b5aa29
Akira Sudoh <asudoh@gmail.com>
2020-09-25 05:38:28 +0900

    fix(components): disable delegatesFocus in Safari (#4042)
    ### Related Ticket(s)
    
    Refs carbon-design-system/carbon-web-components#494.
    
    ### Description
    
    This change disables `delegatesFocus` feature in shadow DOM in recent Safari, notably `14.x`. Safari `14.x` introduced `delegatesFocus`, but using such feature causes browser crash, as reported at: https://bugs.webkit.org/show_bug.cgi?id=215622.
    
    The WebKit version threshold is chosen as it's the number of Blink fork. It means:
    
    * Safari after Blink fork: `delegatesFocus` is disabled (for now)
    * Other brwosers, including Chrome: `delegatesFocus` is enabled, as long as the browser supports it
    
    ### Changelog
    
    **Changed**
    
    - Disables `delegatesFocus` feature in shadow DOM in recent Safari.

--- packages/web-components/src/components/card/card.ts
+++ packages/web-components/src/components/card/card.ts
@@ -121,7 +121,10 @@ class DDSCard extends DDSLink {
   href = '';
 
   createRenderRoot() {
-    return this.attachShadow({ mode: 'open', delegatesFocus: true });
+    return this.attachShadow({
+      mode: 'open',
+      delegatesFocus: Number((/Safari\/(\d+)/.exec(navigator.userAgent) ?? ['', 0])[1]) <= 537,
+    });
   }
 
   updated(changedProperties) {
