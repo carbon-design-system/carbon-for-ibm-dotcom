e398856ccb1a47eb0573a7bdfe18bc7cee907f14
Raphael Amadeu <30945011+RaphaelAmadeu@users.noreply.github.com>
2020-09-28 15:34:10 -0300

    feat(callout-data): pattern created

--- packages/web-components/src/components/callout-data/__stories__/callout-data.stories.ts
+++ packages/web-components/src/components/callout-data/__stories__/callout-data.stories.ts
@@ -8,31 +8,55 @@
  */
 
 import { html } from 'lit-element';
+import textNullable from '../../../../.storybook/knob-text-nullable';
 import '../callout-data';
-// import readme from './README.stories.mdx';
+import readme from './README.stories.mdx';
 
-export const Default = () => html`
-  <div class="bx--grid" style="width: 100%">
-    <div class="bx--row">
-      <div class="bx--offset-lg-3">
-        <dds-callout-data>
-        <span slot="data">51%</span>
-        <span slot="copy">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        </span>
-        <span slot="source">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        </span>
-      </dds-callout-data>
+export const Default = ({ parameters }) => {
+  console.log(parameters);
+  const { data, copy, source } = parameters?.props?.CalloutData?.props ?? {};
+
+  return html`
+    <div class="bx--grid" style="width: 100%">
+      <div class="bx--row">
+        <div class="bx--offset-lg-3 bx--col-lg-9">
+          <dds-callout-data>
+            <span slot="data">${data}</span>
+            <span slot="copy">
+              ${copy}
+            </span>
+            <span slot="source">
+              ${source}
+            </span>
+          </dds-callout-data>
+        </div>
       </div>
     </div>
-  </div>
-`;
+  `;
+};
 
 export default {
   title: 'Components/Callout Data',
   parameters: {
-    // ...readme.parameters,
+    ...readme.parameters,
     hasGrid: true,
+    knobs: {
+      CalloutData: ({ groupId }) => ({
+        props: {
+          data: textNullable('Data (data):', '51%', groupId),
+          copy: textNullable(
+            'Short copy (copy):',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+              ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+            groupId
+          ),
+          source: textNullable(
+            'Source (source):',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            groupId
+          ),
+        },
+      }),
+    },
   },
 };
