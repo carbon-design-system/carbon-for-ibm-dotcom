81a47ad0be421ad7799971aaed9e7cefa2e07199
Anna Wen <>
2020-09-30 17:41:36 -0400

    feat(Masthead): add support for megamenu in masthead l1

--- packages/react/src/components/Masthead/MastheadL1.js
+++ packages/react/src/components/Masthead/MastheadL1.js
@@ -10,6 +10,7 @@ import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/setti
 import HeaderMenu from '../carbon-components-react/UIShell/HeaderMenu';
 import HeaderMenuItem from '../../internal/vendor/carbon-components-react/components/UIShell/HeaderMenuItem';
 import HeaderNavigation from '../../internal/vendor/carbon-components-react/components/UIShell/HeaderNavigation';
+import MegaMenu from './MastheadMegaMenu/MegaMenu';
 import PropTypes from 'prop-types';
 import React from 'react';
 import settings from 'carbon-components/es/globals/js/settings';
@@ -26,22 +27,23 @@ const MastheadL1 = ({ title, titleLink, navigationL1, ...rest }) => {
   });
 
   const mastheadL1Links = navigationL1.map((link, index) => {
-    if (link.hasMenu) {
+    const autoid = `${stablePrefix}--masthead-${rest.navType}__l1-nav${index}`;
+    if (link.hasMenu || link.hasMegapanel) {
       return (
         <HeaderMenu
           aria-label={link.title}
           menuLinkName={link.title}
-          data-autoid={`${stablePrefix}--masthead-${rest.navType}__l1-nav${index}`}
+          className={cx({
+            [`${prefix}--masthead__megamenu__l1-nav`]: link.hasMegapanel,
+          })}
+          autoId={autoid}
           key={index}>
-          {renderNav(link.menuSections, rest.navType)}
+          {renderNav(link, rest.navType, autoid)}
         </HeaderMenu>
       );
     } else {
       return (
-        <HeaderMenuItem
-          href={link.url}
-          data-autoid={`${stablePrefix}--masthead-${rest.navType}__l1-nav${index}`}
-          key={index}>
+        <HeaderMenuItem href={link.url} data-autoid={autoid} key={index}>
           {link.title}
         </HeaderMenuItem>
       );
@@ -65,24 +67,29 @@ const MastheadL1 = ({ title, titleLink, navigationL1, ...rest }) => {
 /**
  * Loops through and renders a list of links for the masthead nav
  *
- * @param {Array} sections A list of links to be rendered
+ * @param {object} link A list of links to be rendered
  * @param {string} navType navigation type for autoids
+ * @param {string} autoid autoid predecessor for megamenu items/menu items data-autoids
  * @returns {object} JSX object
  */
-function renderNav(sections, navType) {
+function renderNav(link, navType, autoid) {
   const navItems = [];
-  sections.forEach((section, i) => {
-    section.menuItems.forEach((item, j) => {
-      navItems.push(
-        <HeaderMenuItem
-          href={item.url}
-          data-autoid={`${stablePrefix}--masthead-${navType}__l1-nav${i}-item${j}`}
-          key={item.title}>
-          {item.title}
-        </HeaderMenuItem>
-      );
+  if (link.hasMegapanel) {
+    navItems.push(<MegaMenu key={link.title} data={link} autoid={autoid} />);
+  } else {
+    link.menuSections.forEach((section, i) => {
+      section.menuItems.forEach((item, j) => {
+        navItems.push(
+          <HeaderMenuItem
+            href={item.url}
+            data-autoid={`${stablePrefix}--masthead-${navType}__l1-nav${i}-item${j}`}
+            key={item.title}>
+            {item.title}
+          </HeaderMenuItem>
+        );
+      });
     });
-  });
+  }
   return navItems;
 }
 
