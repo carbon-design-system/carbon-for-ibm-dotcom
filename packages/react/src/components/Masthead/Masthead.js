e013005e79dd683a714eb839b641a0b0d35ff76a
ariellagilmore <ariellalgilmore@gmail.com>
2020-09-30 20:39:43 -0700

    feat(masthead): fix resizeObserver

--- packages/react/src/components/Masthead/Masthead.js
+++ packages/react/src/components/Masthead/Masthead.js
@@ -6,7 +6,6 @@
  */
 
 import React, { useEffect, useRef, useState } from 'react';
-import calculateTotalWidth from '@carbon/ibmdotcom-utilities/es/utilities/calculateTotalWidth/calculateTotalWidth';
 import cx from 'classnames';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
 import { globalInit } from '@carbon/ibmdotcom-services/es/services/global/global';
@@ -181,44 +180,7 @@ const Masthead = ({
     navType = 'eco';
   }
 
-  const headerContainer = useRef(null);
-  const [overflow, setOverflow] = useState(false);
-  const [showRightCaret, setShowRightCaret] = useState(false);
-  const [showLeftCaret, setShowLeftCaret] = useState(false);
-
-  const resizeObserver = new ResizeObserver(() => {
-    console.log(calculateTotalWidth(['bx--header__search']));
-    const containerWidth =
-      calculateTotalWidth(['bx--header__search']) -
-      calculateTotalWidth(['bx--masthead__search']) -
-      calculateTotalWidth(['bx--header__name']);
-    const totalNavWidth = calculateTotalWidth(['bx--header__nav']);
-    if (totalNavWidth > containerWidth) {
-      setOverflow(true);
-      const offset = document.querySelector('.bx--header__nav-container')
-        .scrollLeft;
-      console.log(
-        'on resize',
-        'container: ',
-        containerWidth,
-        'totalNavWidth: ',
-        totalNavWidth,
-        'offset: ',
-        offset
-      );
-      if (offset == 0 || offset + containerWidth <= totalNavWidth) {
-        setShowRightCaret(true);
-      }
-    } else {
-      setOverflow(false);
-      setShowLeftCaret(false);
-      setShowRightCaret(false);
-    }
-  });
-
-  useEffect(() => {
-    resizeObserver.observe(headerContainer.current);
-  }, [headerContainer, resizeObserver]);
+  const topNavRef = useRef(null);
 
   return (
     <HeaderContainer
@@ -265,7 +227,7 @@ const Masthead = ({
 
                 <div
                   className={`${prefix}--header__search ${hasPlatform}`}
-                  ref={headerContainer}>
+                  ref={topNavRef}>
                   {navigation && !mastheadL1Data && (
                     <MastheadTopNav
                       {...mastheadProps}
@@ -273,11 +235,7 @@ const Masthead = ({
                       navigation={mastheadData}
                       navType={navType}
                       selectedMenuItem={selectedMenuItem}
-                      overflow={overflow}
-                      showRightCaret={showRightCaret}
-                      setShowRightCaret={setShowRightCaret}
-                      showLeftCaret={showLeftCaret}
-                      setShowLeftCaret={setShowLeftCaret}
+                      topNavRef={topNavRef}
                     />
                   )}
                   {hasSearch && (
