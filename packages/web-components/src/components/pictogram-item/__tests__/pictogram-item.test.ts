ef7148c22690edb2e344f5dbf3480df276684269
Raphael Amadeu <30945011+RaphaelAmadeu@users.noreply.github.com>
2020-10-06 15:23:22 -0300

    feat(pictogram-item): web-component created (#4110)
    ### Related Ticket(s)
    
    #3739 
    
    ### Description
    
    Web component created, following the react version.

---
+++ packages/web-components/src/components/pictogram-item/__tests__/pictogram-item.test.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { render } from 'lit-html';
+import '../pictogram-item';
+import { Default } from '../__stories__/pictogram-item.stories';
+
+const template = (props?) =>
+  Default({
+    parameters: {
+      props: {
+        PictogramItem: props,
+      },
+    },
+  });
+
+describe('dds-pictogram-item', function() {
+  it('Renders as expected', async function() {
+    render(template(), document.body);
+    await Promise.resolve();
+    expect(document.body.querySelector('dds-pictogram-item')).toMatchSnapshot({ mode: 'shadow' });
+  });
+  afterEach(async function() {
+    await render(undefined!, document.body);
+  });
+});
