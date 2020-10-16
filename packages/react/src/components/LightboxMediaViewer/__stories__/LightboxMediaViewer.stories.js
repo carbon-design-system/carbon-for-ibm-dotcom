413bac4a666f48af836daa393aab059835d5b864
Ryan Sebade <rfsebade@gmail.com>
2020-10-16 09:31:08 -0400

    feat(LightboxMediaViewer): change required to optional

--- packages/react/src/components/LightboxMediaViewer/__stories__/LightboxMediaViewer.stories.js
+++ packages/react/src/components/LightboxMediaViewer/__stories__/LightboxMediaViewer.stories.js
@@ -90,12 +90,12 @@ EmbeddedVideoPlayer.story = {
           src: '1_9h94wo6b',
           type: 'video',
           title: text(
-            'title (required)',
+            'title (optional)',
             'Curabitur malesuada varius mi eu posuere',
             groupId
           ),
           description: text(
-            'description (required)',
+            'description (optional)',
             `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est.Mauris iaculis eget dolor nec hendrerit. Phasellus at elit sollicitudin, sodales nulla quis, consequat libero. Here are some common categories:`,
             groupId
           ),
