262d4e0d2f43c37994bbe344a0ffa53a82bf4d7d
ariellagilmore <ariellalgilmore@gmail.com>
2020-10-22 14:03:57 -0700

    feat(overflow): change to css left properties

--- packages/react/src/components/Masthead/HeaderNavContainer.js
+++ packages/react/src/components/Masthead/HeaderNavContainer.js
@@ -17,138 +17,25 @@ const { prefix } = settings;
  * Header nav container component.
  */
 const HeaderNavContainer = ({ children }) => {
-  // const headerNavContainerRef = useRef(null);
-  // const [resizeObserver, setResizeObserver] = useState(null);
-  // const [showLeftCaret, setShowLeftCaret] = useState(false);
-  // const [showRightCaret, setShowRightCaret] = useState(false);
-  // const [containerWidth, setContainerWidth] = useState(0);
-  // const [totalNavWidth, setTotalNavWidth] = useState(0);
-  //
-  // const paginateLeft = useCallback(() => {
-  //   headerNavContainerRef.current.scrollLeft -= containerWidth;
-  //   setShowRightCaret(true);
-  //   // 40 accounts for caret size
-  //   if (headerNavContainerRef.current.scrollLeft <= 40) {
-  //     setShowLeftCaret(false);
-  //     headerNavContainerRef.current.scrollLeft = 0;
-  //   }
-  // }, [containerWidth]);
-  //
-  // const paginateRight = useCallback(() => {
-  //   headerNavContainerRef.current.scrollLeft += containerWidth;
-  //   setShowLeftCaret(true);
-  //   // 80 accounts for caret sizes
-  //   if (
-  //     headerNavContainerRef.current.scrollLeft + containerWidth >=
-  //     totalNavWidth - 80
-  //   ) {
-  //     setShowRightCaret(false);
-  //     headerNavContainerRef.current.scrollLeft += 80;
-  //   }
-  // }, [containerWidth, totalNavWidth]);
-  //
-  // useEffect(() => {
-  //   if (window.ResizeObserver) {
-  //     setResizeObserver(
-  //       new ResizeObserver(() => {
-  //         setContainerWidth(calculateTotalWidth(['bx--header__nav-container']));
-  //         setTotalNavWidth(calculateTotalWidth(['bx--header__nav']));
-  //       })
-  //     );
-  //   }
-  // }, []);
-  //
-  // useEffect(() => {
-  //   if (totalNavWidth > containerWidth) {
-  //     // 80 accounts for caret sizes
-  //     if (
-  //       headerNavContainerRef.current.scrollLeft === 0 ||
-  //       headerNavContainerRef.current.scrollLeft + containerWidth <
-  //         totalNavWidth - 80
-  //     ) {
-  //       setShowRightCaret(true);
-  //     }
-  //     if (headerNavContainerRef.current.scrollLeft > 0) {
-  //       setShowLeftCaret(true);
-  //     }
-  //   } else {
-  //     setShowLeftCaret(false);
-  //     setShowRightCaret(false);
-  //   }
-  // }, [containerWidth, totalNavWidth]);
-  //
-  // useEffect(() => {
-  //   const { current: headerNavContainerNode } = headerNavContainerRef;
-  //   if (resizeObserver) {
-  //     resizeObserver.observe(headerNavContainerNode);
-  //   }
-  //
-  //   return () => {
-  //     if (resizeObserver) {
-  //       resizeObserver.disconnect();
-  //     }
-  //   };
-  // }, [resizeObserver]);
-
   const containerRef = useRef(null);
+  const contentRef = useRef(null);
   const [io, setIO] = useState(null);
+  const [position, setPosition] = useState(0);
 
   const paginateLeft = useCallback(() => {
-    containerRef.current.scrollLeft = Math.max(
-      containerRef.current.scrollLeft - containerRef.current.offsetWidth,
-      0
-    );
-    // scrollLeft(
-    //   containerRef.current,
-    //   -Math.max(
-    //     document.querySelector('.content').offsetWidth -
-    //       containerRef.current.scrollLeft,
-    //     0
-    //   ),
-    //   100
-    // );
-  }, []);
+    const moveLeft = Math.min(position + containerRef.current.offsetWidth, 0);
+    contentRef.current.style.left = String(moveLeft) + 'px';
+    setPosition(moveLeft);
+  }, [position]);
 
   const paginateRight = useCallback(() => {
-    containerRef.current.scrollLeft = Math.min(
-      containerRef.current.scrollLeft + containerRef.current.offsetWidth,
-      document.querySelector('.content').offsetWidth -
-        containerRef.current.offsetWidth
+    const moveRight = -Math.min(
+      -position + containerRef.current.offsetWidth,
+      contentRef.current.offsetWidth - containerRef.current.offsetWidth
     );
-
-    // scrollLeft(
-    //   containerRef.current,
-    //   containerRef.current.scrollLeft + containerRef.current.offsetWidth,
-    //   100
-    // );
-  }, []);
-
-  // function scrollLeft(element, change, duration) {
-  //   let start = element.scrollLeft,
-  //     currentTime = 0,
-  //     increment = 1;
-  //
-  //   const animateScroll = function() {
-  //     currentTime += increment;
-  //     let val = Math.easeInOutQuad(currentTime, start, change, duration);
-  //     element.scrollLeft = val;
-  //     if (currentTime < duration) {
-  //       setTimeout(animateScroll, increment);
-  //     }
-  //   };
-  //   animateScroll();
-  // }
-  //
-  // //t = current time
-  // //b = start value
-  // //c = change in value
-  // //d = duration
-  // Math.easeInOutQuad = function(t, b, c, d) {
-  //   t /= d / 2;
-  //   if (t < 1) return (c / 2) * t * t * t + b;
-  //   t -= 2;
-  //   return (c / 2) * (t * t * t + 2) + b;
-  // };
+    contentRef.current.style.left = String(moveRight) + 'px';
+    setPosition(moveRight);
+  }, [position]);
 
   useEffect(() => {
     setIO(
@@ -191,12 +78,11 @@ const HeaderNavContainer = ({ children }) => {
       <button
         className={`${prefix}--header__nav-caret-left`}
         aria-label="Masthead left caret"
-        // hidden={!showLeftCaret}
         onClick={paginateLeft}>
         <CaretLeft20 />
       </button>
       <div className={`${prefix}--header__nav-container`} ref={containerRef}>
-        <div className="content">
+        <div className={`${prefix}--header__nav-content`} ref={contentRef}>
           <div className="sub-content-left"></div>
           <div className="sub-content-right"></div>
           {children}
@@ -205,7 +91,6 @@ const HeaderNavContainer = ({ children }) => {
       <button
         className={`${prefix}--header__nav-caret-right`}
         aria-label="Masthead right caret"
-        // hidden={!showRightCaret}
         onClick={paginateRight}>
         <CaretRight20 />
       </button>
