75b7e58ed2bfe766ff0077627f6796c344ce49f8
Guilherme L. Moraes <42848561+guilhermelMoraes@users.noreply.github.com>
2020-10-30 15:16:18 -0300

    feat(leadspace-with-search): introducing new pattern (behind feature flag) (#3987)
    ### Description
    
    This pattern was first introduced on the Support page. Here are the [visual specs](https://app.abstract.com/projects/7f83c114-7627-4169-8af7-6a85e7147ea6/branches/master/commits/latest/files/01ee3bd8-c63d-4b3b-bddb-aabaddb5c31d/layers/A3E455FA-D017-4878-8078-A0E1D9B0BCD1?collectionId=c963cb53-73c2-42a5-af39-f5538e24c454&collectionLayerId=3bbd5a17-7d2f-464d-8840-4794846b1227).
    
    ### Changelog
    
    **New**
    
    - `LeadspaceWithSearch` pattern;

---
+++ packages/react/src/components/LeadspaceWithSearch/__stories__/LeadspaceWithSearch.stories.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright IBM Corp. 2016, 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { DDS_LEADSPACE_WITH_SEARCH } from '../../../internal/FeatureFlags';
+import LeadspaceWithSearch from '../LeadspaceWithSearch';
+import React from 'react';
+import readme from '../README.stories.mdx';
+import { text } from '@storybook/addon-knobs';
+
+export default !DDS_LEADSPACE_WITH_SEARCH
+  ? undefined
+  : {
+      title: 'Components|LeadspaceWithSearch',
+      parameters: {
+        ...readme.parameters,
+        knobs: {
+          LeadspaceWithSearch: ({ groupId }) => ({
+            heading: text('Heading (heading):', 'Find a product', groupId),
+            copy: text(
+              'Copy (copy):',
+              'Duis aute irure dolor in reprehen deritirure',
+              groupId
+            ),
+            searchProps: {
+              placeHolder: {
+                mobile: text(
+                  'Mobile placeholder (searchProps.placeHolder.mobile):',
+                  'Search keywords',
+                  groupId
+                ),
+                desktop: text(
+                  'Desktop placeholder (searchProps.placeHolder.desktop)',
+                  'Search known issues, documentation and support forums',
+                  groupId
+                ),
+              },
+              labelText: 'Search keywords',
+              onChange: () => {},
+              onKeyDown: () => {},
+            },
+          }),
+        },
+      },
+    };
+
+export const Default = !DDS_LEADSPACE_WITH_SEARCH
+  ? undefined
+  : ({ parameters }) => {
+      const { heading, copy, searchProps } =
+        parameters?.props?.LeadspaceWithSearch ?? {};
+
+      return (
+        <LeadspaceWithSearch
+          heading={heading}
+          copy={copy}
+          searchProps={searchProps}
+        />
+      );
+    };
