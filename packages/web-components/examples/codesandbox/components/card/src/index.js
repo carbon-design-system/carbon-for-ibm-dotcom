9ec05701048ba568ae3525be754d242a66efe792
Akira Sudoh <asudoh@gmail.com>
2020-11-11 07:49:59 +0900

    docs(examples): add JS extension (#4437)
    ### Related Ticket(s)
    
    Refs #4436.
    
    ### Description
    
    Adds `.js` extensions to the `import`s in CodeSandbox example as well as our READMEs, because some module bundlers work with latest Node module resolution algorithm that disallows ESM imports without `.js`.
    
    Also removes JSPM instructions from READMEs because we have seen issues with it (likely in JSPM side) and our adaptors have reported it even.
    
    It's worth re-evaluating JSPM usage in future, though, when such issue will potentially be resolved.
    
    ### Changelog
    
    **Changed**
    
    - Adds `.js` extensions to the `import`s in CodeSandbox example as well as our READMEs
    
    **Removed**
    
    - JSPM instructions.

--- packages/web-components/examples/codesandbox/components/card/src/index.js
+++ packages/web-components/examples/codesandbox/components/card/src/index.js
@@ -7,5 +7,5 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import '@carbon/ibmdotcom-web-components/es/components/card/card';
-import '@carbon/ibmdotcom-web-components/es/components/card/card-footer';
+import '@carbon/ibmdotcom-web-components/es/components/card/card.js';
+import '@carbon/ibmdotcom-web-components/es/components/card/card-footer.js';
