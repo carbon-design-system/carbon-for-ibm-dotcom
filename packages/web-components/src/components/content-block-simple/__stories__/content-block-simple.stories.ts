13b0181a73d895fa309412a02bd346bac6db407c
kennylam <kennylam@protonmail.com>
2020-11-12 14:52:10 -0500

    fix(Images): update FPO images url (#4482)
    ### Description
    
    [Dummyimage](https://dummyimage.com/), the online image generator used for our FPO images has gone offline. This change updates all images to use [fpoimg](https://fpoimg.com/).
    
    ### Changelog
    
    **Changed**
    
    - update FPO image generator to https://fpoimg.com/
    
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/src/components/content-block-simple/__stories__/content-block-simple.stories.ts
+++ packages/web-components/src/components/content-block-simple/__stories__/content-block-simple.stories.ts
@@ -55,12 +55,25 @@ const copy = `Lorem ipsum *dolor* sit amet, consectetur adipiscing elit. Aenean 
 
 // TODO: Replace with <dds-image-with-caption>
 const image = html`
-  <dds-image slot="media" alt="Image alt text" default-src="https://dummyimage.com/672x378/ee5396/161616&amp;text=16:9">
-    <dds-image-item media="(min-width: 672px)" srcset="https://dummyimage.com/672x378/ee5396/161616&amp;text=16:9">
+  <dds-image
+    slot="media"
+    alt="Image alt text"
+    default-src="https://fpoimg.com/672x378?text=16:9&amp;bg_color=ee5396&amp;text_color=161616"
+  >
+    <dds-image-item
+      media="(min-width: 672px)"
+      srcset="https://fpoimg.com/672x378?text=16:9&amp;bg_color=ee5396&amp;text_color=161616"
+    >
     </dds-image-item>
-    <dds-image-item media="(min-width: 400px)" srcset="https://dummyimage.com/400x225/ee5396/161616&amp;text=16:9">
+    <dds-image-item
+      media="(min-width: 400px)"
+      srcset="https://fpoimg.com/400x225?text=16:9&amp;bg_color=ee5396&amp;text_color=161616"
+    >
     </dds-image-item>
-    <dds-image-item media="(min-width: 320px)" srcset="https://dummyimage.com/320x180/ee5396/161616&amp;text=16:9">
+    <dds-image-item
+      media="(min-width: 320px)"
+      srcset="https://fpoimg.com/320x180?text=16:9&amp;bg_color=ee5396&amp;text_color=161616"
+    >
     </dds-image-item>
   </dds-image>
 `;
