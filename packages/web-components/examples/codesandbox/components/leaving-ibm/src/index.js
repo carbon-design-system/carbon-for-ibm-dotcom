2539ab56fb9853e2a4a8c1043670ffaf1e2f8a55
Anna Wen <54281166+annawen1@users.noreply.github.com>
2020-11-18 09:03:40 -0500

    chore(leaving-ibm): set up codesandbox for leaving-ibm (#4491)

---
+++ packages/web-components/examples/codesandbox/components/leaving-ibm/src/index.js
@@ -0,0 +1,16 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import '@carbon/ibmdotcom-web-components/es/components/leaving-ibm/leaving-ibm-modal.js';
+import '@carbon/ibmdotcom-web-components/es/components/leaving-ibm/leaving-ibm-modal-body.js';
+import '@carbon/ibmdotcom-web-components/es/components/leaving-ibm/leaving-ibm-modal-heading.js';
+import 'carbon-web-components/es/components/modal/modal-header.js';
+import 'carbon-web-components/es/components/modal/modal-close-button.js';
+import 'carbon-web-components/es/components/button/button.js';
+import './index.scss';
