d4f15ddea50e342e888d8cecfa2527a058f0ab8d
Raphael Amadeu <30945011+RaphaelAmadeu@users.noreply.github.com>
2020-11-27 22:40:28 -0300

    feat(content-block-media): web-component created

---
+++ packages/web-components/examples/codesandbox/components/content-block-media/src/index.js
@@ -0,0 +1,18 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import '@carbon/ibmdotcom-web-components/es/components/content-block-media/content-block-media';
+import '@carbon/ibmdotcom-web-components/es/components/content-block-media/content-block-media-content';
+import '@carbon/ibmdotcom-web-components/es/components/content-block/content-block-heading';
+import '@carbon/ibmdotcom-web-components/es/components/content-block/content-block-paragraph';
+import '@carbon/ibmdotcom-web-components/es/components/content-group/content-group-heading';
+import '@carbon/ibmdotcom-web-components/es/components/content-item/content-item-heading';
+import '@carbon/ibmdotcom-web-components/es/components/content-item/content-item-copy';
+import '@carbon/ibmdotcom-web-components/es/components/content-item/content-item';
+import './index.scss';
