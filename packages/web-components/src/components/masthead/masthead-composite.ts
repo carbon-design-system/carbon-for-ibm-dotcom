195f68f085ae87fabeb66d47e478aa48b02a6713
Akira Sudoh <asudoh@gmail.com>
2020-11-30 23:57:48 +0900

    fix(masthead): introduce separate L1 nav variant
    introduces `<dds-top-nav-l1>` that disables the feature to hide the nav
    when search box is open.

--- packages/web-components/src/components/masthead/masthead-composite.ts
+++ packages/web-components/src/components/masthead/masthead-composite.ts
@@ -34,6 +34,7 @@ import './megamenu-category-link';
 import './megamenu-category-group';
 import './megamenu-overlay';
 import './top-nav';
+import './top-nav-l1';
 import './top-nav-name';
 import './top-nav-item';
 import './top-nav-menu';
@@ -82,7 +83,7 @@ class DDSMastheadComposite extends LitElement {
     if (menuItems) {
       return target === NAV_ITEMS_RENDER_TARGET.TOP_NAV
         ? html`
-            <dds-top-nav hide-divider>
+            <dds-top-nav-l1>
               ${menuItems.map((elem: MastheadL1Item, i) => {
                 return elem.menuItems
                   ? html`
@@ -110,7 +111,7 @@ class DDSMastheadComposite extends LitElement {
                       ></dds-top-nav-item>
                     `;
               })}
-            </dds-top-nav>
+            </dds-top-nav-l1>
           `
         : menuItems.map((elem: MastheadL1Item, i) =>
             elem.menuItems
