abe4a735e888082cc919644de95cea20c2a1a7ee
Kenny Lam <kennylam@protonmail.com>
2020-12-03 12:14:52 -0500

    feat(sameheight): add sameheight util

--- packages/web-components/src/components/content-block-headlines/__stories__/content-block-headlines.stories.ts
+++ packages/web-components/src/components/content-block-headlines/__stories__/content-block-headlines.stories.ts
@@ -8,18 +8,11 @@
  */
 
 import { html } from 'lit-element';
-// import { select } from '@storybook/addon-knobs';
-// import ifNonNull from 'carbon-web-components/es/globals/directives/if-non-null.js';
 import readme from './README.stories.mdx';
 import textNullable from '../../../../.storybook/knob-text-nullable';
-// import '../../content-block/content-block-heading';
-// import '../../content-block/content-block-copy';
-// import '../../content-block/content-block-complementary';
-import '../../content-item-horizontal/content-item-horizontal';
-import '../../content-item-horizontal/content-item-horizontal-copy';
+import '../../content-item/content-item-copy';
 import '../content-block-headlines';
 import '../content-block-headlines-item';
-// import { CONTENT_BLOCK_COMPLEMENTARY_STYLE_SCHEME } from '../../content-block/content-block';
 
 const contentItemRow1 = html`
   <dds-content-block-headlines-item slot="content">
@@ -46,13 +39,28 @@ const contentItemRow2 = html`
   </dds-content-block-headlines-item>
 `;
 
+const contentItemRow3 = html`
+  <dds-content-item slot="content">
+    <dds-content-item-headline slot="heading">1.34M</dds-content-item-headline>
+    <dds-content-item-copy>Save 1.34M per year by optimizing your time and IT expenses.</dds-content-item-copy>
+    <dds-link-list slot="cta" type="vertical">
+      <dds-link-list-item-cta icon-placement="right" href="https://www.ibm.com" cta-type="local">
+        Link text
+      </dds-link-list-item-cta>
+      <dds-link-list-item-cta icon-placement="right" href="https://www.ibm.com" cta-type="external">
+        External link text
+      </dds-link-list-item-cta>
+    </dds-link-list>
+  </dds-content-item>
+`;
+
 export const Default = ({ parameters }) => {
   const { heading, copy } = parameters?.props?.ContentBlockHeadlines ?? {};
   return html`
     <dds-content-block-headlines>
       <dds-content-block-heading>${heading}</dds-content-block-heading>
       <dds-content-block-copy slot="copy">${copy}</dds-content-block-copy>
-      ${contentItemRow1} ${contentItemRow1} ${contentItemRow2}
+      ${contentItemRow1} ${contentItemRow1} ${contentItemRow2} ${contentItemRow3}
     </dds-content-block-headlines>
   `;
 };
