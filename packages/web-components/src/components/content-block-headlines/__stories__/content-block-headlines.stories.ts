710364311c36628abb813990c7ca0047c701dccd
Kenny Lam <kennylam@protonmail.com>
2020-12-03 21:29:49 -0500

    feat(content-block-headlines): switch to css grid for items

--- packages/web-components/src/components/content-block-headlines/__stories__/content-block-headlines.stories.ts
+++ packages/web-components/src/components/content-block-headlines/__stories__/content-block-headlines.stories.ts
@@ -21,6 +21,11 @@ const contentItemRow1 = html`
       >Modernize mission-critical applications and infrastructure in a hybrid multicloud environment up to 25 percent
       faster.</dds-content-item-copy
     >
+    <dds-link-list slot="cta" type="vertical">
+      <dds-link-list-item-cta icon-placement="right" href="https://www.ibm.com" cta-type="local">
+        Link text
+      </dds-link-list-item-cta>
+    </dds-link-list>
   </dds-content-block-headlines-item>
 `;
 
@@ -32,26 +37,8 @@ const contentItemRow2 = html`
       <dds-link-list-item-cta icon-placement="right" href="https://www.ibm.com" cta-type="local">
         Link text
       </dds-link-list-item-cta>
-      <dds-link-list-item-cta icon-placement="right" href="https://www.ibm.com" cta-type="external">
-        External link text
-      </dds-link-list-item-cta>
     </dds-link-list>
   </dds-content-block-headlines-item>
-`;
-
-const contentItemRow3 = html`
-  <dds-content-item slot="content">
-    <dds-content-item-headline slot="heading">1.34M</dds-content-item-headline>
-    <dds-content-item-copy>Save 1.34M per year by optimizing your time and IT expenses.</dds-content-item-copy>
-    <dds-link-list slot="cta" type="vertical">
-      <dds-link-list-item-cta icon-placement="right" href="https://www.ibm.com" cta-type="local">
-        Link text
-      </dds-link-list-item-cta>
-      <dds-link-list-item-cta icon-placement="right" href="https://www.ibm.com" cta-type="external">
-        External link text
-      </dds-link-list-item-cta>
-    </dds-link-list>
-  </dds-content-item>
 `;
 
 export const Default = ({ parameters }) => {
@@ -60,7 +47,7 @@ export const Default = ({ parameters }) => {
     <dds-content-block-headlines>
       <dds-content-block-heading>${heading}</dds-content-block-heading>
       <dds-content-block-copy slot="copy">${copy}</dds-content-block-copy>
-      ${contentItemRow1} ${contentItemRow1} ${contentItemRow2} ${contentItemRow3}
+      ${contentItemRow1} ${contentItemRow1} ${contentItemRow2} ${contentItemRow1}
     </dds-content-block-headlines>
   `;
 };
@@ -71,7 +58,7 @@ export default {
     (story, { parameters }) => html`
       <div class="bx--grid dds-ce-demo-devenv--grid--stretch">
         <div class="bx--row dds-ce-demo-devenv--grid-row">
-          <div class=" bx--col-sm-4 bx--offset-lg-4 ${parameters.gridLargeColumnClass}">
+          <div class="bx--col-sm-4 bx--offset-lg-4 ${parameters.gridLargeColumnClass}">
             ${story()}
           </div>
         </div>
