ee1bf2f025e2060361490be43cc78b53e8fb533f
Kenny Lam <kennylam@protonmail.com>
2020-12-04 01:08:12 -0500

    chore(feature-flag): add feature flag to story

--- packages/web-components/src/components/content-block-headlines/__stories__/content-block-headlines.stories.ts
+++ packages/web-components/src/components/content-block-headlines/__stories__/content-block-headlines.stories.ts
@@ -13,6 +13,7 @@ import textNullable from '../../../../.storybook/knob-text-nullable';
 import '../../content-item/content-item-copy';
 import '../content-block-headlines';
 import '../content-block-headlines-item';
+import { DDS_CONTENT_BLOCK_HEADLINES } from '../../../globals/internal/feature-flags';
 
 const contentItemRow1 = html`
   <dds-content-block-headlines-item slot="content">
@@ -41,46 +42,48 @@ const contentItemRow2 = html`
   </dds-content-block-headlines-item>
 `;
 
-export const Default = ({ parameters }) => {
-  const { heading, copy } = parameters?.props?.ContentBlockHeadlines ?? {};
-  return html`
-    <dds-content-block-headlines>
-      <dds-content-block-heading>${heading}</dds-content-block-heading>
-      <dds-content-block-copy slot="copy">${copy}</dds-content-block-copy>
-      ${contentItemRow1} ${contentItemRow1} ${contentItemRow2} ${contentItemRow1}
-    </dds-content-block-headlines>
-  `;
-};
+export const Default = !DDS_CONTENT_BLOCK_HEADLINES
+  ? undefined
+  : ({ parameters }) => {
+      const { heading, copy } = parameters?.props?.ContentBlockHeadlines ?? {};
+      return html`
+        <dds-content-block-headlines>
+          <dds-content-block-heading>${heading}</dds-content-block-heading>
+          <dds-content-block-copy slot="copy">${copy}</dds-content-block-copy>
+          ${contentItemRow1} ${contentItemRow1} ${contentItemRow2} ${contentItemRow1}
+        </dds-content-block-headlines>
+      `;
+    };
 
-export default {
-  title: 'Components/Content Block Headlines',
-  decorators: [
-    (story, { parameters }) => html`
-      <div class="bx--grid dds-ce-demo-devenv--grid--stretch">
-        <div class="bx--row dds-ce-demo-devenv--grid-row">
-          <div class="bx--col-sm-4 bx--offset-lg-4 ${parameters.gridLargeColumnClass}">
-            ${story()}
-          </div>
-        </div>
-      </div>
-    `,
-  ],
-  parameters: {
-    ...readme.parameters,
-    hasGrid: true,
-    hasVerticalSpacingInComponent: true,
-    gridLargeColumnClass: 'bx--col-lg-12',
-    knobs: {
-      ContentBlockHeadlines: () => ({
-        heading: textNullable('Heading (required)', 'Aliquam condimentum'),
-        copy: textNullable(
-          'copy',
-          `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est.
-           Mauris iaculis eget dolor nec hendrerit. Phasellus at elit sollicitudin, sodales
-           nulla quis, consequat libero. Lorem ipsum dolor sit amet, consectetur adipiscing
-           elit. Aenean et ultricies est. Mauris iaculis eget dolor nec hendrerit.`
-        ),
-      }),
-    },
-  },
-};
+export default !DDS_CONTENT_BLOCK_HEADLINES
+  ? undefined
+  : {
+      title: 'Components/Content Block Headlines',
+      parameters: {
+        ...readme.parameters,
+        hasGrid: true,
+        knobs: {
+          ContentBlockHeadlines: () => ({
+            heading: textNullable('Heading (required)', 'Aliquam condimentum'),
+            copy: textNullable(
+              'copy',
+              `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et ultricies est.
+              Mauris iaculis eget dolor nec hendrerit. Phasellus at elit sollicitudin, sodales
+              nulla quis, consequat libero. Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Aenean et ultricies est. Mauris iaculis eget dolor nec hendrerit.`
+            ),
+          }),
+        },
+        decorators: [
+          story => html`
+            <div class="bx--grid dds-ce-demo-devenv--grid--stretch">
+              <div class="bx--row dds-ce-demo-devenv--grid-row">
+                <div class="bx--col-sm-4 bx--offset-lg-4 bx--col-lg-12">
+                  ${story()}
+                </div>
+              </div>
+            </div>
+          `,
+        ],
+      },
+    };
