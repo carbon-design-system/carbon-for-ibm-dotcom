1e89655f6221267e384fdec10802e5b5a338f890
Raphael Amadeu <raphael.s.amadeu@gmail.com>
2020-12-07 19:03:28 -0300

    feat(link-list-section): component created

--- packages/web-components/src/components/link-list-section/__stories__/link-list-section.stories.ts
+++ packages/web-components/src/components/link-list-section/__stories__/link-list-section.stories.ts
@@ -11,13 +11,16 @@ import { html } from 'lit-element';
 import ArrowRight20 from 'carbon-web-components/es/icons/arrow--right/20.js';
 import '../link-list-section';
 import '../../content-section/content-section-heading';
-// import readme from './README.stories.mdx';
-
+import '../../link-list/link-list';
+import '../../link-list/link-list-item';
+import textNullable from '../../../../.storybook/knob-text-nullable';
+import readme from './README.stories.mdx';
 
 export const Default = ({ parameters }) => {
+  const { heading } = parameters?.props?.LinkListSection ?? {};
   return html`
     <dds-link-list-section>
-      <dds-content-section-heading>Lorem ipsum dolor sit amet</dds-content-section-heading>
+      <dds-content-section-heading>${heading}</dds-content-section-heading>
       <dds-link-list>
         <dds-link-list-item href="https://example.com">
           Learn more about Kubernetes ${ArrowRight20({ slot: 'icon' })}
@@ -45,11 +48,19 @@ export const Default = ({ parameters }) => {
 export default {
   title: 'Components/Link List Section',
   parameters: {
-    // ...readme.parameters,
+    ...readme.parameters,
     hasGrid: true,
+    knobs: {
+      LinkListSection: ({ groupId }) => {
+        const heading = textNullable('Link list section heading:', 'Lorem ipsum dolor sit amet', groupId);
+        return {
+          heading,
+        };
+      },
+    },
   },
   decorators: [
-    (story) => {
+    story => {
       return html`
         <div>
           ${story()}
