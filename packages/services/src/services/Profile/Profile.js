44feb25275407d660a451ce34d8d6ae6e03a156c
Anna Wen <54281166+annawen1@users.noreply.github.com>
2021-01-06 16:56:06 -0500

    fix(profile): use new endpoint for profile log in status

--- packages/services/src/services/Profile/Profile.js
+++ packages/services/src/services/Profile/Profile.js
@@ -1,18 +1,17 @@
 /**
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+import axios from 'axios';
 import jsonp from 'jsonp';
 
 /**
  * @constant {string | string} Host for the profile status API call
  * @private
  */
-const _host =
-  (process && process.env.PROFILE_HOST) || 'https://idaas.iam.ibm.com';
+const _host = (process && process.env.PROFILE_HOST) || 'https://login.ibm.com';
 
 /**
  * @constant {string | string} API version
@@ -44,12 +43,14 @@ class ProfileAPI {
    *   return response;
    * }
    */
-  static getUserStatus() {
-    const url = _endpoint;
-
-    return new Promise((resolve, reject) =>
-      jsonp(url, null, (err, data) => (err ? reject(err) : resolve(data)))
-    );
+  static async getUserStatus() {
+    return await axios
+      .get(_endpoint, {
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+      })
+      .then(response => response.data);
   }
 
   /**
