2bcdc44c197d4c2403b430cca3ad94ec8132c3a5
Jeff Chew <jeff.chew@gmail.com>
2021-01-13 08:58:04 -0500

    chore(images): multiple image fixes in react

--- packages/react/src/components/CardSectionImages/__stories__/CardSectionImages.stories.js
+++ packages/react/src/components/CardSectionImages/__stories__/CardSectionImages.stories.js
@@ -5,73 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import cards from '../../CardGroup/__stories__/data/cards';
 import CardSectionImages from '../CardSectionImages';
-import imgXlg16x9 from '../../../../../storybook-images/assets/1312/fpo--16x9--1312x738--005.jpg';
-import imgXlg1x1 from '../../../../../storybook-images/assets/1312/fpo--1x1--1312x1312--001.jpg';
-import imgXlg2x1 from '../../../../../storybook-images/assets/1312/fpo--2x1--1312x656--003.jpg';
-import imgXlg3x2 from '../../../../../storybook-images/assets/1312/fpo--3x2--874--004.jpg';
-import imgXlg4x3 from '../../../../../storybook-images/assets/1312/fpo--4x3--1312x984--002.jpg';
 import React from 'react';
 import readme from '../README.stories.mdx';
 import { text } from '@storybook/addon-knobs';
-
-const cards = [
-  {
-    image: {
-      defaultSrc: imgXlg1x1,
-      alt: 'Image alt text',
-    },
-    eyebrow: 'Topic',
-    heading: 'Natural language processing.',
-    cta: {
-      href: 'https://www.example.com',
-    },
-  },
-  {
-    image: {
-      defaultSrc: imgXlg4x3,
-      alt: 'Image alt text',
-    },
-    eyebrow: 'Blog',
-    heading: 'Natural language processing.',
-    cta: {
-      href: 'https://www.example.com',
-    },
-  },
-  {
-    image: {
-      defaultSrc: imgXlg2x1,
-      alt: 'Image alt text',
-    },
-    eyebrow: 'Topic',
-    heading: 'Natural language processing.',
-    cta: {
-      href: 'https://www.example.com',
-    },
-  },
-  {
-    image: {
-      defaultSrc: imgXlg3x2,
-      alt: 'Image alt text',
-    },
-    eyebrow: 'Blog',
-    heading: 'Serving society ethically in the age of Artificial Intelligence.',
-    cta: {
-      href: 'https://www.example.com',
-    },
-  },
-  {
-    image: {
-      defaultSrc: imgXlg16x9,
-      alt: 'Image alt text',
-    },
-    eyebrow: 'Topic',
-    heading: 'Serving society ethically in the age of Artificial Intelligence.',
-    cta: {
-      href: 'https://www.example.com',
-    },
-  },
-];
 
 export default {
   title: 'Components|CardSectionImages',
@@ -85,13 +23,13 @@ export default {
           'Aliquam condimentum interdum',
           groupId
         ),
-        cards: cards,
+        cards: cards.Images,
       }),
     },
     propsSet: {
       default: {
         CardSectionImages: {
-          cards: cards,
+          cards: cards.Images,
         },
       },
     },
