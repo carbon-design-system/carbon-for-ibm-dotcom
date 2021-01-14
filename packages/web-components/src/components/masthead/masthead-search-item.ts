ae69654b27eea62a7c90a30120e6c7af3812e7de
Ariella Gilmore <ariellalgilmore@gmail.com>
2021-01-14 13:44:48 -0800

    feat(Masthead): add aria labels (#4875)
    ### Related Ticket(s)
    
    #4774 
    
    ### Description
    
    add aria labels to masthead search suggestions. This allows for keyboard up and down arrows to read the search suggestions.
    
    ### Changelog
    
    **New**
    
    - aria-labels and override _navigate function
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/src/components/masthead/masthead-search-item.ts
+++ packages/web-components/src/components/masthead/masthead-search-item.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -77,7 +77,7 @@ class DDSMastheadSearchItem extends LitElement {
 
   updated(changedProperties) {
     super.updated(changedProperties);
-    if (changedProperties.has('highighted')) {
+    if (changedProperties.has('highlighted')) {
       this.setAttribute('aria-selected', String(Boolean(this.highlighted)));
     }
   }
