9e632e0339de20433eb95cab1ed4081b5577d605
IgnacioBecerra <i1becerr@ucsd.edu>
2021-01-19 07:59:55 -0800

    fix(content-section): added heading role

--- packages/web-components/src/components/content-section/content-section-heading.ts
+++ packages/web-components/src/components/content-section/content-section-heading.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -22,6 +22,13 @@ const { stablePrefix: ddsPrefix } = ddsSettings;
 class DDSContentSectionHeading extends LitElement {
   @property({ reflect: true })
   slot = 'heading';
+
+  connectedCallback() {
+    if (!this.hasAttribute('role')) {
+      this.setAttribute('role', 'heading');
+    }
+    super.connectedCallback();
+  }
 
   render() {
     return html`
