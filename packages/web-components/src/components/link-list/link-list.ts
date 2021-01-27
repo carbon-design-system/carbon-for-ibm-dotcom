52892813c22c6510af2745689f8051cdb743eef7
Raphael Amadeu <30945011+RaphaelAmadeu@users.noreply.github.com>
2021-01-27 14:30:41 -0300

    chore(docs): updated documentation

--- packages/web-components/src/components/link-list/link-list.ts
+++ packages/web-components/src/components/link-list/link-list.ts
@@ -82,10 +82,10 @@ class DDSLinkList extends StableSelectorMixin(LitElement) {
   /**
    * The link list type.
    * possible values are:
-   * default - Vertically stacked card-like links
-   * vertical - Vertically stacked inline links
-   * horizontal - Horizontaly stacked inline links
-   * end - End of section variant - Inline links stacked up to three columns based on the quantity of links
+   * default - Vertically stacked card-like links;
+   * vertical - Vertically stacked inline links;
+   * horizontal - Horizontaly stacked inline links;
+   * end - End of section variant - Inline links stacked up to three columns based on the quantity of links;
    */
   @property({ reflect: true })
   type = LINK_LIST_TYPE.DEFAULT;
