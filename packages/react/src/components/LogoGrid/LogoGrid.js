1e14abea14b6550ae2a2dc144341bf25f5edeecd
Anna Wen <54281166+annawen1@users.noreply.github.com>
2021-01-27 17:33:36 -0500

    fix(expressive): remove productive mixin from component styles and replace with expressive tokens (#4957)
    ### Related Ticket(s)
    
    Remove productive mixin & apply expressive tokens to Carbon for IBM.com components #4748
    
    ### Description
    
    Remove productive mixin from components (leave as is in the masthead) styles and replace with expressive tokens
    
    ### Changelog
    
    **Changed**
    
    - swap tokens with expressive tokens
    
    **Removed**
    
    - unused card video footer styles
    - edit the LogoGrid card props to set card heading as intended from the LogoGrid design specs (https://ibm.ent.box.com/file/661851053906)
    - productive token mixin use from components other than masthead
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/react/src/components/LogoGrid/LogoGrid.js
+++ packages/react/src/components/LogoGrid/LogoGrid.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2020
+ * Copyright IBM Corp. 2016, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -31,7 +31,7 @@ const LogoGrid = ({ heading, logosGroup, ctaCopy, ctaHref, hideBorder }) => {
     cta = {
       style: 'card',
       type: 'local',
-      copy: ctaCopy,
+      heading: ctaCopy,
       cta: {
         href: ctaHref,
       },
