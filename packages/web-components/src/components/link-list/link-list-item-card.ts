2def50b4dbc4024c5c3e19a46a3528d9b87047fc
Ariella Gilmore <ariellalgilmore@gmail.com>
2021-01-28 19:29:05 -0800

    Revert "fix(linkList): add aria attribute to item-card (#5010)"
    This reverts commit 09ea8d8a360668bb567d8b6aa5c51a433d90f109.

--- packages/web-components/src/components/link-list/link-list-item-card.ts
+++ packages/web-components/src/components/link-list/link-list-item-card.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2021
+ * Copyright IBM Corp. 2020
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -23,10 +23,7 @@ const { stablePrefix: ddsPrefix } = ddsSettings;
 class DDSLinkListItem extends DDSCardLink {
   connectedCallback() {
     if (!this.hasAttribute('role')) {
-      this.setAttribute('role', 'region');
-    }
-    if (this.innerText) {
-      this.setAttribute('aria-label', this.innerText);
+      this.setAttribute('role', 'listitem');
     }
     super.connectedCallback();
   }
