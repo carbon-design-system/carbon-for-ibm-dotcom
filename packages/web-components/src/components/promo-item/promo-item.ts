47e91062e04542cd6d84228c1de24130512619b7
proeung <putra.roeung@gmail.com>
2021-02-02 18:35:29 -0500

    feat(promo-group): add promo group and promo item components

---
+++ packages/web-components/src/components/promo-item/promo-item.ts
@@ -0,0 +1,59 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { customElement, html } from 'lit-element';
+import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings.js';
+import settings from 'carbon-components/es/globals/js/settings';
+import DDSContentItem from '../content-item/content-item';
+import styles from './promo-item.scss';
+import StableSelectorMixin from '../../globals/mixins/stable-selector';
+
+const { prefix } = settings;
+const { stablePrefix: ddsPrefix } = ddsSettings;
+
+/**
+ * Promo item.
+ *
+ * @element dds-promo-item
+ * @slot heading - The Card heading.
+ * @slot statistic - The statistic content.
+ * @slot pictogram - The pictogram content.
+ * @slot image - The Image content.
+ * @slot copy - The Content Item Copy.
+ * @slot footer - The link with icon content.
+ */
+@customElement(`${ddsPrefix}-promo-item`)
+class DDSPromoItem extends StableSelectorMixin(DDSContentItem) {
+  render() {
+    return html`
+      <div class="${prefix}--promo-item__heading">
+        <slot name="heading"></slot>
+      </div>
+      <div class="${prefix}--promo-item__media">
+        <slot class="${prefix}--promo-item__image" name="image" @slotchange="${this._handleSlotChange}"></slot>
+        <slot class="${prefix}--promo-item__statistic" name="statistic"></slot>
+        <slot class="${prefix}--promo-item__pictogram" name="pictogram"></slot>
+      </div>
+      <div class="${prefix}--promo-item__content">
+        <slot class="${prefix}--promo-item__copy" name="copy"></slot>
+      </div>
+      <div class="${prefix}--promo-item__footer">
+        <slot class="${prefix}--promo-item__cta" name="footer"></slot>
+      </div>
+    `;
+  }
+
+  static get stableSelector() {
+    return `${ddsPrefix}--promo-item`;
+  }
+
+  static styles = styles;
+}
+
+export default DDSPromoItem;
