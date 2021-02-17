25de2063d69ec26d1c7e2c2eb57878c87e0d592d
proeung <putra.roeung@gmail.com>
2021-02-17 10:20:13 -0500

    feat(promo-group): fix minor build issue

--- packages/web-components/src/components/promo-group/promo-group.ts
+++ packages/web-components/src/components/promo-group/promo-group.ts
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { customElement, html } from 'lit-element';
+import { html } from 'lit-element';
 import settings from 'carbon-components/es/globals/js/settings';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings.js';
 import StableSelectorMixin from '../../globals/mixins/stable-selector';
