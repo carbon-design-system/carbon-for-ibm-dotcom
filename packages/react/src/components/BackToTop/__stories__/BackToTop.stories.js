aed0e952cf3285dda9f2af3cda76e46d9ef274e0
Kenny Lam <kennylam@protonmail.com>
2021-03-02 11:40:42 -0500

    chore(storybook): add react story; update storyshots

---
+++ packages/react/src/components/BackToTop/__stories__/BackToTop.stories.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright IBM Corp. 2016, 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import readme from '../README.stories.mdx';
+
+export default {
+  title: 'Components|Carousel',
+
+  parameters: {
+    ...readme.parameters,
+    percy: {
+      skip: true,
+    },
+    proxy: true,
+  },
+};
+
+export const Default = () => {
+  return (
+    <p>
+      This component is maintined in{' '}
+      <code>@carbon/ibmdotcom-web-components</code> library with a{' '}
+      <a
+        className="bx--link"
+        target="_blank"
+        href="https://ibmdotcom-web-components-react.mybluemix.net/?path=/story/components-back-to-top--default">
+        React wrapper
+      </a>
+      .
+    </p>
+  );
+};
