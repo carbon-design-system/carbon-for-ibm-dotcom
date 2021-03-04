0acb5a25aa4cdfe0afc136aad38a7c5935ee9b7b
RaphaelAmadeu <raphael.s.amadeu@gmail.com>
2021-03-03 21:10:07 -0300

    feat(test-component): component created

--- packages/web-components/src/components/test-component/test-component.ts
+++ packages/web-components/src/components/test-component/test-component.ts
@@ -7,12 +7,18 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { html, LitElement } from 'lit-element';
+import { customElement, html, LitElement } from 'lit-element';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
 import StableSelectorMixin from '../../globals/mixins/stable-selector';
-const { stablePrefix: ddsPrefix } = ddsSettings;
 import { DDS_TEST_COMPONENT } from '../../globals/internal/feature-flags';
 
+const { stablePrefix: ddsPrefix } = ddsSettings;
+
+/**
+ * Test Component
+ *
+ */
+@customElement(`${ddsPrefix}-test-component`)
 class DDSTestComponent extends StableSelectorMixin(LitElement) {
   render() {
     return html`
---
+++ packages/web-components/tests/snapshots/dds-test-component.md
@@ -0,0 +1,15 @@
+# `dds-test-component`
+
+#### `renders dds-test-component properly`
+
+```
+<div>
+  <h1>
+    Test component
+  </h1>
+  <slot>
+  </slot>
+</div>
+
+```
+
