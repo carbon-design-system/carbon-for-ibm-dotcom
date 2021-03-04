c20f341ee50ccfb307650c1bdadf8a2e81f5d3d7
Ian Whitcomb <iwhitco@us.ibm.com>
2021-03-04 10:29:12 -0600

    fix(content-item-horizontal-media): Fixed test errors.

--- packages/web-components/src/components/content-item-horizontal/content-item-horizontal-media.ts
+++ packages/web-components/src/components/content-item-horizontal/content-item-horizontal-media.ts
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { customElement, html, internalProperty, property } from 'lit-element';
+import { customElement, html, property } from 'lit-element';
 import settings from 'carbon-components/es/globals/js/settings';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings.js';
 import DDSContentItem from '../content-item/content-item';
