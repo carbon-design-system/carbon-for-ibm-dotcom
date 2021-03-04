af4ecbac1232f5b36d03d5ae1aec3c1844eafc08
Ian Whitcomb <iwhitco@us.ibm.com>
2021-03-04 09:19:55 -0600

    feat(content-item-horizontal-media): Added content-item-horizontal-media component.

--- packages/web-components/src/components/content-item-horizontal/__stories__/content-item-horizontal.stories.ts
+++ packages/web-components/src/components/content-item-horizontal/__stories__/content-item-horizontal.stories.ts
@@ -9,21 +9,37 @@
 
 import { html } from 'lit-element';
 import ifNonNull from 'carbon-web-components/es/globals/directives/if-non-null.js';
-import { select } from '@storybook/addon-knobs';
+import { boolean, select } from '@storybook/addon-knobs';
 import readme from './README.stories.mdx';
 import textNullable from '../../../../.storybook/knob-text-nullable';
+import '../../image/image';
 import '../content-item-horizontal';
+import '../content-item-horizontal-media';
 import '../content-item-horizontal-copy';
 import '../content-item-horizontal-eyebrow';
 import '../../content-item/content-item-heading';
 import '../../link-list/link-list';
 import '../../cta/link-list-item-cta';
+import '../../video-player/video-player-container';
+
 import { ICON_PLACEMENT } from '../../link-with-icon/link-with-icon';
 import { CTA_TYPE } from '../../cta/defs';
+import { MEDIA_ALIGN, MEDIA_TYPE } from '../defs';
+import imgLg16x9 from '../../../../../storybook-images/assets/720/fpo--16x9--720x405--001.jpg';
 
 const types = {
   [`Local (${CTA_TYPE.LOCAL})`]: CTA_TYPE.LOCAL,
   [`External (${CTA_TYPE.EXTERNAL})`]: CTA_TYPE.EXTERNAL,
+};
+
+const media_align = {
+  [`Left`]: MEDIA_ALIGN.LEFT,
+  [`Right`]: MEDIA_ALIGN.RIGHT,
+};
+
+const media_type = {
+  [`Image`]: MEDIA_TYPE.IMAGE,
+  [`Video`]: MEDIA_TYPE.VIDEO,
 };
 
 export const Default = ({ parameters }) => {
@@ -54,6 +70,44 @@ export const Default = ({ parameters }) => {
   `;
 };
 
+export const WithMedia = ({ parameters }) => {
+  const { align, type, alt, eyebrow, heading, copy, ctaType1, ctaCopy1, href1, ctaType2, ctaCopy2, href2 } =
+  parameters?.props?.ContentItemHorizontal ?? {};
+  return html`
+    <dds-content-item-horizontal-media align="${align}">
+      ${type == MEDIA_TYPE.IMAGE
+        ? html`
+            <dds-image slot="media" alt="${ifNonNull(alt)}" default-src="${imgLg16x9}"></dds-image>
+          `
+        : null}
+      ${type == MEDIA_TYPE.VIDEO
+        ? html`
+            <dds-video-player-container slot="media" video-id="1_9h94wo6b"></dds-video-player-container>
+          `
+        : null}
+      <dds-content-item-horizontal-eyebrow>${eyebrow}</dds-content-item-horizontal-eyebrow>
+      <dds-content-item-heading>${heading}</dds-content-item-heading>
+      <dds-content-item-horizontal-copy>${copy}</dds-content-item-horizontal-copy>
+      <dds-link-list slot="footer" type="vertical">
+        <dds-link-list-item-cta
+          icon-placement="${ICON_PLACEMENT.RIGHT}"
+          href="${ifNonNull(href1)}"
+          cta-type="${ifNonNull(ctaType1)}"
+        >
+          ${ctaCopy1}
+        </dds-link-list-item-cta>
+        <dds-link-list-item-cta
+          icon-placement="${ICON_PLACEMENT.RIGHT}"
+          href="${ifNonNull(href2)}"
+          cta-type="${ifNonNull(ctaType2)}"
+        >
+          ${ctaCopy2}
+        </dds-link-list-item-cta>
+      </dds-link-list>
+    </dds-content-item-horizontal-media>
+  `;
+};
+
 export default {
   title: 'Components/Content Item Horizontal',
   decorators: [
@@ -69,6 +123,9 @@ export default {
     hasGrid: true,
     knobs: {
       ContentItemHorizontal: () => ({
+        align: select('Alignment', media_align, MEDIA_ALIGN.LEFT),
+        type: select('Media type', media_type, MEDIA_TYPE.IMAGE),
+        alt: textNullable('Image alt text', 'Lorem ipsum'),
         eyebrow: textNullable('Eyebrow (eyebrow):', 'Lorem ipsum'),
         heading: textNullable('Heading (heading):', 'Aliquam condimentum'),
         copy: textNullable(
