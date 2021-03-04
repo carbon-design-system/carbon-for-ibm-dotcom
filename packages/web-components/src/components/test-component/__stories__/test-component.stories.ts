42605ff7231ebe38e81deedb746c266498d9a2d6
RaphaelAmadeu <raphael.s.amadeu@gmail.com>
2021-03-04 10:24:41 -0300

    feat(test-component): component created

--- packages/web-components/src/components/test-component/__stories__/test-component.stories.ts
+++ packages/web-components/src/components/test-component/__stories__/test-component.stories.ts
@@ -1,3 +1,12 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020, 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { html } from 'lit-element';
 import readme from './README.stories.mdx';
 import '../test-component';
@@ -8,29 +17,29 @@ export const Default = () =>
     ? undefined
     : html`
         <dds-test-component>
-          <h2>Hey</h2>
+          <h1>Hello Summit!</h1>
         </dds-test-component>
       `;
 
 export default !DDS_TEST_COMPONENT
   ? undefined
   : {
-        title: 'Components/Test Component',
-        parameters: {
-          ...readme.parameters,
-          hasGrid: true,
-        },
-        decorators: [
-          story => {
-            return html`
-              <div class="bx--grid dds-ce-demo-devenv--grid--stretch">
-                <div class="bx--row">
-                  <div class="bx--col-sm-4 bx--offset-lg-4">
-                    ${story()}
-                  </div>
+      title: 'Components/Test Component',
+      parameters: {
+        ...readme.parameters,
+        hasGrid: true,
+      },
+      decorators: [
+        story => {
+          return html`
+            <div class="bx--grid dds-ce-demo-devenv--grid--stretch">
+              <div class="bx--row">
+                <div class="bx--col-sm-4 bx--offset-lg-4">
+                  ${story()}
                 </div>
               </div>
-            `;
-          },
-        ],
+            </div>
+          `;
+        },
+      ],
     };
--- packages/web-components/src/components/test-component/test-component.ts
+++ packages/web-components/src/components/test-component/test-component.ts
@@ -7,17 +7,15 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { html, LitElement } from 'lit-element';
+import { customElement, html, LitElement } from 'lit-element';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
 import StableSelectorMixin from '../../globals/mixins/stable-selector';
-const { stablePrefix: ddsPrefix } = ddsSettings;
 import { DDS_TEST_COMPONENT } from '../../globals/internal/feature-flags';
 
-class DDSTestComponent extends StableSelectorMixin(LitElement) {
-  firstUpdated() {
-    console.log('YOUr commit SHALL NOT PASS - Gandalf');
-  }
+const { stablePrefix: ddsPrefix } = ddsSettings;
 
+@customElement(`${ddsPrefix}-test-component`)
+class DDSTestComponent extends StableSelectorMixin(LitElement) {
   render() {
     return html`
       <div>
@@ -25,10 +23,6 @@ class DDSTestComponent extends StableSelectorMixin(LitElement) {
       </div>
     `;
   }
-}
-
-if (DDS_TEST_COMPONENT) {
-  customElements.define(`${ddsPrefix}-test-component`, DDSTestComponent);
 }
 
 export default !DDS_TEST_COMPONENT ? undefined : DDSTestComponent;
