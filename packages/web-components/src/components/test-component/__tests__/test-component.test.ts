494dbec62a4b47da8b3cbd26c1b3e35c3e307c52
ariellagilmore <ariellalgilmore@gmail.com>
2021-03-03 21:40:39 -0800

    fix(test-component): update tests

--- packages/web-components/src/components/test-component/__tests__/test-component.test.ts
+++ packages/web-components/src/components/test-component/__tests__/test-component.test.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -11,18 +11,17 @@ import { html, render } from 'lit-html';
 import '../test-component';
 
 const template = () => {
-    return html`
-      <dds-test-component>
-        <h2>Hey</h2>
-      </dds-test-component>
-    `;
-  };
+  return html`
+    <dds-test-component>
+      <h2>Hey</h2>
+    </dds-test-component>
+  `;
+};
 
 describe('dds-test-component', function() {
   it('renders dds-test-component properly', async function() {
     render(template(), document.body);
-    await Promise.resolve(); //Update cycle
-    console.log('dds-test-component', document.body.querySelector('dds-test-component'))
+    await Promise.resolve();
     expect(document.body.querySelector('dds-test-component')).toMatchSnapshot({ mode: 'shadow' });
   });
 
