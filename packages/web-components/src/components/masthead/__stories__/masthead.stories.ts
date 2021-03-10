d5d4177cad3ad8c533a0573e88bfe450ef44f90a
Ariella Gilmore <ariellalgilmore@gmail.com>
2021-03-09 16:50:27 -0800

    Update packages/web-components/src/components/masthead/__stories__/masthead.stories.ts
    Co-authored-by: Akira Sudoh <asudoh@gmail.com>

--- packages/web-components/src/components/masthead/__stories__/masthead.stories.ts
+++ packages/web-components/src/components/masthead/__stories__/masthead.stories.ts
@@ -152,8 +152,8 @@ export const withPlatform = ({ parameters }) => {
             searchPlaceholder="${ifNonNull(searchPlaceholder)}"
             .authenticatedProfileItems="${ifNonNull(authenticatedProfileItems)}"
             .navLinks="${navLinks}"
-            ?has-profile="${profile}"
-            ?has-search="${search}"
+            ?has-profile="${hasProfile}"
+            ?has-search="${hasSearch}"
             .unauthenticatedProfileItems="${ifNonNull(unauthenticatedProfileItems)}"
           ></dds-masthead-composite>
         `
