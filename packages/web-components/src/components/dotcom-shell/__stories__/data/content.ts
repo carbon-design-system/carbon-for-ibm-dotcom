8e2b232ac04e99f1e19648e9534eca15c5e99313
Ignacio Becerra <i1becerr@ucsd.edu>
2021-03-10 06:36:51 -0800

    fix(scroll-animations): align story and stick mobile ToC to the top (#5424)
    ### Related Ticket(s)
    #5161
    
    ### Description
    
    This PR addresses the Scroll Animations story being wrongly aligned, as well as making sure the ToC stays on top in mobile view.
    
    ### Changelog
    
    **New**
    
    - `<dds-table-of-contents>` mobile bar now sticks to the top of the Scroll Animations story
    
    **Changed**
    - now using grid for Scroll Animation story
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/src/components/dotcom-shell/__stories__/data/content.ts
+++ packages/web-components/src/components/dotcom-shell/__stories__/data/content.ts
@@ -92,169 +92,165 @@ const cardGroupItems = html`
 const StoryContent = () => html`
   <main class="bx--content dds-ce-demo-devenv--ui-shell-content">
     <div class="bx--grid">
-      <div class="bx--row">
-        <div class="bx--offset-lg-3 bx--col-lg-13">
-          <dds-table-of-contents stickyOffset="48">
-            <a name="1" data-title="Lorem ipsum dolor sit amet"></a>
-            <dds-leadspace-block>
-              <dds-leadspace-block-heading>Lorem ipsum dolor sit amet</dds-leadspace-block-heading>
-              <dds-leadspace-block-content>
-                <dds-content-block-heading>
-                  Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
-                </dds-content-block-heading>
-                <dds-content-block-copy slot="copy"
-                  >Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                  ut aliquip ex ea commodo consequat.
-                </dds-content-block-copy>
-                <dds-leadspace-block-media slot="media">
-                  <dds-video-player-container video-id="1_9h94wo6b"></dds-video-player-container>
-                </dds-leadspace-block-media>
-                <dds-link-list type="end">
-                <dds-link-list-heading>Featured products</dds-link-list-heading>
-                <dds-link-list-item href="https://example.com">
-                  IBM Cloud Continuous Delivery ${ArrowRight20({ slot: 'icon' })}
-                </dds-link-list-item>
-                <dds-link-list-item href="https://example.com">
-                  UrbanCode ${ArrowRight20({ slot: 'icon' })}
-                </dds-link-list-item>
-                <dds-link-list-item href="https://example.com">
-                  View all products ${ArrowRight20({ slot: 'icon' })}
-                </dds-link-list-item>
-              </dds-link-list>
-              <dds-leadspace-block-cta>
-                <dds-button-group-item href="www.ibm.com">Contact sales ${ArrowRight20({ slot: 'icon' })}</dds-button-group-item>
-              </dds-leadspace-block-cta>
-              </dds-leadspace-block-content>
-            </dds-leadspace-block>
+      <dds-table-of-contents stickyOffset="48">
+        <a name="1" data-title="Lorem ipsum dolor sit amet"></a>
+        <dds-leadspace-block>
+          <dds-leadspace-block-heading>Lorem ipsum dolor sit amet</dds-leadspace-block-heading>
+          <dds-leadspace-block-content>
+            <dds-content-block-heading>
+              Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
+            </dds-content-block-heading>
+            <dds-content-block-copy slot="copy"
+              >Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+              ut aliquip ex ea commodo consequat.
+            </dds-content-block-copy>
+            <dds-leadspace-block-media slot="media">
+              <dds-video-player-container video-id="1_9h94wo6b"></dds-video-player-container>
+            </dds-leadspace-block-media>
+            <dds-link-list type="end">
+            <dds-link-list-heading>Featured products</dds-link-list-heading>
+            <dds-link-list-item href="https://example.com">
+              IBM Cloud Continuous Delivery ${ArrowRight20({ slot: 'icon' })}
+            </dds-link-list-item>
+            <dds-link-list-item href="https://example.com">
+              UrbanCode ${ArrowRight20({ slot: 'icon' })}
+            </dds-link-list-item>
+            <dds-link-list-item href="https://example.com">
+              View all products ${ArrowRight20({ slot: 'icon' })}
+            </dds-link-list-item>
+          </dds-link-list>
+          <dds-leadspace-block-cta>
+            <dds-button-group-item href="www.ibm.com">Contact sales ${ArrowRight20({ slot: 'icon' })}</dds-button-group-item>
+          </dds-leadspace-block-cta>
+          </dds-leadspace-block-content>
+        </dds-leadspace-block>
 
-            <a name="2" data-title="Pharetra pharetra massa massa ultricies mi quis."></a>
-            <dds-content-block-segmented>
-              <dds-content-block-heading>Pharetra pharetra massa massa ultricies mi quis.</dds-content-block-heading>
-                ${Array.from([1, 2]).map(() => contentBlockSegmentedItems)}
-            </dds-content-block-segmented>
+        <a name="2" data-title="Pharetra pharetra massa massa ultricies mi quis."></a>
+        <dds-content-block-segmented>
+          <dds-content-block-heading>Pharetra pharetra massa massa ultricies mi quis.</dds-content-block-heading>
+            ${Array.from([1, 2]).map(() => contentBlockSegmentedItems)}
+        </dds-content-block-segmented>
 
-            <dds-feature-card-block-large href="https://example.com">
-              <dds-image slot="image" alt="Image alt text"
-                default-src="${imgLg1x1}"
-              >
-                </dds-image-item>
-                <dds-image-item
-                  media="(min-width: 991px)"
-                  srcset="${imgLg1x1}"
-                >
-              </dds-image>
-              <dds-card-eyebrow>scelerisque purus</dds-card-eyebrow>
-              <dds-card-heading>Elementum nibh tellus molestie nunc?</dds-card-heading>
-              <p>Habitant morbi tristique senectus et netus et malesuada fames. Habitant morbu tristique.</p>
-              <dds-feature-card-block-large-footer>
-                ${ArrowRight20({ slot: 'icon' })}
-              </dds-feature-card-block-large-footer>
-            </dds-feature-card-block-large>
+        <dds-feature-card-block-large href="https://example.com">
+          <dds-image slot="image" alt="Image alt text"
+            default-src="${imgLg1x1}"
+          >
+            </dds-image-item>
+            <dds-image-item
+              media="(min-width: 991px)"
+              srcset="${imgLg1x1}"
+            >
+          </dds-image>
+          <dds-card-eyebrow>scelerisque purus</dds-card-eyebrow>
+          <dds-card-heading>Elementum nibh tellus molestie nunc?</dds-card-heading>
+          <p>Habitant morbi tristique senectus et netus et malesuada fames. Habitant morbu tristique.</p>
+          <dds-feature-card-block-large-footer>
+            ${ArrowRight20({ slot: 'icon' })}
+          </dds-feature-card-block-large-footer>
+        </dds-feature-card-block-large>
 
-            <a name="3" data-title="Elementum nibh tellus molestie nunc non."></a>
-            <dds-content-block-segmented>
-              <dds-content-block-heading>Elementum nibh tellus molestie nunc non.
-              </dds-content-block-heading>
-                ${Array.from([1, 2]).map(() => contentBlockSegmentedItemsWithImage)}
-                <dds-card-cta slot="footer" cta-type="local" href="https://example.com">
-                  Lorem ipsum dolor
-                  <dds-card-cta-footer></dds-card-cta-footer>
-                </dds-card-cta>
-            </dds-content-block-segmented>
+        <a name="3" data-title="Elementum nibh tellus molestie nunc non."></a>
+        <dds-content-block-segmented>
+          <dds-content-block-heading>Elementum nibh tellus molestie nunc non.
+          </dds-content-block-heading>
+            ${Array.from([1, 2]).map(() => contentBlockSegmentedItemsWithImage)}
+            <dds-card-cta slot="footer" cta-type="local" href="https://example.com">
+              Lorem ipsum dolor
+              <dds-card-cta-footer></dds-card-cta-footer>
+            </dds-card-cta>
+        </dds-content-block-segmented>
 
-            <dds-callout-with-media>
-              <dds-content-block-heading slot="heading">Mauris ultrices eros in cursus</dds-content-block-heading>
-              <dds-content-item-copy
-                >Porttitor eget dolor morbi non arcu. Et ligula ullamcorper malesuada proin libero nunc consequat.
-                In est ante in nibh mauris cursus mattis. Turpis tincidunt id aliquet risus feugiat in.
-                Vel facilisis volutpat est velit egestas dui.
-              </dds-content-item-copy>
-              <dds-callout-with-media-video video-id="1_9h94wo6b"></dds-callout-with-media-video>
-            </dds-callout-with-media>
+        <dds-callout-with-media>
+          <dds-content-block-heading slot="heading">Mauris ultrices eros in cursus</dds-content-block-heading>
+          <dds-content-item-copy
+            >Porttitor eget dolor morbi non arcu. Et ligula ullamcorper malesuada proin libero nunc consequat.
+            In est ante in nibh mauris cursus mattis. Turpis tincidunt id aliquet risus feugiat in.
+            Vel facilisis volutpat est velit egestas dui.
+          </dds-content-item-copy>
+          <dds-callout-with-media-video video-id="1_9h94wo6b"></dds-callout-with-media-video>
+        </dds-callout-with-media>
 
-            <a name="4" data-title="Tincidunt ornare massa"></a>
-            <dds-content-group-horizontal>
-              <dds-content-block-heading>Tincidunt ornare massa</dds-content-block-heading>
-              ${Array.from([1, 2]).map(() => contentItemHorizontal)}
-            </dds-content-group-horizontal>
+        <a name="4" data-title="Tincidunt ornare massa"></a>
+        <dds-content-group-horizontal>
+          <dds-content-block-heading>Tincidunt ornare massa</dds-content-block-heading>
+          ${Array.from([1, 2]).map(() => contentItemHorizontal)}
+        </dds-content-group-horizontal>
 
-            <a name="5" data-title="Lobortis elementum nibh tellus"></a>
-            <dds-logo-grid ?hide-border="true">
-              <dds-content-block-heading>Lobortis elementum nibh tellus</dds-content-block-heading>
-              ${logosGroup &&
-                logosGroup.map(
-                  elem => html`
-                    <dds-logo-grid-item default-src="${elem.imgSrc}" alt="${elem.altText}"></dds-logo-grid-item>
-                  `
-                )}
-              <dds-logo-grid-link href="https://example.com">
-                <p>Amet justo donec</p>
-                ${ArrowRight20({ slot: 'footer' })}
-              </dds-logo-grid-link>
-            </dds-logo-grid>
+        <a name="5" data-title="Lobortis elementum nibh tellus"></a>
+        <dds-logo-grid ?hide-border="true">
+          <dds-content-block-heading>Lobortis elementum nibh tellus</dds-content-block-heading>
+          ${logosGroup &&
+            logosGroup.map(
+              elem => html`
+                <dds-logo-grid-item default-src="${elem.imgSrc}" alt="${elem.altText}"></dds-logo-grid-item>
+              `
+            )}
+          <dds-logo-grid-link href="https://example.com">
+            <p>Amet justo donec</p>
+            ${ArrowRight20({ slot: 'footer' })}
+          </dds-logo-grid-link>
+        </dds-logo-grid>
 
-            <a name="6" data-title="Aliquam condimentum interdum"></a>
-            <dds-content-block-cards>
-              <dds-content-block-heading>Aliquam condimentum interdum</dds-content-block-heading>
-              <dds-card-group>
-                ${Array.from([1, 2, 3]).map(() => cardGroupItems)}
-              </dds-card-group>
-            </dds-content-block-cards>
+        <a name="6" data-title="Aliquam condimentum interdum"></a>
+        <dds-content-block-cards>
+          <dds-content-block-heading>Aliquam condimentum interdum</dds-content-block-heading>
+          <dds-card-group>
+            ${Array.from([1, 2, 3]).map(() => cardGroupItems)}
+          </dds-card-group>
+        </dds-content-block-cards>
 
-            <dds-callout-quote>
-              Duis aute irure dolor in reprehenderit
-            <dds-quote-source-heading>
-              Lorem ipsum dolor sit amet
-            </dds-quote-source-heading>
-            <dds-quote-source-copy>
-              consectetur adipiscing elit
-            </dds-quote-source-copy>
-            <dds-quote-source-bottom-copy>
-              IBM Cloud
-            </dds-quote-source-bottom-copy>
-            <dds-link-with-icon slot="footer" href="https://example.com">
-              Link with Icon ${ArrowRight20({ slot: 'icon' })}
-            </dds-link-with-icon>
-          </dds-callout-quote>
+        <dds-callout-quote>
+          Duis aute irure dolor in reprehenderit
+        <dds-quote-source-heading>
+          Lorem ipsum dolor sit amet
+        </dds-quote-source-heading>
+        <dds-quote-source-copy>
+          consectetur adipiscing elit
+        </dds-quote-source-copy>
+        <dds-quote-source-bottom-copy>
+          IBM Cloud
+        </dds-quote-source-bottom-copy>
+        <dds-link-with-icon slot="footer" href="https://example.com">
+          Link with Icon ${ArrowRight20({ slot: 'icon' })}
+        </dds-link-with-icon>
+      </dds-callout-quote>
 
-            <a name="7" data-title="Duis aute irure dolor in reprehenderit"></a>
-            <dds-cta-section>
-              <dds-content-block-heading>Take the next step</dds-content-block-heading>
-              <dds-cta-section-copy>
-                Want to discuss your options with a DevOps expert? Contact our sales team to evaluate your needs.
-              </dds-cta-section-copy>
+        <a name="7" data-title="Duis aute irure dolor in reprehenderit"></a>
+        <dds-cta-section>
+          <dds-content-block-heading>Take the next step</dds-content-block-heading>
+          <dds-cta-section-copy>
+            Want to discuss your options with a DevOps expert? Contact our sales team to evaluate your needs.
+          </dds-cta-section-copy>
 
-              <dds-button-group slot="action">
-                <dds-button-group-item href="https://example.com">
-                  Secondary Button ${ArrowRight20({ slot: 'icon' })}
-                </dds-button-group-item>
-                <dds-button-group-item href="https://example.com">
-                  Primary button ${ArrowRight20({ slot: 'icon' })}
-                </dds-button-group-item>
-              </dds-button-group>
+          <dds-button-group slot="action">
+            <dds-button-group-item href="https://example.com">
+              Secondary Button ${ArrowRight20({ slot: 'icon' })}
+            </dds-button-group-item>
+            <dds-button-group-item href="https://example.com">
+              Primary button ${ArrowRight20({ slot: 'icon' })}
+            </dds-button-group-item>
+          </dds-button-group>
 
-              <dds-cta-section-item>
-                <dds-cta-section-item-heading>Get connected</dds-cta-section-item-heading>
-                <dds-cta-section-item-copy
-                  >IBM DevOps partners have a wide range of expertise. Find one to build the right solution for
-                  you.</dds-cta-section-item-copy
-                >
-                <dds-text-cta slot="footer" cta-type="local" icon-placement="right" href="example.com">
-                  Find a partner
-                </dds-text-cta>
-              </dds-cta-section-item>
+          <dds-cta-section-item>
+            <dds-cta-section-item-heading>Get connected</dds-cta-section-item-heading>
+            <dds-cta-section-item-copy
+              >IBM DevOps partners have a wide range of expertise. Find one to build the right solution for
+              you.</dds-cta-section-item-copy
+            >
+            <dds-text-cta slot="footer" cta-type="local" icon-placement="right" href="example.com">
+              Find a partner
+            </dds-text-cta>
+          </dds-cta-section-item>
 
-              <dds-cta-section-item>
-                <dds-cta-section-item-heading>Learn how</dds-cta-section-item-heading>
-                <dds-cta-section-item-copy>IBM DevOps partners have a wide range of expertise</dds-cta-section-item-copy>
-                <dds-text-cta slot="footer" cta-type="local" icon-placement="right" href="example.com">
-                  Browse tutorials
-                </dds-text-cta>
-              </dds-cta-section-item>
-            </dds-cta-section>
-          </dds-table-of-contents>
-        </div>
-      </div>
+          <dds-cta-section-item>
+            <dds-cta-section-item-heading>Learn how</dds-cta-section-item-heading>
+            <dds-cta-section-item-copy>IBM DevOps partners have a wide range of expertise</dds-cta-section-item-copy>
+            <dds-text-cta slot="footer" cta-type="local" icon-placement="right" href="example.com">
+              Browse tutorials
+            </dds-text-cta>
+          </dds-cta-section-item>
+        </dds-cta-section>
+      </dds-table-of-contents>
     </div>
   </main>
 `;
