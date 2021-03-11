ce7cab759593a872021e4ab186bca03747f79815
ariellagilmore <ariellalgilmore@gmail.com>
2021-03-11 07:42:50 -0800

    fix(card-group): add region role to card-group-item

--- packages/web-components/src/components/card-group/card-group-item.ts
+++ packages/web-components/src/components/card-group/card-group-item.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -21,6 +21,14 @@ const { stablePrefix: ddsPrefix } = ddsSettings;
  */
 @customElement(`${ddsPrefix}-card-group-item`)
 class DDSCardGroupItem extends DDSCardCTA {
+  connectedCallback() {
+    if (!this.hasAttribute('role')) {
+      this.setAttribute('role', 'region');
+    }
+
+    super.connectedCallback();
+  }
+
   static styles = styles;
 }
 
