0603f1415e0872bb521a158f6b0a93a8b83de2dd
proeung <putra.roeung@gmail.com>
2021-03-18 10:21:55 -0400

    fix(card-in-card): remove ifNonNull import since it’s no longer being used

--- packages/web-components/src/components/card-group/__stories__/card-group.stories.ts
+++ packages/web-components/src/components/card-group/__stories__/card-group.stories.ts
@@ -17,7 +17,6 @@ import '../card-group-item';
 import ArrowRight20 from 'carbon-web-components/es/icons/arrow--right/20';
 import { html } from 'lit-element';
 import { select, number } from '@storybook/addon-knobs';
-import ifNonNull from 'carbon-web-components/es/globals/directives/if-non-null.js';
 // eslint-disable-next-line sort-imports
 import imgXlg4x3 from '../../../../../storybook-images/assets/1312/fpo--4x3--1312x984--003.jpg';
 import imgXlg16x9 from '../../../../../storybook-images/assets/1312/fpo--16x9--1312x738--005.jpg';
