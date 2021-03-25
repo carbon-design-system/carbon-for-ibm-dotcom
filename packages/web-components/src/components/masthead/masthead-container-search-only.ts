cc3154c5d47fd38b35c2a3222b549e5fb2f04732
IgnacioBecerra <i1becerr@ucsd.edu>
2021-03-24 17:01:18 -0700

    fix(masthead): fixing ci check error

--- packages/web-components/src/components/masthead/masthead-container-search-only.ts
+++ packages/web-components/src/components/masthead/masthead-container-search-only.ts
@@ -11,19 +11,43 @@ import pickBy from 'lodash-es/pickBy.js';
 import { Dispatch, Store, bindActionCreators } from 'redux';
 import { customElement } from 'lit-element';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings.js';
+import { LocaleAPIState } from '../../internal/vendor/@carbon/ibmdotcom-services-store/types/localeAPI.d';
+import { TranslateAPIState } from '../../internal/vendor/@carbon/ibmdotcom-services-store/types/translateAPI.d';
+import { ProfileAPIState } from '../../internal/vendor/@carbon/ibmdotcom-services-store/types/profileAPI.d';
 import store from '../../internal/vendor/@carbon/ibmdotcom-services-store/store';
 import { loadSearchResults } from '../../internal/vendor/@carbon/ibmdotcom-services-store/actions/searchAPI';
 import { SearchAPIActions } from '../../internal/vendor/@carbon/ibmdotcom-services-store/actions/searchAPI.d';
 import ConnectMixin from '../../globals/mixins/connect';
+import { MastheadSearchContainerState } from './masthead-search-container';
 import DDSMastheadComposite from './masthead-composite';
 
 const { stablePrefix: ddsPrefix } = ddsSettings;
 
 /**
+ * The Redux state used for `<dds-masthead-container>`.
+ */
+export interface MastheadContainerState extends MastheadSearchContainerState {
+  /**
+   * The Redux state for `LocaleAPI`.
+   */
+  localeAPI?: LocaleAPIState;
+
+  /**
+   * The Redux state for `TranslateAPI`.
+   */
+  translateAPI?: TranslateAPIState;
+
+  /**
+   * The Redux state for `ProfileAPI`.
+   */
+  profileAPI?: ProfileAPIState;
+}
+
+/**
  * @param state The Redux state for masthead.
  * @returns The converted version of the given state, tailored for `<dds-masthead-container>`.
  */
-export function mapStateToProps(state) {
+export function mapStateToProps(state: MastheadContainerState) {
   const { localeAPI, searchAPI } = state;
   const { language } = localeAPI ?? {};
   const { currentSearchQueryString, searchResults } = searchAPI ?? {};
@@ -58,8 +82,8 @@ export function mapDispatchToProps(dispatch: Dispatch<SearchAPIActions>) {
  * @element dds-masthead-container-with-search
  */
 @customElement(`${ddsPrefix}-masthead-container-search-only`)
-class DDSMastheadContainerSearchOnly extends ConnectMixin<SearchAPIActions>(
-  store as Store<SearchAPIActions>,
+class DDSMastheadContainerSearchOnly extends ConnectMixin<MastheadContainerState, SearchAPIActions>(
+  store as Store<MastheadContainerState, SearchAPIActions>,
   mapStateToProps,
   mapDispatchToProps
 )(DDSMastheadComposite) {}
