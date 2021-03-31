bf6e6684bd2a383dd2a0597078be67f06bf9f623
Hunter MacDermut <whosebluesanyway@gmail.com>
2021-03-31 12:05:24 -0400

    feat(cloud-masthead): create separate story, move/wire-up cookie logic

--- packages/web-components/src/components/masthead/cloud/cloud-masthead-container.ts
+++ packages/web-components/src/components/masthead/cloud/cloud-masthead-container.ts
@@ -6,8 +6,11 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import pickBy from 'lodash-es/pickBy.js';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings.js';
 import { ActionCreatorsMapObject, Dispatch, Store, bindActionCreators } from 'redux';
+import { customElement } from 'lit-element';
+import Cookies from 'js-cookie';
 import store from '../../../internal/vendor/@carbon/ibmdotcom-services-store/store';
 import { LocaleAPIActions } from '../../../internal/vendor/@carbon/ibmdotcom-services-store/actions/localeAPI.d';
 import { TranslateAPIActions } from '../../../internal/vendor/@carbon/ibmdotcom-services-store/actions/translateAPI.d';
@@ -16,16 +19,48 @@ import ConnectMixin from '../../../globals/mixins/connect';
 import { loadTranslation } from '../../../internal/vendor/@carbon/ibmdotcom-services-store/actions/translateAPI';
 import { loadLanguage, setLanguage } from '../../../internal/vendor/@carbon/ibmdotcom-services-store/actions/localeAPI';
 import { loadSearchResults } from '../../../internal/vendor/@carbon/ibmdotcom-services-store/actions/searchAPI';
-import {
-  MastheadContainerState,
-  MastheadContainerStateProps,
-  MastheadContainerActions,
-  mapStateToProps,
-} from '../masthead-container';
+import { MastheadContainerState, MastheadContainerStateProps, MastheadContainerActions } from '../masthead-container';
 import DDSCloudMastheadComposite from './cloud-masthead-composite';
-import { DDS_CLOUD_MASTHEAD } from '../../../globals/internal/feature-flags';
 
 const { stablePrefix: ddsPrefix } = ddsSettings;
+
+/**
+ * The cookie name for determining user login status for cloud.ibm.com.
+ */
+const loginStatusCookieName = 'com.ibm.cloud.iam.LoggedIn.manual';
+
+/**
+ * Returns user login status (authenticated or anonymous)
+ */
+function getUserLoginStatus() {
+  return Cookies.get(loginStatusCookieName) === '1' ? 'authenticated' : 'anonymous';
+}
+
+/**
+ * @param state The Redux state for masthead.
+ * @returns The converted version of the given state, tailored for `<dds-masthead-container>`.
+ */
+export function mapStateToProps(state: MastheadContainerState): MastheadContainerStateProps {
+  const { localeAPI, translateAPI, searchAPI } = state;
+  const { language } = localeAPI ?? {};
+  const { translations } = translateAPI ?? {};
+  const { currentSearchQueryString, searchResults } = searchAPI ?? {};
+  let currentSearchResults;
+  for (let { length = 0 } = currentSearchQueryString ?? {}; !currentSearchResults && length > 0; --length) {
+    currentSearchResults = searchResults?.[currentSearchQueryString!.substr(0, length)]?.[language!];
+  }
+  return pickBy(
+    {
+      authenticatedProfileItems: !language ? undefined : translations?.[language]?.profileMenu.signedin,
+      navLinks: !language ? undefined : translations?.[language]?.mastheadNav?.links,
+      unauthenticatedProfileItems: !language ? undefined : translations?.[language]?.profileMenu.signedout,
+      userStatus: getUserLoginStatus(),
+      currentSearchResults: currentSearchResults ?? [],
+    },
+    value => value !== undefined
+  );
+}
+
 /**
  * @param dispatch The Redux `dispatch()` API.
  * @returns The methods in `<dds-cloud-masthead-container>` to dispatch Redux actions.
@@ -42,10 +77,11 @@ export function mapDispatchToProps(dispatch: Dispatch<LocaleAPIActions | Transla
   );
 }
 /**
- * EXPERIMENTAL: Container component for Cloud masthead.
+ * Container component for Cloud masthead.
  *
  * @element dds-cloud-masthead-container
  */
+@customElement(`${ddsPrefix}-cloud-masthead-container`)
 class DDSCloudMastheadContainer extends ConnectMixin<
   MastheadContainerState,
   LocaleAPIActions | TranslateAPIActions | SearchAPIActions,
@@ -58,8 +94,4 @@ class DDSCloudMastheadContainer extends ConnectMixin<
   // @ts-ignore: Behind feature flag
 )(DDSCloudMastheadComposite) {}
 
-if (DDS_CLOUD_MASTHEAD) {
-  customElements.define(`${ddsPrefix}-cloud-masthead-container`, DDSCloudMastheadContainer);
-}
-
-export default !DDS_CLOUD_MASTHEAD ? undefined : DDSCloudMastheadContainer;
+export default DDSCloudMastheadContainer;
