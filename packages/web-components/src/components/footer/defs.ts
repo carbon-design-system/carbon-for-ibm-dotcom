806ed1045625767571eaeb2c848042e0ec7133b0
ariellagilmore <ariellalgilmore@gmail.com>
2021-04-07 15:42:05 -0700

    Revert "fix(card-in-card): accidental merge"
    This reverts commit b64ba281

--- packages/web-components/src/components/footer/defs.ts
+++ packages/web-components/src/components/footer/defs.ts
@@ -7,8 +7,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export { FORM_ELEMENT_COLOR_SCHEME as DROPDOWN_COLOR_SCHEME } from 'carbon-web-components/es/globals/shared-enums';
-
 /**
  * Footer size.
  */
@@ -29,8 +27,6 @@ export enum FOOTER_SIZE {
   MICRO = 'micro',
 }
 
-// TODO: Remove when JAWS bug has been fixed
-
 /**
  * The style scheme of language selector.
  */
@@ -45,74 +41,4 @@ export enum LANGUAGE_SELECTOR_STYLE_SCHEME {
    * The primary use case is for used in micro footer.
    */
   BLENDED = 'blended',
-}
-
-/**
- * Navigation direction, associated with key symbols.
- */
-export const NAVIGATION_DIRECTION = {
-  Up: -1,
-  ArrowUp: -1,
-  Down: 1,
-  ArrowDown: 1,
-};
-
-/**
- * The keyboard action categories for dropdown.
- */
-export enum DROPDOWN_KEYBOARD_ACTION {
-  /**
-   * Not doing any action.
-   */
-  NONE = 'none',
-
-  /**
-   * Keyboard action to close menu.
-   */
-  CLOSING = 'closing',
-
-  /**
-   * Keyboard action to navigate back/forward.
-   */
-  NAVIGATING = 'navigating',
-
-  /**
-   * Keyboard action to open/close menu on the trigger button or select/deselect a menu item.
-   */
-  TRIGGERING = 'triggering',
-}
-
-/**
- * Dropdown size.
- */
-export enum DROPDOWN_SIZE {
-  /**
-   * Regular size.
-   */
-  REGULAR = '',
-
-  /**
-   * Small size.
-   */
-  SMALL = 'sm',
-
-  /**
-   * Extra large size.
-   */
-  EXTRA_LARGE = 'xl',
-}
-
-/**
- * Dropdown types.
- */
-export enum DROPDOWN_TYPE {
-  /**
-   * Regular type.
-   */
-  REGULAR = '',
-
-  /**
-   * Inline type.
-   */
-  INLINE = 'inline',
 }
