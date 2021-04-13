9e10abb5b82176a45d40cad78241970d7aecd48f
Jeffrey Chew <jeff.chew@gmail.com>
2021-04-13 15:52:46 -0400

    fix(percy): adding space for the placeholder in percy

--- packages/web-components/src/components/dotcom-shell/__stories__/dotcom-shell.stories.ts
+++ packages/web-components/src/components/dotcom-shell/__stories__/dotcom-shell.stories.ts
@@ -692,7 +692,7 @@ export default {
         platform: select('Platform (platform)', { none: null, platform: platformData.name }, null, groupId),
         hasProfile: boolean('show the profile functionality (profile)', true, groupId),
         hasSearch: boolean('show the search functionality (search)', true, groupId),
-        searchPlaceholder: textNullable('search placeholder (searchPlaceholder)', inPercy() ? '' : 'Search all of IBM', groupId),
+        searchPlaceholder: textNullable('search placeholder (searchPlaceholder)', inPercy() ? ' ' : 'Search all of IBM', groupId),
         selectedMenuItem: textNullable('selected menu item (selected-menu-item)', 'Services & Consulting', groupId),
         userStatus: select('The user authenticated status (user-status)', userStatuses, userStatuses.unauthenticated, groupId),
       }),
