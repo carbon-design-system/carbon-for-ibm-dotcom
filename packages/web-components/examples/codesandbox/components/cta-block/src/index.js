e714d243eed8fad69d87bad646a2bd2dba0ee48e
Raphael Amadeu <30945011+RaphaelAmadeu@users.noreply.github.com>
2021-04-13 22:42:47 -0300

    feat(cta-section): created component (#5767)
    ### Related Ticket(s)
    
    #5218 
    
    ### Description
    
    Created `DDSCtaBlock` and changed `CTASection` to match the new specs.
    
    <img width="1435" alt="image" src="https://user-images.githubusercontent.com/30945011/114500448-64cbac00-9bfe-11eb-9ade-0f77e316ef70.png">

--- packages/web-components/examples/codesandbox/components/cta-block/src/index.js
+++ packages/web-components/examples/codesandbox/components/cta-block/src/index.js
@@ -8,16 +8,13 @@
  */
 
 import '@carbon/ibmdotcom-web-components/es/components/button-group/button-group-item.js';
-import '@carbon/ibmdotcom-web-components/es/components/content-item/content-item-copy.js';
-import '@carbon/ibmdotcom-web-components/es/components/content-item/content-item-heading.js';
 import '@carbon/ibmdotcom-web-components/es/components/content-block/content-block-heading.js';
 import '@carbon/ibmdotcom-web-components/es/components/content-block/content-block-copy.js';
-import '@carbon/ibmdotcom-web-components/es/components/cta/link-list-item-cta.js';
+import '@carbon/ibmdotcom-web-components/es/components/content-item/content-item-copy.js';
+import '@carbon/ibmdotcom-web-components/es/components/content-item/content-item-heading.js';
 import '@carbon/ibmdotcom-web-components/es/components/cta-block/cta-block.js';
 import '@carbon/ibmdotcom-web-components/es/components/cta-block/cta-block-item-row.js';
 import '@carbon/ibmdotcom-web-components/es/components/cta-block/cta-block-item.js';
-import '@carbon/ibmdotcom-web-components/es/components/cta-section/cta-section.js';
 import '@carbon/ibmdotcom-web-components/es/components/link-list/link-list.js';
 import '@carbon/ibmdotcom-web-components/es/components/link-list/link-list-heading.js';
 import '@carbon/ibmdotcom-web-components/es/components/link-list/link-list-item.js';
-import '@carbon/ibmdotcom-web-components/es/components/video-player/video-player-container.js';
---
+++ packages/web-components/src/components/cta-section/__stories__/content.ts
@@ -0,0 +1,81 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020, 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const content = [
+  {
+    heading: 'Lorem ipsum dolor sit amet',
+    copy: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    links: [
+      {
+        href: 'https://example.com',
+        copy: 'Lorem ipsum',
+      },
+    ],
+  },
+  {
+    heading: 'liquet nec ullamcorper sit amet risus',
+    copy: 'Sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus.',
+    links: [
+      {
+        href: 'https://example.com',
+        copy: 'Lorem ipsum',
+      },
+      {
+        href: 'https://example.com',
+        copy: 'Lorem ipsum',
+      },
+    ],
+  },
+  {
+    heading: 'Amet consectetur adipiscing elit ut aliquam purus',
+    copy: 'Faucibus purus in massa tempor. Nibh ipsum consequat nisl vel pretium lectus.',
+    links: [
+      {
+        href: 'https://example.com',
+        copy: 'Lorem ipsum',
+      },
+    ],
+  },
+  {
+    heading: 'In hac habitasse platea dictumst quisque sagittis',
+    copy: 'Erat imperdiet sed euismod nisi porta. Amet venenatis urna cursus.s',
+    links: [
+      {
+        href: 'https://example.com',
+        copy: 'Lorem ipsum',
+      },
+    ],
+  },
+  {
+    heading: 'Tellus integer feugiat scelerisque varius morbi',
+    copy: 'Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse.',
+    links: [
+      {
+        href: 'https://example.com',
+        copy: 'Lorem ipsum',
+      },
+    ],
+  },
+  {
+    heading: 'Risus quis varius quam quisque id',
+    copy: 'Eu feugiat pretium nibh ipsum consequat nisl.',
+    links: [
+      {
+        href: 'https://example.com',
+        copy: 'Lorem ipsum',
+      },
+      {
+        href: 'https://example.com',
+        copy: 'Lorem ipsum',
+      },
+    ],
+  },
+];
+
+export default content;
