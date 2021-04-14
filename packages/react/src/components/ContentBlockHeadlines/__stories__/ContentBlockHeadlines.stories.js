1cfac1ce330b4173e566ec4b10b8965e0f4aeb58
Julio Marroquín <juliomarroquin@gmail.com>
2021-04-14 11:00:13 -0600

    fix: react corrections

--- packages/react/src/components/ContentBlockHeadlines/__stories__/ContentBlockHeadlines.stories.js
+++ packages/react/src/components/ContentBlockHeadlines/__stories__/ContentBlockHeadlines.stories.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2020
+ * Copyright IBM Corp. 2016, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -15,7 +15,7 @@ import { text } from '@storybook/addon-knobs';
 export default !DDS_CONTENTBLOCK_HEADLINES
   ? undefined
   : {
-      title: 'Components|ContentBlockHeadlines',
+      title: 'Components|Content block headlines',
 
       parameters: {
         ...readme.parameters,
