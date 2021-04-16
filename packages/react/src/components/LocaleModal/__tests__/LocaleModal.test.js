4296f67a451caa30a262a284e456b3ff3a4383a3
ariellagilmore <ariellalgilmore@gmail.com>
2021-04-15 19:58:11 -0600

    fix(localeModal): update tests

--- packages/react/src/components/LocaleModal/__tests__/LocaleModal.test.js
+++ packages/react/src/components/LocaleModal/__tests__/LocaleModal.test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2020
+ * Copyright IBM Corp. 2016, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -117,7 +117,7 @@ describe('Locale Modal Regions', () => {
       />
     );
     wrapper
-      .find('.bx--tile--clickable')
+      .find('.bx--tile')
       .at(0)
       .simulate('click');
     const testButton = document.createElement('button');
