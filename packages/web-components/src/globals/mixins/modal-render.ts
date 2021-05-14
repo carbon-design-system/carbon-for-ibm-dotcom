c870bfdb20533e9e423d2a57dd74a7897e1b5fba
Jeff Chew <jeff.chew@gmail.com>
2021-05-14 17:34:44 -0400

    fix(footer): lazy load the locale modal when triggered

--- packages/web-components/src/globals/mixins/modal-render.ts
+++ packages/web-components/src/globals/mixins/modal-render.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -60,7 +60,7 @@ const ModalRenderMixin = <T extends Constructor<HTMLElement>>(Base: T) => {
     /**
      * @returns The template of the modal.
      */
-    abstract renderModal(): TemplateResult | void;
+    abstract renderModal(): TemplateResult | null | void;
 
     /**
      * The render target of the modal.
