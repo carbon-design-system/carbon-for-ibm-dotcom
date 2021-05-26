3cb622359912ec8bbbaeac22a81d55206240d815
Julio Marroquín <juliomarroquin@gmail.com>
2021-05-26 11:22:40 -0600

    fix(card-group): unused item

--- packages/web-components/src/components/card-group/__stories__/card-group.stories.ts
+++ packages/web-components/src/components/card-group/__stories__/card-group.stories.ts
@@ -272,7 +272,7 @@ export default {
           length: number('Number of cards', 5, {}, groupId),
         })
           .slice(0, -1)
-          .map(index => cardsDiffLengthPhrase(index)),
+          .map((_item, index) => cardsDiffLengthPhrase(index)),
         optionalBorder: boolean('Outlined cards:', false, groupId),
       }),
     },
