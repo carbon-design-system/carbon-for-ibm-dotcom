f29435dbcdbde3cff0926e53b0e46a5a07db3fba
Ignacio Becerra <i1becerr@ucsd.edu>
2021-06-02 07:30:50 -0700

    chore(feature-card): refactored block large and medium into component (#5950)
    ### Related Ticket(s)
    #5416
    
    ### Description
    This PR deprecates Feature Card Block Medium and Large, and adds a Large variant to the regular FeatureCard component for both React and Web Components.
    
    ### Changelog
    
    **New**
    
    - new `large` variant for FeatureCard that replaces `FeatureCardBlockLarge`
    
    **Removed**
    
    - `FeatureCardBlockMedium` and `FeatureCardBlockLarge` are now deprecated
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/react/src/components/FeatureCard/__stories__/FeatureCard.stories.js
+++ packages/react/src/components/FeatureCard/__stories__/FeatureCard.stories.js
@@ -7,7 +7,12 @@
 
 import ArrowRight20 from '@carbon/icons-react/es/arrow--right/20';
 import FeatureCard from '../FeatureCard';
-import imgLg1x1 from '../../../../../storybook-images/assets/720/fpo--1x1--720x720--003.jpg';
+import imgLg1x1 from '../../../../../storybook-images/assets/720/fpo--1x1--720x720--005.jpg';
+import imgMax1x1 from '../../../../../storybook-images/assets/1584/fpo--1x1--1584x1584--005.jpg';
+import imgMd2x1 from '../../../../../storybook-images/assets/480/fpo--2x1--480x240--005.jpg';
+import imgSm2x1 from '../../../../../storybook-images/assets/320/fpo--2x1--320x160--005.jpg';
+import imgXlg2x1 from '../../../../../storybook-images/assets/1312/fpo--2x1--1312x656--005.jpg';
+import mediumImgLg1x1 from '../../../../../storybook-images/assets/720/fpo--1x1--720x720--003.jpg';
 import React from 'react';
 import readme from '../README.stories.mdx';
 import { text } from '@storybook/addon-knobs';
@@ -15,6 +20,29 @@ import { text } from '@storybook/addon-knobs';
 export default {
   title: 'Components|Feature card',
 
+  parameters: {
+    ...readme.parameters,
+  },
+};
+
+export const Default = ({ parameters }) => {
+  const { card } = parameters?.props?.FeatureCard ?? {};
+
+  return (
+    <div className="bx--grid">
+      <div className="bx--row">
+        <div
+          style={{ paddingTop: '20px' }}
+          className="bx--col-sm-4 bx--col-lg-8 bx--offset-lg-4">
+          <FeatureCard card={card} size={'medium'} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Default.story = {
+  name: 'Medium',
   parameters: {
     ...readme.parameters,
     knobs: {
@@ -26,7 +54,7 @@ export default {
             groupId
           ),
           image: {
-            defaultSrc: imgLg1x1,
+            defaultSrc: mediumImgLg1x1,
             alt: 'Image alt text',
           },
           cta: {
@@ -42,29 +70,10 @@ export default {
         },
       }),
     },
-    propsSet: {
-      default: {
-        FeatureCard: {
-          card: {
-            heading: 'Explore AI use cases in all industries',
-            image: {
-              defaultSrc: imgLg1x1,
-              alt: 'Image alt text',
-            },
-            cta: {
-              href: 'https://www.example.com',
-              icon: {
-                src: ArrowRight20,
-              },
-            },
-          },
-        },
-      },
-    },
   },
 };
 
-export const Default = ({ parameters }) => {
+export const Large = ({ parameters }) => {
   const { card } = parameters?.props?.FeatureCard ?? {};
 
   return (
@@ -72,10 +81,73 @@ export const Default = ({ parameters }) => {
       <div className="bx--row">
         <div
           style={{ paddingTop: '20px' }}
-          className="bx--col-sm-4 bx--col-lg-8 bx--offset-lg-4">
-          <FeatureCard card={card} />
+          className="bx--col-sm-4 bx--col-lg-12 bx--offset-lg-2">
+          <FeatureCard card={card} size={'large'} />
         </div>
       </div>
     </div>
   );
+};
+
+Large.story = {
+  parameters: {
+    ...readme.parameters,
+    knobs: {
+      FeatureCard: ({ groupId }) => ({
+        card: {
+          eyebrow: text(
+            'Card Eyebrow (required) (eyebrow):',
+            'This is an eyebrow',
+            groupId
+          ),
+          heading: text(
+            'Card Heading (heading):',
+            'Explore AI use cases in all industries',
+            groupId
+          ),
+          copy: text(
+            'Card copy (copy):',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            groupId
+          ),
+          cta: {
+            href: text(
+              'Card href (cta.href):',
+              'https://www.example.com',
+              groupId
+            ),
+            icon: {
+              src: ArrowRight20,
+            },
+          },
+          image: {
+            sources: [
+              {
+                src: imgSm2x1,
+                breakpoint: 'sm',
+              },
+              {
+                src: imgMd2x1,
+                breakpoint: 'md',
+              },
+              {
+                src: imgLg1x1,
+                breakpoint: 991,
+              },
+              {
+                src: imgXlg2x1,
+                breakpoint: 'lg',
+              },
+              {
+                src: imgMax1x1,
+                breakpoint: 'xlg',
+              },
+            ],
+            defaultSrc: imgXlg2x1,
+            alt: 'Image alt text',
+          },
+        },
+      }),
+    },
+  },
 };
