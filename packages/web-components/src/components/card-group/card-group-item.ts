edd00b98e671d760990f08def0d833a1c0627cfc
Ignacio Becerra <i1becerr@ucsd.edu>
2021-06-22 12:19:23 -0700

    chore(stable-selectors): add missing selectors (#6434)
    ### Related Ticket(s)
    None.
    
    ### Description
    This PR adds the stable selector to any of our components that were missing it.
    
    ### Changelog
    
    **New**
    
    - added missing stable selector to many components
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/src/components/card-group/card-group-item.ts
+++ packages/web-components/src/components/card-group/card-group-item.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -21,6 +21,10 @@ const { stablePrefix: ddsPrefix } = ddsSettings;
  */
 @customElement(`${ddsPrefix}-card-group-item`)
 class DDSCardGroupItem extends DDSCardCTA {
+  static get stableSelector() {
+    return `${ddsPrefix}--card-group-item`;
+  }
+
   static styles = styles;
 }
 
