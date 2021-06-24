882ae6dfccbd20588e9b15a84d758c4e44c86f76
Hunter MacDermut <hmacdermut@caktusgroup.com>
2021-06-24 11:13:52 -0600

    feat(cloud-masthead): hc-1993 adjust spacing to resolve lint error

--- packages/services/src/services/CloudAccountAuth/index.js
+++ packages/services/src/services/CloudAccountAuth/index.js
@@ -5,4 +5,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
- export { default as CloudAccountAuthAPI } from './CloudAccountAuth';
+export { default as CloudAccountAuthAPI } from './CloudAccountAuth';
