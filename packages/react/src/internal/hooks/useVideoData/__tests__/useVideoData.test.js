4d6129a5084fb4c15158b6220a28308b4787ffd2
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2021-07-23 16:27:17 -0300

    chore(KalturaPlayerAPI): updating `videoPlayerAPI` references

--- packages/react/src/internal/hooks/useVideoData/__tests__/useVideoData.test.js
+++ packages/react/src/internal/hooks/useVideoData/__tests__/useVideoData.test.js
@@ -1,19 +1,19 @@
 /**
- * Copyright IBM Corp. 2016, 2020
+ * Copyright IBM Corp. 2016, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import KalturaPlayerAPI from '@carbon/ibmdotcom-services/es/services/KalturaPlayer/KalturaPlayer';
 import { mount } from 'enzyme';
 import React from 'react';
 import useVideoData from '../useVideoData';
-import VideoPlayerAPI from '@carbon/ibmdotcom-services/es/services/VideoPlayer/VideoPlayer';
 
 jest.mock(
   '@carbon/ibmdotcom-services/lib/services/VideoPlayer/VideoPlayer',
   () => ({
     api: jest.fn(() => Promise.resolve({ msDuration: 10000 })),
-    getVideoDuration: jest.fn(() => '1:30'),
+    getMediaDuration: jest.fn(() => '1:30'),
   })
 );
 
@@ -41,8 +41,8 @@ describe('useVideoData hook', () => {
     mount(<TestComponent />);
     return returnVal;
   }
-  it('should call the VideoPlayerAPI', async () => {
+  it('should call the KalturaPlayerAPI', async () => {
     setup(type, videoId);
-    expect(VideoPlayerAPI.api).toHaveBeenCalled();
+    expect(KalturaPlayerAPI.api).toHaveBeenCalled();
   });
 });
