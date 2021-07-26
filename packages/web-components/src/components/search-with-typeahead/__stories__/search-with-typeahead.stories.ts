b5090ea2b9923db63648d485dbea1130ad4d4412
IgnacioBecerra <i1becerr@ucsd.edu>
2021-07-26 14:24:28 -0700

    feat(search-with-typeahead): introducing component

---
+++ packages/web-components/src/components/search-with-typeahead/__stories__/search-with-typeahead.stories.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020, 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { html } from 'lit-element';
+import '../search-with-typeahead';
+import readme from './README.stories.mdx';
+
+export const Default = () => html`
+  <dds-search-with-typeahead should-remain-open search-open-on-load> </dds-search-with-typeahead>
+`;
+
+export default {
+  title: 'Components/Search with typeahead',
+  parameters: {
+    ...readme.parameters,
+  },
+};
