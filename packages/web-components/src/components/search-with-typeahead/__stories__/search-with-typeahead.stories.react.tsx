085a4864b359d91729342282e77f97a95fa4e8cc
IgnacioBecerra <i1becerr@ucsd.edu>
2021-07-27 17:03:45 -0700

    chore(search-with-typeahead): fix wrapper story

--- packages/web-components/src/components/search-with-typeahead/__stories__/search-with-typeahead.stories.react.tsx
+++ packages/web-components/src/components/search-with-typeahead/__stories__/search-with-typeahead.stories.react.tsx
@@ -9,16 +9,20 @@
 import React from 'react';
 // Below path will be there when an application installs `@carbon/ibmdotcom-web-components` package.
 // In our dev env, we auto-generate the file and re-map below path to to point to the generated file.
+/* eslint-disable max-len */
 // @ts-ignore
-import DDSSearchWithTypeahead from '@carbon/ibmdotcom-web-components/es/components-react/search-with-typeahead/index';
+import DDSSearchWithTypeahead from '@carbon/ibmdotcom-web-components/es/components-react/search-with-typeahead/search-with-typeahead';
+/* eslint-enable max-len */
 import readme from './README.stories.react.mdx';
 
 export const Default = () => {
-  return <DDSSearchWithTypeahead should-remain-open active></DDSSearchWithTypeahead>;
+  return <DDSSearchWithTypeahead should-remain-open={true} active={true}></DDSSearchWithTypeahead>;
 };
 
+Default.story = {};
+
 export default {
-  title: 'Components/Tag group',
+  title: 'Components/Search with typeahead',
   decorators: [
     story => {
       return <>{story()}</>;
