7ad17dae33c54a53213b4873a5c9052d3d0fdd5f
proeung <putra.roeung@gmail.com>
2021-08-05 15:29:25 -0400

    feat(tab-extended-vertical): add vertical tabs style & sample content

--- packages/web-components/src/components/tabs-extended/__stories__/tabs-extended.stories.ts
+++ packages/web-components/src/components/tabs-extended/__stories__/tabs-extended.stories.ts
@@ -10,6 +10,7 @@
 import { html } from 'lit-element';
 import '../index';
 import ifNonNull from 'carbon-web-components/es/globals/directives/if-non-null.js';
+import ArrowRight20 from 'carbon-web-components/es/icons/arrow--right/20';
 import { select } from '@storybook/addon-knobs';
 import { ORIENTATION } from '../defs';
 import readme from './README.stories.mdx';
@@ -19,12 +20,28 @@ const orientationType = {
   [`Vertical`]: ORIENTATION.VERTICAL,
 };
 
+const defaultCardGroupItem = html`
+  <dds-card-group-item href="https://example.com">
+    <dds-card-eyebrow>Label</dds-card-eyebrow>
+    <dds-card-heading>The United Nations Environment Program works with IBM to reduce marine litter</dds-card-heading>
+    <dds-card-cta-footer slot="footer">
+      ${ArrowRight20({ slot: 'icon' })}
+    </dds-card-cta-footer>
+  </dds-card-group-item>
+`;
+
 export const Default = ({ parameters }) => {
-  const { orientation } = parameters?.props?.['dds-tabs-extended'] ?? {};
+  const { orientation, cards } = parameters?.props?.['dds-tabs-extended'] ?? {};
   return html`
     <dds-tabs-extended orientation="${ifNonNull(orientation)}">
       <dds-tab label="Tools for developers" selected="true">
-        <p>Content for first tab goes here.</p>
+        ${orientation === ORIENTATION.VERTICAL
+          ? html`
+              <dds-card-group>${cards}</dds-card-group>
+            `
+          : html`
+              <p>Content for first tab goes here.</p>
+            `}
       </dds-tab>
       <dds-tab label="Second tab">
         <p>Content for second tab goes here.</p>
@@ -42,28 +59,25 @@ export const Default = ({ parameters }) => {
   `;
 };
 
-Default.story = {
-  parameters: {
-    gridContentClasses: 'dds-ce-demo-devenv--simple-grid--tabs-extended',
-  },
-};
-
 export default {
   title: 'Components/Tabs extended',
   decorators: [
-    (story, { parameters }) => html`
-      <div class="dds-ce-demo-devenv--simple-grid ${parameters.gridContentClasses}">
+    story => html`
+      <div class="bx--grid">
         ${story()}
       </div>
     `,
   ],
   parameters: {
     ...readme.parameters,
-    hasVerticalSpacingInComponent: true,
+    useRawContainer: true,
     hasGrid: true,
     knobs: {
       'dds-tabs-extended': ({ groupId }) => ({
-        orientation: select('Orientation (orientation):', orientationType, ORIENTATION.VERTICAL, groupId),
+        orientation: select('Orientation (orientation):', orientationType, ORIENTATION.HORIZONTAL, groupId),
+        cards: Array.from({
+          length: 8,
+        }).map(() => defaultCardGroupItem),
       }),
     },
   },
