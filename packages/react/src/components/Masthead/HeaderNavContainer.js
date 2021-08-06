589a99b079e95cbc4a7a735485d788f8f42adc90
Anna Wen <54281166+annawen1@users.noreply.github.com>
2021-08-06 14:37:40 -0400

    fix(masthead): react rtl overflow (#6828)
    ### Related Ticket(s)
    
    [Masthead RTL] - issue with overflow in react #6750
    
    ### Description
    
    Problem: 
    Masthead overflow functionality was not working in RTL. 
    
    Solution:
    Detect when the page is RTL and swap the overflow logic.
    
    ![overflow rtl](https://user-images.githubusercontent.com/54281166/128523533-6264b8bd-1265-4b95-bd4d-16f78235aeae.gif)
    
    ### Changelog
    
    **Changes**
    
    - add overflow logic for `paginateRight()`, `paginateLeft()`, and for the tabbing in rtl
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/react/src/components/Masthead/HeaderNavContainer.js
+++ packages/react/src/components/Masthead/HeaderNavContainer.js
@@ -13,6 +13,7 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import CaretLeft20 from '@carbon/icons-react/es/caret--left/20';
 import CaretRight20 from '@carbon/icons-react/es/caret--right/20';
 import PropTypes from 'prop-types';
+import root from 'window-or-global';
 import settings from 'carbon-components/es/globals/js/settings';
 
 const { prefix } = settings;
@@ -30,75 +31,140 @@ const HeaderNavContainer = ({ children }) => {
   const [io, setIO] = useState(null);
   const [position, setPosition] = useState(0);
   const buttonSize = 48; // 40px(width) + 8px(gradient)
+  const pageIsRTL = root.document?.dir === 'rtl';
 
   const paginateLeft = useCallback(() => {
     let menuItems = contentRef.current.querySelectorAll(
-      '.bx--header__menu-bar li'
+      '.bx--header__menu-bar > li'
     );
-    for (let i = 0; i < menuItems.length; i++) {
-      // checks if first visible item is partially hidden
-      if (
-        menuItems[i].offsetLeft + menuItems[i].offsetWidth + position >=
-        buttonSize
-      ) {
-        // checks if there is space for remaining menuItems
+    if (pageIsRTL) {
+      for (let i = 0; i < menuItems.length; i++) {
         if (
-          menuItems[i].offsetLeft + menuItems[i].offsetWidth >
-          containerRef.current.offsetWidth - buttonSize
+          contentRef.current.offsetWidth -
+            menuItems[i].offsetLeft -
+            buttonSize >
+          -parseFloat(window.getComputedStyle(contentRef.current).right)
         ) {
           setPosition(
-            containerRef.current.offsetWidth -
-              menuItems[i].offsetLeft -
-              menuItems[i].offsetWidth -
-              buttonSize
+            Math.min(
+              -contentRef.current.offsetWidth +
+                containerRef.current.offsetWidth +
+                menuItems[i].offsetLeft -
+                buttonSize,
+              0
+            )
           );
-          contentRef.current.style.left =
+          contentRef.current.style.right =
             String(
+              Math.min(
+                -contentRef.current.offsetWidth +
+                  containerRef.current.offsetWidth +
+                  menuItems[i].offsetLeft -
+                  buttonSize,
+                0
+              )
+            ) + 'px';
+          break;
+        }
+      }
+    } else {
+      for (let i = 0; i < menuItems.length; i++) {
+        // checks if first visible item is partially hidden
+        if (
+          menuItems[i].offsetLeft + menuItems[i].offsetWidth + position >=
+          buttonSize
+        ) {
+          // checks if there is space for remaining menuItems
+          if (
+            menuItems[i].offsetLeft + menuItems[i].offsetWidth >
+            containerRef.current.offsetWidth - buttonSize
+          ) {
+            setPosition(
               containerRef.current.offsetWidth -
                 menuItems[i].offsetLeft -
                 menuItems[i].offsetWidth -
                 buttonSize
-            ) + 'px';
-        } else {
-          setPosition(0);
-          contentRef.current.style.left = '0px';
+            );
+            contentRef.current.style.left =
+              String(
+                containerRef.current.offsetWidth -
+                  menuItems[i].offsetLeft -
+                  menuItems[i].offsetWidth -
+                  buttonSize
+              ) + 'px';
+          } else {
+            setPosition(0);
+            contentRef.current.style.left = '0px';
+          }
+          break;
         }
-        break;
       }
     }
-  }, [position]);
+  }, [position, pageIsRTL]);
 
   const paginateRight = useCallback(() => {
     let menuItems = contentRef.current.querySelectorAll(
-      '.bx--header__menu-bar li'
+      '.bx--header__menu-bar > li'
     );
-    for (let i = 0; i < menuItems.length; i++) {
-      // checks if the right most visible element is partially hidden
-      if (
-        menuItems[i].offsetLeft + menuItems[i].offsetWidth + position >
-        containerRef.current.offsetWidth - buttonSize
-      ) {
-        // checks if there is space for remaining menuItems
+    if (pageIsRTL) {
+      for (let i = 0; i < menuItems.length; i++) {
+        // checks if the right most visible element is partially hidden
         if (
-          contentRef.current.offsetWidth - menuItems[i].offsetLeft <
-          containerRef.current.offsetWidth - buttonSize
+          contentRef.current.offsetWidth - menuItems[i].offsetLeft >
+          containerRef.current.offsetWidth - buttonSize - position
         ) {
           setPosition(
-            containerRef.current.offsetWidth - contentRef.current.offsetWidth
-          );
-          contentRef.current.style.left =
-            String(
+            Math.max(
+              -contentRef.current.offsetWidth +
+                menuItems[i].offsetLeft +
+                menuItems[i].offsetWidth +
+                buttonSize,
               containerRef.current.offsetWidth - contentRef.current.offsetWidth
+            )
+          );
+          contentRef.current.style.right =
+            String(
+              Math.max(
+                -contentRef.current.offsetWidth +
+                  menuItems[i].offsetLeft +
+                  menuItems[i].offsetWidth +
+                  buttonSize,
+                containerRef.current.offsetWidth -
+                  contentRef.current.offsetWidth
+              )
             ) + 'px';
-        } else {
-          setPosition(buttonSize - menuItems[i].offsetLeft);
-          contentRef.current.style.left =
-            String(buttonSize - menuItems[i].offsetLeft) + 'px';
+          break;
         }
-        break;
+      }
+    } else {
+      for (let i = 0; i < menuItems.length; i++) {
+        if (
+          menuItems[i].offsetLeft + menuItems[i].offsetWidth + position >
+          containerRef.current.offsetWidth - buttonSize
+        ) {
+          // checks if there is space for remaining menuItems
+          if (
+            contentRef.current.offsetWidth - menuItems[i].offsetLeft <
+            containerRef.current.offsetWidth - buttonSize
+          ) {
+            setPosition(
+              containerRef.current.offsetWidth - contentRef.current.offsetWidth
+            );
+            contentRef.current.style.left =
+              String(
+                containerRef.current.offsetWidth -
+                  contentRef.current.offsetWidth
+              ) + 'px';
+          } else {
+            setPosition(buttonSize - menuItems[i].offsetLeft);
+            contentRef.current.style.left =
+              String(buttonSize - menuItems[i].offsetLeft) + 'px';
+          }
+          break;
+        }
       }
     }
-  }, [position]);
+  }, [position, pageIsRTL]);
 
   useEffect(() => {
     if (window.IntersectionObserver) {
@@ -155,25 +221,50 @@ const HeaderNavContainer = ({ children }) => {
    */
   const handleOnKeyDown = event => {
     if (matches(event, [keys.Tab])) {
-      if (event.shiftKey) {
-        //Focus previous input
-        if (
-          document.activeElement.parentElement.previousSibling &&
-          document.activeElement.parentElement.previousSibling.offsetLeft +
-            position <=
-            buttonSize
-        ) {
-          paginateLeft();
+      if (pageIsRTL) {
+        if (event.shiftKey) {
+          if (
+            document.activeElement?.parentElement?.previousSibling &&
+            document.activeElement?.parentElement?.previousSibling?.offsetLeft +
+              document.activeElement?.parentElement?.previousSibling
+                ?.offsetWidth -
+              parseFloat(window.getComputedStyle(contentRef.current).right) +
+              buttonSize >
+              contentRef.current.offsetWidth
+          ) {
+            paginateLeft();
+          }
+        } else {
+          if (
+            document.activeElement?.parentElement?.nextSibling &&
+            contentRef.current.offsetWidth -
+              document.activeElement?.parentElement?.nextSibling?.offsetLeft >
+              containerRef.current.offsetWidth - buttonSize - position
+          ) {
+            paginateRight();
+          }
         }
       } else {
-        //Focus next input
-        if (
-          document.activeElement.parentElement.nextSibling &&
-          document.activeElement.parentElement.nextSibling.offsetLeft +
-            document.activeElement.parentElement.nextSibling.offsetWidth >=
-            containerRef.current.offsetWidth - buttonSize
-        ) {
-          paginateRight();
+        if (event.shiftKey) {
+          //Focus previous input
+          if (
+            document.activeElement.parentElement.previousSibling &&
+            document.activeElement.parentElement.previousSibling.offsetLeft +
+              position <=
+              buttonSize
+          ) {
+            paginateLeft();
+          }
+        } else {
+          //Focus next input
+          if (
+            document.activeElement.parentElement.nextSibling &&
+            document.activeElement.parentElement.nextSibling.offsetLeft +
+              document.activeElement.parentElement.nextSibling.offsetWidth >=
+              containerRef.current.offsetWidth - buttonSize
+          ) {
+            paginateRight();
+          }
         }
       }
     }
@@ -200,7 +291,7 @@ const HeaderNavContainer = ({ children }) => {
             onClick={paginateLeft}
             tabIndex="-1"
             aria-hidden="true">
-            <CaretLeft20 />
+            {pageIsRTL ? <CaretRight20 /> : <CaretLeft20 />}
           </button>
           <div className={`${prefix}--header__nav-caret-left-gradient`} />
         </div>
@@ -215,7 +306,7 @@ const HeaderNavContainer = ({ children }) => {
             onClick={paginateRight}
             tabIndex="-1"
             aria-hidden="true">
-            <CaretRight20 />
+            {pageIsRTL ? <CaretLeft20 /> : <CaretRight20 />}
           </button>
         </div>
       </div>
