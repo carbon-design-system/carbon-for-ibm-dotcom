1d6db450e2201d9f2002870b4d099021a607c6a9
Anna Wen <54281166+annawen1@users.noreply.github.com>
2021-08-11 12:49:52 -0400

    chore(storybook): add leadspace knobs

--- packages/web-components/src/components/leadspace/__stories__/leadspace.stories.react.tsx
+++ packages/web-components/src/components/leadspace/__stories__/leadspace.stories.react.tsx
@@ -8,6 +8,7 @@
  */
 
 import React from 'react';
+import { text, select } from '@storybook/addon-knobs';
 import ArrowRight20 from '@carbon/icons-react/es/arrow--right/20.js';
 // Below path will be there when an application installs `@carbon/ibmdotcom-web-components` package.
 // In our dev env, we auto-generate the file and re-map below path to to point to the generated file.
@@ -23,19 +24,52 @@ import DDSButtonGroup from '@carbon/ibmdotcom-web-components/es/components-react
 import DDSButtonGroupItem from '@carbon/ibmdotcom-web-components/es/components-react/button-group/button-group-item';
 // @ts-ignore
 import DDSImageItem from '@carbon/ibmdotcom-web-components/es/components-react/image/image-item';
+// @ts-ignore
+import DDSTagLink from '@carbon/ibmdotcom-web-components/es/components-react/tag-link/tag-link';
+// @ts-ignore
+import DDSTagGroup from '@carbon/ibmdotcom-web-components/es/components-react/tag-group/tag-group';
 
 import leadspaceImg from '../../../../../storybook-images/assets/leadspace/leadspaceMax.jpg';
 import readme from './README.stories.react.mdx';
 import { LEADSPACE_SIZE } from '../defs';
 
-const title = 'Heading can go on two lines max';
-const copy = 'Use this area for a short line of copy to support the title';
-const alt = 'Image alt text';
+const navigationOptions = ['with Tag group (using Tag link)', 'with Breadcrumbs', 'none'];
+// const getAriaLabel = type => {
+//   switch (type) {
+//     case 'ArrowDown20':
+//       return 'anchor link';
+//     case 'Pdf20':
+//       return 'pdf link';
+//     default:
+//       return '';
+//   }
+// };
 
-export const Tall = ({ parameters }) => {
-  const { size, hasImage, type } = parameters?.props?.Leadspace ?? {};
+// const iconMap = {
+//   ArrowRight20: ArrowRight20({ slot: 'icon' }),
+//   ArrowDown20: ArrowDown20({ slot: 'icon' }),
+//   // Pdf20: Pdf20({ slot: 'icon' }),
+// };
+
+// const iconOptions = {
+//   None: null,
+//   'Arrow Right': 'ArrowRight20',
+//   'Arrow Down': 'ArrowDown20',
+//   PDF: 'Pdf20',
+// };
+
+const navigationWithTagGroup = (
+  <DDSTagGroup slot="navigation">
+    <DDSTagLink href={'https://example.com'}>Marketing Analytics</DDSTagLink>
+    <DDSTagLink href={'https://example.com'}>Cloud</DDSTagLink>
+  </DDSTagGroup>
+);
+
+const Default = ({ parameters }) => {
+  const { alt, copy, size, hasImage, type, title, navElements } = parameters?.props?.Leadspace ?? {};
   return (
     <DDSLeadspace size={size} type={type} {...(hasImage ? { alt } : {})}>
+      {navElements === navigationOptions[0] ? navigationWithTagGroup : ``}
       <DDSLeadspaceHeading>{title}</DDSLeadspaceHeading>
       {copy}
       <DDSButtonGroup slot="action">
@@ -58,31 +92,55 @@ export const Tall = ({ parameters }) => {
   );
 };
 
-export const TallWithImage = context => Tall(context);
+export const Tall = context => Default(context);
+Tall.story = {
+  parameters: {
+    knobs: {
+      Leadspace: () => ({
+        size: LEADSPACE_SIZE.NONE,
+        hasImage: false,
+        navElements: select('navigation elements (optional)', navigationOptions, navigationOptions[2]),
+        title: text('title (title)', 'Heading can go on two lines max'),
+        copy: text('copy (copy)', 'Use this area for a short line of copy to support the title'),
+        alt: text('Image alt text (alt)', 'Image alt text'),
+      }),
+    },
+  },
+};
+
+export const TallWithImage = context => Default(context);
 TallWithImage.story = {
   parameters: {
     knobs: {
       Leadspace: () => ({
         size: LEADSPACE_SIZE.NONE,
         hasImage: true,
+        navElements: select('navigation elements (optional)', navigationOptions, navigationOptions[2]),
+        title: text('title (title)', 'Heading can go on two lines max'),
+        copy: text('copy (copy)', 'Use this area for a short line of copy to support the title'),
+        alt: text('Image alt text (alt)', 'Image alt text'),
       }),
     },
   },
 };
 
-export const Medium = context => Tall(context);
+export const Medium = context => Default(context);
 
 Medium.story = {
   parameters: {
     knobs: {
       Leadspace: () => ({
         size: LEADSPACE_SIZE.MEDIUM,
+        navElements: select('navigation elements (optional)', navigationOptions, navigationOptions[2]),
+        title: text('title (title)', 'Heading can go on two lines max'),
+        copy: text('copy (copy)', 'Use this area for a short line of copy to support the title'),
+        alt: text('Image alt text (alt)', 'Image alt text'),
       }),
     },
   },
 };
 
-export const MediumWithImage = context => Tall(context);
+export const MediumWithImage = context => Default(context);
 
 MediumWithImage.story = {
   parameters: {
@@ -90,24 +148,32 @@ MediumWithImage.story = {
       Leadspace: () => ({
         size: LEADSPACE_SIZE.MEDIUM,
         hasImage: true,
+        navElements: select('navigation elements (optional)', navigationOptions, navigationOptions[2]),
+        title: text('title (title)', 'Heading can go on two lines max'),
+        copy: text('copy (copy)', 'Use this area for a short line of copy to support the title'),
+        alt: text('Image alt text (alt)', 'Image alt text'),
       }),
     },
   },
 };
 
-export const Centered = context => Tall(context);
+export const Centered = context => Default(context);
 
 Centered.story = {
   parameters: {
     knobs: {
       Leadspace: () => ({
         type: 'centered',
+        navElements: select('navigation elements (optional)', navigationOptions, navigationOptions[2]),
+        title: text('title (title)', 'Heading can go on two lines max'),
+        copy: text('copy (copy)', 'Use this area for a short line of copy to support the title'),
+        alt: text('Image alt text (alt)', 'Image alt text'),
       }),
     },
   },
 };
 
-export const CenteredWithImage = context => Tall(context);
+export const CenteredWithImage = context => Default(context);
 
 CenteredWithImage.story = {
   parameters: {
@@ -115,6 +181,10 @@ CenteredWithImage.story = {
       Leadspace: () => ({
         hasImage: true,
         type: 'centered',
+        navElements: select('navigation elements (optional)', navigationOptions, navigationOptions[2]),
+        title: text('title (title)', 'Heading can go on two lines max'),
+        copy: text('copy (copy)', 'Use this area for a short line of copy to support the title'),
+        alt: text('Image alt text (alt)', 'Image alt text'),
       }),
     },
   },
@@ -127,6 +197,10 @@ Super.story = {
     knobs: {
       Leadspace: () => ({
         size: LEADSPACE_SIZE.SUPER,
+        navElements: select('navigation elements (optional)', navigationOptions, navigationOptions[2]),
+        title: text('title (title)', 'Heading can go on two lines max'),
+        copy: text('copy (copy)', 'Use this area for a short line of copy to support the title'),
+        alt: text('Image alt text (alt)', 'Image alt text'),
       }),
     },
   },
@@ -140,6 +214,10 @@ SuperWithImage.story = {
       Leadspace: () => ({
         size: LEADSPACE_SIZE.SUPER,
         hasImage: true,
+        navElements: select('navigation elements (optional)', navigationOptions, navigationOptions[2]),
+        title: text('title (title)', 'Heading can go on two lines max'),
+        copy: text('copy (copy)', 'Use this area for a short line of copy to support the title'),
+        alt: text('Image alt text (alt)', 'Image alt text'),
       }),
     },
   },
