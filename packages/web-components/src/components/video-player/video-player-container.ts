a600476bb2bb57770d2020da1a05ea27996af22c
Ariella Gilmore <ariellalgilmore@gmail.com>
2021-08-17 16:04:26 -0700

    fix(content-item-horizontal): create react wrapper (#6903)
    ### Related Ticket(s)
    
    #5338 
    
    ### Description
    
    Create content Item horizontal react wrapper.
    
    ### Changelog
    
    **New**
    
    - create react wrapper stories and codesandbox examples
    - added react story linking to react wrapper
    
    **Changed**
    
    - {{changed thing}}
    
    **Removed**
    
    - {{removed thing}}
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/src/components/video-player/video-player-container.ts
+++ packages/web-components/src/components/video-player/video-player-container.ts
@@ -210,4 +210,5 @@ class DDSVideoPlayerContainer extends ConnectMixin<
   mapDispatchToProps
 )(DDSVideoPlayerContainerMixin(DDSVideoPlayerComposite)) {}
 
+/* @__GENERATE_REACT_CUSTOM_ELEMENT_TYPE__ */
 export default DDSVideoPlayerContainer;
