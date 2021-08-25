b9f806a60c8275717c271ff375a9a1cb0af5d38f
proeung <putra.roeung@gmail.com>
2021-08-25 15:07:36 -0400

    feat(card-group-with-card-link): remove Card Group example

--- packages/web-components/src/components/tabs-extended/__stories__/tabs-extended.stories.ts
+++ packages/web-components/src/components/tabs-extended/__stories__/tabs-extended.stories.ts
@@ -11,8 +11,7 @@ import { html } from 'lit-element';
 import '../index';
 import '../../card-group/index';
 import ifNonNull from 'carbon-web-components/es/globals/directives/if-non-null.js';
-import ArrowRight20 from 'carbon-web-components/es/icons/arrow--right/20';
-import { select, number } from '@storybook/addon-knobs';
+import { select } from '@storybook/addon-knobs';
 import { ORIENTATION } from '../defs';
 import readme from './README.stories.mdx';
 
@@ -20,16 +19,6 @@ const orientationType = {
   [`horizontal`]: ORIENTATION.HORIZONTAL,
   [`vertical`]: ORIENTATION.VERTICAL,
 };
-
-const cardGroupItemWithCardLinks = html`
-  <dds-card-group-card-link-item href="https://example.com" pattern-background>
-    <dds-card-link-heading>IBM Developer</dds-card-link-heading>
-    <p>Learn, code and connect with your community</p>
-    <dds-card-cta-footer slot="footer">
-      ${ArrowRight20({ slot: 'icon' })}
-    </dds-card-cta-footer>
-  </dds-card-group-card-link-item>
-`;
 
 export const Default = ({ parameters }) => {
   const { orientation } = parameters?.props?.TabsExtended ?? {};
@@ -55,38 +44,6 @@ export const Default = ({ parameters }) => {
       </dds-tab>
     </dds-tabs-extended>
   `;
-};
-
-export const withCardGroupCardLinks = ({ parameters }) => {
-  const { cards } = parameters?.props?.TabsExtended ?? {};
-  return html`
-    <dds-tabs-extended orientation="vertical">
-      <dds-tab label="Tools for developers" selected="true">
-        <dds-card-group grid-mode="narrow">
-          ${cards}
-        </dds-card-group>
-      </dds-tab>
-      <dds-tab label="Tools for business">
-        <p>Content for second tab goes here.</p>
-      </dds-tab>
-      <dds-tab label="Trending in tech">
-        <p>Content for third tab goes here.</p>
-      </dds-tab>
-    </dds-tabs-extended>
-  `;
-};
-
-withCardGroupCardLinks.story = {
-  parameters: {
-    ...readme.parameters,
-    knobs: {
-      TabsExtended: ({ groupId }) => ({
-        cards: Array.from({
-          length: number('Number of cards', 8, {}, groupId),
-        }).map(() => cardGroupItemWithCardLinks),
-      }),
-    },
-  },
 };
 
 export default {
