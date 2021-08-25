800791c669f0d3ff90a2131f121275d3b9bb3dd3
ariellagilmore <ariellalgilmore@gmail.com>
2021-08-25 06:37:05 -0700

    fix(cta-section): create react wrapper

--- packages/web-components/src/components/link-list/link-list-heading.ts
+++ packages/web-components/src/components/link-list/link-list-heading.ts
@@ -50,4 +50,5 @@ class DDSLinkListHeading extends StableSelectorMixin(LitElement) {
   static styles = styles;
 }
 
+/* @__GENERATE_REACT_CUSTOM_ELEMENT_TYPE__ */
 export default DDSLinkListHeading;
