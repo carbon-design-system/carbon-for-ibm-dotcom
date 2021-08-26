14be07fd606de25caf335dc729d3ce0058717b05
Putra Roeung Bonaccorsi <putra.roeung@gmail.com>
2021-08-26 08:40:46 -0400

    feat(card-group): modify to allow Card link design pattern & allow CTA on Card Link (#6934)
    ### Related Ticket(s)
    
    DDS Consulting Issue: https://github.com/carbon-design-system/carbon-for-ibm-dotcom/issues/6876
    HC JIRA Ticket: https://jsw.ibm.com/browse/HC-2138
    
    ### Description
    - This PR is a follow-up based on the discussion mentioned in this PR - https://github.com/carbon-design-system/carbon-for-ibm-dotcom/pull/6826
    
    ### Design & Functional Specs
    - https://github.com/carbon-design-system/carbon-for-ibm-dotcom-website/wiki/Tabs-extended#22-orientation 
    - https://github.com/carbon-design-system/carbon-for-ibm-dotcom-website/wiki/Card-link
    
    ![Screen Shot 2021-08-19 at 5 24 54 PM](https://user-images.githubusercontent.com/1815714/130146499-f0f9612d-fce4-4734-b900-b50cb1f1353a.png)
    
    
    ### Changelog
    
    **New Components/Props**
    
    - `pattern-background` - this prop/attribute is used when the card group item has the same background color as the pattern container.
    - `<dds-card-link-heading>` - a component extended from `<dds-card-heading>` that captures the proper expressive heading styling for the `Card Link UI` component.
    - `<dds-card-group-card-link-item>`
    - `<dds-card-link-cta>`
    
    **Removed**
    
    - N/A
    
    ### HTML (with Card Link)
    
    ```html
    <dds-card-group grid-mode="narrow">
      <dds-card-group-card-link-item href="https://example.com" pattern-background>
        <dds-card-link-heading slot="heading">IBM Developer - Card 01</dds-card-link-heading>
        <p>Learn, code and connect with your community</p>
        <dds-card-cta-footer slot="footer">
          <svg slot="icon" focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
            <path d="M11.8 2.8L10.8 3.8 16.2 9.3 1 9.3 1 10.7 16.2 10.7 10.8 16.2 11.8 17.2 19 10z"></path>
          </svg>
        </dds-card-cta-footer>
      </dds-card-group-card-link-item>
    </dds-card-group>
    ```
    
    ### Testing Instructions
    - Visit the `Card Group with Card Links` layout component - `/?path=/story/components-card-group--with-card-links`
    - Ensure that the `Card links` look good within the `Card group` layout component in all breakpoints.
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "package: styles": Carbon Expressive -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/src/components/tabs-extended/__stories__/tabs-extended.stories.ts
+++ packages/web-components/src/components/tabs-extended/__stories__/tabs-extended.stories.ts
@@ -9,6 +9,7 @@
 
 import { html } from 'lit-element';
 import '../index';
+import '../../card-group/index';
 import ifNonNull from 'carbon-web-components/es/globals/directives/if-non-null.js';
 import { select } from '@storybook/addon-knobs';
 import { ORIENTATION } from '../defs';
@@ -16,11 +17,11 @@ import readme from './README.stories.mdx';
 
 const orientationType = {
   [`horizontal`]: ORIENTATION.HORIZONTAL,
-  [`Vertical`]: ORIENTATION.VERTICAL,
+  [`vertical`]: ORIENTATION.VERTICAL,
 };
 
 export const Default = ({ parameters }) => {
-  const { orientation } = parameters?.props?.['dds-tabs-extended'] ?? {};
+  const { orientation } = parameters?.props?.TabsExtended ?? {};
   return html`
     <dds-tabs-extended orientation="${ifNonNull(orientation)}">
       <dds-tab
@@ -59,7 +60,7 @@ export default {
     useRawContainer: true,
     hasGrid: true,
     knobs: {
-      'dds-tabs-extended': ({ groupId }) => ({
+      TabsExtended: ({ groupId }) => ({
         orientation: select('Orientation (orientation):', orientationType, ORIENTATION.HORIZONTAL, groupId),
       }),
     },
