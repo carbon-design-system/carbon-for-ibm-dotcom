162fa2e2b9552505c26340ba104f3f60fbbf9a2e
IgnacioBecerra <i1becerr@ucsd.edu>
2021-09-01 14:02:11 -0700

    fix(table-of-contents): add heading content story in wrapper

--- packages/web-components/src/components/table-of-contents/__stories__/table-of-contents.stories.react.tsx
+++ packages/web-components/src/components/table-of-contents/__stories__/table-of-contents.stories.react.tsx
@@ -11,10 +11,18 @@ import React from 'react';
 // Below path will be there when an application installs `@carbon/ibmdotcom-web-components` package.
 // In our dev env, we auto-generate the file and re-map below path to to point to the generated file.
 // @ts-ignore
+import DDSHorizontalRule from '@carbon/ibmdotcom-web-components/es/components-react/horizontal-rule/horizontal-rule';
+import DDSImage from '@carbon/ibmdotcom-web-components/es/components-react/image/image';
+import DDSImageItem from '@carbon/ibmdotcom-web-components/es/components-react/image/image-item';
+import { boolean } from '@storybook/addon-knobs';
 import DDSTableOfContents from '@carbon/ibmdotcom-web-components/es/components-react/table-of-contents/table-of-contents';
 import content from './wrapper-content';
 import readme from './README.stories.react.mdx';
 import styles from './table-of-contents.stories.scss';
+import imgMd16x9 from '../../../../../storybook-images/assets/480/fpo--16x9--480x270--005.jpg';
+import imgLg16x9 from '../../../../../storybook-images/assets/720/fpo--16x9--720x405--001.jpg';
+import imgLg1x1 from '../../../../../storybook-images/assets/720/fpo--1x1--720x720--001.jpg';
+import imgXlg16x9 from '../../../../../storybook-images/assets/1312/fpo--16x9--1312x738--001.jpg';
 
 export const Default = () => {
   return (
@@ -22,6 +30,33 @@ export const Default = () => {
       <DDSTableOfContents>{content()}</DDSTableOfContents>
     </>
   );
+};
+
+export const WithHeadingContent = ({ parameters }) => {
+  const { menuRule } = parameters?.props?.Other ?? {};
+  return (
+    <>
+      <DDSTableOfContents>
+        <DDSImage slot="heading" alt="Alt text" default-src={imgLg1x1}>
+          <DDSImageItem media="(min-width: 1056px)" srcset={imgXlg16x9}></DDSImageItem>
+          <DDSImageItem media="(min-width: 672px)" srcset={imgLg16x9}></DDSImageItem>
+          <DDSImageItem media="(min-width: 400px)" srcset={imgMd16x9}></DDSImageItem>
+        </DDSImage>
+        {!menuRule ? null : <DDSHorizontalRule slot="menu-rule"></DDSHorizontalRule>}
+        {content()}
+      </DDSTableOfContents>
+    </>
+  );
+};
+
+WithHeadingContent.story = {
+  parameters: {
+    knobs: {
+      Other: () => ({
+        menuRule: boolean('Put a horizontal rule', false),
+      }),
+    },
+  },
 };
 
 export const Horizontal = () => {
