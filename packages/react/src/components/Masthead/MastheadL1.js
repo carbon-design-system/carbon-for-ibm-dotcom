64e5f266706936bc8fa76ff7dbafe9733efc2693
emyarod <afw5059@gmail.com>
2021-10-14 12:31:49 -0500

    refactor(MastheadL1): remove unneeded ternary

--- packages/react/src/components/Masthead/MastheadL1.js
+++ packages/react/src/components/Masthead/MastheadL1.js
@@ -48,7 +48,7 @@ const MastheadL1 = ({ navigationL1, ...rest }) => {
 
     return (
       <HeaderMenuItem
-        data-selected={selected ? 'true' : 'false'}
+        data-selected={!!selected}
         href={link.url}
         data-autoid={autoid}
         key={index}>
