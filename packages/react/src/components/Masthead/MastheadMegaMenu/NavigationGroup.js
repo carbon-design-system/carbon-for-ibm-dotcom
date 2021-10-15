66231707ca007635a223c74b394b6349f45d00cb
emyarod <afw5059@gmail.com>
2021-10-15 11:25:17 -0500

    fix(MegaMenu): forward HeaderMenu `ref` to NavigationGroup

--- packages/react/src/components/Masthead/MastheadMegaMenu/NavigationGroup.js
+++ packages/react/src/components/Masthead/MastheadMegaMenu/NavigationGroup.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2020
+ * Copyright IBM Corp. 2016, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -15,18 +15,22 @@ const { prefix } = settings;
 /**
  * MegaMenu panel
  */
-const NavigationGroup = ({ children, ...rest }) => (
-  <section
-    className={`${prefix}--masthead__megamenu`}
-    data-autoid={`${stablePrefix}--masthead__megamenu`}
-    {...rest}>
-    <div className={`${prefix}--masthead__megamenu__container`}>
-      <div className={`${prefix}--masthead__megamenu__container--row`}>
-        {children}
+const NavigationGroup = React.forwardRef(function NavigationGroup(props, ref) {
+  const { children, ...rest } = props;
+  return (
+    <section
+      className={`${prefix}--masthead__megamenu`}
+      data-autoid={`${stablePrefix}--masthead__megamenu`}
+      ref={ref}
+      {...rest}>
+      <div className={`${prefix}--masthead__megamenu__container`}>
+        <div className={`${prefix}--masthead__megamenu__container--row`}>
+          {children}
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+});
 
 NavigationGroup.propTypes = {
   /**
