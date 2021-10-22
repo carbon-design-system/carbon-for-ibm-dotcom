8d9be98d2435f98a337f7557aacae31897c79d7c
andysherman2121 <andrew.sherman1@ibm.com>
2021-10-22 15:59:24 -0400

    test(e2e-masthead-l1): added l1 tests for react

--- packages/react/tests/e2e-storybook/cypress/integration/Masthead/Masthead.e2e.js
+++ packages/react/tests/e2e-storybook/cypress/integration/Masthead/Masthead.e2e.js
@@ -30,6 +30,14 @@ const _pathCustom =
  */
 const _pathPlatform = '/iframe.html?id=components-masthead--with-platform';
 
+/**
+ * Sets the correct path (Masthead with L1)
+ *
+ * @type {string}
+ * @private
+ */
+const _pathl1 = '/iframe.html?id=components-masthead--with-l-1';
+
 describe('Masthead | default (desktop)', () => {
   beforeEach(() => {
     cy.visit(`/${_pathDefault}`);
@@ -142,6 +150,110 @@ describe('Masthead | with platform (desktop)', () => {
     cy.screenshot();
     // Take a snapshot for visual diffing
     cy.percySnapshot('Masthead | with platform - search', {
+      widths: [1280],
+    });
+  });
+});
+
+describe('Masthead | with L1 (desktop)', () => {
+  beforeEach(() => {
+    cy.visit(`/${_pathl1}`);
+    cy.viewport(1280, 780);
+  });
+
+  it('should render 5 menu items', () => {
+    cy.get('.bx--header__menu-bar > li').should('have.length', 5);
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    cy.percySnapshot('Masthead | Number of custom menu items', {
+      widths: [1280],
+    });
+  });
+
+  it('should load l1 menu item with selected state', () => {
+    cy.get('[data-autoid="dds--masthead-eco__l1-nav0"] a').then($menuItem => {
+      expect($menuItem).to.have.attr('data-selected', 'true');
+    });
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    cy.percySnapshot('dds-masthead | menu item with selected state', {
+      widths: [1280],
+    });
+  });
+
+  it('should load regular menu - first l1 nav item', () => {
+    cy.get('[data-autoid="dds--masthead-eco__l1-nav0"]')
+      .click()
+      .find('a')
+      .then($menuItem => {
+        expect($menuItem).to.have.attr('aria-expanded', 'true');
+      });
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    cy.percySnapshot('Masthead | custom nav (nav 0)', {
+      widths: [1280],
+    });
+  });
+
+  it('should load regular menu - second l1 nav item', () => {
+    cy.get('[data-autoid="dds--masthead-eco__l1-nav1"]')
+      .click()
+      .find('a')
+      .then($menuItem => {
+        expect($menuItem).to.have.attr('aria-expanded', 'true');
+      });
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    cy.percySnapshot('Masthead | custom nav (nav 1)', {
+      widths: [1280],
+    });
+  });
+
+  it('should load third nav l1 item', () => {
+    cy.get('[data-autoid="dds--masthead-eco__l1-nav2"]').then($link => {
+      const url = $link.prop('href');
+      expect(url).not.to.be.empty;
+    });
+  });
+
+  it('should load regular menu - fourth l1 nav item', () => {
+    cy.get('[data-autoid="dds--masthead-eco__l1-nav3"]')
+      .click()
+      .find('a')
+      .then($menuItem => {
+        expect($menuItem).to.have.attr('aria-expanded', 'true');
+      });
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    cy.percySnapshot('Masthead | custom nav (nav 3)', {
+      widths: [1280],
+    });
+  });
+
+  it('should load fifth nav l1 item', () => {
+    cy.get('[data-autoid="dds--masthead-eco__l1-nav4"]').then($link => {
+      const url = $link.prop('href');
+      expect(url).not.to.be.empty;
+    });
+  });
+
+  it('should scroll the L1 overflow properly', () => {
+    cy.get('.bx--header__nav-caret-right').click();
+
+    cy.wait(500);
+
+    cy.get('.bx--header__nav-caret-right-container').then($button => {
+      expect($button).to.have.attr('hidden');
+    });
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    cy.percySnapshot('Masthead | with L1 - overflow', {
       widths: [1280],
     });
   });
