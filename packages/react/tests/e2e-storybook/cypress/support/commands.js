cc754bca29eb75576d5060ef85948e1f1108e811
Jeff Chew <jeff.chew@gmail.com>
2021-10-26 17:38:53 -0400

    chore(cypress): remove unused library

--- packages/react/tests/e2e-storybook/cypress/support/commands.js
+++ packages/react/tests/e2e-storybook/cypress/support/commands.js
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import 'cypress-localstorage-commands';
 import countrylist from './data/countrylist.json';
 import translation from './data/translation.json';
 
