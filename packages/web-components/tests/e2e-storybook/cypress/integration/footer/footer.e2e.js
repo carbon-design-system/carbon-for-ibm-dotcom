c86e834b42676b3c0dee6ee862efa1010ed2490f
Brunno Marchetti <BrunnoM7@users.noreply.github.com>
2021-11-01 14:45:13 -0300

    test(footer): add e2e tests for web-components footer short

--- packages/web-components/tests/e2e-storybook/cypress/integration/footer/footer.e2e.js
+++ packages/web-components/tests/e2e-storybook/cypress/integration/footer/footer.e2e.js
@@ -10,6 +10,14 @@ import settings from 'carbon-components/es/globals/js/settings';
 const { prefix } = settings;
 
 /**
+ * Sets the correct path (Short)
+ *
+ * @type {string}
+ * @private
+ */
+const _pathShort = '/iframe.html?id=components-footer--short';
+
+/**
  * Sets the correct path (Short language only)
  *
  * @type {string}
@@ -77,6 +85,119 @@ describe('dds-footer | Short language only (mobile)', () => {
     // TODO: click states currently not working in percy for web components
     // cy.percySnapshot('dds-footer | Short language only (desktop language selector)', {
     //   widths: [320],
+    // });
+  });
+});
+
+describe('dds-footer | Short (desktop)', () => {
+  beforeEach(() => {
+    cy.visit(`${_pathShort}`);
+    cy.viewport(1280, 780);
+  });
+
+  it('should display clickable IBM logo', () => {
+    cy.get('dds-footer-logo')
+      .shadow()
+      .find('a.bx--footer-logo__link')
+      .then($link => {
+        const url = $link.prop('href');
+        expect(url).not.to.be.empty;
+      });
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    // TODO: click states currently not working in percy for web components
+    // cy.percySnapshot('dds-footer | Short | load clickable IBM logo', {
+    //   widths: [1280],
+    // });
+  });
+
+  it('should open locale modal with 4 geos when clicked on locale button', () => {
+    cy.get('dds-locale-button').click();
+
+    cy.wait(500);
+
+    cy.get('dds-locale-modal').should('have.attr', 'open');
+
+    cy.get('dds-regions > dds-region-item').should('have.length', 4);
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    // TODO: click states currently not working in percy for web components
+    // cy.percySnapshot('dds-footer | Short | open locale modal with 4 geos when clicked on locale button', {
+    //   widths: [1280],
+    // });
+  });
+
+  it('should display the specific locations and languages of a selected geo', () => {
+    cy.get('dds-locale-button').click();
+
+    cy.wait(500);
+
+    cy.get('dds-regions')
+      .find('dds-region-item[name="Middle East and Africa"]')
+      .click();
+
+    cy.wait(500);
+
+    cy.get('dds-locale-search')
+      .find('dds-locale-item')
+      .each($locale => {
+        if (!$locale.attr('region') === 'Middle East and Africa') {
+          $locale.should('have.attr', 'hidden');
+        }
+      });
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    // TODO: click states currently not working in percy for web components
+    // cy.percySnapshot('dds-footer | Short | display the specific locations and languages of a selected geo', {
+    //   widths: [1280],
+    // });
+  });
+
+  it('should display interactive search field and with keywords for locations and languages', () => {
+    cy.get('dds-locale-button').click();
+
+    cy.wait(500);
+
+    cy.get('dds-regions')
+      .find('dds-region-item[name="Americas"]')
+      .click();
+
+    cy.wait(500);
+
+    cy.get('dds-locale-search')
+      .find('dds-search')
+      .shadow()
+      .find('input')
+      .type('gu')
+      .get('[country="Brazil (Brasil)"]')
+      .should('not.have.attr', 'hidden')
+      .get('[country="Guyana"]')
+      .should('not.have.attr', 'hidden');
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    // TODO: click states currently not working in percy for web components
+    // cy.percySnapshot('dds-footer | Short | display interactive search field and with keywords for locations and languages', {
+    //   widths: [1280],
+    // });
+  });
+
+  it('should load footer legal navigation with clickable links', () => {
+    cy.get('dds-legal-nav')
+      .find('dds-legal-nav-item')
+      .each($link => {
+        const url = $link.prop('href');
+        expect(url).not.to.be.empty;
+      });
+
+    cy.screenshot();
+    // Take a snapshot for visual diffing
+    // TODO: click states currently not working in percy for web components
+    // cy.percySnapshot('dds-footer | Short | load footer legal navigation with clickable links', {
+    //   widths: [1280],
     // });
   });
 });
