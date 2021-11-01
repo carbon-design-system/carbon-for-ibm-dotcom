14e8b3f3f10ea10c51705871740584ed2d3d4ac3
Brunno Marchetti <BrunnoM7@users.noreply.github.com>
2021-11-01 20:46:53 -0300

    Revert "Merge branch 'master' of github.com:carbon-design-system/carbon-for-ibm-dotcom into test/footer-short"
    This reverts commit 49f0da7511864f2f1454d8b4e3006f5aab8b6960, reversing
    changes made to 5ac92407a96228f688b3d922ff9f8d5accfd733f.

--- packages/react/tests/e2e-storybook/cypress/integration/Footer/Footer.e2e.js
+++ packages/react/tests/e2e-storybook/cypress/integration/Footer/Footer.e2e.js
@@ -23,139 +23,9 @@ const _pathShort = '/iframe.html?id=components-footer--short';
  * @type {string}
  * @private
  */
+
 const _pathShortLanguageOnly =
   '/iframe.html?id=components-footer--short-language-only';
-
-/**
- * Sets the correct path (Micro language only)
- *
- * @type {string}
- * @private
- */
-const _pathMicroLanguageOnly =
-  '/iframe.html?id=components-footer--micro-language-only';
-
-describe('Footer | Short (desktop)', () => {
-  beforeEach(() => {
-    cy.visit(`${_pathShort}`);
-    cy.viewport(1280, 780);
-  });
-
-  it('should display clickable IBM logo', () => {
-    cy.get('a.bx--footer-logo__link').then($link => {
-      const url = $link.prop('href');
-      expect(url).not.to.be.empty;
-    });
-
-    cy.screenshot();
-
-    cy.percySnapshot('Footer | Short | load clickable IBM logo', {
-      widths: [1280],
-    });
-  });
-
-  it('should open locale modal with 4 geos when clicked on locale button', () => {
-    cy.get('button[data-autoid="dds--locale-btn"]').click();
-
-    cy.wait(500);
-
-    cy.get('div[data-autoid="dds--locale-modal"]').should('have.attr', 'open');
-
-    cy.get('.bx--locale-modal [data-region]').should('have.length', 4);
-
-    cy.screenshot();
-
-    cy.percySnapshot(
-      'Footer | Short | open locale modal with 4 geos when clicked on locale button',
-      {
-        widths: [1280],
-      }
-    );
-  });
-
-  it('should display the specific locations and languages of a selected geo', () => {
-    cy.get('button[data-autoid="dds--locale-btn"]').click();
-
-    cy.wait(500);
-
-    cy.get('.bx--locale-modal [data-region="mea"]')
-      .find('a')
-      .click();
-
-    cy.wait(500);
-
-    cy.get('.bx--locale-modal__filter')
-      .find('.bx--locale-modal__list li a')
-      .each($locale => {
-        if (!$locale.attr('data-region') === 'mea') {
-          $locale.should('have.class', 'bx--locale-modal__locales-hidden');
-        }
-      });
-
-    cy.screenshot();
-
-    cy.percySnapshot(
-      'Footer | Short | display the specific locations and languages of a selected geo',
-      {
-        widths: [1280],
-      }
-    );
-  });
-
-  it('should display interactive search field and with keywords for locations and languages', () => {
-    cy.get('button[data-autoid="dds--locale-btn"]').click();
-
-    cy.wait(500);
-
-    cy.get('.bx--locale-modal [data-region="am"]')
-      .find('a')
-      .click();
-
-    cy.wait(500);
-
-    cy.get('.bx--locale-modal__search input')
-      .type('gen')
-      .get(
-        '.bx--locale-modal__list li a:not(.bx--locale-modal__locales-hidden)'
-      )
-      .contains('Argentina');
-
-    cy.get('.bx--locale-modal__search input')
-      .clear()
-      .type('por')
-      .get(
-        '.bx--locale-modal__list li a:not(.bx--locale-modal__locales-hidden)'
-      )
-      .contains('Brazil (Brasil)');
-
-    cy.screenshot();
-
-    cy.percySnapshot(
-      'Footer | Short | display interactive search field and with keywords for locations and languages',
-      {
-        widths: [1280],
-      }
-    );
-  });
-
-  it('should load footer legal navigation with clickable links', () => {
-    cy.get('[data-autoid="dds--footer-legal-nav"]')
-      .find('.bx--legal-nav__list-item a')
-      .each($link => {
-        const url = $link.prop('href');
-        expect(url).not.to.be.empty;
-      });
-
-    cy.screenshot();
-
-    cy.percySnapshot(
-      'Footer | Short | load footer legal navigation with clickable links',
-      {
-        widths: [1280],
-      }
-    );
-  });
-});
 
 describe('Footer | Short language only (desktop)', () => {
   beforeEach(() => {
@@ -207,50 +77,38 @@ describe('Footer | Short language only (desktop)', () => {
   });
 });
 
-describe('Footer | Micro language only (desktop)', () => {
+describe('Footer | Short language only (mobile)', () => {
   beforeEach(() => {
-    cy.visit(`/${_pathMicroLanguageOnly}`);
-    cy.viewport(1280, 780);
+    cy.visit(`/${_pathShortLanguageOnly}`);
+    cy.viewport(320, 780);
   });
 
-  it('should load language selector dropdown', () => {
-    cy.get(`[data-autoid="dds--language-selector"]`).click();
+  it('should load language selector and be interactive', () => {
+    cy.get('[data-autoid="dds--language-selector__select"]').should(
+      'have.length',
+      1
+    );
+
+    const languageSelector = cy.get(
+      '[data-autoid="dds--language-selector__select"]'
+    );
+    languageSelector.select('Arabic / عربية');
+    languageSelector.should('have.value', 'ar');
 
     cy.screenshot();
     cy.percySnapshot(
-      'Footer | Micro language only (desktop language selector)',
+      'Footer | Short language only (desktop language selector)',
       {
-        widths: [1280],
-      }
-    );
-  });
-
-  it('should be able to select a language from combo box', () => {
-    cy.get(`[data-autoid="dds--language-selector"]`).click();
-    cy.get(`[data-autoid="dds--footer"]`)
-      .find(
-        `.${prefix}--list-box__menu > .${prefix}--list-box__menu-item:nth-of-type(1) .${prefix}--list-box__menu-item__option`
-      )
-      .click();
-    cy.get(`[data-autoid="dds--language-selector"]`).should(
-      'have.value',
-      'Arabic / عربية'
-    );
-
-    cy.screenshot();
-    cy.percySnapshot(
-      'Footer | Micro language only (desktop language selector)',
-      {
-        widths: [1280],
+        widths: [320],
       }
     );
   });
 });
 
-describe('Footer | Short (mobile)', () => {
+describe('dds-footer | Short (desktop)', () => {
   beforeEach(() => {
     cy.visit(`${_pathShort}`);
-    cy.viewport(320, 780);
+    cy.viewport(1280, 780);
   });
 
   it('should display clickable IBM logo', () => {
@@ -261,8 +119,8 @@ describe('Footer | Short (mobile)', () => {
 
     cy.screenshot();
 
-    cy.percySnapshot('Footer | Short | load clickable IBM logo', {
-      widths: [320],
+    cy.percySnapshot('dds-footer | Short | load clickable IBM logo', {
+      widths: [1280],
     });
   });
 
@@ -278,9 +136,9 @@ describe('Footer | Short (mobile)', () => {
     cy.screenshot();
 
     cy.percySnapshot(
-      'Footer | Short | open locale modal with 4 geos when clicked on locale button',
+      'dds-footer | Short | open locale modal with 4 geos when clicked on locale button',
       {
-        widths: [320],
+        widths: [1280],
       }
     );
   });
@@ -307,9 +165,9 @@ describe('Footer | Short (mobile)', () => {
     cy.screenshot();
 
     cy.percySnapshot(
-      'Footer | Short | display the specific locations and languages of a selected geo',
+      'dds-footer | Short | display the specific locations and languages of a selected geo',
       {
-        widths: [320],
+        widths: [1280],
       }
     );
   });
@@ -343,9 +201,9 @@ describe('Footer | Short (mobile)', () => {
     cy.screenshot();
 
     cy.percySnapshot(
-      'Footer | Short | display interactive search field and with keywords for locations and languages',
+      'dds-footer | Short | display interactive search field and with keywords for locations and languages',
       {
-        widths: [320],
+        widths: [1280],
       }
     );
   });
@@ -361,65 +219,9 @@ describe('Footer | Short (mobile)', () => {
     cy.screenshot();
 
     cy.percySnapshot(
-      'Footer | Short | load footer legal navigation with clickable links',
+      'dds-footer | Short | load footer legal navigation with clickable links',
       {
-        widths: [320],
-      }
-    );
-  });
-});
-
-describe('Footer | Short language only (mobile)', () => {
-  beforeEach(() => {
-    cy.visit(`/${_pathShortLanguageOnly}`);
-    cy.viewport(320, 780);
-  });
-
-  it('should load language selector and be interactive', () => {
-    cy.get('[data-autoid="dds--language-selector__select"]').should(
-      'have.length',
-      1
-    );
-
-    const languageSelector = cy.get(
-      '[data-autoid="dds--language-selector__select"]'
-    );
-    languageSelector.select('Arabic / عربية');
-    languageSelector.should('have.value', 'ar');
-
-    cy.screenshot();
-    cy.percySnapshot(
-      'Footer | Short language only (desktop language selector)',
-      {
-        widths: [320],
-      }
-    );
-  });
-});
-
-describe('Footer | Micro language only (mobile)', () => {
-  beforeEach(() => {
-    cy.visit(`/${_pathMicroLanguageOnly}`);
-    cy.viewport(320, 780);
-  });
-
-  it('should load language selector and be interactive', () => {
-    cy.get('[data-autoid="dds--language-selector__select"]').should(
-      'have.length',
-      1
-    );
-
-    const languageSelector = cy.get(
-      '[data-autoid="dds--language-selector__select"]'
-    );
-    languageSelector.select('Arabic / عربية');
-    languageSelector.should('have.value', 'ar');
-
-    cy.screenshot();
-    cy.percySnapshot(
-      'Footer | Micro language only (desktop language selector)',
-      {
-        widths: [320],
+        widths: [1280],
       }
     );
   });
