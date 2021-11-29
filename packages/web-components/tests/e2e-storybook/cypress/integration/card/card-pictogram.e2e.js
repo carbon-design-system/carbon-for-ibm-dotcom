0104c61ee14bd8547645ef65aec2512bc486ccc3
IgnacioBecerra <i1becerr@ucsd.edu>
2021-11-29 14:30:54 -0800

    chore(format): ran prettier

--- packages/web-components/tests/e2e-storybook/cypress/integration/card/card-pictogram.e2e.js
+++ packages/web-components/tests/e2e-storybook/cypress/integration/card/card-pictogram.e2e.js
@@ -12,39 +12,36 @@ import getCssPropertyForRule from '../../utils/get-css-property-for-rule';
  * @type {string}
  * @private
  */
- const _path = '/iframe.html?id=components-card--pictogram';
+const _path = '/iframe.html?id=components-card--pictogram';
 
- /* eslint-disable cypress/no-unnecessary-waiting */
- describe('dds-card | pictogram', () => {
-	 beforeEach(() => {
-		 cy.visit(`/${_path}`);
-		 cy.viewport(1280, 780);
-	 });
- 
-	 it("should check that pictogram is loaded and card is clickable", () => {
+/* eslint-disable cypress/no-unnecessary-waiting */
+describe('dds-card | pictogram', () => {
+  beforeEach(() => {
+    cy.visit(`/${_path}`);
+    cy.viewport(1280, 780);
+  });
 
-		cy.get('dds-card').should('have.attr', 'href');
+  it('should check that pictogram is loaded and card is clickable', () => {
+    cy.get('dds-card').should('have.attr', 'href');
     cy.takeSnapshots();
-	 });
+  });
 
-	 it("should check for pictogram at the top", () => {
-		cy.get('dds-card').should('have.attr', 'pictogram-placement', 'top');
-		cy.get('dds-card svg')
-		.then($content => {
-			expect($content[0].getBoundingClientRect().top).to.equal(32);
-			expect($content[0].getBoundingClientRect().bottom).to.equal(80);
-		});
-	 });
+  it('should check for pictogram at the top', () => {
+    cy.get('dds-card').should('have.attr', 'pictogram-placement', 'top');
+    cy.get('dds-card svg').then($content => {
+      expect($content[0].getBoundingClientRect().top).to.equal(32);
+      expect($content[0].getBoundingClientRect().bottom).to.equal(80);
+    });
+  });
 
-	 it("should have pictogram at the bottom with text showing on hover", () => {
-		cy.visit(`/${_path}&knob-Pictogram%20position:_PictogramCard=bottom`);
-		
-		cy.get('dds-card').should('have.attr', 'pictogram-placement', 'bottom');
-		cy.get('dds-card svg')
-		.then($content => {
-			expect($content[0].getBoundingClientRect().top).to.equal(186);
-			expect($content[0].getBoundingClientRect().bottom).to.equal(234);
-		});
+  it('should have pictogram at the bottom with text showing on hover', () => {
+    cy.visit(`/${_path}&knob-Pictogram%20position:_PictogramCard=bottom`);
+
+    cy.get('dds-card').should('have.attr', 'pictogram-placement', 'bottom');
+    cy.get('dds-card svg').then($content => {
+      expect($content[0].getBoundingClientRect().top).to.equal(186);
+      expect($content[0].getBoundingClientRect().bottom).to.equal(234);
+    });
 
     cy.get('dds-card').then($el => {
       const sheets = $el[0].shadowRoot.adoptedStyleSheets;
@@ -56,13 +53,13 @@ import getCssPropertyForRule from '../../utils/get-css-property-for-rule';
       );
       expect(hover).to.not.equal('none');
     });
-		cy.get('dds-card p').should('not.be.empty');
-		cy.takeSnapshots();
-	 });
+    cy.get('dds-card p').should('not.be.empty');
+    cy.takeSnapshots();
+  });
 
-	 it('should load correctly in all themes', () => {
+  it('should load correctly in all themes', () => {
     cy.visit(`/${_path}`);
     cy.viewport(1280, 780);
     cy.carbonThemesScreenshot();
   });
- });
+});
