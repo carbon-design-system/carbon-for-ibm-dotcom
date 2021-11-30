3a51de183020dd9a483f5e18c24f7d2309c76709
ariellagilmore <ariellalgilmore@gmail.com>
2021-11-30 15:32:34 -0800

    chore(card): cleanup e2e storybook tests

--- packages/web-components/tests/e2e-storybook/cypress/integration/card/card.e2e.js
+++ packages/web-components/tests/e2e-storybook/cypress/integration/card/card.e2e.js
@@ -7,6 +7,7 @@
 
 'use strict';
 
+import getCssPropertyForRule from '../../utils/get-css-property-for-rule';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings.js';
 import settings from 'carbon-components/es/globals/js/settings';
 
@@ -20,6 +21,14 @@ const { prefix } = settings;
  * @private
  */
 const _path = '/iframe.html?id=components-card--default';
+
+/**
+ * Defines the pictogram component variant path.
+ *
+ * @type {string}
+ * @private
+ */
+const _pathPictogram = '/iframe.html?id=components-card--pictogram';
 
 /**
  * Defines the static component variant path.
@@ -95,31 +104,37 @@ const _tests = {
       });
     });
   },
-  checkClickableCard: () => {
-    it('should check for link', () => {
-      cy.get(`${ddsPrefix}-card > ${ddsPrefix}-card-footer`)
-        .shadow()
-        .find(`a.${prefix}--card__footer`)
-        .then($link => {
-          const url = $link.prop('href');
-          expect(url).not.to.be.empty;
-        });
-    });
+  checkClickableCard: pictogram => {
+    if (pictogram) {
+      it('should check for link', () => {
+        cy.get(`${ddsPrefix}-card`).should('have.attr', 'href');
+      });
+    } else {
+      it('should check for link', () => {
+        cy.get(`${ddsPrefix}-card > ${ddsPrefix}-card-footer`)
+          .shadow()
+          .find(`a.${prefix}--card__footer`)
+          .then($link => {
+            const url = $link.prop('href');
+            expect(url).not.to.be.empty;
+          });
+      });
 
-    it("should check that the footer's pseudo class takes up entire card to be clickable", () => {
-      cy.get(`${ddsPrefix}-card > ${ddsPrefix}-card-footer`)
-        .shadow()
-        .find('a')
-        .then($els => {
-          const win = $els[0].ownerDocument.defaultView;
-          const after = win.getComputedStyle($els[0], ':after');
-          const positionValue = after.getPropertyValue('position');
-          const insetValue = after.getPropertyValue('inset');
+      it("should check that the footer's pseudo class takes up entire card to be clickable", () => {
+        cy.get(`${ddsPrefix}-card > ${ddsPrefix}-card-footer`)
+          .shadow()
+          .find('a')
+          .then($els => {
+            const win = $els[0].ownerDocument.defaultView;
+            const after = win.getComputedStyle($els[0], ':after');
+            const positionValue = after.getPropertyValue('position');
+            const insetValue = after.getPropertyValue('inset');
 
-          expect(positionValue).to.eq('absolute');
-          expect(insetValue).to.eq('0px');
-        });
-    });
+            expect(positionValue).to.eq('absolute');
+            expect(insetValue).to.eq('0px');
+          });
+      });
+    }
   },
   checkNonClickableCard: () => {
     it('should not respond to a click ', () => {
@@ -136,39 +151,85 @@ const _tests = {
         });
     });
   },
-  checkImageRenders: () => {
-    cy.get(_selectors.image).should('have.length', 1);
-    cy.takeSnapshots();
+  checkImageRenders: path => {
+    it('should render with image', () => {
+      cy.visit(`${path}&knob-Add%20image:_Card=true`);
+      cy.get(_selectors.image).should('have.length', 1);
+      cy.takeSnapshots();
+    });
   },
-  checkTagGroupRenders: () => {
-    cy.get(_selectors.tagGroup).should('have.length', 1);
-    cy.takeSnapshots();
+  checkTagGroupRenders: path => {
+    it('should render with tag group', () => {
+      cy.visit(`${path}&knob-Add%20tags:_Card=true`);
+      cy.get(_selectors.tagGroup).should('have.length', 1);
+      cy.takeSnapshots();
+    });
   },
-  checkOutlineRenders: () => {
-    cy.get(_selectorBase).should('have.attr', 'border');
-    // converted HEX var(--cds-ui-03, #e0e0e0) to RGB
-    cy.get(_selectorBase)
-      .should('have.css', 'border')
-      .and('equal', '1px solid rgb(224, 224, 224)');
+  checkOutlineRenders: path => {
+    it('should render with outline', () => {
+      cy.visit(path);
+      cy.get(_selectorBase).should('have.attr', 'border');
+      // converted HEX var(--cds-ui-03, #e0e0e0) to RGB
+      cy.get(_selectorBase)
+        .should('have.css', 'border')
+        .and('equal', '1px solid rgb(224, 224, 224)');
 
-    cy.get(_selectorBase).should('have.attr', 'color-scheme', 'light');
-    // converted HEX var(--cds-ui-02, #ffffff) to RGB
-    cy.get(_selectorBase)
-      .shadow()
-      .find(`.${prefix}--card__wrapper`)
-      .should('have.css', 'background-color')
-      .and('equal', 'rgb(255, 255, 255)');
+      cy.get(_selectorBase).should('have.attr', 'color-scheme', 'light');
+      // converted HEX var(--cds-ui-02, #ffffff) to RGB
+      cy.get(_selectorBase)
+        .shadow()
+        .find(`.${prefix}--card__wrapper`)
+        .should('have.css', 'background-color')
+        .and('equal', 'rgb(255, 255, 255)');
 
-    cy.takeSnapshots();
+      cy.takeSnapshots();
+    });
   },
-  checkInverseRenders: () => {
-    cy.get(_selectorBase).should('have.attr', 'color-scheme', 'inverse');
-    // converted HEX var(--cds-inverse-02, #393939) to RGB
-    cy.get(_selectorBase)
-      .should('have.css', 'background-color')
-      .and('equal', 'rgb(57, 57, 57)');
+  checkInverseRenders: path => {
+    it('should render with inverse', () => {
+      cy.visit(path);
+      cy.get(_selectorBase).should('have.attr', 'color-scheme', 'inverse');
+      // converted HEX var(--cds-inverse-02, #393939) to RGB
+      cy.get(_selectorBase)
+        .should('have.css', 'background-color')
+        .and('equal', 'rgb(57, 57, 57)');
 
-    cy.takeSnapshots();
+      cy.takeSnapshots();
+    });
+  },
+  pictogramPosition: position => {
+    if (position === 'top') {
+      it('should check for pictogram at the top', () => {
+        cy.get(`${ddsPrefix}-card`).should('have.attr', 'pictogram-placement', 'top');
+        cy.get(`${ddsPrefix}-card svg`).then($content => {
+          expect($content[0].getBoundingClientRect().top).to.equal(32);
+          expect($content[0].getBoundingClientRect().bottom).to.equal(80);
+        });
+      });
+    } else {
+      it('should check for pictogram at the bottom with text showing on hover', () => {
+        cy.visit(`/${_pathPictogram}&knob-Pictogram%20position:_PictogramCard=bottom`);
+
+        cy.get(`${ddsPrefix}-card`).should('have.attr', 'pictogram-placement', 'bottom');
+        cy.get(`${ddsPrefix}-card svg`).then($content => {
+          expect($content[0].getBoundingClientRect().top).to.equal(186);
+          expect($content[0].getBoundingClientRect().bottom).to.equal(234);
+        });
+
+        cy.get(`${ddsPrefix}-card`).then($el => {
+          const sheets = $el[0].shadowRoot.adoptedStyleSheets;
+
+          const hover = getCssPropertyForRule(
+            ':host(dds-card[pictogram-placement="bottom"]:hover) .bx--card__copy',
+            'display',
+            sheets
+          );
+          expect(hover).to.not.equal('none');
+        });
+        cy.get(`${ddsPrefix}-card p`).should('not.be.empty');
+        cy.takeSnapshots();
+      });
+    }
   },
 };
 
@@ -180,7 +241,24 @@ describe('dds-card | default (desktop)', () => {
 
   _tests.checkTextRenders();
   _tests.checkClickableCard();
+  _tests.checkImageRenders(_path);
+  _tests.checkTagGroupRenders(_path);
+  _tests.checkInverseRenders(`${_path}&knob-Card%20style:_Card=Inverse%20card`);
+  _tests.checkOutlineRenders(`${_path}&knob-Card%20style:_Card=Outlined%20card`);
+  it('should render correctly in all themes', _tests.screenshotThemes);
+});
 
+describe('dds-card | pictogram (desktop)', () => {
+  beforeEach(() => {
+    cy.visit(`/${_pathPictogram}`);
+    cy.viewport(1280, 780);
+  });
+
+  _tests.checkClickableCard(true);
+  _tests.pictogramPosition('top');
+  _tests.pictogramPosition('bottom');
+  _tests.checkInverseRenders(`${_pathPictogram}&knob-Card%20style:_PictogramCard=Inverse%20card`);
+  _tests.checkOutlineRenders(`${_pathPictogram}&knob-Card%20style:_PictogramCard=Outlined%20card`);
   it('should render correctly in all themes', _tests.screenshotThemes);
 });
 
@@ -192,69 +270,8 @@ describe('dds-card | static (desktop)', () => {
 
   _tests.checkTextRenders();
   _tests.checkClickableCard();
-
+  _tests.checkImageRenders(_pathStatic);
+  _tests.checkTagGroupRenders(_pathStatic);
+  _tests.checkOutlineRenders(`${_pathStatic}&knob-Outlined%20card_Card=true`);
   it('should render correctly in all themes', _tests.screenshotThemes);
-});
-
-describe('dds-card | default with image (desktop)', () => {
-  beforeEach(() => {
-    cy.viewport(1280, 780);
-    cy.visit(`${_path}&knob-Add%20image:_Card=true`);
-  });
-
-  it('should render with image', _tests.checkImageRenders);
-});
-
-describe('dds-card | static with image (desktop)', () => {
-  beforeEach(() => {
-    cy.viewport(1280, 780);
-    cy.visit(`${_pathStatic}&knob-Add%20image:_Card=true`);
-  });
-
-  it('should render with image', _tests.checkImageRenders);
-});
-
-describe('dds-card | default with tag group (desktop)', () => {
-  beforeEach(() => {
-    cy.viewport(1280, 780);
-    cy.visit(`${_path}&knob-Add%20tags:_Card=true`);
-  });
-
-  it('should render with tag group', _tests.checkTagGroupRenders);
-});
-
-describe('dds-card | static with tag group (desktop)', () => {
-  beforeEach(() => {
-    cy.viewport(1280, 780);
-    cy.visit(`${_pathStatic}&knob-Add%20tags:_Card=true`);
-  });
-
-  it('should render with tag group', _tests.checkTagGroupRenders);
-});
-
-describe('dds-card | default with outline (desktop)', () => {
-  beforeEach(() => {
-    cy.viewport(1280, 780);
-    cy.visit(`${_path}&knob-Card%20style:_Card=Outlined%20card`);
-  });
-
-  it('should render with outline', _tests.checkOutlineRenders);
-});
-
-describe('dds-card | static with outline (desktop)', () => {
-  beforeEach(() => {
-    cy.viewport(1280, 780);
-    cy.visit(`${_pathStatic}&knob-Outlined%20card_Card=true`);
-  });
-
-  it('should render with outline', _tests.checkOutlineRenders);
-});
-
-describe('dds-card | default with inverse (desktop)', () => {
-  beforeEach(() => {
-    cy.viewport(1280, 780);
-    cy.visit(`${_path}&knob-Card%20style:_Card=Inverse%20card`);
-  });
-
-  it('should render with inverse', _tests.checkInverseRenders);
 });
