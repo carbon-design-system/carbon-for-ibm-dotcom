983e054669bd8c0b2652a3d5bdbbe940a3a05475
John Kaeser <jakaeser44@gmail.com>
2021-12-06 09:42:10 -0500

    test(cta): add e2e tests for text and button variants (#7753, #7754)

---
+++ packages/web-components/tests/e2e-storybook/cypress/integration/cta/text.e2e.js
@@ -0,0 +1,62 @@
+/**
+ * Copyright IBM Corp. 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+import createTests from './_tests';
+
+/**
+ * Defines the text CTA variant path.
+ *
+ * @type {string}
+ * @private
+ */
+const _path = '/iframe.html?id=components-cta--text';
+
+/**
+ * Path definitions for the test creation function.
+ *
+ * @type {Object.<string, string>}
+ * @private
+ */
+const _paths = {
+  ctaType: `${_path}&knob-CTA%20type%20(cta-type)_TextCTA=`,
+};
+
+/**
+ * Defines the text CTA variant selector.
+ *
+ * @type {string}
+ * @private
+ */
+const _selector = '[data-autoid="dds--text-cta"]';
+
+/**
+ * An array of test scenarios.
+ *
+ * @type {Array<Function>}
+ * @private
+ */
+const _tests = createTests(_selector, _paths);
+
+describe('dds-cta | text (desktop)', () => {
+  beforeEach(() => {
+    cy.visit(_path);
+    cy.viewport(1280, 780);
+  });
+
+  _tests.forEach(test => test());
+});
+
+describe('dds-cta | text (mobile)', () => {
+  beforeEach(() => {
+    cy.visit(_path);
+    cy.viewport(325, 720);
+  });
+
+  _tests.forEach(test => test());
+});
