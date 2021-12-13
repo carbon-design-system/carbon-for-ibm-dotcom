d4a583e11f317b95c4eb6e630eb6f9e21db6252b
emyarod <afw5059@gmail.com>
2021-12-13 16:15:41 -0600

    test(LightboxMediaViewer): add embedded video player e2e tests

--- packages/react/tests/e2e-storybook/cypress/integration/LightboxMediaViewer/LightboxMediaViewer.e2e.js
+++ packages/react/tests/e2e-storybook/cypress/integration/LightboxMediaViewer/LightboxMediaViewer.e2e.js
@@ -13,6 +13,8 @@
  */
 const _defaultPath =
   '/iframe.html?id=components-lightbox-media-viewer--default';
+const _embeddedVideoPlayerPath =
+  '/iframe.html?id=components-lightbox-media-viewer--embedded-video-player';
 
 describe('LightboxMediaViewer | default', () => {
   beforeEach(() => {
@@ -79,5 +81,67 @@ describe('LightboxMediaViewer | default', () => {
     cy.visit(`/${_defaultPath}`);
     cy.viewport(1280, 780);
     cy.carbonThemesScreenshot();
+  });
+});
+
+describe('LightboxMediaViewer | embedded video player', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 780);
+    cy.visit(`/${_embeddedVideoPlayerPath}`);
+  });
+
+  it('should render with all elements', () => {
+    cy.get('.bx--video-player').should('have.length', 1);
+    cy.get('.bx--video-player__video').should('not.be.empty');
+    cy.get('[data-autoid="dds--lightbox-media-viewer__content__title"]').should(
+      'not.be.empty'
+    );
+    cy.get('[data-autoid="dds--lightbox-media-viewer__content__desc"]').should(
+      'not.be.empty'
+    );
+
+    cy.takeSnapshots();
+  });
+
+  it('should align elements correctly', () => {
+    cy.get('.bx--video-player__video').then($video => {
+      expect($video[0].getBoundingClientRect().left).to.equal(64);
+    });
+
+    cy.get('.bx--lightbox-media-viewer__content').then($content => {
+      expect($content[0].getBoundingClientRect().right).to.equal(1280 - 64);
+    });
+  });
+
+  it('should have interactive video controls', () => {
+    cy.get('.bx--video-player__video .controlsContainer').should(
+      'not.be.empty'
+    );
+
+    // play/pause
+    cy.get('.bx--video-player__video iframe')
+      .find('.playPauseBtn')
+      .should('not.be.empty');
+
+    // mute
+    cy.get('.bx--video-player__video iframe')
+      .find('button[title="Mute"]')
+      .should('not.be.empty');
+
+    // full screen
+    cy.get('.bx--video-player__video iframe')
+      .find('.fullScreenBtn')
+      .should('not.be.empty');
+
+    cy.takeSnapshots();
+  });
+
+  it('should load correctly in all themes', () => {
+    cy.carbonThemesScreenshot();
+  });
+
+  it('should close the modal when close button is clicked', () => {
+    cy.get('.bx--modal-close').click();
+    cy.takeSnapshots();
   });
 });
