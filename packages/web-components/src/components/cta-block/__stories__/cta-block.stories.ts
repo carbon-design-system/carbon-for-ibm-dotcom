carbon-design-system/carbon-for-ibm-dotcom

commit e2fb14d8541b688a14b9dbf69cf6cf21f848ff3d

fix(cta-section) fix various style errors at sm breakpoint (#7536)
* fix(cta-section): adjust spacing on heading and copy

* fix(cta-section): add hr styling

* fix(cta-section): add hr to sm, spacing adjustments

* fix(cta-section): correct bottom border and padding

* fix(cta-section): adjust heading and leading spacing

* fix(cta-section): remove changes to non-sm breakpoints

* fix(cta-section): more spacing adjustments

* fix(cta-section): remove bottom padding on item row

* fix(cta-section): even more spacing adjustments

Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

packages/web-components/src/components/cta-block/cta-block.scss
@@ -104,10 +104,19 @@
   grid-template-columns: 1fr;
   grid-template-rows: auto;
 
-  &:not(:first-of-type) {
-    @include carbon--breakpoint-down('md') {
-      padding-top: $carbon--layout-03;
+  ::slotted(#{$dds-prefix}-cta-block-item) {
+    margin-top: $spacing-07;
+    border-bottom: 1px solid $gray-20;
+
+    @include carbon--breakpoint('md') {
+      margin-top: 0;
+      border: 0;
     }
+  }
+
+  ::slotted(#{$dds-prefix}-cta-block-item:last-of-type) {
+    border: 0;
+    padding-bottom: 0;
   }
 
   @include carbon--breakpoint('md') {
@@ -116,10 +125,6 @@
 
   @include carbon--breakpoint('lg') {
     grid-template-columns: repeat(3, 1fr);
-  }
-
-  &.#{$prefix}--cta-block-item-row__border {
-    border-bottom: 1px solid $ui-03;
   }
 }
 
@@ -137,10 +142,6 @@
   position: relative;
   display: flex;
   flex-direction: column;
-
-  &:last-of-type {
-    padding-bottom: 0;
-  }
 
   ::slotted(#{$dds-prefix}-text-cta) {
     margin-left: 0;

packages/web-components/src/components/cta-section/__stories__/cta-section.stories.ts
@@ -116,23 +116,9 @@ const contentItemTypeOptions = {
 };
 
 const renderItems = (item, count) => {
-  if (count.length < 4) {
-    return html`
-      <dds-cta-block-item-row no-border>
-        ${count.map((_, index) => item({ ...content[index] }))}
-      </dds-cta-block-item-row>
-    `;
-  }
-
-  const itemArray = count;
-  const spliced = itemArray.splice(3);
-
   return html`
     <dds-cta-block-item-row>
-      ${itemArray.map((_, index) => item({ ...content[index] }))}
-    </dds-cta-block-item-row>
-    <dds-cta-block-item-row no-border>
-      ${spliced.map((_, index) => item({ ...content[index] }))}
+      ${count.map((_, index) => item({ ...content[index] }))}
     </dds-cta-block-item-row>
   `;
 };

packages/web-components/src/components/cta-section/cta-section.scss
@@ -25,23 +25,33 @@
   }
 
   .#{$prefix}--content-section__leading {
-    margin-top: $carbon--layout-04;
-    padding: $spacing-05;
-    padding-left: 0;
+    padding-bottom: 0;
+
+    @include carbon--breakpoint('md') {
+      margin-top: $carbon--layout-04;
+      padding: $spacing-05;
+      padding-bottom: 0;
+    }
 
     @include carbon--breakpoint('lg') {
       padding-left: $spacing-05;
-    }
-
-    padding-bottom: 0;
-    margin-bottom: $carbon--layout-01;
-
-    @include carbon--breakpoint('lg') {
       margin-bottom: $carbon--layout-04;
     }
   }
 
+  ::slotted(#{$dds-prefix}-content-section-heading) {
+    margin-bottom: $spacing-07;
+
+    @include carbon--breakpoint('md') {
+      margin-bottom: $spacing-06;
+    }
+  }
+
   ::slotted(#{$dds-prefix}-cta-block) {
-    padding-top: $carbon--layout-04;
+    padding-top: 0;
+
+    @include carbon--breakpoint('md') {
+      padding-top: $carbon--layout-04;
+    }
   }
 }

commit ab025c2f2f87ca938def098bcc58344f97b20d08

fix(cta-section): use correct bottom spacing and spacing between item rows (#7764)
### Related Ticket(s)

- Resolves https://github.com/carbon-design-system/carbon-for-ibm-dotcom/issues/7482
- Resolves https://github.com/carbon-design-system/carbon-for-ibm-dotcom/issues/7713

### Description

Incorrect layout and spacing tokens were used for certain parts of the `<dds-cta-block>` component, visible in the "CTA section with content items" story. This PR's main goal was to correct those spacing issues, but it also fixes a bug that was introduced in https://github.com/carbon-design-system/carbon-for-ibm-dotcom/commit/e2fb14d8541b688a14b9dbf69cf6cf21f848ff3d where the horizontal rules between item rows went missing.

### Changelog

**Added**

- Re-established horizontal rules between `<dds-cta-block-item-row>` rows beyond the `md` breakpoint.

**Changed**

- Adjusted space between children of `<dds-cta-block-item-row>` elements to match latest designs.
- Adjusted `<dds-cta-block>` bottom spacing to match latest designs.

<!-- React and Web Component deploy previews are enabled by default. -->
<!-- To enable additional available deploy previews, apply the following -->
<!-- labels for the corresponding package: -->
<!-- *** "package: services": Services -->
<!-- *** "package: utilities": Utilities -->
<!-- *** "RTL": React / Web Components (RTL) -->
<!-- *** "feature flag": React / Web Components (experimental) -->

packages/web-components/src/components/cta-block/__stories__/cta-block.stories.ts
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { number, select } from '@storybook/addon-knobs';
+import { boolean, number, select } from '@storybook/addon-knobs';
 import ArrowRight20 from 'carbon-web-components/es/icons/arrow--right/20';
 import Launch20 from 'carbon-web-components/es/icons/launch/20';
 import { html } from 'lit-element';
@@ -62,33 +62,19 @@ const contentItemTypeOptions = {
 };
 
 const renderItems = (item, count) => {
-  if (count.length < 4) {
-    return html`
-      <dds-cta-block-item-row no-border>
-        ${count.map((_, index) => item({ ...content[index] }))}
-      </dds-cta-block-item-row>
-    `;
-  }
-
-  const itemArray = count;
-  const spliced = itemArray.splice(3);
-
   return html`
-    <dds-cta-block-item-row>
-      ${itemArray.map((_, index) => item({ ...content[index] }))}
-    </dds-cta-block-item-row>
     <dds-cta-block-item-row no-border>
-      ${spliced.map((_, index) => item({ ...content[index] }))}
+      ${count.map((_, index) => item({ ...content[index] }))}
     </dds-cta-block-item-row>
   `;
 };
 
 export const Default = ({ parameters }) => {
-  const { heading, copy, renderIcon } = parameters?.props?.CTABlock ?? {};
+  const { heading, border, copy, renderIcon } = parameters?.props?.CTABlock ?? {};
   const target = renderIcon === iconMap.Launch20 ? '_blank' : '';
 
   return html`
-    <dds-cta-block no-border>
+    <dds-cta-block ?no-border=${!border}>
       <dds-content-block-heading>${ifNonNull(heading)}</dds-content-block-heading>
       <dds-content-block-copy>${copy}</dds-content-block-copy>
 
@@ -105,12 +91,14 @@ export const Default = ({ parameters }) => {
 };
 
 export const WithContentItems = ({ parameters }) => {
-  const { heading, copy, renderIcon } = parameters?.props?.CTABlock ?? {};
+  const { heading, border, copy, renderIcon } = parameters?.props?.CTABlock ?? {};
   const { contentItemType, contentItemCount } = parameters?.props?.WithContentItems ?? {};
   const target = renderIcon === iconMap.Launch20 ? '_blank' : '';
 
+  console.log('DEBUG:', border);
+
   return html`
-    <dds-cta-block no-border>
+    <dds-cta-block ?no-border=${!border}>
       <dds-content-block-heading>${ifNonNull(heading)}</dds-content-block-heading>
       <dds-content-block-copy>${ifNonNull(copy)}</dds-content-block-copy>
 
@@ -209,6 +197,7 @@ export default {
     knobs: {
       CTABlock: ({ groupId }) => ({
         heading: textNullable('Heading (required)', 'Take the next step', groupId),
+        border: boolean('Border', false, groupId),
         copy: 'Want to discuss your options with a DevOps expert? Contact our sales team to evaluate your needs.',
         renderIcon: iconMap[select(`Icon`, iconOptions, iconOptions.Default, groupId) ?? 0],
       }),

packages/web-components/src/components/cta-block/cta-block.scss
@@ -13,6 +13,18 @@
 
   display: block;
 
+  @include carbon--breakpoint('md') {
+    padding-bottom: $carbon--layout-03;
+  }
+
+  @include carbon--breakpoint('lg') {
+    padding-bottom: $carbon--layout-06;
+  }
+
+  &.#{$prefix}--cta-block__border {
+    border-bottom: 1px solid $ui-04;
+  }
+
   .#{$prefix}--content-layout {
     @include carbon--breakpoint('sm') {
       padding-left: $carbon--layout-01;
@@ -22,10 +34,6 @@
       margin-left: 0;
       margin-right: 0;
     }
-  }
-
-  &.#{$prefix}--cta-block__border {
-    border-bottom: 1px solid $ui-04;
   }
 
   ::slotted(#{$dds-prefix}-content-block-heading) {
@@ -49,14 +57,6 @@
 
     @include carbon--breakpoint('md') {
       width: 90%;
-    }
-  }
-
-  ::slotted([slot='action']) {
-    margin-bottom: $carbon--layout-03;
-
-    @include carbon--breakpoint('md') {
-      margin-bottom: $carbon--layout-05;
     }
   }
 
@@ -85,6 +85,10 @@
       margin-bottom: $carbon--layout-03;
     }
   }
+
+  ::slotted([slot='action']) {
+    margin-bottom: $carbon--spacing-10;
+  }
 }
 
 :host(#{$dds-prefix}-cta-section-item-heading) {
@@ -103,24 +107,37 @@
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: auto;
+  row-gap: $carbon--spacing-07;
+  position: relative;
 
-  ::slotted(#{$dds-prefix}-cta-block-item) {
-    margin-top: $spacing-07;
-    border-bottom: 1px solid $gray-20;
-
+  // Hide last row's items' bottom border
+  &::after {
     @include carbon--breakpoint('md') {
-      margin-top: 0;
-      border: 0;
+      content: '';
+      background-color: $ui-background;
+      height: 3px;
+      position: absolute;
+      bottom: -1px;
+      left: 0;
+      right: 0;
+      pointer-events: none;
     }
   }
 
+  ::slotted(#{$dds-prefix}-cta-block-item) {
+    border-bottom: 1px solid $ui-03;
+  }
+
   ::slotted(#{$dds-prefix}-cta-block-item:last-of-type) {
-    border: 0;
-    padding-bottom: 0;
+    @include carbon--breakpoint-down('md') {
+      border: 0;
+      padding-bottom: 0;
+    }
   }
 
   @include carbon--breakpoint('md') {
     grid-template-columns: repeat(2, 1fr);
+    row-gap: $carbon--spacing-10;
   }
 
   @include carbon--breakpoint('lg') {
@@ -142,6 +159,10 @@
   position: relative;
   display: flex;
   flex-direction: column;
+
+  @include carbon--breakpoint(md) {
+    padding-bottom: $carbon--spacing-10;
+  }
 
   ::slotted(#{$dds-prefix}-text-cta) {
     margin-left: 0;