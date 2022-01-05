d7d0e5e031179df873423abb092568f1e0d1da13
Kenny Lam <kennylam@protonmail.com>
2022-01-05 14:58:09 -0500

    chore(deps): upgrade marked to 1.2.9

--- packages/utilities/src/utilities/markdownToHtml/markdownToHtml.js
+++ packages/utilities/src/utilities/markdownToHtml/markdownToHtml.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2020, 2021
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -75,7 +75,10 @@ function markdownToHtml(
     },
   };
 
-  marked.use({ renderer: Object.assign(defaultRenderer, renderer) });
+  marked.use({
+    smartypants: true,
+    renderer: Object.assign(defaultRenderer, renderer),
+  });
 
   if (customTags) {
     DOMPurify.addHook('uponSanitizeElement', function(
