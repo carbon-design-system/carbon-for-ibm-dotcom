894a643a61be5ef07d4afdae1c0c012de8cbeac6
ariellagilmore <ariellalgilmore@gmail.com>
2022-01-10 12:44:25 -0800

    feat(achecker-compliance): remove console warning

--- packages/web-components/tests/utils/achecker-compliance.js
+++ packages/web-components/tests/utils/achecker-compliance.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -79,6 +79,7 @@ beforeEach(function() {
             );
             if (issues.length > 0) {
               if (process.env.AAT_VERBOSE) {
+                /* eslint-disable no-console */
                 console.error(
                   'a11y compliance issues:',
                   issues.map(issue => {
