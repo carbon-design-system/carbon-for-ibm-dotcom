6e6f39e8f4fcfb33130aed1fad3ea077965a0f9c
ariellagilmore <ariellalgilmore@gmail.com>
2022-01-11 08:32:31 -0800

    fix(karma): change to use test:a11y

--- packages/web-components/src/components/cta-block/__stories__/cta-block.stories.ts
+++ packages/web-components/src/components/cta-block/__stories__/cta-block.stories.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2021
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -128,7 +128,7 @@ WithContentItems.story = {
     propsSet: {
       default: {
         WithContentItems: {
-          contentItemType: contentItemTypeOptions.Text,
+          contentItemType: contentItemTypeMap[contentItemTypeOptions.Text],
           contentItemCount: Array(3),
         },
       },
