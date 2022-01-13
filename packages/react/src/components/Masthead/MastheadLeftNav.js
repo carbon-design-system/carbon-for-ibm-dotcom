ac9437f804d2513e826df38e2da99685e1eca8da
Kenny Lam <kennylam@protonmail.com>
2022-01-13 05:03:39 -0500

    fix(yarn): rebuild yarn.lock to fix mismatched versions

--- packages/react/src/components/Masthead/MastheadLeftNav.js
+++ packages/react/src/components/Masthead/MastheadLeftNav.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2016, 2021
+ * Copyright IBM Corp. 2016, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -220,6 +220,7 @@ const MastheadLeftNav = ({
       <nav
         data-autoid={`${stablePrefix}--masthead-${rest.navType}-sidenav__l0`}>
         {platform && (
+          // eslint-disable-next-line jsx-a11y/role-supports-aria-props
           <a
             data-autoid={`${stablePrefix}--masthead-${rest.navType}-sidenav__l0-productname`}
             href={platform.url}
