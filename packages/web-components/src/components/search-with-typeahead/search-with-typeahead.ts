56acb5421b31f6f952a9a5a3be3519dd0ff1273c
ariellagilmore <ariellalgilmore@gmail.com>
2022-01-26 15:35:57 -0800

    fix(search-with-typeahead): remove reflect: true in placeholder

--- packages/web-components/src/components/search-with-typeahead/search-with-typeahead.ts
+++ packages/web-components/src/components/search-with-typeahead/search-with-typeahead.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2021
+ * Copyright IBM Corp. 2019, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -579,7 +579,7 @@ class DDSSearchWithTypeahead extends HostListenerMixin(StableSelectorMixin(BXDro
   /**
    * Value to display when the input has an empty `value`.
    */
-  @property({ reflect: true })
+  @property()
   placeholder = 'Search all of IBM';
 
   /**
