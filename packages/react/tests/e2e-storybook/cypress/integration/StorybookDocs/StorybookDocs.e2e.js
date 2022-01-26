65f6691deb1c6683fa3105c2249c616208ac0412
Anna Wen <54281166+annawen1@users.noreply.github.com>
2022-01-26 16:17:15 -0500

    test(storybook): add e2e test for storybook docs

---
+++ packages/react/tests/e2e-storybook/cypress/integration/StorybookDocs/StorybookDocs.e2e.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright IBM Corp. 2021, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+let components;
+
+/* eslint-disable cypress/no-unnecessary-waiting */
+describe('Storybook Docs | default', () => {
+  it('should load all Storybook Docs without exceptions', () => {
+    cy.viewport(1280, 720);
+    cy.visit('/');
+    /**
+     * grab all components and get their href values which contain
+     * url to the corresponding docs
+     */
+    const components = cy.get('a[id*="explorercomponents"]');
+
+    components.each($component => {
+      const docsPath = $component.prop('href');
+
+      if (docsPath) {
+        const url = docsPath.split('?')[1].replace('path=/story', 'path=/docs');
+
+        cy.visit(`?${url}`);
+
+        cy.get('iframe').then($iframe => {
+          const $body = $iframe.contents().find('body');
+
+          cy.wrap($body)
+            .find('code[id="error-stack"]')
+            .children();
+        });
+      }
+    });
+  });
+});
