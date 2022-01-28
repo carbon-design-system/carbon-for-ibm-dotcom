2ced05eca53f24613c095b834ed65a2f3c3ed95b
ariellagilmore <ariellalgilmore@gmail.com>
2022-01-28 13:38:52 -0800

    fix(content-section): update child elements alignment to the top

--- packages/react/src/components/ContentSection/ContentSection.js
+++ packages/react/src/components/ContentSection/ContentSection.js
@@ -1,20 +1,23 @@
 /**
- * Copyright IBM Corp. 2016, 2021
+ * Copyright IBM Corp. 2016, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
+import { baseFontSize, breakpoints } from '@carbon/layout';
+import React, { useCallback, useEffect, useRef } from 'react';
 import classNames from 'classnames';
 import { CTA } from '../CTA';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
 import markdownToHtml from '@carbon/ibmdotcom-utilities/es/utilities/markdownToHtml/markdownToHtml';
 import PropTypes from 'prop-types';
-import React from 'react';
 import settings from 'carbon-components/es/globals/js/settings';
 
 const { stablePrefix } = ddsSettings;
 const { prefix } = settings;
+
+const breakpoint = parseFloat(breakpoints.lg.width) * baseFontSize;
 
 /**
  * ContentSection Component, for use with cardSection.
@@ -29,6 +32,77 @@ const ContentSection = ({
   childrenCustomClassName,
   ...otherProps
 }) => {
+  const containerRef = useRef();
+  const leftSectionRef = useRef();
+  const childrenRef = useRef();
+
+  /**
+   * Resize observer to trigger same height function.
+   *
+   * @private
+   */
+  const resizeObserver = useRef(null);
+
+  useEffect(() => {
+    resizeObserver.current = new ResizeObserver(resizeHandler);
+    resizeObserver.current.observe(document.documentElement);
+    return () => (resizeObserver.current = null);
+  }, [children, resizeHandler]);
+
+  /**
+   * Function that activates the sameHeight utility
+   */
+  const resizeHandler = useCallback(() => {
+    window.requestAnimationFrame(() => {
+      const { current: childrenNode } = childrenRef;
+      const { current: leftSectionNode } = leftSectionRef;
+      const { current: containerNode } = containerRef;
+
+      // gets first child element
+      const firstElem = childrenNode?.children[0];
+      const topPadding = firstElem
+        ? parseInt(
+            window
+              .getComputedStyle(firstElem.children[0], null)
+              .getPropertyValue('padding-top')
+          )
+        : 0;
+      const topMargin = firstElem
+        ? parseInt(
+            window
+              .getComputedStyle(firstElem.children[0], null)
+              .getPropertyValue('margin-top')
+          )
+        : 0;
+
+      // keeps styles for card-section-images and card-section-simple
+      if (firstElem && topPadding === 0 && topMargin === 0) {
+        containerNode.style.paddingTop = '3rem';
+        containerNode.style.paddingBottom = '4rem';
+        leftSectionNode.style.marginTop = '';
+        leftSectionNode.style.marginBottom = '';
+      } else if (window.innerWidth > breakpoint) {
+        if (!firstElem) {
+          containerNode.style.paddingTop = '1rem';
+          leftSectionNode.style.marginTop = '';
+          leftSectionNode.style.marginBottom = '';
+        } else {
+          leftSectionNode.style.marginTop = `${topPadding || topMargin}px`;
+          leftSectionNode.style.marginBottom = '';
+          containerNode.style.paddingTop = '';
+        }
+      } else if (!firstElem) {
+        containerNode.style.paddingTop = '1rem';
+        leftSectionNode.style.marginBottom = '';
+        leftSectionNode.style.marginTop = '';
+      } else {
+        leftSectionNode.style.marginBottom = `-${topPadding || topMargin}px`;
+        leftSectionNode.style.marginTop = '1rem';
+        containerNode.style.marginTop = '';
+      }
+    });
+  }, []);
+
   return (
     <section
       className={classNames(`${prefix}--content-section`, customClassName, {
@@ -38,10 +112,13 @@ const ContentSection = ({
         otherProps.autoid
           ? otherProps.autoid
           : `${stablePrefix}--content-section`
-      }>
+      }
+      ref={containerRef}>
       <div className={`${prefix}--content-section__grid`}>
         <div className={`${prefix}--content-section__row`}>
-          <div className={`${prefix}--content-section__left`}>
+          <div
+            className={`${prefix}--content-section__left`}
+            ref={leftSectionRef}>
             {heading && (
               <h2 className={`${prefix}--content-section__heading`}>
                 {heading}
@@ -69,7 +146,8 @@ const ContentSection = ({
             className={classNames(
               `${prefix}--content-section__children`,
               childrenCustomClassName
-            )}>
+            )}
+            ref={childrenRef}>
             {children}
           </div>
         </div>
--- packages/web-components/src/components/content-section/content-section.ts
+++ packages/web-components/src/components/content-section/content-section.ts
@@ -1,20 +1,23 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2021
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-import { customElement, html, LitElement, property } from 'lit-element';
+import { customElement, html, LitElement, property, query } from 'lit-element';
 import settings from 'carbon-components/es/globals/js/settings';
+import { baseFontSize, breakpoints } from '@carbon/layout';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings.js';
 import StableSelectorMixin from '../../globals/mixins/stable-selector';
 import styles from './content-section.scss';
 
 const { prefix } = settings;
 const { stablePrefix: ddsPrefix } = ddsSettings;
+
+const breakpoint = parseFloat(breakpoints.lg.width) * baseFontSize;
 
 /**
  * The Content Section component for use with cardSection
@@ -31,6 +34,92 @@ class DDSContentSection extends StableSelectorMixin(LitElement) {
   childrenCustomClass = '';
 
   /**
+   * The default slot.
+   */
+  @query('slot:not([name])')
+  private _slotNode?: HTMLSlotElement;
+
+  /**
+   * The observer for the resize of the viewport.
+   */
+  private _observerResizeRoot: any | null = null; // TODO: Wait for `.d.ts` update to support `ResizeObserver`
+
+  /**
+   * Cleans-up and creats the resize observer for the scrolling container.
+   *
+   * @param [options] The options.
+   * @param [options.create] `true` to create the new resize observer.
+   */
+  private _cleanAndCreateObserverResize({ create }: { create?: boolean } = {}) {
+    if (this._observerResizeRoot) {
+      this._observerResizeRoot.disconnect();
+      this._observerResizeRoot = null;
+    }
+    if (create) {
+      // TODO: Wait for `.d.ts` update to support `ResizeObserver`
+      // @ts-ignore
+      this._observerResizeRoot = new ResizeObserver(this._resizeHandler);
+      this._observerResizeRoot.observe(this);
+    }
+  }
+
+  /**
+   * Handles `slotchange` event.
+   */
+  protected _handleChildrenSlotChange() {
+    this._resizeHandler();
+  }
+
+  /**
+   * The observer for the resize of the viewport
+   */
+  private _resizeHandler = () => {
+    window.requestAnimationFrame(() => {
+      // gets first child element
+      const firstElem = this._slotNode!.assignedNodes().filter(
+        node => node.nodeType !== Node.TEXT_NODE || node!.textContent!.trim()
+      )[0];
+      const contentSection = this.shadowRoot!.querySelector('.bx--content-section') as HTMLElement;
+      const contentSectionLeading = this.shadowRoot!.querySelector('.bx--content-section__leading') as HTMLElement;
+
+      const topPadding = firstElem
+        ? parseInt(window.getComputedStyle(firstElem as HTMLElement, null).getPropertyValue('padding-top'), 10)
+        : 0;
+      const topMargin = firstElem
+        ? parseInt(window.getComputedStyle(firstElem as HTMLElement, null).getPropertyValue('margin-top'), 10)
+        : 0;
+
+      // keeps styles for card-section-images, card-section-simple, and link-list-section
+      if (contentSection && contentSectionLeading) {
+        if (firstElem && topPadding === 0 && topMargin === 0) {
+          contentSection.style.paddingTop = '3rem';
+          contentSection.style.paddingBottom = '4rem';
+          contentSectionLeading.style.marginTop = '';
+          contentSectionLeading.style.marginBottom = '';
+        } else if (window.innerWidth > breakpoint) {
+          if (!firstElem) {
+            contentSection.style.paddingTop = '1rem';
+            contentSectionLeading.style.marginTop = '';
+            contentSectionLeading.style.marginBottom = '';
+          } else {
+            contentSectionLeading.style.marginTop = `${topPadding || topMargin}px`;
+            contentSectionLeading.style.marginBottom = '';
+            contentSection.style.paddingTop = '';
+          }
+        } else if (!firstElem) {
+          contentSection.style.paddingTop = '1rem';
+          contentSectionLeading.style.marginBottom = '';
+          contentSectionLeading.style.marginTop = '';
+        } else {
+          contentSectionLeading.style.marginBottom = `-${topPadding || topMargin}px`;
+          contentSectionLeading.style.marginTop = '1rem';
+          contentSection.style.marginTop = '';
+        }
+      }
+    });
+  };
+
+  /**
    * Applies section attribute
    */
   connectedCallback() {
@@ -38,6 +127,21 @@ class DDSContentSection extends StableSelectorMixin(LitElement) {
       this.setAttribute('role', 'section');
     }
     super.connectedCallback();
+    this._cleanAndCreateObserverResize({ create: true });
+  }
+
+  disconnectedCallback() {
+    this._cleanAndCreateObserverResize();
+    super.disconnectedCallback();
+  }
+
+  firstUpdated() {
+    super.connectedCallback();
+    this._cleanAndCreateObserverResize({ create: true });
+  }
+
+  updated() {
+    this._resizeHandler();
   }
 
   render() {
@@ -49,7 +153,7 @@ class DDSContentSection extends StableSelectorMixin(LitElement) {
           <slot name="footer"></slot>
         </div>
         <div class="${prefix}--content-section__body ${this.childrenCustomClass}">
-          <slot></slot>
+          <slot @slotchange="${this._handleChildrenSlotChange}"></slot>
         </div>
       </div>
     `;
