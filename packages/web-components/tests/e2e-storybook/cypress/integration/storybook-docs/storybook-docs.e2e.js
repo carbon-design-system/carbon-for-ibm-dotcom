bd4fe90e2f6ce7070acc173dd4bd3fdc7d3b7337
Anna Wen <54281166+annawen1@users.noreply.github.com>
2022-02-02 12:41:52 -0500

    test(storybook): generate components fixture for wc test

--- packages/web-components/tests/e2e-storybook/cypress/integration/storybook-docs/storybook-docs.e2e.js
+++ packages/web-components/tests/e2e-storybook/cypress/integration/storybook-docs/storybook-docs.e2e.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2021, 2022
+ * Copyright IBM Corp. 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -7,37 +7,24 @@
 
 /* eslint-disable cypress/no-unnecessary-waiting */
 describe('storybook docs | default', () => {
-  it('should load all Storybook Docs without exceptions', () => {
-    cy.viewport(1280, 720);
-    cy.visit('/');
-    /**
-     * grab all components and get their href values which contain
-     * url to the corresponding docs
-     */
-    const components = cy.get('a[id*="explorercomponents"]');
+  const components = require('../../fixtures/components');
 
-    components.each($component => {
-      const docsPath = $component.prop('href');
+  components.forEach(component => {
+    it(`should load Storybook Docs tab for ${component} without exceptions`, () => {
+      cy.viewport(1280, 720);
+      cy.visit(`/?path=/docs/components-${component}--default`);
 
-      if (docsPath) {
-        const url = docsPath.split('?')[1].replace('path=/story', 'path=/docs');
+      const iframe = cy
+      .get('iframe')
+      .its('0.contentDocument.body')
+      .should('be.visible')
+      .then(cy.wrap);
 
-        cy.visit(`?${url}`);
+      iframe.within({}, $iframe => {
+        cy.get('code[id="error-stack"]').should('be.empty');
+      });
 
-        cy.wait(1000);
-
-        const iframe = cy
-          .get('iframe')
-          .its('0.contentDocument.body')
-          .should('be.visible')
-          .then(cy.wrap);
-
-        iframe.within({}, $iframe => {
-          cy.get('code[id="error-stack"]').should('be.empty');
-        });
-
-        cy.takeSnapshots();
-      }
+      cy.takeSnapshots();
     });
   });
 });
