67fde51195f9ca0600b5dbfef05578182cdeff23
IgnacioBecerra <i1becerr@ucsd.edu>
2022-02-07 08:17:40 -0800

    chore(format): ran prettier

--- packages/react/config/jest/setup.js
+++ packages/react/config/jest/setup.js
@@ -25,13 +25,13 @@ global.window.location = {
 // mock matchMedia to resolve JSOM error
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
   })),
 });
 
