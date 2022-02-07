341f416990c02aca933332c930f8b551e50ecdc3
Anna Wen <54281166+annawen1@users.noreply.github.com>
2022-02-07 14:19:37 -0500

    test(storybook): react storybook docs testing

--- packages/react/tests/e2e-storybook/cypress/integration/StorybookDocs/StorybookDocs.e2e.js
+++ packages/react/tests/e2e-storybook/cypress/integration/StorybookDocs/StorybookDocs.e2e.js
@@ -1,41 +1,30 @@
 /**
- * Copyright IBM Corp. 2021, 2022
+ * Copyright IBM Corp. 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 /* eslint-disable cypress/no-unnecessary-waiting */
-describe('Storybook Docs | default', () => {
-  it('should load all Storybook Docs without exceptions', () => {
-    cy.viewport(1280, 720);
-    cy.visit('/');
-    /**
-     * grab all components and get their href values which contain
-     * url to the corresponding docs
-     */
-    const components = cy.get('a[id*="explorercomponents"]');
+describe('storybook docs | default', () => {
+  const components = require('../../fixtures/components.json');
 
-    components.each($component => {
-      const docsPath = $component.prop('href');
+  components.forEach(component => {
+    it(`should load Storybook Docs tab for ${component.name} without exceptions`, () => {
+      cy.viewport(1280, 720);
+      cy.visit(`/?path=/docs/components-${component.url}--default`);
 
-      if (docsPath) {
-        const url = docsPath.split('?')[1].replace('path=/story', 'path=/docs');
+      const iframe = cy
+      .get('iframe')
+      .its('0.contentDocument.body')
+      .should('be.visible')
+      .then(cy.wrap);
 
-        cy.visit(`?${url}`);
+      iframe.within({}, $iframe => {
+        cy.get('code[id="error-stack"]').should('be.empty');
+      });
 
-        cy.wait(2000);
-
-        cy.get('iframe')
-          .its('0.contentDocument')
-          .should('exist')
-          .then(cy.wrap)
-          .within({}, $iframe => {
-            cy.get('code[id="error-stack"]').should('be.empty');
-          });
-
-        cy.takeSnapshots();
-      }
+      cy.takeSnapshots();
     });
   });
 });
