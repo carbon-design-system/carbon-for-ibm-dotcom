947b10e5d3dd1dfb88dc262de7836b9a2b670a94
Marlene Wanberg <marlene.wanberg@ibm.com>
2022-02-21 17:21:49 -0800

    fix(tabs-extended): wip grid fixes

--- packages/web-components/src/components/tabs-extended-media/__stories__/tabs-extended-media.stories.ts
+++ packages/web-components/src/components/tabs-extended-media/__stories__/tabs-extended-media.stories.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2021
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -225,7 +225,7 @@ export default {
     story => html`
       <div class="bx--grid">
         <div class="bx--row">
-          <div class="bx--col-lg-16 bx--no-gutter">
+          <div class="bx--col-lg-16">
             ${story()}
           </div>
         </div>
