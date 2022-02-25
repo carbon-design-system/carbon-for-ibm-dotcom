47e207c40f52d5d96a427e8b5f5679ed3213e0c2
Jeff Chew <jeff.chew@gmail.com>
2022-02-25 11:23:07 -0500

    fix(e2e): fix to fixtures in e2e integration tests (#8350)
    ### Related Ticket(s)
    
    No related issue
    
    ### Description
    
    This makes adjustments to the fixtures in cypress for the e2e integration tests to behave properly (like the e2e storybook tests).
    
    ### Changelog
    
    **Changed**
    
    - Moved cypress fixtures to load globally in all tests
    - Removed the custom command for mocking fixtures

--- packages/web-components/tests/e2e/cypress/support/commands.js
+++ packages/web-components/tests/e2e/cypress/support/commands.js
@@ -1,19 +1,6 @@
 /**
- * Copyright IBM Corp. 2021
+ * Copyright IBM Corp. 2021, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-/**
- * Mocks the Masthead/Footer data
- */
-Cypress.Commands.add('mockMastheadFooterData', () => {
-  cy.intercept('https://1.www.s81c.com/common/js/dynamicnav/www/countrylist/jsononly/usen-utf8.json', {
-    fixture: 'countrylist.json',
-  });
-  cy.intercept('https://1.www.s81c.com/common/carbon-for-ibm-dotcom/translations/masthead-footer/usen.json', {
-    fixture: 'translation.json',
-  });
-  cy.intercept('https://login.ibm.com/v1/mgmt/idaas/user/status/', { fixture: 'status.json' });
-});
