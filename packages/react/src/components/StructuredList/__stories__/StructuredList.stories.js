57661541f6faaffd20091a255602d0e2fd5584ff
Andy Blum <andy.blum.01@gmail.com>
2022-03-04 11:01:21 -0500

    Revert "create structured-list component, initial WC build"
    This reverts commit 4371e5edebc2518cfd7096072a980bf31bfd6c09.

--- packages/react/src/components/StructuredList/__stories__/StructuredList.stories.js
+++
@@ -1,3 +0,0 @@
-# Structured list
-
-This component is maintained in `@carbon/ibmdotcom-web-components` library with a [React wrapper](https://ibmdotcom-web-components-react.mybluemix.net/?path=/story/components-structured-list).
