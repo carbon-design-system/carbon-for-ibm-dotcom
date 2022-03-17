6ee8b233c8600458c654788ef05b0bc19aa86109
Marlene Wanberg <marlene.wanberg@ibm.com>
2022-03-16 21:34:24 -0700

    test(carousel): adding more scenarios for the carousel

--- packages/web-components/tests/e2e-storybook/cypress/integration/carousel/carousel.e2e.js
+++ packages/web-components/tests/e2e-storybook/cypress/integration/carousel/carousel.e2e.js
@@ -38,34 +38,17 @@ const _selectorBase = `[data-autoid="dds--carousel"]`;
  * @private
  */
 const _selectors = {
-  card: `[data-autoid="dds--card"`,
-  heading: `${_selectorBase} [data-autoid="dds--card-heading"]`,
+  card: `${_selectorBase} [data-autoid="dds--card"]`,
+  videoCard: `${_selectorBase} dds-video-cta-container`,
+  heading: `[data-autoid="dds--card-heading"]`,
   copy: `.bx--card__copy`,
-  footer: `${_selectorBase} [data-autoid="dds--card-footer"]`,
+  footer: `[data-autoid="dds--card-footer"]`,
+  videoFooter: `[data-autoid="dds--card-cta-footer"]`,
   image: `${_selectorBase} [data-autoid="dds--image"]`,
   video: `dds-card-cta-image`,
   buttonNext: `button[part="next-button"]`,
   buttonPrevious: `button[part="prev-button"]`,
 };
-
-/**
- * Helper function for determining if an element is in the viewport.
- */
- function isInViewport(el) {
-  cy.get(el)
-    .then($el => {
-      cy.window().then(window => {
-        const { documentElement } = window.document;
-        const bottom = documentElement.clientHeight;
-        const right = documentElement.clientWidth;
-        const rect = $el[0].getBoundingClientRect();
-        expect(rect.top).to.be.lessThan(bottom);
-        expect(rect.bottom).to.be.greaterThan(0);
-        expect(rect.right).to.be.greaterThan(0);
-        expect(rect.left).to.be.lessThan(right);
-      });
-    });
-}
 
 /**
  * Collection of test scenarios.
@@ -83,27 +66,35 @@ const _tests = {
     });
   },
   checkTextRenders: () => {
-    it('should render card text', () => {
+    it('should render card text and arrow icon', () => {
 
-      if (_selectorBase.children('dds-card')) {
-        cy.get(_selectors.heading).each($heading => {
-          expect($heading).should('be.visible');
-        });
+      cy.get(_selectors.heading).then($heading => {
+        expect($heading).not.to.be.empty;
+      });
 
-        cy.get(_selectors.copy).each($copy => {
-            expect($copy).should('be.visible');
-          });
+      cy.get(_selectors.copy).then($copy => {
+        expect($copy).not.to.be.empty;
+      });
 
-        cy.get(_selectors.footer)
-          .find('svg path')
-          .each($icon => {
-            expect($icon).to.have.attr('d', 'M11.8 2.8L10.8 3.8 16.2 9.3 1 9.3 1 10.7 16.2 10.7 10.8 16.2 11.8 17.2 19 10z');
-        });
+      cy.get(_selectorBase).then(($carousel) => {
+        if ($carousel.find(_selectors.footer).length > 0) {
+          cy.get(_selectors.footer)
+            .find('svg[slot="icon"] path')
+            .each($icon => {
+              expect($icon).to.have.attr('d', 'M11.8 2.8L10.8 3.8 16.2 9.3 1 9.3 1 10.7 16.2 10.7 10.8 16.2 11.8 17.2 19 10z');
+            });
+        }
 
-        cy.takeSnapshots();
-      } else if (_selectorBase.children('dds-video-cta-container')) {
-        console.log('this is a video');
-      }
+        if ($carousel.find(_selectors.videoFooter).length > 0) {
+          cy.get(_selectors.videoFooter)
+            .find('svg[slot="icon"] path')
+            .each($icon => {
+              expect($icon).to.have.attr('d', 'M11.8 2.8L10.8 3.8 16.2 9.3 1 9.3 1 10.7 16.2 10.7 10.8 16.2 11.8 17.2 19 10z');
+            });
+        }
+      });
+
+      cy.takeSnapshots();
     });
   },
   checkImageRenders: () => {
@@ -121,55 +112,77 @@ const _tests = {
         .should('be.visible');
 
       cy.get(_selectors.video)
-        .find('svg[slot="icon"]')
-        .should('be.visible');
+        .find('svg[slot="icon"] path')
+        .each($icon => {
+          expect($icon).to.have.attr('d', 'M26.5555476,43.111135 C26.0032708,43.111135 25.5555476,42.6633959 25.5555476,42.1111111 L25.5555476,20.1111111 C25.5541311,19.7531358 25.7441673,19.4217049 26.0538295,19.2421008 C26.3634918,19.0624967 26.745539,19.0621192 27.0555476,19.2411111 L46.0555476,30.2411111 C46.368866,30.4186723 46.5625038,30.7509842 46.5625038,31.1111111 C46.5625038,31.4712381 46.368866,31.8035499 46.0555476,31.9811111 L27.0555476,42.9811111 C26.9031526,43.0674916 26.7307319,43.1123209 26.5555476,43.111135 Z');
+        });
 
       cy.takeSnapshots();
+    });
+  },
+  checkVideoDurationText: () => {
+    it('should render the video duration in the footer', () => {
+      cy.get(_selectors.videoFooter)
+        .find('span.bx--card__cta__copy')
+        .then($duration => {
+          expect($duration).not.to.be.empty;
+        });
     });
   },
   checkSameHeight: () => {
     it('should have headings all the same height', () => {
       cy.get(_selectors.heading).then(($heading) => {
-        const headingHeight = $heading.first().height();
+        let headingHeightNum = $heading.first().height();
+        let headingHeight = headingHeightNum + 'px';
 
-        // $heading.each($heading => {
-        //   expect($heading).to.have.css('height', headingHeight);
-        // });
-
-        console.log('headingHeight: ' + headingHeight);
-      });
-
-      cy.get(_selectors.heading).each($heading => {
+        expect($heading).to.not.have.css('height', '0px');
         expect($heading).to.have.css('height', headingHeight);
       });
     });
   },
   checkClickableCard: () => {
-    it('should check for link', () => {
-      cy.get('dds-card > dds-card-footer')
-        .shadow()
-        .find('a.bx--card__footer')
-        .then($link => {
-          const url = $link.prop('href');
-          expect(url).not.to.be.empty;
-        });
-    });
-
     it("should check that the footer's pseudo class takes up entire card to be clickable", () => {
-      cy.get('dds-card > dds-card-footer')
-        .shadow()
-        .find('a')
-        .then($els => {
-          const win = $els[0].ownerDocument.defaultView;
-          const after = win.getComputedStyle($els[0], ':after');
-          const positionValue = after.getPropertyValue('position');
-          const insetValue = after.getPropertyValue('inset');
+      cy.get(_selectorBase).then(($carousel) => {
+        if ($carousel.find(_selectors.footer).length > 0) {
+          cy.get(_selectors.footer)
+            .shadow()
+            .find('a.bx--card__footer')
+            .then($link => {
+              const url = $link.prop('href');
+              expect(url).not.to.be.empty;
 
-          expect(positionValue).to.eq('absolute');
-          if (Cypress.browser.name !== 'firefox') {
-            expect(insetValue).to.eq('0px');
-          }
-        });
+              const win = $link[0].ownerDocument.defaultView;
+              const after = win.getComputedStyle($link[0], ':after');
+              const positionValue = after.getPropertyValue('position');
+              const insetValue = after.getPropertyValue('inset');
+
+              expect(positionValue).to.eq('absolute');
+              if (Cypress.browser.name !== 'firefox') {
+                expect(insetValue).to.eq('0px');
+              }
+            });
+        }
+
+        if ($carousel.find(_selectors.videoFooter).length > 0) {
+          cy.get(_selectors.videoFooter)
+            .shadow()
+            .find('a.bx--card__footer')
+            .then($link => {
+              const url = $link.prop('href');
+              expect(url).not.to.be.empty;
+
+              const win = $link[0].ownerDocument.defaultView;
+              const after = win.getComputedStyle($link[0], ':after');
+              const positionValue = after.getPropertyValue('position');
+              const insetValue = after.getPropertyValue('inset');
+
+              expect(positionValue).to.eq('absolute');
+              if (Cypress.browser.name !== 'firefox') {
+                expect(insetValue).to.eq('0px');
+              }
+            });
+        }
+      });
     });
   },
   checkScroll: () => {
@@ -185,69 +198,107 @@ const _tests = {
   },
 };
 
-// describe('dds-carousel | default (desktop)', () => {
-//   beforeEach(() => {
-//     cy.viewport(1280, 720);
-//     cy.visit(`${_paths.default}`);
-//     cy.injectAxe();
-//   });
+describe('dds-carousel | default (desktop)', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720);
+    cy.visit(`${_paths.default}`);
+    cy.injectAxe();
+  });
 
-//   it('should check a11y', _tests.checkA11y);
-//   it('should render correctly in all themes', _tests.screenshotThemes);
+  it('should check a11y', _tests.checkA11y);
+  it('should render correctly in all themes', _tests.screenshotThemes);
 
-//   _tests.checkTextRenders();
-//   _tests.checkClickableCard();
-//   _tests.checkScroll();
-// });
+  _tests.checkTextRenders();
+  _tests.checkSameHeight();
+  _tests.checkClickableCard();
+  _tests.checkScroll();
+});
 
-// describe('dds-carousel | default (mobile)', () => {
-//   beforeEach(() => {
-//     cy.viewport(320, 720);
-//     cy.visit(`/${_paths.default}`);
-//     cy.injectAxe();
-//   });
+describe('dds-carousel | default (mobile)', () => {
+  beforeEach(() => {
+    cy.viewport(320, 720);
+    cy.visit(`/${_paths.default}`);
+    cy.injectAxe();
+  });
 
-//   it('should check a11y', _tests.checkA11y);
-//   it('should render correctly in all themes', _tests.screenshotThemes);
+  it('should check a11y', _tests.checkA11y);
+  it('should render correctly in all themes', _tests.screenshotThemes);
 
-//   _tests.checkTextRenders();
-//   _tests.checkClickableCard();
-//   _tests.checkScroll();
-// });
+  _tests.checkTextRenders();
+  _tests.checkSameHeight();
+  _tests.checkClickableCard();
+  _tests.checkScroll();
+});
 
-// describe('dds-carousel | with images (desktop)', () => {
-//   beforeEach(() => {
-//     cy.viewport(1280, 720);
-//     cy.visit(`${_paths.withImages}`);
-//     cy.injectAxe();
-//   });
+describe('dds-carousel | with images (desktop)', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720);
+    cy.visit(`${_paths.withImages}`);
+    cy.injectAxe();
+  });
 
-  // it('should check a11y', _tests.checkA11y);
-  // it('should render correctly in all themes', _tests.screenshotThemes);
+  it('should check a11y', _tests.checkA11y);
+  it('should render correctly in all themes', _tests.screenshotThemes);
 
-  // _tests.checkTextRenders();
-  // _tests.checkImageRenders();
-  // _tests.checkSameHeight();
-  // _tests.checkClickableCard();
-  // _tests.checkScroll();
-// });
+  _tests.checkTextRenders();
+  _tests.checkImageRenders();
+  _tests.checkSameHeight();
+  _tests.checkClickableCard();
+  _tests.checkScroll();
+});
 
-// describe('dds-carousel | with videos (desktop)', () => {
-//   beforeEach(() => {
-//     cy.viewport(1280, 720);
-//     cy.visit(`${_paths.withVideos}`);
-//     cy.injectAxe();
-//   });
+describe('dds-carousel | with images (mobile)', () => {
+  beforeEach(() => {
+    cy.viewport(320, 720);
+    cy.visit(`${_paths.withImages}`);
+    cy.injectAxe();
+  });
 
-  // it('should check a11y', _tests.checkA11y);
-  // it('should render correctly in all themes', _tests.screenshotThemes);
+  it('should check a11y', _tests.checkA11y);
+  it('should render correctly in all themes', _tests.screenshotThemes);
 
-  // _tests.checkTextRenders();
-  // _tests.checkVideoRenders();
-  // _tests.checkSameHeight();
-  // _tests.checkClickableCard();
-  // _tests.checkScroll();
-// });
+  _tests.checkTextRenders();
+  _tests.checkImageRenders();
+  _tests.checkSameHeight();
+  _tests.checkClickableCard();
+  _tests.checkScroll();
+});
+
+describe('dds-carousel | with videos (desktop)', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720);
+    cy.visit(`${_paths.withVideos}`);
+    cy.injectAxe();
+  });
+
+  it('should check a11y', _tests.checkA11y);
+  it('should render correctly in all themes', _tests.screenshotThemes);
+
+  _tests.checkTextRenders();
+  _tests.checkVideoRenders();
+  _tests.checkVideoDurationText();
+  _tests.checkSameHeight();
+  _tests.checkClickableCard();
+  _tests.checkScroll();
+});
+
+describe('dds-carousel | with videos (mobile)', () => {
+  beforeEach(() => {
+    cy.viewport(320, 720);
+    cy.visit(`${_paths.withVideos}`);
+    cy.injectAxe();
+  });
+
+  it('should check a11y', _tests.checkA11y);
+  it('should render correctly in all themes', _tests.screenshotThemes);
+
+  _tests.checkTextRenders();
+  _tests.checkVideoRenders();
+  _tests.checkVideoDurationText();
+  _tests.checkSameHeight();
+  _tests.checkClickableCard();
+  _tests.checkScroll();
+});
 
 describe('dds-carousel | with media (desktop)', () => {
   beforeEach(() => {
@@ -256,13 +307,33 @@ describe('dds-carousel | with media (desktop)', () => {
     cy.injectAxe();
   });
 
-  // it('should check a11y', _tests.checkA11y);
-  // it('should render correctly in all themes', _tests.screenshotThemes);
+  it('should check a11y', _tests.checkA11y);
+  it('should render correctly in all themes', _tests.screenshotThemes);
 
   _tests.checkTextRenders();
-  // _tests.checkImageRenders();
-  // _tests.checkVideoRenders();
-  // _tests.checkSameHeight();
-  // _tests.checkClickableCard();
-  // _tests.checkScroll();
+  _tests.checkImageRenders();
+  _tests.checkVideoRenders();
+  _tests.checkVideoDurationText();
+  _tests.checkSameHeight();
+  _tests.checkClickableCard();
+  _tests.checkScroll();
+});
+
+describe('dds-carousel | with media (mobile)', () => {
+  beforeEach(() => {
+    cy.viewport(320, 720);
+    cy.visit(`${_paths.withMedia}`);
+    cy.injectAxe();
+  });
+
+  it('should check a11y', _tests.checkA11y);
+  it('should render correctly in all themes', _tests.screenshotThemes);
+
+  _tests.checkTextRenders();
+  _tests.checkImageRenders();
+  _tests.checkVideoRenders();
+  _tests.checkVideoDurationText();
+  _tests.checkSameHeight();
+  _tests.checkClickableCard();
+  _tests.checkScroll();
 });
