62b3360abdc35e32f026df0ba2f0b4b68ceb7a36
John Kaeser <jakaeser44@gmail.com>
2022-03-25 13:15:17 -0400

    fix(leaving-ibm): replace stories with configurable default (#8467)

--- packages/web-components/src/components/leaving-ibm/__stories__/leaving-ibm.stories.ts
+++ packages/web-components/src/components/leaving-ibm/__stories__/leaving-ibm.stories.ts
@@ -8,125 +8,73 @@
  */
 
 import { html } from 'lit-element';
-import { boolean, text } from '@storybook/addon-knobs';
+import { text, select } from '@storybook/addon-knobs';
+import Launch20 from 'carbon-web-components/es/icons/launch/20.js';
 import styles from './leaving-ibm.stories.scss';
+import imgLg1x1 from '../../../../../storybook-images/assets/720/fpo--1x1--720x720--002.jpg';
+import imgLg2x1 from '../../../../../storybook-images/assets/720/fpo--2x1--720x360--002.jpg';
+import imgMax2x1 from '../../../../../storybook-images/assets/1584/fpo--2x1--1312x656--002.jpg';
+import imgSm2x1 from '../../../../../storybook-images/assets/320/fpo--2x1--320x160--002.jpg';
+import imgXlg2x1 from '../../../../../storybook-images/assets/1312/fpo--2x1--1312x656--002.jpg';
 import '../leaving-ibm-container';
 
 import readme from './README.stories.mdx';
 
-const demoKnobs = groupId => ({
-  linkText: text('Link text', 'Learn more about Carbon', groupId),
-  href: text('href (href)', 'https://www.carbondesignsystem.com/all-about-carbon/what-is-carbon/', groupId),
-});
-
-const demoPropsSet = {
-  default: {
-    'leaving-ibm': {
-      open: true,
-      href: 'https://www.carbondesignsystem.com/all-about-carbon/what-is-carbon/',
-    },
-  },
-};
-
-export const WithHTMLLink = ({ parameters }) => {
-  const { linkText, href } = parameters?.props?.['leaving-ibm'];
-  return html`
-    <dds-leaving-ibm-container></dds-leaving-ibm-container>
-    <a href="${href}" data-leaving-ibm>
-      ${linkText}
-    </a>
-  `;
-};
-WithHTMLLink.story = {
-  title: 'Components/Leaving IBM/With HTML Link',
-  parameters: {
-    ...readme.parameters,
-    knobs: {
-      'leaving-ibm': ({ groupId }) => demoKnobs(groupId),
-    },
-    propsSet: demoPropsSet,
-  },
-};
-
-export const WithCarbonLink = ({ parameters }) => {
-  const { linkText, href } = parameters?.props?.['leaving-ibm'];
-  return html`
-    <dds-leaving-ibm-container></dds-leaving-ibm-container>
-    <bx-link href="${href}" data-leaving-ibm>
-      ${linkText}
-    </bx-link></li>
-  `;
-};
-
-WithCarbonLink.story = {
-  title: 'Components/Leaving IBM/With Carbon Link',
-  parameters: {
-    ...readme.parameters,
-    knobs: {
-      'leaving-ibm': ({ groupId }) => demoKnobs(groupId),
-    },
-    propsSet: demoPropsSet,
-  },
-};
-
-export const WithLinkList = ({ parameters }) => {
-  const { linkText, href } = parameters?.props?.['leaving-ibm'];
-  return html`
-    <dds-leaving-ibm-container></dds-leaving-ibm-container>
-    <dds-link-list type="default">
-      <dds-link-list-heading>Carbon Design System</dds-link-list-heading>
-      <dds-link-list-item-card-cta href="${href}" cta-type="external" data-leaving-ibm>
-        <p>${linkText}</p>
-        <dds-card-cta-footer></dds-card-cta-footer>
-      </dds-link-list-item-card-cta>
-    </dds-link-list>
-  `;
-};
-WithLinkList.story = {
-  title: 'Components/Leaving IBM/With Link List',
-  parameters: {
-    ...readme.parameters,
-    knobs: {
-      'leaving-ibm': ({ groupId }) => demoKnobs(groupId),
-    },
-    propsSet: demoPropsSet,
-  },
-};
-
-export const WithCTABlockItem = ({ parameters }) => {
-  const { linkText, href } = parameters?.props?.['leaving-ibm'];
-  return html`
-    <dds-leaving-ibm-container></dds-leaving-ibm-container>
-    <dds-cta-block-item-row>
-      <dds-cta-block-item>
-        <dds-content-item-heading>Lorem ipsum dolor sit amet</dds-content-item-heading>
-        <dds-content-item-copy
-          >Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</dds-content-item-copy
-        >
-        <dds-text-cta slot="footer" cta-type="external" icon-placement="right" href=${href} data-leaving-ibm>
-          ${linkText}
-        </dds-text-cta>
-      </dds-cta-block-item>
-    </dds-cta-block-item-row>
-  `;
-};
-WithCTABlockItem.story = {
-  title: 'Components/Leaving IBM/With CTA Block Item',
-  parameters: {
-    ...readme.parameters,
-    knobs: {
-      'leaving-ibm': ({ groupId }) => demoKnobs(groupId),
-    },
-    propsSet: demoPropsSet,
-  },
-};
-
 export const Default = ({ parameters }) => {
-  const { open, href } = parameters?.props?.['leaving-ibm'] ?? {};
+  const { href, ctaText, ctaType } = parameters?.props?.['leaving-ibm'] ?? {};
   return html`
-    <dds-leaving-ibm-container ?open=${open} href="${href}"> </dds-leaving-ibm-container>
+    <dds-leaving-ibm-container></dds-leaving-ibm-container>
+
+    ${ctaType === 'Link'
+      ? html`
+      <bx-link href="${href}" data-leaving-ibm>
+        ${ctaText}${Launch20({ slot: 'icon' })}
+      </bx-link></li>
+    `
+      : null}
+    ${ctaType === 'Button'
+      ? html`
+          <dds-button-group>
+            <dds-button-group-item href="${href}" data-leaving-ibm>
+              ${ctaText}${Launch20({ slot: 'icon' })}
+            </dds-button-group-item>
+          </dds-button-group>
+        `
+      : null}
+    ${ctaType === 'Card'
+      ? html`
+          <dds-card-link href="${href}" data-leaving-ibm>
+            <dds-card-link-heading>${ctaText}</dds-card-link-heading>
+            <p>Lorem ipsum dolor sit</p>
+            <dds-card-footer>
+              ${Launch20({ slot: 'icon' })}
+            </dds-card-footer>
+          </dds-card-link>
+        `
+      : null}
+    ${ctaType === 'Feature Card'
+      ? html`
+          <dds-feature-card size="large" href="${href}" data-leaving-ibm>
+            <dds-image slot="image" default-src="${imgLg1x1}">
+              <dds-image-item media="(min-width: 1312px)" srcset="${imgMax2x1}"> </dds-image-item>
+              <dds-image-item media="(min-width: 1056px)" srcset="${imgXlg2x1}"> </dds-image-item>
+              <dds-image-item media="(min-width: 991px)" srcset="${imgXlg2x1}"> </dds-image-item>
+              <dds-image-item media="(min-width: 672px)" srcset="${imgLg2x1}"> </dds-image-item>
+              <dds-image-item media="(min-width: 0px)" srcset="${imgSm2x1}"> </dds-image-item>
+            </dds-image>
+            <dds-card-eyebrow>Lorem ipsum</dds-card-eyebrow>
+            <dds-card-heading>${ctaText}</dds-card-heading>
+            <p>Lorem ipsum dolor sit amet</p>
+            <dds-feature-card-footer>
+              ${Launch20({ slot: 'icon' })}
+            </dds-feature-card-footer>
+          </dds-feature-card>
+        `
+      : null}
   `;
 };
+
+const ctaTypes = ['Link', 'Button', 'Card', 'Feature Card'];
 
 export default {
   title: 'Components/Leaving IBM',
@@ -148,15 +96,17 @@ export default {
     ...readme.parameters,
     knobs: {
       'leaving-ibm': ({ groupId }) => ({
-        open: boolean('open (open)', true, groupId),
+        ctaText: text('CTA text', 'Learn more about Carbon', groupId),
         href: text('href (href)', 'https://www.carbondesignsystem.com/all-about-carbon/what-is-carbon/', groupId),
+        ctaType: select('CTA type:', ctaTypes, ctaTypes[0], groupId),
       }),
     },
     propsSet: {
       default: {
         'leaving-ibm': {
-          open: true,
+          ctaText: 'Learn more about Carbon',
           href: 'https://www.carbondesignsystem.com/all-about-carbon/what-is-carbon/',
+          ctaType: 'Link',
         },
       },
     },
