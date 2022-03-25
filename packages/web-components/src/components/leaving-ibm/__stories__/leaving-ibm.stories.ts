711eafddf6d7873bddf2062d8d1cdc24682d4f41
John Kaeser <jakaeser44@gmail.com>
2022-03-25 13:21:16 -0400

    fix(leaving-ibm): use medium feature card as example (#8467)

--- packages/web-components/src/components/leaving-ibm/__stories__/leaving-ibm.stories.ts
+++ packages/web-components/src/components/leaving-ibm/__stories__/leaving-ibm.stories.ts
@@ -11,11 +11,7 @@ import { html } from 'lit-element';
 import { text, select } from '@storybook/addon-knobs';
 import Launch20 from 'carbon-web-components/es/icons/launch/20.js';
 import styles from './leaving-ibm.stories.scss';
-import imgLg1x1 from '../../../../../storybook-images/assets/720/fpo--1x1--720x720--002.jpg';
-import imgLg2x1 from '../../../../../storybook-images/assets/720/fpo--2x1--720x360--002.jpg';
-import imgMax2x1 from '../../../../../storybook-images/assets/1584/fpo--2x1--1312x656--002.jpg';
-import imgSm2x1 from '../../../../../storybook-images/assets/320/fpo--2x1--320x160--002.jpg';
-import imgXlg2x1 from '../../../../../storybook-images/assets/1312/fpo--2x1--1312x656--002.jpg';
+import mediumImgLg1x1 from '../../../../../storybook-images/assets/720/fpo--1x1--720x720--004.jpg';
 import '../leaving-ibm-container';
 
 import readme from './README.stories.mdx';
@@ -54,17 +50,9 @@ export const Default = ({ parameters }) => {
       : null}
     ${ctaType === 'Feature Card'
       ? html`
-          <dds-feature-card size="large" href="${href}" data-leaving-ibm>
-            <dds-image slot="image" default-src="${imgLg1x1}">
-              <dds-image-item media="(min-width: 1312px)" srcset="${imgMax2x1}"> </dds-image-item>
-              <dds-image-item media="(min-width: 1056px)" srcset="${imgXlg2x1}"> </dds-image-item>
-              <dds-image-item media="(min-width: 991px)" srcset="${imgXlg2x1}"> </dds-image-item>
-              <dds-image-item media="(min-width: 672px)" srcset="${imgLg2x1}"> </dds-image-item>
-              <dds-image-item media="(min-width: 0px)" srcset="${imgSm2x1}"> </dds-image-item>
-            </dds-image>
-            <dds-card-eyebrow>Lorem ipsum</dds-card-eyebrow>
+          <dds-feature-card href="${href}" data-leaving-ibm>
+            <dds-image slot="image" alt="Image alt text" default-src="${mediumImgLg1x1}"></dds-image>
             <dds-card-heading>${ctaText}</dds-card-heading>
-            <p>Lorem ipsum dolor sit amet</p>
             <dds-feature-card-footer>
               ${Launch20({ slot: 'icon' })}
             </dds-feature-card-footer>
@@ -85,7 +73,7 @@ export default {
       </style>
       <div class="bx--grid">
         <div class="bx--row">
-          <div class="bx--col-sm-4">
+          <div class="bx--col-sm-4 bx--col-lg-8 bx--no-gutter">
             ${story()}
           </div>
         </div>
