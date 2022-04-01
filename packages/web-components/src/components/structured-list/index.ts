0027943be85dffd7b192c267fe732ca91399ace2
Andy Blum <andy.blum.01@gmail.com>
2022-04-01 09:48:57 -0400

    feat(structured-list): extend component from carbon-web-components (#8493)
    ### Related Ticket(s)
    
    closes #8388 
    
    ### Description
    
    Adds structured list element to carbon-for-ibm-dotcom.
    
    ### Changelog
    
    **New**
    
    - dds-structured-list
    
    ### Remaining Tasks
    - [ ] Match styles to functional specs
    - [ ] Port WC to React

---
+++ packages/web-components/src/components/structured-list/index.ts
@@ -0,0 +1,17 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2020, 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import './structured-list';
+import './structured-list-head';
+import './structured-list-header-row';
+import './structured-list-header-cell';
+import './structured-list-body';
+import './structured-list-group';
+import './structured-list-row';
+import './structured-list-cell';
