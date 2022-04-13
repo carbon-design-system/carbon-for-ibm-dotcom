46ae134cfddbce2cf2b7e70c25fc11ad6cb0f607
John Kaeser <jakaeser44@gmail.com>
2022-04-13 11:50:54 -0400

    feat(pricing-table): establish base fixed header behavior (#8420)

--- packages/web-components/src/components/pricing-table/pricing-table-header-row.ts
+++ packages/web-components/src/components/pricing-table/pricing-table-header-row.ts
@@ -9,6 +9,9 @@
 
 import { customElement, html } from 'lit-element';
 import ddsSettings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings';
+import settings from 'carbon-components/es/globals/js/settings';
+import HostListenerMixin from 'carbon-web-components/es/globals/mixins/host-listener';
+import HostListener from 'carbon-web-components/es/globals/decorators/host-listener';
 import sameHeight from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/sameHeight/sameHeight';
 import DDSStructuredListHeaderRow from '../structured-list/structured-list-header-row';
 import DDSPricingTableHeaderCell from './pricing-table-header-cell';
@@ -16,10 +19,11 @@ import styles from './pricing-table.scss';
 import { setColumnWidth } from './utils';
 import { PRICING_TABLE_HEADER_CELL_TYPES } from './defs';
 
+const { prefix } = settings;
 const { stablePrefix: ddsPrefix } = ddsSettings;
 
 @customElement(`${ddsPrefix}-pricing-table-header-row`)
-class DDSPricingTableHeaderRow extends DDSStructuredListHeaderRow {
+class DDSPricingTableHeaderRow extends HostListenerMixin(DDSStructuredListHeaderRow) {
   /**
    * Array full of tag wrapper elements within header cells.
    */
@@ -28,17 +32,27 @@ class DDSPricingTableHeaderRow extends DDSStructuredListHeaderRow {
   /**
    * Observer that watches for viewport resizes.
    */
-  private _resizeObserver: any | null = null;
+  private _resizeObserver: ResizeObserver | null = null;
+
+  /**
+   * Tracks width of the parent element.
+   */
+  private maxWidth: string = 'none';
 
   private _createResizeObserver() {
     // TODO: Wait for `.d.ts` update to support `ResizeObserver`
     // @ts-ignore
-    this._resizeObserver = new ResizeObserver(this._setSameHeight);
+    this._resizeObserver = new ResizeObserver(() => {
+      this._setSameHeight();
+      this._setMaxWidth();
+    });
     this._resizeObserver.observe(this.ownerDocument!.documentElement);
   }
 
   private _cleanResizeObserver() {
-    this._resizeObserver.disconnect();
+    if (this._resizeObserver instanceof ResizeObserver) {
+      this._resizeObserver.disconnect();
+    }
     this._resizeObserver = null;
   }
 
@@ -47,6 +61,11 @@ class DDSPricingTableHeaderRow extends DDSStructuredListHeaderRow {
       sameHeight(this._tagWrappers);
     });
   };
+
+  private _setMaxWidth() {
+    this.maxWidth = `${this.parentElement?.clientWidth}px`;
+    this.style.setProperty('--max-width', this.maxWidth);
+  }
 
   protected _handleSlotChange(e) {
     setColumnWidth(this);
@@ -72,6 +91,40 @@ class DDSPricingTableHeaderRow extends DDSStructuredListHeaderRow {
     this._setSameHeight();
   }
 
+  private _setSticky() {
+    const { rowStickyClass, cellStickyClass } = this.constructor as typeof DDSPricingTableHeaderRow;
+
+    this.classList.add(rowStickyClass);
+    Array.from(this.children).forEach(child => {
+      child.classList.add(cellStickyClass);
+    });
+    if (this.parentElement instanceof HTMLElement) {
+      this.parentElement.style.marginTop = `${this.getBoundingClientRect().height}px`;
+    }
+  }
+
+  private _unsetSticky() {
+    const { rowStickyClass, cellStickyClass } = this.constructor as typeof DDSPricingTableHeaderRow;
+
+    this.classList.remove(rowStickyClass);
+    Array.from(this.children).forEach(child => {
+      child.classList.remove(cellStickyClass);
+    });
+    if (this.parentElement instanceof HTMLElement) {
+      this.parentElement.style.marginTop = '';
+    }
+    this._setSameHeight();
+  }
+
+  @HostListener('window:scroll')
+  protected _handleScroll = () => {
+    if (this.offsetTop < window.pageYOffset) {
+      this._setSticky();
+    } else {
+      this._unsetSticky();
+    }
+  };
+
   connectedCallback() {
     super.connectedCallback();
     this._createResizeObserver();
@@ -90,6 +143,14 @@ class DDSPricingTableHeaderRow extends DDSStructuredListHeaderRow {
 
   static get stableSelector() {
     return `${ddsPrefix}--pricing-table-header-row`;
+  }
+
+  static get rowStickyClass() {
+    return `${prefix}--pricing-table-header-row--sticky`;
+  }
+
+  static get cellStickyClass() {
+    return `${prefix}--pricing-table-header-cell--sticky`;
   }
 
   static styles = styles;
