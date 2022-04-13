8b35c9d87d7f419180df9d804fbfa90a058a22d5
Kenny Lam <kennylam@protonmail.com>
2022-04-13 17:42:08 -0400

    chore(deps): upgrade rollup and plugins

--- packages/utilities/scripts/build.js
+++ packages/utilities/scripts/build.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -8,12 +8,7 @@
 'use strict';
 
 const { execSync } = require('child_process');
-const { inInstall } = require('in-publish');
 const path = require('path');
-
-if (inInstall()) {
-  process.exit(0);
-}
 
 const babelPath = path
   .resolve(__dirname, '../node_modules/.bin/babel')
