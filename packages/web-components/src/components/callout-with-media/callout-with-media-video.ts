3c9061b49ec0ff888d5366360406b7ec6046af84
Ignacio Becerra <i1becerr@ucsd.edu>
2022-05-18 13:28:08 -0700

    feat(callout-with-media): introducing react wrapper (#8823)
    ### Related Ticket(s)
    #8615
    
    ### Description
    This PR creates the Callout with Media React Wrapper, with its Storybook and Code Sandbox examples.
    
    ### Changelog
    
    **New**
    
    - CalloutWithMedia react wrapper and code sandbox examples
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "test: e2e": Codesandbox examples and e2e integration tests -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/src/components/callout-with-media/callout-with-media-video.ts
+++ packages/web-components/src/components/callout-with-media/callout-with-media-video.ts
@@ -30,4 +30,5 @@ class DDSCalloutWithMediaVideo extends DDSVideoPlayerContainer {
   static styles = styles;
 }
 
+/* @__GENERATE_REACT_CUSTOM_ELEMENT_TYPE__ */
 export default DDSCalloutWithMediaVideo;
