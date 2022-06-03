79f8a1891b933119b083b059231e10fc122887d2
kennylam <kennylam@protonmail.com>
2022-06-03 15:50:31 -0400

    chore(deps): upgrade isomorphic-dompurify to 0.19.0 (#8946)
    ### Description
    
    Upgrades `isomorphic-dompurify` version to latest (`0.19.0`)
    
    ### Changelog
    
    **New**
    
    - add `css` output dirs to `stylelintignore`
    
    **Changed**
    
    - upgrade `isomorphic-dompurify` to `0.19.0`
    
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "test: e2e": Codesandbox examples and e2e integration tests -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/services/config/jest/setup.js
+++ packages/services/config/jest/setup.js
@@ -1,11 +1,13 @@
 /**
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import 'babel-polyfill';
+
+import { TextDecoder, TextEncoder } from 'util';
 
 jest.unmock('promise');
 jest.unmock('whatwg-fetch');
@@ -14,6 +16,10 @@ jest.unmock('object-assign');
 jest.useFakeTimers();
 
 global.__DEV__ = true;
+
+// required for latest isomorphic-dompurify
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 require('../../../../tasks/jest/env');
 require('../polyfills');
