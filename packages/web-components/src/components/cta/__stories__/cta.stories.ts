e8fa6fe83fa2d1f3de497ba709d617ffa792b81d
Andy Blum <andy.blum.01@gmail.com>
2022-06-09 16:24:43 -0400

    feat(cta): update react-wrapper storybook

--- packages/web-components/src/components/cta/__stories__/cta.stories.ts
+++ packages/web-components/src/components/cta/__stories__/cta.stories.ts
@@ -20,56 +20,10 @@ import { select, boolean } from '@storybook/addon-knobs';
 import { icons as ctaIcons } from '../../../component-mixins/cta/cta';
 // eslint-disable-next-line sort-imports
 import { CTA_TYPE } from '../defs';
+import { hrefsForType, knobNamesForType, footerKnobNamesForType, types } from './ctaTypeConfig';
 import imgLg1x1 from '../../../../../storybook-images/assets/720/fpo--1x1--720x720--001.jpg';
 import readme from './README.stories.mdx';
 import textNullable from '../../../../.storybook/knob-text-nullable';
-
-const hrefsForType = {
-  [CTA_TYPE.REGULAR]: 'https://www.example.com',
-  [CTA_TYPE.LOCAL]: 'https://www.example.com',
-  [CTA_TYPE.JUMP]: '#example',
-  [CTA_TYPE.EXTERNAL]: 'https://www.example.com',
-  [CTA_TYPE.NEW_TAB]: 'https://www.example.com',
-  [CTA_TYPE.DOWNLOAD]: 'https://www.ibm.com/annualreport/assets/downloads/IBM_Annual_Report_2019.pdf',
-  [CTA_TYPE.VIDEO]: '1_9h94wo6b',
-  [CTA_TYPE.PDF]: 'https://www.ibm.com/annualreport/assets/downloads/IBM_Annual_Report_2019.pdf',
-  [CTA_TYPE.BLOG]: 'https://www.example.com',
-};
-
-const knobNamesForType = {
-  [CTA_TYPE.REGULAR]: 'Content link href (href)',
-  [CTA_TYPE.LOCAL]: 'Content link href (href)',
-  [CTA_TYPE.JUMP]: 'Anchor href (href)',
-  [CTA_TYPE.EXTERNAL]: 'Content link href (href)',
-  [CTA_TYPE.NEW_TAB]: 'Content link href (href)',
-  [CTA_TYPE.DOWNLOAD]: 'Download link href (href)',
-  [CTA_TYPE.VIDEO]: 'Video ID (href)',
-  [CTA_TYPE.PDF]: 'Download link href (href)',
-  [CTA_TYPE.BLOG]: 'Content link href (href)',
-};
-
-const footerKnobNamesForType = {
-  [CTA_TYPE.REGULAR]: 'Content link href (href)',
-  [CTA_TYPE.LOCAL]: 'Content link href (href)',
-  [CTA_TYPE.JUMP]: 'Anchor href (href)',
-  [CTA_TYPE.EXTERNAL]: 'Content link href (href)',
-  [CTA_TYPE.NEW_TAB]: 'Content link href (href)',
-  [CTA_TYPE.DOWNLOAD]: 'Download link href (href)',
-  [CTA_TYPE.VIDEO]: 'Video ID (href)',
-  [CTA_TYPE.PDF]: 'Download link href (href)',
-  [CTA_TYPE.BLOG]: 'Content link href (href)',
-};
-
-const types = {
-  [`Local (${CTA_TYPE.LOCAL})`]: CTA_TYPE.LOCAL,
-  [`Jump (${CTA_TYPE.JUMP})`]: CTA_TYPE.JUMP,
-  [`External (${CTA_TYPE.EXTERNAL})`]: CTA_TYPE.EXTERNAL,
-  [`New Tab (${CTA_TYPE.NEW_TAB})`]: CTA_TYPE.NEW_TAB,
-  [`Download (${CTA_TYPE.DOWNLOAD})`]: CTA_TYPE.DOWNLOAD,
-  [`Video (${CTA_TYPE.VIDEO})`]: CTA_TYPE.VIDEO,
-  [`PDF (${CTA_TYPE.PDF})`]: CTA_TYPE.PDF,
-  [`Blog (${CTA_TYPE.BLOG})`]: CTA_TYPE.BLOG,
-};
 
 export const Text = ({ parameters }) => {
   const { copy, ctaType, download, href, customVideoTitle, customVideoDescription } = parameters?.props?.TextCTA ?? {};
