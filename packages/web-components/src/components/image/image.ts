785a633f0cc8c902c8b64882d76fcb60dadfbe99
Abdón Rodríguez Davila <a@abdonrd.com>
2022-07-15 01:25:43 +0200

    fix: add file extensions in web-components: carbon-components/es

--- packages/web-components/src/components/image/image.ts
+++ packages/web-components/src/components/image/image.ts
@@ -10,7 +10,7 @@
 import { html, property, customElement, LitElement, state } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map.js';
 import settings from 'carbon-components/es/globals/js/settings.js';
-import on from 'carbon-components/es/globals/js/misc/on';
+import on from 'carbon-components/es/globals/js/misc/on.js';
 import ifNonNull from 'carbon-web-components/es/globals/directives/if-non-null.js';
 import FocusMixin from 'carbon-web-components/es/globals/mixins/focus.js';
 import '../expressive-modal/expressive-modal';
