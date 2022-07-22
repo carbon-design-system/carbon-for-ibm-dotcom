2a10135e75903e161d6b652f5459d5559abca9a7
IgnacioBecerra <i1becerr@ucsd.edu>
2022-07-21 17:19:07 -0700

    fix(storybook): fix dotcom shell story

--- packages/react/src/components/DotcomShell/__stories__/DotcomShell.stories.js
+++ packages/react/src/components/DotcomShell/__stories__/DotcomShell.stories.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { select, object } from '@storybook/addon-knobs';
+import { boolean, select, object } from '@storybook/addon-knobs';
 import Content from './data/content';
 import DotcomShell from '../DotcomShell';
 import { Micro as footerMicroStory } from '../../Footer/__stories__/Footer.stories.js';
@@ -22,39 +22,189 @@ const footerTypeOptions = {
   micro: 'micro',
 };
 
+const props = {
+  default: () => {
+    const { Masthead: mastheadKnobs } = mastheadStory.story.parameters.knobs;
+    const disableLocaleButton = boolean(
+      'hide the locale button (disableLocaleButton)',
+      false,
+      'Footer'
+    );
+    return {
+      mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
+      footerProps: {
+        disableLocaleButton,
+        type: select(
+          'Footer (footerProps): sets the type of footer (type)',
+          footerTypeOptions,
+          footerTypeOptions.default,
+          'Footer'
+        ),
+      },
+    };
+  },
+
+  defaultLanguageOnly: () => {
+    const languageOnly = true;
+    const { Masthead: mastheadKnobs } = mastheadStory.story.parameters.knobs;
+    const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
+    return {
+      mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
+      footerProps: {
+        ...footerKnobs({ groupId: 'Footer' }),
+        disableLocaleButton: boolean(
+          'hide the locale button (disableLocaleButton)',
+          false
+        ),
+        languageOnly,
+        languageItems: !languageOnly
+          ? undefined
+          : object('language dropdown items (languageItems)', languageItems),
+        languageInitialItem: { id: 'en', text: 'English' },
+      },
+    };
+  },
+
+  searchOpenByDefault: () => {
+    const { Masthead: mastheadKnobs } = mastheadStory.story.parameters.knobs;
+    const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
+    return {
+      mastheadProps: {
+        ...mastheadKnobs({ groupId: 'Masthead' }),
+        searchOpenOnload: true,
+      },
+      footerProps: {
+        ...footerKnobs({ groupId: 'Footer' }),
+        disableLocaleButton: boolean(
+          'hide the locale button (disableLocaleButton)',
+          false
+        ),
+      },
+    };
+  },
+
+  withPlatform: () => {
+    const { Masthead: mastheadKnobs } = mastheadStory.story.parameters.knobs;
+    const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
+    return {
+      mastheadProps: {
+        ...mastheadKnobs({ groupId: 'Masthead' }),
+        platform: {
+          name: 'IBM Cloud',
+          url: 'https://www.ibm.com/cloud',
+        },
+      },
+      footerProps: {
+        ...footerKnobs({ groupId: 'Footer' }),
+        disableLocaleButton: boolean(
+          'hide the locale button (disableLocaleButton)',
+          false
+        ),
+      },
+    };
+  },
+
+  shortFooter: () => {
+    const { Masthead: mastheadKnobs } = mastheadStory.story.parameters.knobs;
+    const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
+    return {
+      mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
+      footerProps: {
+        ...footerKnobs({ groupId: 'Footer' }),
+        disableLocaleButton: boolean(
+          'hide the locale button (disableLocaleButton)',
+          false
+        ),
+        type: 'short',
+      },
+    };
+  },
+
+  shortFooterLanguageOnly: () => {
+    const languageOnly = true;
+    const { Masthead: mastheadKnobs } = mastheadStory.story.parameters.knobs;
+    const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
+    return {
+      mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
+      footerProps: {
+        ...footerKnobs({ groupId: 'Footer' }),
+        type: 'short',
+        disableLocaleButton: boolean(
+          'hide the locale button (disableLocaleButton)',
+          false
+        ),
+        languageOnly,
+        languageItems: !languageOnly
+          ? undefined
+          : object('language dropdown items (languageItems)', languageItems),
+        languageInitialItem: { id: 'en', text: 'English' },
+      },
+    };
+  },
+
+  microFooter: () => {
+    const { Masthead: mastheadKnobs } = mastheadStory.story.parameters.knobs;
+    const { Footer: footerKnobs } = footerMicroStory.story.parameters.knobs;
+    const disableLocaleButton = boolean(
+      'hide the locale button (disableLocaleButton)',
+      false
+    );
+    return {
+      mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
+      footerProps: {
+        ...footerKnobs({ groupId: 'Footer' }),
+        disableLocaleButton,
+        type: 'micro',
+      },
+    };
+  },
+
+  microFooterLanguageOnly: () => {
+    const languageOnly = true;
+    const { Masthead: mastheadKnobs } = mastheadStory.story.parameters.knobs;
+    const { Footer: footerKnobs } = footerMicroStory.story.parameters.knobs;
+    return {
+      mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
+      footerProps: {
+        ...footerKnobs({ groupId: 'Footer' }),
+        type: 'micro',
+        languageOnly,
+        languageItems: !languageOnly
+          ? undefined
+          : object('language dropdown items (languageItems)', languageItems),
+        languageInitialItem: { id: 'en', text: 'English' },
+      },
+    };
+  },
+
+  withL1: () => {
+    const { Masthead: mastheadKnobs } = l1Story.story.parameters.knobs;
+    return {
+      platform: mastheadKnobs.l1Platform,
+      mastheadProps: {
+        ...mastheadKnobs({ groupId: 'Masthead' }),
+      },
+    };
+  },
+};
+
 export default {
   title: 'Components/Dotcom shell',
   parameters: {
     ...readme.parameters,
     'carbon-theme': { disabled: true },
-    knobs: {
-      escapeHTML: false,
-      DotcomShell: () => {
-        const {
-          Masthead: mastheadKnobs,
-        } = mastheadStory.story.parameters.knobs;
-        const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
-        return {
-          mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
-          footerProps: {
-            ...footerKnobs({ groupId: 'Footer' }),
-            type: select(
-              'Footer (footerProps): sets the type of footer (type)',
-              footerTypeOptions,
-              footerTypeOptions.default,
-              'Footer'
-            ),
-          },
-        };
-      },
-    },
   },
 };
 
-export const Default = ({ parameters }) => {
-  const { mastheadProps, footerProps } = parameters?.props?.DotcomShell ?? {};
+export const Default = () => {
+  const { mastheadProps, footerProps } = props.default() ?? {};
+
+  console.log(props.default());
   return (
-    <DotcomShell mastheadProps={mastheadProps} footerProps={footerProps}>
+    <DotcomShell
+      mastheadProps={mastheadProps}
+      footerProps={footerProps}
+      {...props.default()}>
       <main id="main-content">
         <Content withL1={!!mastheadProps.mastheadL1Data} />
       </main>
@@ -62,240 +212,185 @@ export const Default = ({ parameters }) => {
   );
 };
 
-export const DefaultLanguageOnly = ({ parameters }) => (
-  <Default parameters={parameters} />
-);
+export const DefaultLanguageOnly = () => {
+  const { mastheadProps, footerProps } = props.defaultLanguageOnly() ?? {};
+  return (
+    <DotcomShell
+      mastheadProps={mastheadProps}
+      footerProps={footerProps}
+      {...props.defaultLanguageOnly()}>
+      <main id="main-content">
+        <Content withL1={!!mastheadProps.mastheadL1Data} />
+      </main>
+    </DotcomShell>
+  );
+};
 
 DefaultLanguageOnly.story = {
   name: 'Default (footer language only)',
   parameters: {
     knobs: {
       escapeHTML: false,
-      DotcomShell: ({ groupId }) => {
-        const languageOnly = true;
-        const {
-          Masthead: mastheadKnobs,
-        } = mastheadStory.story.parameters.knobs;
-        const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
-        return {
-          mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
-          footerProps: {
-            ...footerKnobs({ groupId: 'Footer' }),
-            languageOnly,
-            languageItems: !languageOnly
-              ? undefined
-              : object(
-                  'language dropdown items (languageItems)',
-                  languageItems,
-                  groupId
-                ),
-            languageInitialItem: { id: 'en', text: 'English' },
-          },
-        };
-      },
     },
   },
 };
 
-export const SearchOpenByDefault = ({ parameters }) => (
-  <Default parameters={parameters} />
-);
+export const SearchOpenByDefault = () => {
+  const { mastheadProps, footerProps } = props.searchOpenByDefault() ?? {};
+  return (
+    <DotcomShell
+      mastheadProps={mastheadProps}
+      footerProps={footerProps}
+      {...props.searchOpenByDefault()}>
+      <main id="main-content">
+        <Content withL1={!!mastheadProps.mastheadL1Data} />
+      </main>
+    </DotcomShell>
+  );
+};
 
 SearchOpenByDefault.story = {
   name: 'Search open',
   parameters: {
     knobs: {
       escapeHTML: false,
-      DotcomShell: () => {
-        const {
-          Masthead: mastheadKnobs,
-        } = mastheadStory.story.parameters.knobs;
-        const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
-        return {
-          mastheadProps: {
-            ...mastheadKnobs({ groupId: 'Masthead' }),
-            searchOpenOnload: true,
-          },
-          footerProps: {
-            ...footerKnobs({ groupId: 'Footer' }),
-          },
-        };
-      },
     },
   },
 };
 
-export const WithPlatform = ({ parameters }) => (
-  <Default parameters={parameters} />
-);
+export const WithPlatform = () => {
+  const { mastheadProps, footerProps } = props.withPlatform() ?? {};
+  return (
+    <DotcomShell
+      mastheadProps={mastheadProps}
+      footerProps={footerProps}
+      {...props.withPlatform()}>
+      <main id="main-content">
+        <Content withL1={!!mastheadProps.mastheadL1Data} />
+      </main>
+    </DotcomShell>
+  );
+};
 
 WithPlatform.story = {
   name: 'With platform',
   parameters: {
     knobs: {
       escapeHTML: false,
-      DotcomShell: () => {
-        const {
-          Masthead: mastheadKnobs,
-        } = mastheadStory.story.parameters.knobs;
-        const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
-        return {
-          mastheadProps: {
-            ...mastheadKnobs({ groupId: 'Masthead' }),
-            platform: {
-              name: 'IBM Cloud',
-              url: 'https://www.ibm.com/cloud',
-            },
-          },
-          footerProps: {
-            ...footerKnobs({ groupId: 'Footer' }),
-          },
-        };
-      },
     },
   },
 };
 
-export const ShortFooter = ({ parameters }) => (
-  <Default parameters={parameters} />
-);
+export const ShortFooter = () => {
+  const { mastheadProps, footerProps } = props.shortFooter() ?? {};
+  return (
+    <DotcomShell
+      mastheadProps={mastheadProps}
+      footerProps={footerProps}
+      {...props.shortFooter()}>
+      <main id="main-content">
+        <Content withL1={!!mastheadProps.mastheadL1Data} />
+      </main>
+    </DotcomShell>
+  );
+};
 
 ShortFooter.story = {
   name: 'With short footer',
   parameters: {
     knobs: {
       escapeHTML: false,
-      DotcomShell: () => {
-        const {
-          Masthead: mastheadKnobs,
-        } = mastheadStory.story.parameters.knobs;
-        const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
-        return {
-          mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
-          footerProps: {
-            ...footerKnobs({ groupId: 'Footer' }),
-            type: 'short',
-          },
-        };
-      },
     },
   },
 };
 
-export const ShortFooterLanguageOnly = ({ parameters }) => (
-  <Default parameters={parameters} />
-);
+export const ShortFooterLanguageOnly = () => {
+  const { mastheadProps, footerProps } = props.shortFooterLanguageOnly() ?? {};
+  return (
+    <DotcomShell
+      mastheadProps={mastheadProps}
+      footerProps={footerProps}
+      {...props.shortFooterLanguageOnly()}>
+      <main id="main-content">
+        <Content withL1={!!mastheadProps.mastheadL1Data} />
+      </main>
+    </DotcomShell>
+  );
+};
 
 ShortFooterLanguageOnly.story = {
   name: 'With short footer (language only)',
   parameters: {
     knobs: {
       escapeHTML: false,
-      DotcomShell: ({ groupId }) => {
-        const languageOnly = true;
-        const {
-          Masthead: mastheadKnobs,
-        } = mastheadStory.story.parameters.knobs;
-        const { Footer: footerKnobs } = footerStory.story.parameters.knobs;
-        return {
-          mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
-          footerProps: {
-            ...footerKnobs({ groupId: 'Footer' }),
-            type: 'short',
-            languageOnly,
-            languageItems: !languageOnly
-              ? undefined
-              : object(
-                  'language dropdown items (languageItems)',
-                  languageItems,
-                  groupId
-                ),
-            languageInitialItem: { id: 'en', text: 'English' },
-          },
-        };
-      },
     },
   },
 };
 
-export const MicroFooter = ({ parameters }) => (
-  <Default parameters={parameters} />
-);
+export const MicroFooter = () => {
+  const { mastheadProps, footerProps } = props.microFooter() ?? {};
+  return (
+    <DotcomShell
+      mastheadProps={mastheadProps}
+      footerProps={footerProps}
+      {...props.microFooter()}>
+      <main id="main-content">
+        <Content withL1={!!mastheadProps.mastheadL1Data} />
+      </main>
+    </DotcomShell>
+  );
+};
 
 MicroFooter.story = {
   name: 'With micro footer',
   parameters: {
     knobs: {
       escapeHTML: false,
-      DotcomShell: () => {
-        const {
-          Masthead: mastheadKnobs,
-        } = mastheadStory.story.parameters.knobs;
-        const { Footer: footerKnobs } = footerMicroStory.story.parameters.knobs;
-        return {
-          mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
-          footerProps: {
-            ...footerKnobs({ groupId: 'Footer' }),
-            type: 'micro',
-          },
-        };
-      },
     },
   },
 };
 
-export const MicroFooterLanguageOnly = ({ parameters }) => (
-  <Default parameters={parameters} />
-);
+export const MicroFooterLanguageOnly = () => {
+  const { mastheadProps, footerProps } = props.microFooterLanguageOnly() ?? {};
+  return (
+    <DotcomShell
+      mastheadProps={mastheadProps}
+      footerProps={footerProps}
+      {...props.microFooterLanguageOnly()}>
+      <main id="main-content">
+        <Content withL1={!!mastheadProps.mastheadL1Data} />
+      </main>
+    </DotcomShell>
+  );
+};
 
 MicroFooterLanguageOnly.story = {
   name: 'With micro footer (language only)',
   parameters: {
     knobs: {
       escapeHTML: false,
-      DotcomShell: ({ groupId }) => {
-        const languageOnly = true;
-        const {
-          Masthead: mastheadKnobs,
-        } = mastheadStory.story.parameters.knobs;
-        const { Footer: footerKnobs } = footerMicroStory.story.parameters.knobs;
-        return {
-          mastheadProps: mastheadKnobs({ groupId: 'Masthead' }),
-          footerProps: {
-            ...footerKnobs({ groupId: 'Footer' }),
-            type: 'micro',
-            languageOnly,
-            languageItems: !languageOnly
-              ? undefined
-              : object(
-                  'language dropdown items (languageItems)',
-                  languageItems,
-                  groupId
-                ),
-            languageInitialItem: { id: 'en', text: 'English' },
-          },
-        };
-      },
     },
   },
 };
 
-export const WithL1 = ({ parameters }) => <Default parameters={parameters} />;
-
+export const WithL1 = () => {
+  const { mastheadProps, footerProps } = props.withL1() ?? {};
+  return (
+    <DotcomShell
+      mastheadProps={mastheadProps}
+      footerProps={footerProps}
+      {...props.withL1()}>
+      <main id="main-content">
+        <Content withL1={!!mastheadProps.mastheadL1Data} />
+      </main>
+    </DotcomShell>
+  );
+};
 WithL1.story = {
   name: 'With L1',
   parameters: {
     knobs: {
       escapeHTML: false,
-      DotcomShell: () => {
-        const { Masthead: mastheadKnobs } = l1Story.story.parameters.knobs;
-        return {
-          platform: mastheadKnobs.l1Platform,
-          mastheadProps: {
-            ...mastheadKnobs({ groupId: 'Masthead' }),
-          },
-        };
-      },
     },
   },
 };
