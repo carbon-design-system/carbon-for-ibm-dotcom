89ac293ec8b93f642de5d880b42796e3fc33fd7e
Anna Wen <54281166+annawen1@users.noreply.github.com>
2022-07-28 10:14:53 -0700

    chore(react): add notice about upcoming deprecation

---
+++ packages/react/scripts/deprecation-notice.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright IBM Corp. 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+console.log(
+  '⛔️⛔️⛔️WARNING: @carbon/ibmdotcom-react package will be deprecated in Carbon for IBM.com V2. This package will fall into maintaince-mode only; we will make updates only for potential security issues and high severity bugs. With Carbon for IBM.com V2, the @carbon/ibmdotcom-web-components package will be the main package moving forward. Applications using React can use the react-wrapper components within the @carbon/ibmdotcom-web-components package.⛔️⛔️⛔️'
+);
