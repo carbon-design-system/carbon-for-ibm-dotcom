0af6a2cbaf1f1a3e8cac03e5cdc054d3d5085e9c
Anna Wen <54281166+annawen1@users.noreply.github.com>
2022-07-29 09:06:06 -0700

    chore(doc): link to website for deprecation notice

--- packages/react/scripts/deprecation-notice.js
+++ packages/react/scripts/deprecation-notice.js
@@ -6,5 +6,5 @@
  */
 
 console.log(
-  '⛔️⛔️⛔️ATTENTION: @carbon/ibmdotcom-react package will be deprecated in Carbon for IBM.com V2. This package will fall into maintaince-mode only; we will make updates only for potential security issues and high severity bugs. With Carbon for IBM.com V2, the @carbon/ibmdotcom-web-components package will be the main package moving forward. Applications using React can use the react-wrapper components within the @carbon/ibmdotcom-web-components package.⛔️⛔️⛔️'
+  '⛔️⛔️⛔️ATTENTION: @carbon/ibmdotcom-react package will be deprecated in Carbon for IBM.com V2. This package will fall into maintaince-mode only; we will make updates only for potential security issues and high severity bugs. Head to https://www.ibm.com/standards/carbon/developing/frameworks/react for more details.⛔️⛔️⛔️'
 );
