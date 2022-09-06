8115d6b8afea4b70a0953ca3c3950d8f7a77cee8
Andy Blum <andy.blum.01@gmail.com>
2022-09-06 15:17:56 -0400

    test(profileAPI): expand test coverage to include cloud masthead

--- packages/services-store/src/actions/__tests__/profileAPI.test.ts
+++ packages/services-store/src/actions/__tests__/profileAPI.test.ts
@@ -11,7 +11,13 @@ import configureMockStore from 'redux-mock-store';
 import { AnyAction } from 'redux';
 import thunk, { ThunkDispatch } from 'redux-thunk';
 import ProfileAPI from '@carbon/ibmdotcom-services/es/services/Profile/Profile.js';
-import { UNAUTHENTICATED_STATUS, PROFILE_API_ACTION, ProfileAPIState, MASTHEAD_AUTH_METHOD } from '../../types/profileAPI';
+import {
+  UNAUTHENTICATED_STATUS,
+  PROFILE_API_ACTION,
+  ProfileAPIState,
+  MASTHEAD_AUTH_METHOD,
+  CLOUD_UNAUTHENTICATED_STATUS,
+} from '../../types/profileAPI';
 import convertValue from '../../../tests/utils/convert-value';
 import { loadUserStatus, setUserStatus } from '../profileAPI';
 
@@ -46,6 +52,57 @@ describe('Redux actions for `ProfileAPI`', () => {
       {
         type: PROFILE_API_ACTION.SET_USER_STATUS,
         request: { user: UNAUTHENTICATED_STATUS },
+      },
+    ]);
+  });
+
+  it('dispatches the action of error in monitoring user authentication status', async () => {
+    ProfileAPI.getUserStatus.mockRejectedValue(new Error('error-getuserstatus'));
+    const store = mockStore();
+    let caught;
+    try {
+      await store.dispatch(loadUserStatus(MASTHEAD_AUTH_METHOD.DEFAULT));
+    } catch (error) {
+      caught = error;
+    }
+    expect(caught?.message).toBe('error-getuserstatus');
+    expect(convertValue(store.getActions())).toEqual([
+      {
+        type: PROFILE_API_ACTION.SET_REQUEST_USER_STATUS_IN_PROGRESS,
+        request: 'PROMISE',
+      },
+      {
+        type: PROFILE_API_ACTION.SET_ERROR_REQUEST_USER_STATUS,
+        error: 'error-getuserstatus',
+      },
+    ]);
+  });
+});
+
+describe('Redux actions for cloud implementation of `ProfileAPI`', () => {
+  it('dispatches the action to set user authentication status', () => {
+    const store = mockStore();
+    store.dispatch(setUserStatus({ user: 'test.user@ibm.com' }));
+    expect(store.getActions()).toEqual([
+      {
+        type: PROFILE_API_ACTION.SET_USER_STATUS,
+        request: { user: 'test.user@ibm.com' },
+      },
+    ]);
+  });
+
+  it('dispatches the action to get user authentication status', async () => {
+    ProfileAPI.getUserStatus.mockResolvedValue({ user: CLOUD_UNAUTHENTICATED_STATUS });
+    const store = mockStore();
+    await store.dispatch(loadUserStatus(MASTHEAD_AUTH_METHOD.DEFAULT));
+    expect(convertValue(store.getActions())).toEqual([
+      {
+        type: PROFILE_API_ACTION.SET_REQUEST_USER_STATUS_IN_PROGRESS,
+        request: 'PROMISE',
+      },
+      {
+        type: PROFILE_API_ACTION.SET_USER_STATUS,
+        request: { user: CLOUD_UNAUTHENTICATED_STATUS },
       },
     ]);
   });
