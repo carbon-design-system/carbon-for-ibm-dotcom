8608fee3078e45857346fea1687c6486120d52eb
proeung <putra.roeung@gmail.com>
2022-09-12 12:05:59 -0400

    fix(masthead-v2): adjust formatting issue on codesandbox ddo.js

--- packages/web-components/examples/codesandbox/components/masthead/src/ddo.js
+++ packages/web-components/examples/codesandbox/components/masthead/src/ddo.js
@@ -10,40 +10,40 @@
 import './index.scss';
 
 window.digitalData = {
-  "page": {
-    "category": {
-      "primaryCategory": ""
+  page: {
+    category: {
+      primaryCategory: ''
     },
-    "pageInfo": {
-      "effectiveDate": "06-14-2022",
-      "expiryDate": "06-14-2026",
-      "language": "en-au",
-      "publishDate": "06-14-2022",
-      "publisher": "IBM Corporation",
-      "version": "Carbon for IBM.com",
-      "ibm": {
-        "contentDelivery": "HTML",
-        "contentProducer": "IBM Experience Engine-Digital",
-        "country": "au",
-        "industry": "zz",
-        "owner": "Corporate Webmaster/New York/IBM",
-        "siteID": "GLOBAL",
-        "subject": "IBM Experience Engine-Digital",
-        "type": "zz",
-        "messaging": {
-          "routing": {
-            "focusArea": "Miscellaneous - Cross IBM - ISV",
-            "languageCode": "en",
-            "regionCode": "AU"
+    pageInfo: {
+      effectiveDate: '06-14-2022',
+      expiryDate: '06-14-2026',
+      language: 'en-au',
+      publishDate: '06-14-2022',
+      publisher: 'IBM Corporation',
+      version: 'Carbon for IBM.com',
+      ibm: {
+        contentDelivery: 'HTML',
+        contentProducer: 'IBM Experience Engine-Digital',
+        country: 'au',
+        industry: 'zz',
+        owner: 'Corporate Webmaster/New York/IBM',
+        siteID: 'GLOBAL',
+        subject: 'IBM Experience Engine-Digital',
+        type: 'zz',
+        messaging: {
+          routing: {
+            focusArea: 'Miscellaneous - Cross IBM - ISV',
+            languageCode: 'en',
+            regionCode: 'AU',
           },
-          "translation": {
-            "languageCode": "en",
-            "regionCode": "AU"
+          translation: {
+            languageCode: 'en',
+            regionCode: 'AU',
           }
         }
       },
-      "hotjar": {
-        "enable": false
+      hotjar: {
+        enable: false
       }
     }
   },
