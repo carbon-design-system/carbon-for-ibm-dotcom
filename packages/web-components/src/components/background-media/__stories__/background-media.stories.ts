e72227160bf36deacdb2983c670adf5bb6e37b72
ariellalgilmore <ariellalgilmore@gmail.com>
2022-09-14 11:50:35 -0700

    fix(background-media): missing video import

--- packages/web-components/src/components/background-media/__stories__/background-media.stories.ts
+++ packages/web-components/src/components/background-media/__stories__/background-media.stories.ts
@@ -8,6 +8,7 @@
  */
 
 import '../index';
+import '../../video-player/video-player-container';
 import { html } from 'lit-element';
 import ifNonNull from 'carbon-web-components/es/globals/directives/if-non-null.js';
 import { text, select, number } from '@storybook/addon-knobs';
