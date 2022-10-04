97fd3f5b90715eb53fe8e9ef8c71e1e9ffdc48c8
ariellalgilmore <ariellalgilmore@gmail.com>
2022-10-04 08:25:30 -0700

    fix(storybook): react/wc experimental

--- packages/react/.storybook/manager.js
+++ packages/react/.storybook/manager.js
@@ -11,3 +11,27 @@ import yourTheme from './theme';
 addons.setConfig({
   theme: yourTheme,
 });
+
+const CSS_TO_HIDE_TEST_SECTION_FROM_SIDEBAR = `
+button[id^="components-audio-player"],
+button[id^="components-content-block-headlines"],
+button[id^="components-leadspace-with-search"],
+button[id^="components-notice-choice"] {
+  display: none !important
+}
+`;
+
+if (
+  !process.env.DDS_FLAGS_ALL &&
+  !process.env.DDS_AUDIO_PLAYER &&
+  !process.env.DDS_CONTENT_BLOCK_HEADLINES &&
+  !process.env.DDS_LEADSPACE_WITH_SEARCH &&
+  !process.env.DDS_NOTICE_CHOICE
+) {
+  const head = document.head || document.getElementsByTagName('head')[0];
+  const style = document.createElement('style');
+  head.appendChild(style);
+  style.appendChild(
+    document.createTextNode(CSS_TO_HIDE_TEST_SECTION_FROM_SIDEBAR)
+  );
+}
