6dad22005ca1ab9f5caaf30c40179b6e55582412
ariellalgilmore <ariellalgilmore@gmail.com>
2022-10-19 12:48:31 -0700

    fix(thumbnail/default): md spacing

--- packages/web-components/src/components/content-item-horizontal/content-item-horizontal.ts
+++ packages/web-components/src/components/content-item-horizontal/content-item-horizontal.ts
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { customElement, html, property, state, TemplateResult } from 'lit-element';
+import { customElement, html, property, TemplateResult } from 'lit-element';
 import settings from 'carbon-components/es/globals/js/settings.js';
 import ddsSettings from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/settings/settings';
 import DDSContentItem from '../content-item/content-item';
@@ -30,47 +30,36 @@ class DDSContentItemHorizontal extends DDSContentItem {
   thumbnail = false;
 
   /**
-   * `true` if there is a pictogram.
-   */
-  @state()
-  protected _hasMedia = false;
-
-  /**
-   * Handles `slotchange` event.
-   */
-  protected _handleSlotMediaChange({ target }: Event) {
-    const hasContent = (target as HTMLSlotElement)
-      .assignedNodes()
-      .some(node => node.nodeType !== Node.TEXT_NODE || node!.textContent!.trim());
-    this._hasMedia = hasContent;
-  }
-
-  /**
    * @returns The body content.
    */
   // eslint-disable-next-line class-methods-use-this
   protected _renderMedia(): TemplateResult | string | void {
     return html`
-      <slot name="media" @slotchange="${this._handleSlotMediaChange}"></slot>
+      <slot name="media"></slot>
     `;
   }
 
   render() {
     return html`
-      <div class="${prefix}--content-item-horizontal__heading-wrapper">
-        ${this.thumbnail
-          ? ''
-          : html`
-              <slot name="eyebrow" @slotchange="${this._handleSlotChange}"> </slot>
-            `}
-        <slot name="heading"></slot>
-      </div>
-      <div class="${prefix}--content-item-horizontal__content-wrapper${this._hasMedia ? '_with-media' : ''}">
-        ${this._renderBody()}${this._renderFooter()}${!this.thumbnail ? this._renderMedia() : ``}
-      </div>
       ${!this.thumbnail
-        ? ''
+        ? html`
+            <div class="${prefix}--content-item-horizontal__heading-wrapper">
+              <slot name="eyebrow" @slotchange="${this._handleSlotChange}"></slot>
+              <slot name="heading"></slot>
+            </div>
+            <div class="${prefix}--content-item-horizontal__content-wrapper">
+              ${this._renderBody()}${this._renderFooter()}${this._renderMedia()}
+            </div>
+          `
         : html`
+            <div class="${prefix}--content-item-horizontal__body-wrapper">
+              <div class="${prefix}--content-item-horizontal__heading-wrapper">
+                <slot name="heading"></slot>
+              </div>
+              <div class="${prefix}--content-item-horizontal__content-wrapper">
+                ${this._renderBody()}${this._renderFooter()}
+              </div>
+            </div>
             <div class="${prefix}--content-item-horizontal__col--2">
               <slot name="thumbnail" @slotchange="${this._handleSlotChange}"> </slot>
             </div>
