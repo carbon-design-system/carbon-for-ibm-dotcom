bc00257d0da48e00031ce9f86a4050304cf8bafc
John Kaeser <jakaeser44@gmail.com>
2022-10-20 14:55:29 -0400

    fix(content-item-horizontal): use video media player (#9365)

--- packages/web-components/src/components/content-item-horizontal/index.ts
+++ packages/web-components/src/components/content-item-horizontal/index.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2021
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -12,6 +12,7 @@ import './content-item-horizontal-copy';
 import './content-item-horizontal-eyebrow';
 import './content-item-horizontal-media';
 import './content-item-horizontal-media-copy';
+import './content-item-horizontal-media-video';
 import './content-item-horizontal-thumbnail-copy';
 import './content-item-horizontal-media-featured';
 import '../content-item/content-item-heading';
