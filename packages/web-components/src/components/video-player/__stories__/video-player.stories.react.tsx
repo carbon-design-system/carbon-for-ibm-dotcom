e38705d21bbb3dbcaca0fd203c18e2095c56d6a1
Anna Wen <54281166+annawen1@users.noreply.github.com>
2022-11-11 12:35:27 -0500

    feat(video-player): storybook controls

--- packages/web-components/src/components/video-player/__stories__/video-player.stories.react.tsx
+++ packages/web-components/src/components/video-player/__stories__/video-player.stories.react.tsx
@@ -7,56 +7,96 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { boolean, text } from '@storybook/addon-knobs';
 import React from 'react';
 import DDSVideoPlayerContainer from '@carbon/ibmdotcom-web-components/es/components-react/video-player/video-player-container';
+// eslint-disable-next-line max-len
+import DDSLightboxVideoPlayerContainer from '@carbon/ibmdotcom-web-components/es/components-react/lightbox-media-viewer/lightbox-video-player-container';
 import readme from './README.stories.react.mdx';
 
-export const Default = args => {
-  const { aspectRatio, caption, hideCaption, videoId } = args?.VideoPlayerContainer ?? {};
-  return <DDSVideoPlayerContainer aspectRatio={aspectRatio} caption={caption} hideCaption={hideCaption} videoId={videoId} />;
+export const Default = ({ caption, hideCaption, thumbnail, videoId }) => {
+  return (
+    <DDSVideoPlayerContainer
+      playing-mode="inline"
+      caption={caption}
+      hideCaption={hideCaption}
+      thumbnail={thumbnail}
+      videoId={videoId}
+    />
+  );
 };
 
-export const aspectRatio1x1 = args => {
-  const { aspectRatio, caption, hideCaption, videoId } = args?.VideoPlayerContainer ?? {};
-  return <DDSVideoPlayerContainer aspectRatio={aspectRatio} caption={caption} hideCaption={hideCaption} videoId={videoId} />;
+export const aspectRatio1x1 = ({ aspectRatio, caption, hideCaption, thumbnail, videoId }) => {
+  return (
+    <DDSVideoPlayerContainer
+      aspectRatio={aspectRatio}
+      caption={caption}
+      hideCaption={hideCaption}
+      thumbnail={thumbnail}
+      videoId={videoId}
+    />
+  );
 };
 
 aspectRatio1x1.story = {
   name: 'Aspect ratio 1:1',
-  parameters: {
-    knobs: {
-      VideoPlayerContainer: () => {
-        return {
-          aspectRatio: '1x1',
-          caption: text('Custom caption (caption):', ''),
-          hideCaption: boolean('Hide caption (hideCaption):', false),
-          thumbnail: text('Custom thumbnail (thumbnail):', ''),
-          videoId: '1_9h94wo6b',
-        };
+  argTypes: {
+    aspectRatio: {
+      defaultValue: '1x1',
+      table: {
+        disable: true,
       },
     },
   },
 };
 
-export const aspectRatio4x3 = args => {
-  const { aspectRatio, caption, hideCaption, videoId } = args?.VideoPlayerContainer ?? {};
-  return <DDSVideoPlayerContainer aspectRatio={aspectRatio} caption={caption} hideCaption={hideCaption} videoId={videoId} />;
+export const aspectRatio4x3 = ({ aspectRatio, caption, hideCaption, thumbnail, videoId }) => {
+  return (
+    <DDSVideoPlayerContainer
+      aspectRatio={aspectRatio}
+      caption={caption}
+      hideCaption={hideCaption}
+      thumbnail={thumbnail}
+      videoId={videoId}
+    />
+  );
 };
 
 aspectRatio4x3.story = {
   name: 'Aspect ratio 4:3',
-  parameters: {
-    knobs: {
-      VideoPlayerContainer: () => {
-        return {
-          aspectRatio: '4x3',
-          caption: text('Custom caption (caption):', ''),
-          hideCaption: boolean('Hide caption (hideCaption):', false),
-          thumbnail: text('Custom thumbnail (thumbnail):', ''),
-          videoId: '1_9h94wo6b',
-        };
+  argTypes: {
+    aspectRatio: {
+      defaultValue: '4x3',
+      table: {
+        disable: true,
       },
+    },
+  },
+};
+
+export const withLightboxMediaViewer = ({ aspectRatio, caption, hideCaption, thumbnail, videoId, customVideoDescription }) => {
+  return (
+    <>
+      <DDSVideoPlayerContainer
+        aspectRatio={aspectRatio}
+        caption={caption}
+        hideCaption={hideCaption}
+        thumbnail={thumbnail}
+        videoId={videoId}
+        videoDescription={customVideoDescription}
+        playingMode="lightbox"
+      />
+      <DDSLightboxVideoPlayerContainer></DDSLightboxVideoPlayerContainer>
+    </>
+  );
+};
+
+withLightboxMediaViewer.story = {
+  name: 'With lightbox media viewer',
+  argTypes: {
+    customVideoDescription: {
+      control: { type: 'text' },
+      name: 'Custom video description',
+      defaultValue: 'This is a custom video description.',
     },
   },
 };
@@ -72,15 +112,31 @@ export default {
       </div>
     ),
   ],
+  argTypes: {
+    caption: {
+      control: { type: 'text' },
+      name: 'Custom caption (caption):',
+      defaultValue: '',
+      description: '',
+    },
+    hideCaption: {
+      control: { type: 'boolean' },
+      name: 'Hide caption (hideCaption):',
+      defaultValue: false,
+    },
+    thumbnail: {
+      control: { type: 'text' },
+      name: 'Custom thumbnail (thumbnail):',
+      defaultValue: '',
+    },
+    videoId: {
+      defaultValue: '1_9h94wo6b',
+      table: {
+        disable: true,
+      },
+    },
+  },
   parameters: {
     ...readme.parameters,
-    knobs: {
-      VideoPlayerContainer: () => ({
-        caption: text('Custom caption (caption):', ''),
-        hideCaption: boolean('Hide caption (hideCaption):', false),
-        thumbnailUrl: text('Custom thumbnail (thumbnail):', ''),
-        videoId: '1_9h94wo6b',
-      }),
-    },
   },
 };
