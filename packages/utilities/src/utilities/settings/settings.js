bd452cc5623bb9e18d86adbf2bead86f2cdfa9d9
Anna Wen <54281166+annawen1@users.noreply.github.com>
2022-11-23 14:52:15 -0500

    chore(settings): adjust settings version comment

--- packages/utilities/src/utilities/settings/settings.js
+++ packages/utilities/src/utilities/settings/settings.js
@@ -11,7 +11,7 @@
  * @exports ibmdotcom.settings
  * @type {object} Settings object
  * @property {string} [prefix=dds]
- * Carbon for IBM.com v1.41.0-rc.0',
+ * Carbon for IBM.com v1.41.0',
  *
  */
 const settings = {
