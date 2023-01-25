4c6b18188b9ebab72a5e612a2b72aaf591c2c136
Andy Blum <andy.blum.01@gmail.com>
2023-01-25 15:00:05 -0500

    chore(element-registry): create BXModalHeaderBase

--- packages/web-components/src/components/expressive-modal/expressive-modal-header.ts
+++ packages/web-components/src/components/expressive-modal/expressive-modal-header.ts
@@ -1,14 +1,14 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2022
+ * Copyright IBM Corp. 2020, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import { property, customElement } from 'lit-element';
-import BXModalHeader from '@carbon/web-components/es/components/modal/modal-header.js';
+import { BXModalHeaderBase } from '@carbon/web-components/es/components/modal/modal-header.js';
 import ddsSettings from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/settings/settings';
 import styles from './expressive-modal.scss';
 import StableSelectorMixin from '../../globals/mixins/stable-selector';
@@ -21,7 +21,7 @@ const { stablePrefix: ddsPrefix } = ddsSettings;
  * @element dds-expressive-modal-header
  */
 @customElement(`${ddsPrefix}-expressive-modal-header`)
-class DDSExpressiveModalHeader extends StableSelectorMixin(BXModalHeader) {
+class DDSExpressiveModalHeader extends StableSelectorMixin(BXModalHeaderBase) {
   @property({ reflect: true })
   slot = 'header';
 
