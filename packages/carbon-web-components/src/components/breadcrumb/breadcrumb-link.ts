6d1a741008c8204e59605d338636f5110840b1ea
Andy Blum <andy.blum.01@gmail.com>
2023-01-25 14:10:46 -0500

    chore(element-registry): create BXBreadcrumbLinkBase

--- packages/carbon-web-components/src/components/breadcrumb/breadcrumb-link.ts
+++ packages/carbon-web-components/src/components/breadcrumb/breadcrumb-link.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2022
+ * Copyright IBM Corp. 2019, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -14,14 +14,16 @@ import styles from './breadcrumb.scss';
 
 const { prefix } = settings;
 
+export class BXBreadcrumbLinkBase extends BXLink {
+  static styles = styles;
+}
+
 /**
  * Link in breadcrumb.
  *
  * @element bx-breadcrumb-link
  */
 @customElement(`${prefix}-breadcrumb-link`)
-class BXBreadcrumbLink extends BXLink {
-  static styles = styles;
-}
+class BXBreadcrumbLink extends BXBreadcrumbLinkBase {}
 
 export default BXBreadcrumbLink;
