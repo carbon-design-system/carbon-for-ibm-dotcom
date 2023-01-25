616d5144ddf67cba5a02568b9ee61eaf23bbeefd
Andy Blum <andy.blum.01@gmail.com>
2023-01-25 14:59:02 -0500

    chore(element-registry): create BXModalFooterButtonBase

--- packages/web-components/src/components/filter-panel/filter-modal-button.ts
+++ packages/web-components/src/components/filter-panel/filter-modal-button.ts
@@ -1,14 +1,14 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2022
+ * Copyright IBM Corp. 2020, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import { customElement } from 'lit-element';
-import BXModalFooterButton from '@carbon/web-components/es/components/modal/modal-footer-button.js';
+import { BXModalFooterButtonBase } from '@carbon/web-components/es/components/modal/modal-footer-button.js';
 import ddsSettings from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/settings/settings';
 import StableSelectorMixin from '../../globals/mixins/stable-selector';
 import styles from './filter-panel.scss';
@@ -21,7 +21,9 @@ const { stablePrefix: ddsPrefix } = ddsSettings;
  * @element dds-filter-modal-footer-button
  */
 @customElement(`${ddsPrefix}-filter-modal-footer-button`)
-class DDSFilterModalButton extends StableSelectorMixin(BXModalFooterButton) {
+class DDSFilterModalButton extends StableSelectorMixin(
+  BXModalFooterButtonBase
+) {
   static get stableSelector() {
     return `${ddsPrefix}-filter-modal-footer-button`;
   }
