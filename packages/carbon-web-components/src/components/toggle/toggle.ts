d6a5c783eff2ae438725e72ce86e1bad199214d5
Andy Blum <andy.blum.01@gmail.com>
2023-01-25 14:14:35 -0500

    chore(element-registry): create BXCheckboxBase

--- packages/carbon-web-components/src/components/toggle/toggle.ts
+++ packages/carbon-web-components/src/components/toggle/toggle.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2022
+ * Copyright IBM Corp. 2019, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -11,7 +11,7 @@ import { classMap } from 'lit-html/directives/class-map';
 import { html, property, customElement } from 'lit-element';
 import settings from 'carbon-components/es/globals/js/settings';
 import ifNonNull from '../../globals/directives/if-non-null';
-import BXCheckbox from '../checkbox/checkbox';
+import { BXCheckboxBase } from '../checkbox/checkbox';
 import { TOGGLE_SIZE } from './defs';
 import styles from './toggle.scss';
 
@@ -29,7 +29,7 @@ const { prefix } = settings;
  * @fires bx-toggle-changed - The custom event fired after this changebox changes its checked state.
  */
 @customElement(`${prefix}-toggle`)
-class BXToggle extends BXCheckbox {
+class BXToggle extends BXCheckboxBase {
   protected _renderCheckmark() {
     if (this.size !== TOGGLE_SIZE.SMALL) {
       return undefined;
