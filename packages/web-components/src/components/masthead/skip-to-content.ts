f4dc110779a4edb35ecbb2aed48704679a3e1a8c
Andy Blum <andy.blum.01@gmail.com>
2023-01-26 16:23:46 -0500

    chore(element-registry): create BXSkipToContentBase

--- packages/web-components/src/components/masthead/skip-to-content.ts
+++ packages/web-components/src/components/masthead/skip-to-content.ts
@@ -1,14 +1,14 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2022
+ * Copyright IBM Corp. 2020, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import { customElement, property } from 'lit-element';
-import BXSkipToContent from '@carbon/web-components/es/components/skip-to-content/skip-to-content.js';
+import { BXSkipToContentBase } from '@carbon/web-components/es/components/skip-to-content/skip-to-content.js';
 import ddsSettings from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/settings/settings';
 import styles from './masthead.scss';
 
@@ -20,7 +20,7 @@ const { stablePrefix: ddsPrefix } = ddsSettings;
  * @element dds-masthead-skip-to-content
  */
 @customElement(`${ddsPrefix}-skip-to-content`)
-class DDSSkipToContent extends BXSkipToContent {
+class DDSSkipToContent extends BXSkipToContentBase {
   /**
    * The shadow slot this logo UI should be in.
    */
