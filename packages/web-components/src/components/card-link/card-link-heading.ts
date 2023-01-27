4e2ca44a8f9d00d385069f4437211d4a12285cef
Andy Blum <andy.blum.01@gmail.com>
2023-01-27 11:16:52 -0500

    chore(element-registry): create DDSCardHeadingBase

--- packages/web-components/src/components/card-link/card-link-heading.ts
+++ packages/web-components/src/components/card-link/card-link-heading.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2021, 2022
+ * Copyright IBM Corp. 2021, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -9,7 +9,7 @@
 
 import { customElement } from 'lit-element';
 import ddsSettings from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/settings/settings';
-import DDSCardHeading from '../card/card-heading';
+import { DDSCardHeadingBase } from '../card/card-heading';
 import styles from './card-link.scss';
 
 const { stablePrefix: ddsPrefix } = ddsSettings;
@@ -20,7 +20,7 @@ const { stablePrefix: ddsPrefix } = ddsSettings;
  * @element dds-card-link-heading
  */
 @customElement(`${ddsPrefix}-card-link-heading`)
-class DDSCardLinkHeading extends DDSCardHeading {
+class DDSCardLinkHeading extends DDSCardHeadingBase {
   static get stableSelector() {
     return `${ddsPrefix}--card-link-heading`;
   }
