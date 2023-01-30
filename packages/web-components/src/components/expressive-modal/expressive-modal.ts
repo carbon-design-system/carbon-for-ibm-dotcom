d1c4c02c0c1540369973240448c76431b2c9ce07
Sanjit Bauli <sanjitbauli@in.ibm.com>
2023-01-30 18:28:41 +0530

    fix: undo changes in expressive-modal

--- packages/web-components/src/components/expressive-modal/expressive-modal.ts
+++ packages/web-components/src/components/expressive-modal/expressive-modal.ts
@@ -17,12 +17,17 @@ import {
   SVGTemplateResult,
   TemplateResult,
 } from 'lit-element';
-import settings from 'carbon-components/es/globals/js/settings.js';
-import on from 'carbon-components/es/globals/js/misc/on.js';
-import { selectorTabbable } from '../../internal/vendor/@carbon/web-components/globals/settings.js';
+import { EXPRESSIVE_MODAL_MODE, EXPRESSIVE_MODAL_SIZE } from './defs';
+
+import { classMap } from 'lit-html/directives/class-map.js';
+import DDSCarousel from '../carousel/carousel';
+import DDSExpressiveModalCloseButton from './expressive-modal-close-button';
+import ddsSettings from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/settings/settings';
 import HostListener from '../../internal/vendor/@carbon/web-components/globals/decorators/host-listener.js';
 import HostListenerMixin from '../../internal/vendor/@carbon/web-components/globals/mixins/host-listener.js';
-import ddsSettings from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/settings/settings';
+import on from 'carbon-components/es/globals/js/misc/on.js';
+import { selectorTabbable } from '../../internal/vendor/@carbon/web-components/globals/settings.js';
+import settings from 'carbon-components/es/globals/js/settings.js';
 import StableSelectorMixin from '../../globals/mixins/stable-selector';
 import styles from './expressive-modal.scss';
 
