99b1170f963608db9c496d1dbeaeab7e5b363feb
Andy Blum <andy.blum.01@gmail.com>
2023-02-15 10:31:50 -0500

    fix(structured-list): fix ci errors

--- packages/web-components/src/components/structured-list/structured-list.ts
+++ packages/web-components/src/components/structured-list/structured-list.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2022
+ * Copyright IBM Corp. 2020, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -104,7 +104,6 @@ class DDSStructuredList extends StableSelectorMixin(LitElement) {
   }
 
   render() {
-    const { wrapperId } = this.constructor as typeof DDSStructuredList;
     return html`
       <div class="overflow-indicator left"></div>
       ${this.renderInner()}
