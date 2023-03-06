704251870dd9391dd529d7d493485618f5f90d04
ariellalgilmore <ariellalgilmore@gmail.com>
2023-03-06 09:31:27 -0700

    fix(cwc): format

--- packages/carbon-web-components/.storybook/theme.ts
+++ packages/carbon-web-components/.storybook/theme.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2022
+ * Copyright IBM Corp. 2019, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -12,5 +12,6 @@ import { version } from '../package.json';
 
 export default create({
   brandTitle: `@carbon/web-components ${version}`,
-  brandUrl: 'https://github.com/carbon-design-system/carbon-for-ibm-dotcom/tree/main/packages/carbon-web-components',
+  brandUrl:
+    'https://github.com/carbon-design-system/carbon-for-ibm-dotcom/tree/main/packages/carbon-web-components',
 } as ThemeVars);
