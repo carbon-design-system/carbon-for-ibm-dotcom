0dc99869fc2f52c965447c7ee435dcdb00c504e2
Kenny Lam <kennylam@proton.me>
2023-03-15 21:03:14 -0400

    chore(jest): test unhandled-rejections

--- tasks/jest/setup.js
+++ tasks/jest/setup.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2018, 2018
+ * Copyright IBM Corp. 2018, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -19,39 +19,5 @@ global.requestAnimationFrame = function requestAnimationFrame(callback) {
 
 const enzyme = require.requireActual('enzyme');
 const Adapter = require.requireActual('enzyme-adapter-react-16');
-
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
-  clear() {
-    this.store = {};
-  }
-  getItem(key) {
-    return this.store[key] || null;
-  }
-  setItem(key, value) {
-    this.store[key] = value.toString();
-  }
-  removeItem(key) {
-    delete this.store[key];
-  }
-}
-
-// const localStorage = new LocalStorageMock();
-
-// jest test environment expects these to exist
-// window.localStorage = localStorage
-
-let storage;
-
-beforeEach(() => {
-  storage = window.localStorage();
-  window.localStorage = new LocalStorageMock();
-});
-
-afterEach(() => {
-  window.localStorage = storage;
-});
 
 enzyme.configure({ adapter: new Adapter() });
