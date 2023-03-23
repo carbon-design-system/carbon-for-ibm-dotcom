1049adf74e51485ab8b1fa907946aa751799c6de
ariellalgilmore <ariellalgilmore@gmail.com>
2023-03-23 10:06:01 -0600

    fix(hr): add separator role

--- packages/web-components/src/components/horizontal-rule/horizontal-rule.ts
+++ packages/web-components/src/components/horizontal-rule/horizontal-rule.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2022
+ * Copyright IBM Corp. 2020, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -50,6 +50,13 @@ class DDSHorizontalRule extends StableSelectorMixin(LitElement) {
    */
   @property({ attribute: 'weight', reflect: true })
   weight = HR_WEIGHT.THIN;
+
+  connectedCallback() {
+    if (!this.hasAttribute('role')) {
+      this.setAttribute('role', 'separator');
+    }
+    super.connectedCallback();
+  }
 
   static get stableSelector() {
     return `${ddsPrefix}--hr`;
