dffa56fe3dea8f1373a400403efa8fb4dac139c6
Anna Wen <54281166+annawen1@users.noreply.github.com>
2023-04-06 13:30:54 -0400

    feat(text-input): text input carbon v11 sync

--- packages/carbon-web-components/src/components/input/input-story.ts
+++ packages/carbon-web-components/src/components/input/input-story.ts
@@ -9,23 +9,17 @@
 
 import { html } from 'lit';
 import { ifDefined } from 'lit/directives/if-defined.js';
-import * as knobs from '@storybook/addon-knobs';
+import { boolean, select, number } from '@storybook/addon-knobs';
 import textNullable from '../../../.storybook/knob-text-nullable';
 import storyDocs from './input-story.mdx';
 import { prefix } from '../../globals/settings';
 import './input';
+import './textinput-skeleton';
+import '../layer';
 import '../form/form-item';
 
 import { action } from '@storybook/addon-actions';
-import { INPUT_COLOR_SCHEME, INPUT_SIZE, INPUT_TYPE } from './input';
-
-const inputTypes = Object.entries(INPUT_TYPE).reduce(
-  (acc, [key, val]) => ({
-    ...acc,
-    [`${key.toLowerCase()}`]: val,
-  }),
-  {}
-);
+import { INPUT_SIZE } from './input';
 
 const sizes = {
   [`Small size (${INPUT_SIZE.SMALL})`]: INPUT_SIZE.SMALL,
@@ -33,73 +27,9 @@ const sizes = {
   [`Large size (${INPUT_SIZE.LARGE})`]: INPUT_SIZE.LARGE,
 };
 
-const colorSchemes = {
-  [`Regular`]: null,
-  [`Light (${INPUT_COLOR_SCHEME.LIGHT})`]: INPUT_COLOR_SCHEME.LIGHT,
-};
-
-const createProps = ({ boolean, textNonEmpty, select }) => {
-  const type = select('Input type (type)', inputTypes, INPUT_TYPE.TEXT);
-  return {
-    colorScheme: select('Color scheme (color-scheme)', colorSchemes, null),
-    disabled: boolean('Disabled (disabled)', false),
-    value: textNonEmpty('Input value (value)', ''),
-    placeholder: textNonEmpty('Placeholder text (placeholder)', ''),
-    invalid: boolean('Invalid (invalid)', false),
-    onInput: action('input'),
-    showPasswordVisibilityToggle:
-      type === INPUT_TYPE.TEXT || type === INPUT_TYPE.PASSWORD
-        ? boolean(
-            'Show password visibility toggle (show-password-visibility-toggle)',
-            false
-          )
-        : null,
-    size: select('Input size (size)', sizes, INPUT_SIZE.REGULAR),
-    type,
-  };
-};
-
-export const Default = (args) => {
-  const {
-    autocomplete,
-    autofocus,
-    colorScheme,
-    disabled,
-    helperText,
-    invalid,
-    labelText,
-    name,
-    pattern,
-    placeholder,
-    readonly,
-    required,
-    showPasswordVisibilityToggle,
-    size,
-    type,
-    validityMessage,
-    value,
-    onInput,
-  } = args?.[`${prefix}-input`] ?? {};
+export const Default = () => {
   return html`
-    <cds-input
-      autocomplete="${ifDefined(autocomplete)}"
-      ?autofocus="${autofocus}"
-      color-scheme="${ifDefined(colorScheme)}"
-      ?disabled="${disabled}"
-      helper-text="${ifDefined(helperText)}"
-      ?invalid="${invalid}"
-      label-text="${ifDefined(labelText)}"
-      name="${ifDefined(name)}"
-      pattern="${ifDefined(pattern)}"
-      placeholder="${ifDefined(placeholder)}"
-      ?readonly="${readonly}"
-      ?required="${required}"
-      ?show-password-visibility-toggle="${showPasswordVisibilityToggle}"
-      size="${ifDefined(size)}"
-      type="${ifDefined(type)}"
-      validity-message="${ifDefined(validityMessage)}"
-      value="${ifDefined(value)}"
-      @input="${onInput}">
+    <cds-input>
       <span slot="label-text">Text input label</span>
       <span slot="helper-text">Optional help text</span>
     </cds-input>
@@ -108,45 +38,139 @@ export const Default = (args) => {
 
 Default.storyName = 'Default';
 
-export const withoutFormItemWrapper = (args) => {
-  const {
-    colorScheme,
-    disabled,
-    invalid,
-    placeholder,
-    showPasswordVisibilityToggle,
-    size,
-    type,
-    value,
-    onInput,
-  } = args?.[`${prefix}-input`] ?? {};
+export const ReadOnly = () => {
   return html`
-    <cds-input
-      value="${ifDefined(value)}"
-      color-scheme="${ifDefined(colorScheme)}"
-      placeholder="${ifDefined(placeholder)}"
-      size="${ifDefined(size)}"
-      @input="${onInput}"
-      ?invalid="${invalid}"
-      ?disabled="${disabled}"
-      ?show-password-visibility-toggle="${showPasswordVisibilityToggle}"
-      type="${ifDefined(type)}">
-      <span slot="label-text">Label text</span>
-      <span slot="helper-text">Optional helper text</span>
-      <span slot="validity-message">Something isn't right</span>
+    <cds-input value="This is read only, you can't type more." readonly="true">
+      <span slot="label-text">Text input label</span>
+      <span slot="helper-text">Optional help text</span>
     </cds-input>
   `;
 };
 
-withoutFormItemWrapper.storyName = 'Without form item wrapper';
+export const Skeleton = () => {
+  return html` <cds-textinput-skeleton></cds-textinput-skeleton> `;
+};
+
+export const TogglePasswordVisibility = () => {
+  return html`
+    <cds-input type="password" show-password-visibility-toggle>
+      <span slot="label-text">Text input label</span>
+      <span slot="helper-text">Optional help text</span>
+    </cds-input>
+  `;
+};
+
+export const WithLayer = () => {
+  return html`
+    <cds-layer>
+      <cds-input>
+        <span slot="label-text">First layer</span>
+        <span slot="helper-text">Optional help text</span>
+      </cds-input>
+      <cds-layer>
+        <cds-input>
+          <span slot="label-text">Second layer</span>
+          <span slot="helper-text">Optional help text</span>
+        </cds-input>
+        <cds-layer>
+          <cds-input>
+            <span slot="label-text">Third layer</span>
+            <span slot="helper-text">Optional help text</span>
+          </cds-input>
+        </cds-layer>
+      </cds-layer>
+    </cds-layer>
+  `;
+};
+
+export const Playground = (args) => {
+  const {
+    disabled,
+    enableCounter,
+    helperText,
+    hideLabel,
+    inline,
+    invalid,
+    invalidText,
+    labelText,
+    maxCount,
+    placeholder,
+    playgroundWidth,
+    readonly,
+    size,
+    type,
+    value,
+    warn,
+    warnText,
+    onInput,
+  } = args?.[`${prefix}-input`] ?? {};
+  return html`
+    <div style="width: ${playgroundWidth}px;">
+      <cds-input
+        ?disabled="${disabled}"
+        ?enable-counter="${ifDefined(enableCounter)}"
+        helper-text="${ifDefined(helperText)}"
+        ?hide-label="${hideLabel}"
+        ?inline="${inline}"
+        ?invalid="${invalid}"
+        invalid-text="${ifDefined(invalidText)}"
+        label="${ifDefined(labelText)}"
+        max-count="${ifDefined(maxCount)}"
+        placeholder="${ifDefined(placeholder)}"
+        ?readonly="${ifDefined(readonly)}"
+        size="${ifDefined(size)}"
+        type="${ifDefined(type)}"
+        value="${ifDefined(value)}"
+        ?warn="${ifDefined(warn)}"
+        warn-text="${ifDefined(warnText)}"
+        @input="${onInput}">
+      </cds-input>
+    </div>
+  `;
+};
+
+Playground.parameters = {
+  knobs: {
+    [`${prefix}-input`]: () => ({
+      disabled: boolean('Disabled (disabled)', false),
+      enableCounter: boolean('Enable counter (enable-counter)', false),
+      helperText: textNullable('Helper text (helper-text)', 'Helper text'),
+      hideLabel: boolean('Hide label (hide-label)', false),
+      inline: boolean('Inline (inline)', false),
+      invalid: boolean('Invalid (invalid)', false),
+      invalidText: textNullable(
+        'Invalid text (invalid-text)',
+        'Error message goes here'
+      ),
+      labelText: textNullable('Label text (label)', 'Label text'),
+      maxCount: textNullable('Max count (max-count)', '100'),
+      placeholder: textNullable(
+        'Placeholder (placeholder)',
+        'Placeholder text'
+      ),
+      playgroundWidth: number('Playground width', 300, {
+        range: true,
+        min: 300,
+        max: 800,
+        step: 50,
+      }),
+      size: select('Dropdown size (size)', sizes, INPUT_SIZE.MEDIUM),
+      readonly: boolean('Read only (readonly)', false),
+      type: textNullable('Type (type)', 'text'),
+      warn: boolean('Warn (warn)', false),
+      warnText: textNullable(
+        'Warn text (warn-text)',
+        'Warning message that is really long can wrap to more lines but should not be excessively long.'
+      ),
+      value: textNullable('Value of input (value)', ''),
+      onInput: action(`${prefix}-select-selected`),
+    }),
+  },
+};
 
 export default {
   title: 'Components/TextInput',
   parameters: {
     ...storyDocs.parameters,
-    knobs: {
-      [`${prefix}-input`]: () =>
-        createProps({ ...knobs, textNonEmpty: textNullable }),
-    },
   },
 };
