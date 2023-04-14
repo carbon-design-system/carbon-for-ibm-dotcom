4f34b9e2829ef021ee8a57d50154e63fc701d11c
Anna Wen <54281166+annawen1@users.noreply.github.com>
2023-04-14 09:53:44 -0400

    chore(notifications): add license to helper file

--- packages/carbon-web-components/src/components/notification/stories/helper.ts
+++ packages/carbon-web-components/src/components/notification/stories/helper.ts
@@ -1,3 +1,12 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { NOTIFICATION_KIND } from '../inline-notification';
 
 const kinds = {
