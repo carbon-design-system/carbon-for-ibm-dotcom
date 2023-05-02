0535990458f17a4ed6143796a9600ebf40f98050
Ignacio Becerra <i1becerr@ucsd.edu>
2023-05-02 14:14:18 -0700

    feat(modal): sync with @carbon/react v11 (#10397)
    * feat(modal): syncing the modal with v11
    
    * feat(modal): finish adding props
    
    * fix(modal): removed unneeded prop
    
    * fix(modal): cleaned styles
    
    * fix(modal): fixed left close bug
    
    ---------
    
    Co-authored-by: kennylam <909118+kennylam@users.noreply.github.com>

--- packages/web-components/src/components/filter-panel/filter-panel-modal.ts
+++ packages/web-components/src/components/filter-panel/filter-panel-modal.ts
@@ -14,7 +14,7 @@ import HostListenerMixin from '../../internal/vendor/@carbon/web-components/glob
 import './filter-group';
 import './filter-modal-button';
 import './filter-modal-heading';
-import BXModal from '../../internal/vendor/@carbon/web-components/components/modal/modal.js';
+import CDSModal from '../../internal/vendor/@carbon/web-components/components/modal/modal.js';
 import ddsSettings from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/settings/settings';
 import './filter-modal-footer';
 import StableSelectorMixin from '../../globals/mixins/stable-selector';
@@ -33,7 +33,7 @@ const { stablePrefix: ddsPrefix } = ddsSettings;
  */
 @customElement(`${ddsPrefix}-filter-panel-modal`)
 class DDSFilterPanelModal extends HostListenerMixin(
-  StableSelectorMixin(BXModal)
+  StableSelectorMixin(CDSModal)
 ) {
   /**
    * Renders the selected values.
@@ -91,14 +91,17 @@ class DDSFilterPanelModal extends HostListenerMixin(
       if (
         this.dispatchEvent(
           new CustomEvent(
-            (this.constructor as typeof BXModal).eventBeforeClose,
+            (this.constructor as typeof CDSModal).eventBeforeClose,
             init
           )
         )
       ) {
         this.open = false;
         this.dispatchEvent(
-          new CustomEvent((this.constructor as typeof BXModal).eventClose, init)
+          new CustomEvent(
+            (this.constructor as typeof CDSModal).eventClose,
+            init
+          )
         );
       }
     }
@@ -112,11 +115,11 @@ class DDSFilterPanelModal extends HostListenerMixin(
         href="javascript:void 0"
         role="navigation"></a>
       <section class="${prefix}--filter-panel__section bx--modal-container">
-        <bx-modal-header>
-          <bx-modal-close-button
-            @click=${this._handleUserClose}></bx-modal-close-button>
+        <cds-modal-header>
+          <cds-modal-close-button
+            @click=${this._handleUserClose}></cds-modal-close-button>
           <dds-filter-modal-heading>${this.heading}</dds-filter-modal-heading>
-        </bx-modal-header>
+        </cds-modal-header>
         <div class="${prefix}--modal-body"><slot></slot></div>
         <dds-filter-modal-footer>
           <dds-filter-modal-footer-button
--- packages/web-components/src/components/leaving-ibm/__tests__/leaving-ibm.steps.js
+++ packages/web-components/src/components/leaving-ibm/__tests__/leaving-ibm.steps.js
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2022
+ * Copyright IBM Corp. 2020, 2023
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -15,9 +15,9 @@ describe('dds-leaving-ibm-*', () => {
   });
 
   it('should have modal closable', async () => {
-    await page.click('bx-modal-close-button button');
+    await page.click('cds-modal-close-button button');
     await expect(page).toHaveSelector(
-      'dds-leaving-ibm-modal .bx--modal-container',
+      'dds-leaving-ibm-modal .cds--modal-container',
       { state: 'hidden' }
     );
   });
