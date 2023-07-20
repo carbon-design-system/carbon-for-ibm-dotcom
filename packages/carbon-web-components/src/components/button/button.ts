e5efa910d0c815877c8b02d5d66998b7653cada8
ariellalgilmore <ariellalgilmore@gmail.com>
2023-07-20 09:09:33 -0700

    fix(button): add layout-size class

--- packages/carbon-web-components/src/components/button/button.ts
+++ packages/carbon-web-components/src/components/button/button.ts
@@ -273,6 +273,7 @@ class CDSButton extends HostListenerMixin(FocusMixin(LitElement)) {
       [`${prefix}--btn--disabled`]: disabled,
       [`${prefix}--btn--icon-only`]: hasIcon && !hasMainContent,
       [`${prefix}--btn--${size}`]: size,
+      [`${prefix}--layout--size-${size}`]: size,
       [`${prefix}-ce--btn--has-icon`]: hasIcon,
       [`${prefix}--btn--expressive`]: isExpressive,
       [`${prefix}--btn--selected`]: isSelected && kind === 'ghost',
