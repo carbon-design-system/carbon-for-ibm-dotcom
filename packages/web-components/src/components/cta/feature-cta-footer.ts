c429e01bb1284f65243f246039af9c5d00be6a8c
Ignacio Becerra Aguilar <i1becerr@ucsd.edu>
2023-08-24 07:12:32 -0700

    fix(cta): fix build

--- packages/web-components/src/components/cta/feature-cta-footer.ts
+++ packages/web-components/src/components/cta/feature-cta-footer.ts
@@ -6,6 +6,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+// @ts-nocheck
 
 import { property } from 'lit/decorators.js';
 import settings from '@carbon/ibmdotcom-utilities/es/utilities/settings/settings.js';
