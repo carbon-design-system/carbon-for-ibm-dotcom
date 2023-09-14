d133d7d9e61b732359ba697caf29e19b870b616b
Anna Wen <54281166+annawen1@users.noreply.github.com>
2023-09-14 14:00:08 -0400

    feat(footer): remove unused svg decorator

--- packages/web-components/src/components/footer/footer-logo.ts
+++ packages/web-components/src/components/footer/footer-logo.ts
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { html, LitElement, svg } from 'lit';
+import { html, LitElement } from 'lit';
 import { unsafeSVG } from 'lit/directives/unsafe-svg.js';
 import { property } from 'lit/decorators.js';
 import FocusMixin from '../../internal/vendor/@carbon/web-components/globals/mixins/focus.js';
