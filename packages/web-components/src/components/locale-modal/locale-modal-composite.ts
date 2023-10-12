0e1c44ac44149ada2a46c54bb68c70593b25e91e
John Kaeser <jakaeser44@gmail.com>
2023-10-12 08:53:50 -0400

    feat(locale-modal): remove unused import

--- packages/web-components/src/components/locale-modal/locale-modal-composite.ts
+++ packages/web-components/src/components/locale-modal/locale-modal-composite.ts
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { html, property, LitElement, query } from 'lit-element';
+import { html, property, LitElement } from 'lit-element';
 import ifNonNull from '../../internal/vendor/@carbon/web-components/globals/directives/if-non-null.js';
 import LocaleAPI from '@carbon/ibmdotcom-services/es/services/Locale/Locale.js';
 import ddsSettings from '../../internal/vendor/@carbon/ibmdotcom-utilities/utilities/settings/settings';
