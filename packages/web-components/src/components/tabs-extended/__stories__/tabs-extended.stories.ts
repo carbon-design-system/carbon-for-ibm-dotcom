74957d8b8b74cb873402d3b82bf02d6f941db787
emyarod <afw5059@gmail.com>
2023-11-14 10:11:08 -0600

    feat(tabs): use v11 styles for extended tabs component

--- packages/web-components/src/components/tabs-extended/__stories__/tabs-extended.stories.ts
+++ packages/web-components/src/components/tabs-extended/__stories__/tabs-extended.stories.ts
@@ -11,16 +11,11 @@ import { html } from 'lit';
 import '../index';
 import '../../content-block-media/index';
 import '../../card-group/index';
-import { ifDefined } from 'lit/directives/if-defined.js';
-import { select } from '@storybook/addon-knobs';
 import ArrowRight20 from '../../../internal/vendor/@carbon/web-components/icons/arrow--right/20';
-import { ORIENTATION } from '../defs';
+import { TABS_TYPE } from '../defs';
 import readme from './README.stories.mdx';
-
-const orientationType = {
-  [`horizontal`]: ORIENTATION.HORIZONTAL,
-  [`vertical`]: ORIENTATION.VERTICAL,
-};
+import styles from './tabs-extended-story.scss';
+import { prefix } from '../../../internal/vendor/@carbon/web-components/globals/settings';
 
 const copy = `Lorem ipsum dolor sit amet, *consectetur* adipiscing elit.
   Vivamus sed interdum tortor. Sed id pellentesque diam.
@@ -29,13 +24,88 @@ const copy = `Lorem ipsum dolor sit amet, *consectetur* adipiscing elit.
   Curabitur pretium elit non blandit lobortis.
   Donec quis pretium odio, in dignissim sapien.`;
 
-export const Default = (args) => {
-  const { orientation } = args?.TabsExtended ?? {};
+export const Default = () => html`
+  <style>
+    ${styles}
+  </style>
+  <c4d-tabs-extended value="first">
+    <c4d-tab id="tab-first" target="panel-first" value="first">
+      First tab with long text that wraps multiple lines. Lorem ipsum dolor sit
+      amet consectetur adipiscing elit
+    </c4d-tab>
+    <c4d-tab id="tab-second" target="panel-second" value="second">
+      Second tab - min amount for tooltip
+    </c4d-tab>
+    <c4d-tab id="tab-third" target="panel-third" value="third">
+      Third tab
+    </c4d-tab>
+    <c4d-tab id="tab-fourth" target="panel-fourth" value="fourth">
+      Fourth tab
+    </c4d-tab>
+    <c4d-tab label="Fifth tab" disabled>Fifth tab</c4d-tab>
+  </c4d-tabs-extended>
+  <div class="${prefix}-ce-demo-devenv--tab-panels">
+    <div id="panel-first" role="tabpanel" aria-labelledby="tab-first" hidden>
+      <c4d-content-block-media-content>
+        <c4d-content-item>
+          <c4d-content-item-heading
+            >Content for first tab goes here.</c4d-content-item-heading
+          >
+          <c4d-content-item-copy>${copy}</c4d-content-item-copy>
+        </c4d-content-item>
+
+        <c4d-card-link-cta slot="footer" href="https://example.com">
+          <c4d-card-link-heading
+            >Lorem ipsum dolor sit amet</c4d-card-link-heading
+          >
+          <c4d-card-cta-footer>
+            ${ArrowRight20({ slot: 'icon' })}
+          </c4d-card-cta-footer>
+        </c4d-card-link-cta>
+      </c4d-content-block-media-content>
+    </div>
+    <div id="panel-second" role="tabpanel" aria-labelledby="tab-second" hidden>
+      <c4d-content-block-media-content>
+        <c4d-content-item>
+          <c4d-content-item-heading
+            >Content for second tab goes here.</c4d-content-item-heading
+          >
+          <c4d-content-item-copy>${copy}</c4d-content-item-copy>
+        </c4d-content-item>
+      </c4d-content-block-media-content>
+    </div>
+    <div id="panel-third" role="tabpanel" aria-labelledby="tab-third" hidden>
+      <p>Content for third tab goes here.</p>
+    </div>
+    <div id="panel-fourth" role="tabpanel" aria-labelledby="tab-fourth" hidden>
+      <p>Content for fourth tab goes here.</p>
+    </div>
+  </div>
+`;
+
+export const Contained = () => {
   return html`
-    <c4d-tabs-extended orientation="${ifDefined(orientation)}">
-      <c4d-tab
-        label="First tab with long text that wraps multiple lines. Lorem ipsum dolor sit amet consectetur adipiscing elit"
-        selected>
+    <style>
+      ${styles}
+    </style>
+    <c4d-tabs-extended value="first" type="${TABS_TYPE.CONTAINED}">
+      <c4d-tab id="tab-first" target="panel-first" value="first">
+        First tab with long text that wraps multiple lines. Lorem ipsum dolor
+        sit amet consectetur adipiscing elit
+      </c4d-tab>
+      <c4d-tab id="tab-second" target="panel-second" value="second">
+        Second tab - min amount for tooltip
+      </c4d-tab>
+      <c4d-tab id="tab-third" target="panel-third" value="third">
+        Third tab
+      </c4d-tab>
+      <c4d-tab id="tab-fourth" target="panel-fourth" value="fourth">
+        Fourth tab
+      </c4d-tab>
+      <c4d-tab label="Fifth tab" disabled>Fifth tab</c4d-tab>
+    </c4d-tabs-extended>
+    <div class="${prefix}-ce-demo-devenv--tab-panels">
+      <div id="panel-first" role="tabpanel" aria-labelledby="tab-first" hidden>
         <c4d-content-block-media-content>
           <c4d-content-item>
             <c4d-content-item-heading
@@ -53,8 +123,12 @@ export const Default = (args) => {
             </c4d-card-cta-footer>
           </c4d-card-link-cta>
         </c4d-content-block-media-content>
-      </c4d-tab>
-      <c4d-tab label="Second tab - min amount for tooltip ">
+      </div>
+      <div
+        id="panel-second"
+        role="tabpanel"
+        aria-labelledby="tab-second"
+        hidden>
         <c4d-content-block-media-content>
           <c4d-content-item>
             <c4d-content-item-heading
@@ -63,17 +137,18 @@ export const Default = (args) => {
             <c4d-content-item-copy>${copy}</c4d-content-item-copy>
           </c4d-content-item>
         </c4d-content-block-media-content>
-      </c4d-tab>
-      <c4d-tab label="Third tab">
+      </div>
+      <div id="panel-third" role="tabpanel" aria-labelledby="tab-third" hidden>
         <p>Content for third tab goes here.</p>
-      </c4d-tab>
-      <c4d-tab label="Fourth tab">
+      </div>
+      <div
+        id="panel-fourth"
+        role="tabpanel"
+        aria-labelledby="tab-fourth"
+        hidden>
         <p>Content for fourth tab goes here.</p>
-      </c4d-tab>
-      <c4d-tab label="Fifth tab" disabled>
-        <p>Content for fifth tab goes here.</p>
-      </c4d-tab>
-    </c4d-tabs-extended>
+      </div>
+    </div>
   `;
 };
 
@@ -81,9 +156,11 @@ export default {
   title: 'Components/Tabs extended',
   decorators: [
     (story) => html`
-      <div class="cds--grid">
-        <div class="cds--row">
-          <div class="cds--col-lg-12 cds--no-gutter">${story()}</div>
+      <div class="${prefix}--grid">
+        <div class="${prefix}--row">
+          <div class="${prefix}--col-lg-12 ${prefix}--no-gutter">
+            ${story()}
+          </div>
         </div>
       </div>
     `,
@@ -91,21 +168,5 @@ export default {
   parameters: {
     ...readme.parameters,
     hasStoryPadding: true,
-    knobs: {
-      TabsExtended: () => ({
-        orientation: select(
-          'Orientation (orientation):',
-          orientationType,
-          ORIENTATION.HORIZONTAL
-        ),
-      }),
-    },
-    propsSet: {
-      default: {
-        TabsExtended: {
-          orientation: 'horizontal',
-        },
-      },
-    },
   },
 };
