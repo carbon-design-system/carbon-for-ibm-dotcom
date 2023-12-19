01740161a9de771206f07e178927b88bddce4ab5
John Kaeser <jakaeser44@gmail.com>
2023-12-19 15:16:53 -0500

    feat(services-store): remove outdated translateAPI types

--- packages/services-store/src/types/translateAPI.ts
+++ packages/services-store/src/types/translateAPI.ts
@@ -29,69 +29,29 @@ export interface BasicLinkSet {
   links: BasicLink[];
 }
 
-/**
- * A feature in mega panel.
- */
-export interface MegapanelFeature {
-  heading?: string;
-  imageUrl?: string;
-  linkTitle?: string;
-  linkUrl?: string;
+export interface L0Menu {
+  items: L0MenuItem[];
 }
 
-/**
- * A content in mega panel.
- */
-export interface MegapanelContent {
-  headingTitle?: string;
-  headingUrl?: string;
-  description?: string;
-  quickLinks: BasicLinkSet;
-  feature: MegapanelFeature;
+export interface L0MenuItem extends BasicLink {
+  submenu?: L0Megamenu | BasicLink[];
 }
 
-/**
- * A menu item in masthead.
- */
-export interface MastheadMenuItem {
-  title: string;
-  titleEnglish?: string;
-  url?: string;
-  highlighted?: boolean;
-  megaPanelViewAll?: boolean;
-  megapanelContent?: MegapanelContent;
+export interface L0Megamenu {
+  sections: Megapanel[];
+  highlights?: MegapanelLinkGroup[];
+  viewAll?: BasicLink;
 }
 
-/**
- * A menu section in masthead.
- */
-export interface MastheadMenuSection {
-  heading?: string;
-  menuItems: MastheadMenuItem[];
+export interface Megapanel {
+  heading?: BasicLink;
+  groups: MegapanelLinkGroup[];
+  viewAll?: BasicLink;
 }
 
-/**
- * An item in masthead.
- */
-export interface MastheadLink {
-  title: string;
-  titleEnglish?: string;
-  url?: string;
-  hasMenu?: boolean;
-  hasMegapanel?: boolean;
-  megamenuLayout?: 'tab' | 'list';
-  menuSections?: MastheadMenuSection[];
-}
-
-/**
- * A menu section for masthead
- *
- * @deprecated
- */
-export interface LegacyMastheadL1 {
-  title: string;
-  url?: string;
-  menuItems?: MastheadLink[];
+export interface MegapanelLinkGroup {
+  heading?: BasicLink;
+  links?: BasicLink[];
 }
 
 /**
@@ -198,7 +158,7 @@ export interface Translation {
     /**
      * The nav links.
      */
-    links: MastheadLink[];
+    links: L0MenuItem[];
   };
 
   /**
@@ -224,18 +184,6 @@ export interface Translation {
      * The profile menu for logged out state.
      */
     signedout: MastheadProfileItem[];
-  };
-
-  /**
-   * Cloud masthead items
-   */
-  masthead: {
-    logo: MastheadLogoData;
-    contact: MastheadProfileContent;
-    profileMenu: {
-      signedout: MastheadProfileContent;
-      signedin: MastheadProfileContent;
-    };
   };
 
   /**
@@ -295,30 +243,4 @@ export interface TranslateAPIState {
    * The errors from the requests for the translation data, keyed by the language.
    */
   errorsRequestTranslation?: { [language: string]: Error };
-}
-
-// New for v2.1.0
-export interface L0Menu {
-  items: L0MenuItem[];
-}
-
-export interface L0MenuItem extends BasicLink {
-  submenu?: L0Megamenu | BasicLink[];
-}
-
-export interface L0Megamenu {
-  sections: Megapanel[];
-  highlights?: MegapanelLinkGroup[];
-  viewAll?: BasicLink;
-}
-
-export interface Megapanel {
-  heading?: BasicLink;
-  groups: MegapanelLinkGroup[];
-  viewAll?: BasicLink;
-}
-
-export interface MegapanelLinkGroup {
-  heading?: BasicLink;
-  links?: BasicLink[];
 }
