0495b58131030757d4b8d6928a1d0eb70f5b9e33
John Kaeser <jakaeser44@gmail.com>
2024-01-02 15:32:54 -0500

    fix(stackblitz): remove unnecessary lit-element references

--- packages/web-components/examples/stackblitz/components/masthead-l1/src/index.js
+++ packages/web-components/examples/stackblitz/components/masthead-l1/src/index.js
@@ -1,17 +1,10 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2023
+ * Copyright IBM Corp. 2020, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
- */
-
-/**
- * the `lit-element` package is not required in `package.json`, but is included
- * in this example for CodeSandbox compatibility
- *
- * https://github.com/codesandbox/codesandbox-client/issues/4456
  */
 
 import '@carbon/ibmdotcom-web-components/es/components/masthead';
--- packages/web-components/examples/stackblitz/components/masthead/src/index.js
+++ packages/web-components/examples/stackblitz/components/masthead/src/index.js
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2023
+ * Copyright IBM Corp. 2020, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -9,10 +9,3 @@
 
 import '@carbon/ibmdotcom-web-components/es/components/masthead';
 import './index.scss';
-
-/**
- * the `lit-element` package is not required in `package.json`, but is included
- * in this example for CodeSandbox compatibility
- *
- * https://github.com/codesandbox/codesandbox-client/issues/4456
- */
