97130821edfa77abe15e69da4021f809650f3b2e
IgnacioBecerra <i1becerr@ucsd.edu>
2024-01-05 14:54:38 -0800

    fix(loading): update story to v7

--- packages/carbon-web-components/.storybook/main.ts
+++ packages/carbon-web-components/.storybook/main.ts
@@ -11,6 +11,8 @@ const stories = glob.sync(
     // '../src/**/*.mdx',
     // '../src/**/*.stories.@(js|jsx|ts|tsx)',
     // add mdx/story files as they are being worked on
+    '../src/**/loading.stories.ts',
+    '../src/**/loading.mdx',
     '../src/**/link.mdx',
     '../src/**/link.stories.ts',
     '../src/**/file-uploader.mdx',
