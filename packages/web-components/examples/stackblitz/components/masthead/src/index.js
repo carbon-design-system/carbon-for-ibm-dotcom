c3ddeb8016b0259ce4f202cb2d99c16b6b5c2545
John Kaeser <jakaeser44@gmail.com>
2024-01-31 11:29:52 -0500

    fix(stackblitz): fix and standardize masthead examples for v2 (#11199)
    ### Related Ticket(s)
    
    ### Description
    
    Updates the masthead component's ~~CodeSandbox~~ Stackblitz examples with up-to-date demonstrations for v2.
    
    ### Changelog
    
    **Changed**
    
    - Updates masthead Stackblitz examples for v2.
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "test: e2e": Codesandbox examples and e2e integration tests -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/examples/stackblitz/components/masthead/src/index.js
+++ packages/web-components/examples/stackblitz/components/masthead/src/index.js
@@ -1,18 +1,11 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2023
+ * Copyright IBM Corp. 2020, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead-container.js';
+import '@carbon/ibmdotcom-web-components/es/components/masthead';
 import './index.scss';
-
-/**
- * the `lit-element` package is not required in `package.json`, but is included
- * in this example for CodeSandbox compatibility
- *
- * https://github.com/codesandbox/codesandbox-client/issues/4456
- */
