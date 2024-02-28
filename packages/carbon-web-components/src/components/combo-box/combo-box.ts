ce3a936aef66db78427f5044f192ecfaefa44464
Matthew Oliveira <m4olivei@gmail.com>
2024-02-28 14:33:50 -0500

    fix(combobox): remove use of ifNonEmpty

--- packages/carbon-web-components/src/components/combo-box/combo-box.ts
+++ packages/carbon-web-components/src/components/combo-box/combo-box.ts
@@ -18,7 +18,6 @@ import CDSComboBoxItem from './combo-box-item';
 import styles from './combo-box.scss';
 import { carbonElement as customElement } from '../../globals/decorators/carbon-element';
 import { ifDefined } from 'lit/directives/if-defined';
-import ifNonEmpty from '../../globals/directives/if-non-empty';
 
 export { DROPDOWN_DIRECTION, DROPDOWN_SIZE } from '../dropdown/dropdown';
 
@@ -245,7 +244,7 @@ class CDSComboBox extends CDSDropdown {
         placeholder="${label}"
         .value=${filterInputValue}
         role="combobox"
-        aria-label="${ifNonEmpty(inputLabel)}"
+        aria-label="${ifDefined(inputLabel)}"
         aria-controls="menu-body"
         aria-haspopup="listbox"
         aria-autocomplete="list"
