798e6a81b9f1aaec3eefc87694681c50a75db5d1
Jeff Chew <jeff.chew@gmail.com>
2024-03-08 13:40:03 -0500

    fix(cdn): emit the CSS grid classes in a new artifact cssgrid.css

--- packages/carbon-web-components/gulp-tasks/build/sass-cdn.js
+++ packages/carbon-web-components/gulp-tasks/build/sass-cdn.js
@@ -14,13 +14,30 @@ const sass = require('gulp-sass')(require('sass'));
 const config = require('../config');
 
 /**
- * Builds the sass file for the carbon grid
+ * Builds the sass file for the carbon (flex) grid
  *
  * @returns {*} gulp stream
  */
 function _buildGrid() {
   return gulp
     .src([`${config.srcDir}/globals/scss/grid.scss`])
+    .pipe(
+      sass({
+        includePaths: ['node_modules'],
+        outputStyle: 'compressed',
+      }).on('error', sass.logError)
+    )
+    .pipe(gulp.dest(config.bundleDestDir));
+}
+
+/**
+ * Builds the sass file for the carbon (css) grid
+ *
+ * @returns {*} gulp stream
+ */
+function _buildCSSGrid() {
+  return gulp
+    .src([`${config.srcDir}/globals/scss/cssgrid.scss`])
     .pipe(
       sass({
         includePaths: ['node_modules'],
@@ -49,11 +66,13 @@ function _buildThemes() {
 
 
 gulp.task('build:sass:cdn:grid', _buildGrid);
+gulp.task('build:sass:cdn:cssgrid', _buildCSSGrid);
 gulp.task('build:sass:cdn:themes', _buildThemes);
 gulp.task(
   'build:sass:cdn',
   gulp.parallel(
     'build:sass:cdn:grid',
+    'build:sass:cdn:cssgrid',
     'build:sass:cdn:themes',
   )
 );
