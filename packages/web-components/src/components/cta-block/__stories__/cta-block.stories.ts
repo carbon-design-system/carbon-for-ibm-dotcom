67f9e5920a5bbb3a9580bde6cbc651dd8f09d29b
Anna Wen <54281166+annawen1@users.noreply.github.com>
2024-03-20 21:24:08 -0400

    fix(button-group): extend button icon styles for button-group-item (#11659)
    ### Related Ticket(s)
    
    Closes # {{Provide issue number link(s) to the related ticket(s) that this pull request addresses}}
    
    ### Description
    
    Spacing with the icon within the `button` and `button-group` elements in the `cta-block` story was off.
    
    Swapped `button-cta` for `button` instead in `cta-block` story and extended the button icon styles within `button-group-item`
    
    ### Changelog
    
    **Changed**
    
    - use `button` instead of `button-cta` in `cta-block`
    - extend the button icon styles within `button-group-item` so the spacing styles are added
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "test: e2e": Codesandbox examples and e2e integration tests -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/src/components/cta-block/__stories__/cta-block.stories.ts
+++ packages/web-components/src/components/cta-block/__stories__/cta-block.stories.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2023
+ * Copyright IBM Corp. 2020, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -41,20 +41,26 @@ const renderCTA = {
     >
   `,
   button: (renderIcon) => html`
-    <c4d-button-cta
+    <c4d-button
       slot="action"
       cta-type=${renderIcon === iconMap.Launch20 ? 'external' : 'local'}
       href="https://example.com"
-      >CTA Button link</c4d-button-cta
+      >CTA Button link</c4d-button
     >
   `,
   buttonGroup: (renderIcon, target) => html`
     <c4d-button-group slot="action">
-      <c4d-button-group-item target="${target}" href="https://example.com">
-        Secondary Button ${renderIcon}
+      <c4d-button-group-item
+        target="${target}"
+        href="https://example.com"
+        cta-type=${renderIcon === iconMap.Launch20 ? 'external' : 'local'}>
+        Secondary button
       </c4d-button-group-item>
-      <c4d-button-group-item target="${target}" href="https://example.com">
-        Primary button ${renderIcon}
+      <c4d-button-group-item
+        target="${target}"
+        href="https://example.com"
+        cta-type=${renderIcon === iconMap.Launch20 ? 'external' : 'local'}>
+        Primary button
       </c4d-button-group-item>
     </c4d-button-group>
   `,
