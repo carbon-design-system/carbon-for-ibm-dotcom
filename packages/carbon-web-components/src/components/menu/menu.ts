d6234c629459c58af5751d9deed12310e418c553
Sangeetha Babu <58620134+sangeethababu9223@users.noreply.github.com>
2024-04-03 11:57:45 +0530

    feat(menu): test context

--- packages/carbon-web-components/src/components/menu/menu.ts
+++ packages/carbon-web-components/src/components/menu/menu.ts
@@ -8,7 +8,7 @@
  */
 
 import { LitElement, html } from 'lit';
-import { property } from 'lit/decorators.js';
+// import { property } from 'lit/decorators.js';
 import { prefix } from '../../globals/settings';
 import HostListenerMixin from '../../globals/mixins/host-listener';
 import { carbonElement as customElement } from '../../globals/decorators/carbon-element';
@@ -37,96 +37,103 @@ import { myContext } from "./menu-context";
 class CDSMenu extends HostListenerMixin(LitElement) {
   @provide({ context: myContext })
   data: string = "...";
+
   //   private _provider = new ContextProvider(this, {context: menuContext, initialValue: {
   //     state: menuDefaultState
   //   }});
   /**
    * A required label titling the Menu. Will be rendered as its aria-label content.
    */
-  @property({ type: String })
-  label;
+  // @property({ type: String })
+  // label;
   /**
    * Provide an optional function to be called when the MenuItem is clicked.
    */
-  onClose?: () => void;
+  // onClose?: () => void;
 
-  _handleKeyDown(e: KeyboardEvent) {
-    e.stopPropagation();
-    const { onClose, _handleClose: handleClose, _focusItem: focusItem } = this;
-    if (e.key === 'Escape' || (!isRoot && e.key === 'ArrowLeft' && onClose)) {
-      handleClose(e);
-    } else {
-      focusItem(e);
-    }
-  }
-  _handleClose(e: Pick<KeyboardEvent, 'type'>) {
-    const { _returnFocus: returnFocus, onClose } = this;
-    // if (/^key/.test(e.type)) {
-    //   window.addEventListener('keyup', returnFocus, { once: true });
-    // } else if (e.type === 'click' && menu.current) {
-    //   menu.current.addEventListener('focusout', returnFocus, { once: true });
-    // } else {
-    //   returnFocus();
-    // }
+  // _handleKeyDown(e: KeyboardEvent) {
+  //   e.stopPropagation();
+  //   const { onClose, _handleClose: handleClose, _focusItem: focusItem } = this;
+  //   if (e.key === 'Escape' || (!isRoot && e.key === 'ArrowLeft' && onClose)) {
+  //     handleClose(e);
+  //   } else {
+  //     focusItem(e);
+  //   }
+  // }
+  // _handleClose(e: Pick<KeyboardEvent, 'type'>) {
+  //   const { _returnFocus: returnFocus, onClose } = this;
+  //   if (/^key/.test(e.type)) {
+  //     window.addEventListener('keyup', returnFocus, { once: true });
+  //   } else if (e.type === 'click' && menu.current) {
+  //     menu.current.addEventListener('focusout', returnFocus, { once: true });
+  //   } else {
+  //     returnFocus();
+  //   }
 
-    if (onClose) {
-      onClose();
-    }
-  }
-  _focusItem(e: KeyboardEvent) {
-    // const currentItem = focusableItems.findIndex((item) =>
-    //   item.ref.current.contains(document.activeElement)
-    // );
-    // let indexToFocus = currentItem;
-    // // if currentItem is -1, no menu item is focused yet.
-    // // in this case, the first item should receive focus.
-    // if (currentItem === -1) {
-    //   indexToFocus = 0;
-    // } else if (e) {
-    //   if (e.key == 'ArrowUp') {
-    //     indexToFocus = indexToFocus - 1;
-    //   }
-    //   if (e.key === 'ArrowDown') {
-    //     indexToFocus = indexToFocus + 1;
-    //   }
-    // }
-    // if (indexToFocus < 0) {
-    //   indexToFocus = focusableItems.length - 1;
-    // }
-    // if (indexToFocus >= focusableItems.length) {
-    //   indexToFocus = 0;
-    // }
-    // if (indexToFocus !== currentItem) {
-    //   const nodeToFocus = focusableItems[indexToFocus];
-    //   nodeToFocus.ref.current.focus();
-    // }
-  }
-  _returnFocus() {
-    // if (focusReturn.current) {
-    //   focusReturn.current.focus();
-    // }
-  }
-  _handleBlur() {
-    // if (open && onClose && isRoot && !menu.current?.contains(e.relatedTarget)) {
-    //   handleClose(e);
-    // }
-  }
+  //   if (onClose) {
+  //     onClose();
+  //   }
+  // }
+  // _focusItem(e: KeyboardEvent) {
+  //   const currentItem = focusableItems.findIndex((item) =>
+  //     item.ref.current.contains(document.activeElement)
+  //   );
+  //   let indexToFocus = currentItem;
+  //   // if currentItem is -1, no menu item is focused yet.
+  //   // in this case, the first item should receive focus.
+  //   if (currentItem === -1) {
+  //     indexToFocus = 0;
+  //   } else if (e) {
+  //     if (e.key == 'ArrowUp') {
+  //       indexToFocus = indexToFocus - 1;
+  //     }
+  //     if (e.key === 'ArrowDown') {
+  //       indexToFocus = indexToFocus + 1;
+  //     }
+  //   }
+  //   if (indexToFocus < 0) {
+  //     indexToFocus = focusableItems.length - 1;
+  //   }
+  //   if (indexToFocus >= focusableItems.length) {
+  //     indexToFocus = 0;
+  //   }
+  //   if (indexToFocus !== currentItem) {
+  //     const nodeToFocus = focusableItems[indexToFocus];
+  //     nodeToFocus.ref.current.focus();
+  //   }
+  // }
+  // _returnFocus() {
+  //   if (focusReturn.current) {
+  //     focusReturn.current.focus();
+  //   }
+  // }
+  // _handleBlur() {
+  //   if (open && onClose && isRoot && !menu.current?.contains(e.relatedTarget)) {
+  //     handleClose(e);
+  //   }
+  // }
+  // render() {
+  //   const {
+  //     label,
+  //     _handleKeyDown: handleKeyDown,
+  //     _handleBlur: handleBlur,
+  //   } = this;
+  //   return html`
+  //     <ul
+  //       role="menu"
+  //       aria-label="${label}"
+  //       tabindex="-1"
+  //       @keyDown="${handleKeyDown}"
+  //       @blur="${handleBlur}">
+  //       <slot></slot>
+  //     </ul>
+  //   `;
+  // }
+
   render() {
-    const {
-      label,
-      _handleKeyDown: handleKeyDown,
-      _handleBlur: handleBlur,
-    } = this;
-    return html`
-      <ul
-        role="menu"
-        aria-label="${label}"
-        tabindex="-1"
-        @keyDown="${handleKeyDown}"
-        @blur="${handleBlur}">
-        <slot></slot>
-      </ul>
-    `;
+    return html `
+    <h1>Menu</h1>
+    `
   }
 }
 export default CDSMenu;
