c5eeb3a937dcf1e6e15f9b9433e171cc27893745
kennylam <909118+kennylam@users.noreply.github.com>
2024-04-04 20:54:50 -0400

    chore(storybook): content-block-card storybook link fix (#11692)
    ### Description
    
    Deprecated component name in Storybook story was causing the components to render incorrectly.
    
    ### Changelog
    
    **Changed**
    
    - use `c4d-card-footer` instead of `c4d-card-cta-footer`
    
    <!-- React and Web Component deploy previews are enabled by default. -->
    <!-- To enable additional available deploy previews, apply the following -->
    <!-- labels for the corresponding package: -->
    <!-- *** "test: e2e": Codesandbox examples and e2e integration tests -->
    <!-- *** "package: services": Services -->
    <!-- *** "package: utilities": Utilities -->
    <!-- *** "RTL": React / Web Components (RTL) -->
    <!-- *** "feature flag": React / Web Components (experimental) -->

--- packages/web-components/tests/e2e-storybook/cypress/integration/masthead/masthead-authenticated.e2e.js
+++ packages/web-components/tests/e2e-storybook/cypress/integration/masthead/masthead-authenticated.e2e.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2021, 2023
+ * Copyright IBM Corp. 2021, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -14,38 +14,38 @@
 const _pathDefault =
   '/iframe.html?id=components-masthead--default&knob-use%20mock%20nav%20data%20(use-mock)=true&knob-The%20user%20authenticated%20status%20(user-status)=test.user@ibm.com';
 
-describe('dds-masthead | authenticated (desktop)', () => {
+describe('c4d-masthead | authenticated (desktop)', () => {
   beforeEach(() => {
     cy.viewport(1280, 780).visit(`/${_pathDefault}`);
   });
 
   it('should open the login menu with 4 items', () => {
-    cy.get('dds-masthead-profile')
+    cy.get('c4d-masthead-profile')
       .shadow()
       .find('a')
       .click();
-    cy.get('dds-masthead-profile-item').should('have.length', 4);
+    cy.get('c4d-masthead-profile-item').should('have.length', 4);
     cy.takeSnapshots();
   });
 
   it('should not render profile menu when disabled', () => {
-    cy.get('dds-masthead-composite').invoke('attr', 'has-profile', 'false');
-    cy.get('dds-masthead-profile').should('not.exist');
+    cy.get('c4d-masthead-composite').invoke('attr', 'has-profile', 'false');
+    cy.get('c4d-masthead-profile').should('not.exist');
     cy.takeSnapshots();
   });
 });
 
-describe('dds-masthead | default (mobile)', () => {
+describe('c4d-masthead | default (mobile)', () => {
   beforeEach(() => {
     cy.viewport(320, 780).visit(`/${_pathDefault}`);
   });
 
   it('should open the login menu with 4 items', () => {
-    cy.get('dds-masthead-profile')
+    cy.get('c4d-masthead-profile')
       .shadow()
       .find('a')
       .click();
-    cy.get('dds-masthead-profile-item').should('have.length', 4);
+    cy.get('c4d-masthead-profile-item').should('have.length', 4);
     cy.takeSnapshots('mobile');
   });
 });
