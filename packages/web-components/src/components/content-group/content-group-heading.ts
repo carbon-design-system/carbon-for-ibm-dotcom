8d61228ec20f67d47d0a5bf6b09cf8fba22c032d
Marcelo JCS <marcelojcs@outlook.com>
2024-05-08 16:28:38 -0300

    fix(content-group): adding h-tags to headings (#11738)
    * fix(content-group): adding h-tags to headings
    
    * Update packages/web-components/src/components/content-group/content-group-heading.ts
    
    Co-authored-by: Andy Blum <andy.blum.01@gmail.com>
    
    * fix(group-item): remove role and aria-level
    
    * fix(content-group): ran  refresh Sanapshots
    
    * fix(content-group): update dotcom web component unit snapshots
    
    * fix(content-grounp): fixing heading styles
    
    * fix(content-grounp): formatting styles
    
    * fix(content-group): Update content-group-heading styles
    
    * fix(content-item): inherit heading element font rules
    
    ---------
    
    Co-authored-by: Andy Blum <andy.blum.01@gmail.com>

--- packages/web-components/src/components/content-group/content-group-heading.ts
+++ packages/web-components/src/components/content-group/content-group-heading.ts
@@ -29,17 +29,11 @@ class DDSContentGroupHeading extends StableSelectorMixin(LitElement) {
   slot = 'heading';
 
   connectedCallback() {
-    if (!this.hasAttribute('role')) {
-      this.setAttribute('role', 'heading');
-    }
-    if (!this.hasAttribute('aria-level')) {
-      this.setAttribute('aria-level', '3');
-    }
     super.connectedCallback();
   }
 
   render() {
-    return html` <slot></slot> `;
+    return html` <h3><slot></slot></h3>`;
   }
 
   static get stableSelector() {
--- packages/web-components/src/components/content-item/content-item-heading.ts
+++ packages/web-components/src/components/content-item/content-item-heading.ts
@@ -29,17 +29,11 @@ class DDSContentItemHeading extends StableSelectorMixin(LitElement) {
   slot = 'heading';
 
   connectedCallback() {
-    if (!this.hasAttribute('role')) {
-      this.setAttribute('role', 'heading');
-    }
-    if (!this.hasAttribute('aria-level')) {
-      this.setAttribute('aria-level', '4');
-    }
     super.connectedCallback();
   }
 
   render() {
-    return html` <slot></slot> `;
+    return html` <h4><slot></slot></h4>`;
   }
 
   static get stableSelector() {
