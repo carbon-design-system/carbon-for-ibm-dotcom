3358ce532d1a743257bd2ac1c9f2a7d1b2250d4d
Andy Blum <andy.blum.01@gmail.com>
2024-05-31 15:06:32 -0400

    fix(left-nav): yarn format

--- packages/web-components/src/components/masthead/left-nav.ts
+++ packages/web-components/src/components/masthead/left-nav.ts
@@ -230,7 +230,7 @@ class DDSLeftNav extends StableSelectorMixin(BXSideNav) {
           import('./left-nav-menu-section'),
           import('./left-nav-menu-item'),
           import('./left-nav-menu-category-heading'),
-          import('./left-nav-overlay')
+          import('./left-nav-overlay'),
         ]);
         this._importedSideNav = true;
       }
