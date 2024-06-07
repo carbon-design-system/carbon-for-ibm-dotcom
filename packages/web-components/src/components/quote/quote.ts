6dc085bedd2fc3c4b1f9c8755ec2c739c9981843
Valentin Nicolae <valentin.sorin.nicolae@ro.ibm.com>
2024-06-07 10:52:05 +0300

    feat(quote): add shadow parts

--- packages/web-components/src/components/quote/quote.ts
+++ packages/web-components/src/components/quote/quote.ts
@@ -96,10 +96,14 @@ class C4DQuote extends StableSelectorMixin(LitElement) {
     switch (this.markType) {
       case QUOTE_TYPES.SINGLE_CURVED:
         return html`
-          <span class="${prefix}--quote__mark" part="mark mark--opening">‘</span>
+          <span class="${prefix}--quote__mark" part="mark mark--opening"
+            >‘</span
+          >
           <blockquote class="${prefix}--quote__copy" part="copy">
             <slot></slot
-            ><span class="${prefix}--quote__mark-closing" part="mark mark--closing"
+            ><span
+              class="${prefix}--quote__mark-closing"
+              part="mark mark--closing"
               >’</span
             >
           </blockquote>
