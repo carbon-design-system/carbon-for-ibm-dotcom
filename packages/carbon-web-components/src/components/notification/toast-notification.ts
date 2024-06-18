dc83920f6197089a387dc9ccdd4d6dff93727cd7
Mike Cordes <mjcordes@us.ibm.com>
2024-06-18 16:22:41 -0400

    fix(toast-notification): remove divs duplicated on lines 36 and 47

--- packages/carbon-web-components/src/components/notification/toast-notification.ts
+++ packages/carbon-web-components/src/components/notification/toast-notification.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2023
+ * Copyright IBM Corp. 2019, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -55,13 +55,8 @@ class CDSToastNotification extends CDSInlineNotification {
   caption = '';
 
   render() {
-    const { _type: type } = this;
     return html`
-      ${this._renderIcon()}
-      <div class="${prefix}--${type}-notification__details">
-        ${this._renderText()}
-      </div>
-      ${this._renderButton()}
+      ${this._renderIcon()} ${this._renderText()} ${this._renderButton()}
     `;
   }
 
