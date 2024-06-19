3bea340aef6332c3866b2237878ade3e4390ffab
Matthew Oliveira <m4olivei@gmail.com>
2024-06-19 10:18:19 -0400

    feat(table-of-contents): condition the list-item--active class

--- packages/web-components/src/components/table-of-contents/table-of-contents.ts
+++ packages/web-components/src/components/table-of-contents/table-of-contents.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2023
+ * Copyright IBM Corp. 2020, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -846,7 +846,7 @@ class C4DTableOfContents extends MediaQueryMixin(
                     return html`
                       <li
                         class="${itemClasses}"
-                        part="list-item list-item--active"
+                        part="list-item${selected ? ' list-item--active' : ''}"
                         @click="${handleClickItem}"
                         @keydown="${handleOnKeyDown}">
                         <a
