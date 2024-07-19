78e54c4ab5fb8439089c266c3057f9ebe11038a0
Valentin Nicolae <valentin.sorin.nicolae@ro.ibm.com>
2024-07-19 16:35:16 +0300

    feat(textarea): add shadow parts

--- packages/carbon-web-components/src/components/textarea/textarea-skeleton.ts
+++ packages/carbon-web-components/src/components/textarea/textarea-skeleton.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2023
+ * Copyright IBM Corp. 2019, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -14,13 +14,20 @@ import { carbonElement as customElement } from '../../globals/decorators/carbon-
 
 /**
  * Skeleton of text area.
+ *
+ * @element cds-textarea-skeleton
+ *
+ * @csspart label - The label. Usage `cds-textarea-skeleton::part(label)`
+ * @csspart textarea - The textarea. Usage `cds-textarea-skeleton::part(textarea)`
  */
 @customElement(`${prefix}-textarea-skeleton`)
 class CDSTextareaSkeleton extends LitElement {
   render() {
     return html`
-      <span class="${prefix}--label ${prefix}--skeleton"></span>
-      <div class="${prefix}--skeleton ${prefix}--text-area"></div>
+      <span class="${prefix}--label ${prefix}--skeleton" part="label"></span>
+      <div
+        class="${prefix}--skeleton ${prefix}--text-area"
+        part="textarea"></div>
     `;
   }
 
