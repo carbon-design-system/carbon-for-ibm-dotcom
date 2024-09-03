882cc36cf2f6e033ed4e26764b6f73380948f5dc
Andy Blum <andy.blum.01@gmail.com>
2024-09-03 13:49:43 -0400

    fix(quote): invert quote punctuation in RTL

--- packages/web-components/src/components/quote/quote.ts
+++ packages/web-components/src/components/quote/quote.ts
@@ -104,50 +104,51 @@ class DDSQuote extends StableSelectorMixin(LitElement) {
   }
 
   protected _renderQuote() {
+    const isLTR = getComputedStyle(this).direction.toLowerCase() === "ltr";
     switch (this.markType) {
       case QUOTE_TYPES.SINGLE_CURVED:
         return html`
-          <span class="${prefix}--quote__mark">‘</span>
+          <span class="${prefix}--quote__mark">${isLTR ? "‘" : "’"}</span>
           <blockquote class="${prefix}--quote__copy">
-            <slot></slot><span class="${prefix}--quote__mark-closing">’</span>
+            <slot></slot><span class="${prefix}--quote__mark-closing">${!isLTR ? "‘" : "’"}</span>
           </blockquote>
         `;
       case QUOTE_TYPES.DOUBLE_ANGLE:
         return html`
-          <span class="${prefix}--quote__mark">«</span>
+          <span class="${prefix}--quote__mark">${isLTR ? "«" : "»"}</span>
           <blockquote class="${prefix}--quote__copy">
-            <slot></slot><span class="${prefix}--quote__mark-closing">»</span>
+            <slot></slot><span class="${prefix}--quote__mark-closing">${!isLTR ? "«" : "»"}</span>
           </blockquote>
         `;
       case QUOTE_TYPES.SINGLE_ANGLE:
         return html`
-          <span class="${prefix}--quote__mark">‹</span>
+          <span class="${prefix}--quote__mark">${isLTR ? "‹" : "›"}</span>
           <blockquote class="${prefix}--quote__copy">
-            <slot></slot><span class="${prefix}--quote__mark-closing">›</span>
+            <slot></slot><span class="${prefix}--quote__mark-closing">${!isLTR ? "‹" : "›"}</span>
           </blockquote>
         `;
       case QUOTE_TYPES.LOW_HIGH_REVERSED_DOUBLE_CURVED:
         return html`
-          <span class="${prefix}--quote__mark">„</span>
+          <span class="${prefix}--quote__mark">${isLTR ? "„" : "“"}</span>
           <blockquote class="${prefix}--quote__copy">
-            <slot></slot><span class="${prefix}--quote__mark-closing">“</span>
+            <slot></slot><span class="${prefix}--quote__mark-closing">${!isLTR ? "„" : "“"}</span>
           </blockquote>
         `;
       case QUOTE_TYPES.CORNER_BRACKET:
         return html`
           <span
             class="${prefix}--quote__mark ${prefix}--quote__mark-corner-bracket"
-            >「</span
+            >${isLTR ? "「" : "」"}</span
           >
           <blockquote class="${prefix}--quote__copy">
-            <slot></slot><span class="${prefix}--quote__mark-closing">」</span>
+            <slot></slot><span class="${prefix}--quote__mark-closing">${!isLTR ? "「" : "」"}</span>
           </blockquote>
         `;
       default:
         return html`
-          <span class="${prefix}--quote__mark">“</span>
+          <span class="${prefix}--quote__mark">${isLTR ? "“" : "”"}</span>
           <blockquote class="${prefix}--quote__copy">
-            <slot></slot><span class="${prefix}--quote__mark-closing">”</span>
+            <slot></slot><span class="${prefix}--quote__mark-closing">${!isLTR ? "“" : "”"}</span>
           </blockquote>
         `;
     }
