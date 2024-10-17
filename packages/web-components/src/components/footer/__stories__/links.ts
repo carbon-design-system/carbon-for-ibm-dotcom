0349dd7b9567cb08f5ad8aab24399b5c322cca68
Matthew Oliveira <m4olivei@gmail.com>
2024-10-17 16:02:27 -0400

    fix(ci): adjust @carbon/ibmdotcom-services-store to internal module

--- packages/web-components/src/components/footer/__stories__/links.ts
+++ packages/web-components/src/components/footer/__stories__/links.ts
@@ -7,7 +7,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { BasicLinkSet } from '@carbon/ibmdotcom-services-store/es/types/translateAPI';
+import { BasicLinkSet } from '../../../internal/vendor/@carbon/ibmdotcom-services-store/types/translateAPI';
 
 /**
  * Footer links.
