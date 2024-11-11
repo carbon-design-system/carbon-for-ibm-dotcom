029a50201a4966003bd242568eb38e0456db7f6e
Marcelo JCS <marcelojcs@outlook.com>
2024-11-11 10:59:41 -0300

    feat(star-rating): moved the files from CAEM

---
+++ packages/web-components/src/components/star-rating/__stories__/star-rating.stories.ts
@@ -0,0 +1,92 @@
+import { html } from 'lit-element';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import '../index';
+import readme from './README.stories.mdx';
+import { bxGrid8ColCentered } from '../../../globals/internal/storybook-decorators';
+
+export const Default = ({
+  disableTooltip,
+  label,
+  labelHref,
+  rating,
+  starCount,
+  tooltipText,
+}) => html`
+  <caem-star-rating
+    ?disableTooltip="${disableTooltip}"
+    label="${ifDefined(label || undefined)}"
+    label-href="${ifDefined(labelHref || undefined)}"
+    rating="${rating}"
+    star-count="${starCount}"
+    tooltip="${tooltipText}"
+  ></caem-star-rating>
+`;
+
+export const NoLabel = ({
+  disableTooltip,
+  rating,
+  starCount,
+  tooltipText,
+}) => html`
+  <caem-star-rating
+    ?disableTooltip="${disableTooltip}"
+    rating="${rating}"
+    star-count="${starCount}"
+    tooltip="${tooltipText}"
+  ></caem-star-rating>
+`;
+
+NoLabel.argTypes = {
+  label: {
+    table: { disable: true },
+  },
+  labelHref: {
+    table: { disable: true },
+  },
+};
+
+export default {
+  title: 'Components/Star Rating',
+  argTypes: {
+    rating: {
+      name: 'Star rating',
+      control: { type: 'number' },
+      defaultValue: 4.5,
+    },
+    label: {
+      name: 'Label (optional)',
+      control: { type: 'text' },
+      defaultValue: '42 G2 reviews',
+    },
+    labelHref: {
+      name: 'Label link (optional)',
+      control: { type: 'text' },
+      defaultValue: '',
+    },
+    starCount: {
+      name: 'Max number of stars (optional)',
+      control: { type: 'number' },
+      defaultValue: 5,
+    },
+    tooltipText: {
+      name: 'Tooltip text (optional)',
+      control: { type: 'text' },
+      defaultValue: '',
+    },
+    disableTooltip: {
+      name: 'Disable tooltip (optional)',
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+  },
+  parameters: {
+    ...readme.parameters,
+  },
+  decorators: [
+    story => bxGrid8ColCentered(story),
+    story =>
+      html`
+        <div style="margin-block-start: 2rem;">${story()}</div>
+      `,
+  ],
+};
