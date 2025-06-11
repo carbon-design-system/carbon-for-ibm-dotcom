98550711e9572ea2e8eeb46b83b7b3525d20a539
Rajaneesh <grajaneesh22@gmail.com>
2025-06-11 17:28:43 +0530

    Fixed ci check

--- packages/web-components/src/components/notice-choice/services.ts
+++ packages/web-components/src/components/notice-choice/services.ts
@@ -68,7 +68,7 @@ export function checkEmailStatus(
   onError: (err?: any) => void
 ) {
   const environment = env === 'prod' ? '1.www.s81c.com' : '1.wwwstage.s81c.com';
-  const url = `https://pf-api-dummyemail.urx-perform.wdc.dev.cirrus.ibm.com/pf/api/v1/userEmail?email=${encodeURIComponent(email)&&encodeURIComponent(country)}&env=${environment}`;
+  const url = `https://pf-api-dummyemail.urx-perform.wdc.dev.cirrus.ibm.com/pf/api/v1/userEmail?email=${encodeURIComponent(email) && encodeURIComponent(country)}&env=${environment}`;
 
   fetch(url)
     .then((response) => {
