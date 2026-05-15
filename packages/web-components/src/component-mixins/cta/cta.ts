12d03acfdfe6ff044fceb1f12ff85c7034b1d7e7
bruno-amorim <bruno.amorim07@hotmail.com>
2026-05-15 11:30:11 -0300

    feat(cta): adding shadow part to CTAs icon__new

--- packages/web-components/src/component-mixins/cta/cta.ts
+++ packages/web-components/src/component-mixins/cta/cta.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020, 2024
+ * Copyright IBM Corp. 2020, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -203,6 +203,7 @@ const CTAMixin = <T extends Constructor<HTMLElement>>(Base: T) => {
           >
           ${icon?.({
             class: `${c4dPrefix}--card__cta ${c4dPrefix}-ce--cta__icon`,
+            part: 'cta-icon',
           })}
         </slot>
       `;
