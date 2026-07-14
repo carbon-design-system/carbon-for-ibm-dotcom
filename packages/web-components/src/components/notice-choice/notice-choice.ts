48dae77aaa8e8214f8f655a7235cfcb48ee8f62f
Rajaneesh <grajaneesh22@gmail.com>
2026-07-14 17:19:01 +0530

    Fixed Supresion and Permission issue

--- packages/web-components/src/components/notice-choice/notice-choice.ts
+++ packages/web-components/src/components/notice-choice/notice-choice.ts
@@ -718,8 +718,7 @@ class NoticeChoice extends StableSelectorMixin(LitElement) {
       <span part="container">
         <div
           class="${prefix}--form-item cds--checkbox-wrapper"
-          part="checkbox-wrapper"
-        >
+          part="checkbox-wrapper">
           <input
             type="checkbox"
             class="${prefix}--checkbox"
@@ -727,18 +726,15 @@ class NoticeChoice extends StableSelectorMixin(LitElement) {
             id="${checkboxId}"
             name="${checkboxId}"
             ?checked="${checked}"
-            @change="${this.checkCombineEmailPhoneBoxChange}"
-          />
+            @change="${this.checkCombineEmailPhoneBoxChange}" />
           <label
             for="${checkboxId}"
             class="${prefix}--checkbox-label ${prefix}--nc__checkbox-${checkboxId}"
-            part="checkbox-label"
-          >
+            part="checkbox-label">
             <span
               class="${prefix}--checkbox-label-text"
               part="checkbox-label-text"
-              dir="auto"
-            >
+              dir="auto">
               ${unsafeHTML(preText)}
             </span>
           </label>
@@ -902,8 +898,7 @@ class NoticeChoice extends StableSelectorMixin(LitElement) {
           ? html`<span
               class="nc-error"
               part="error"
-              style="color:#da1e28;font-size:.75rem"
-            >
+              style="color:#da1e28;font-size:.75rem">
               ${checkbox.error}
             </span>`
           : null;
@@ -912,8 +907,7 @@ class NoticeChoice extends StableSelectorMixin(LitElement) {
         <span>
           <div
             class="${prefix}--form-item bx--checkbox-wrapper"
-            part="checkbox-wrapper checkbox-wrapper--mandatory"
-          >
+            part="checkbox-wrapper checkbox-wrapper--mandatory">
             <p part=${legalTextName} class=${legalTextName}>
               <input
                 type="checkbox"
@@ -921,20 +915,17 @@ class NoticeChoice extends StableSelectorMixin(LitElement) {
                 part="checkbox checkbox--mandatory"
                 id="${checkbox.mrs_field}"
                 name="${checkbox.mrs_field}"
-                @change="${this.checkBoxLegalChange}"
-              />
+                @change="${this.checkBoxLegalChange}" />
               <label
                 for="${checkbox.mrs_field}"
                 class="${prefix}--checkbox-label ${prefix}--nc__checkbox-${checkbox.mrs_field}"
                 part="checkbox-label checkbox-label--mandatory"
                 @click="${(e: Event) =>
-                  this.mandatoryCheckboxLabelClick(e, checkbox.mrs_field)}"
-              >
+                  this.mandatoryCheckboxLabelClick(e, checkbox.mrs_field)}">
                 <span
                   class="${prefix}--checkbox-label-text"
                   part="checkbox-label-text checkbox-label-text--mandatory"
-                  dir="auto"
-                >
+                  dir="auto">
                   ${checkbox.text}
                 </span>
               </label>
@@ -1127,8 +1118,7 @@ class NoticeChoice extends StableSelectorMixin(LitElement) {
         <cds-skeleton-text
           linecount="3"
           width="100%"
-          paragraph="true"
-        ></cds-skeleton-text>
+          paragraph="true"></cds-skeleton-text>
       </div>`;
     }
 
